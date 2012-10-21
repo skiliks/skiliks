fd9c69edd1c06d7db041f12357acc819476bc4aa
gugu <gugu@f4a4231c-4c1c-4590-bfba-d6a1cfedac6c>
2012-10-21 14:22:29 +0000

    simple tests

---
+++ protected/tests/RegistrationTest.php
@@ -0,0 +1,23 @@
+<?php
+require_once('test/CWebTestCase.php');
+
+class RegistrationTest extends CWebTestCase
+{
+    protected function setUp()
+    {
+        $this->setBrowser('*firefox');
+        $this->setBrowserUrl('http://live.skiliks.com/');
+        parent::setUp();
+    }
+
+    public function testRegistration() {
+        $this->open('/');
+        $this->waitForXpathCount("//input[@value='Регистрация']", 1);
+        $this->click("//input[@value='Регистрация']");
+        $this->type('id=email', 'andrey' . time() . '@kostenko.name');
+        $this->type('id=pass1', 'test');
+        $this->type('id=pass2', 'test');
+        $this->click('css=input.btn');
+    }
+
+}
