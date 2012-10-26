2cf1ce52b86e0ae097e11804f5df113c87626d22
gugu <gugu@f4a4231c-4c1c-4590-bfba-d6a1cfedac6c>
2012-10-26 22:40:16 +0000

    phing + config support

---
+++ protected/config/users/gugu.php
@@ -0,0 +1,48 @@
+<?php
+
+/* GuGu's personal config, copyright mopyright */
+
+define('SKILIKS_SPEED_FACTOR', 8);
+
+return array(
+    'import' => array(
+        'application.models.*',
+        'application.components.*'
+    ),
+    'components' => array(
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;dbname=skiliks',
+            'emulatePrepare' => true,
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+
+            'enableParamLogging' => true,
+            'enableProfiling' => true
+        ),
+
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning, info, trace, log',
+
+
+                ),
+
+            ),
+        )
+    ),
+
+
+    'preload' => array('log'),
+
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params' => array(
+        'frontendUrl' => 'http://front.skiliks.loc/',
+    )
+);
+
+?>
