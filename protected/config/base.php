2d1153e6c4ed643313ed61ada291810855a4c492
gugu <gugu@f4a4231c-4c1c-4590-bfba-d6a1cfedac6c>
2012-10-28 18:24:19 +0000

    phing + config support, rewritten

--- protected/config/base.php
+++ protected/config/base.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Base app config
+ */
+
 
 define('SKILIKS_SPEED_FACTOR', 8);
 
@@ -8,40 +12,29 @@ return array(
         'application.components.*'
     ),
     'components'=>array(
-        'db'=>array(
-            'connectionString' => 'mysql:host=localhost;dbname=skiliks',
-            'emulatePrepare' => true,
-            'username' => 'root',
-            'password' => 'Vtufpfdh',
-            'charset' => 'utf8',
-            
-             'enableParamLogging'=>true,
-            'enableProfiling'=>true
-        ),
-        
         'log'=>array(
             'class'=>'CLogRouter',
             'routes'=>array(
                 array(
                     'class'=>'CFileLogRoute',
                     'levels'=>'error, warning, info, trace, log',
-                    
-                     
-                    
+
+
+
                 ),
-                
+
             ),
         )
     ),
-    
-    
+
+
     'preload'=>array('log'),
-    
+
     // application-level parameters that can be accessed
     // using Yii::app()->params['paramName']
     'params'=>array(
-        'frontendUrl'=>'http://front.skiliks.com/',
+        'frontendUrl'=>'http://front.skiliks.loc/',
     )
-);    
+);
 
 ?>
