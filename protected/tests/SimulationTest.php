bb0691d5e0bfc495dc58b10f4978b3e3a35eb955
gugu <gugu@f4a4231c-4c1c-4590-bfba-d6a1cfedac6c>
2012-11-08 00:49:36 +0000

    maybo this helps

--- protected/tests/SimulationTest.php
+++ protected/tests/SimulationTest.php
@@ -71,7 +71,6 @@ class SimulationTest extends ControllerTestCase
         $result = $this->callJSONAction('DialogController', 'actionGet');
         $this->assertEquals(array('result' => 1, 'events' => array(array('result' => 1, 'data' => array(), 'eventType' => 1))), $result);
         $result = $this->callJSONAction('EventsController', 'actionGetState');
-        print_r($result);
     }
 
     function testSimulatorFail()
