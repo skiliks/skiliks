7eb035049fa6e2bb6bdd95c3400f1c787c463563
gugu <gugu@f4a4231c-4c1c-4590-bfba-d6a1cfedac6c>
2012-11-18 22:15:51 +0000

    do not exit after helper execution

--- protected/components/Helper.php
+++ protected/components/Helper.php
@@ -7,6 +7,7 @@ class Helper {
         include_once __DIR__.'/../controllers/'.$controller_class.'.php';
         $controller = new $controller_class($controller_class);
         ob_start();
+        $controller->is_test = true;
         $controller->$action();
         $result = ob_get_contents();
         ob_clean();
