db6477de98f2b3e015c1ae33375bf110f3cf29b1
gugu <gugu@f4a4231c-4c1c-4590-bfba-d6a1cfedac6c>
2012-11-26 18:18:33 +0000

    Игнор

---
+++ protected/tests/PasswordTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class PasswordTest extends SeleniumTestCase
+{
+
+    /**
+     * @large
+     */
+    public function testPasswordRecovery() {
+        $session = $this->webdriver->session('firefox');
+        $session->open($this->browser_url);
+        $element = $this->waitForElement($session, 'xpath', "//input[@value='Регистрация']");
+
+        $element->click("");
+        $login = 'andrey' . time() . '@kostenko.name';
+        $this->waitForElement($session, 'id', 'email')->value(array('value' => str_split($login)));
+        $session->element('id','pass1')->value(array('value' => str_split('test')));
+        $session->element('id','pass2')->value(array('value' => str_split('test')));
+        $session->element('xpath','//input[@class="btn"]')->click();
+        $session->open($this->browser_url);
+        $this->waitForElement($session, 'xpath', "//input[@value='Вход']");
+        $session->element('xpath','//input[@value="Забыли пароль?"]')->click();
+        $session->element('id','email')->value(array('value' => str_split($login)));
+        $session->element('xpath','//input[@value="Восстановить пароль"]')->click();
+        $this->waitForElement($session, 'xpath', '//div[@id="message"]//a[@class="btn"]')->click();
+        $this->assertEquals(1,1);
+    }
+
+}
