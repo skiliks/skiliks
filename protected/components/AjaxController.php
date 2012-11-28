a7b43de8eb8227f6be0c260149c644660f165a19
Andrii Kostenko <andrey@kostenko.name>
2012-11-29 01:18:05 +0400

    windows test fix

--- protected/components/AjaxController.php
+++ protected/components/AjaxController.php
@@ -10,26 +10,29 @@
  *
  * @author dorian
  */
-class AjaxController extends CController{
+class AjaxController extends CController
+{
 
     public $is_test = false;
-    
+
     protected function _sendResponse($status = 200, $body = '', $content_type = 'application/json')
     {
-        header("HTTP/1.0 200 OK");
-        header('Content-type: '.$content_type.'; charset=UTF-8');
-        header("Cache-Control: no-store, no-cache, must-revalidate");
-        header("Cache-Control: post-check=0, pre-check=0", false);
-        header("Access-Control-Allow-Origin: *");
-
-	    echo $body;
+        if (!$this->is_test) {
+            header("HTTP/1.0 200 OK");
+            header('Content-type: ' . $content_type . '; charset=UTF-8');
+            header("Cache-Control: no-store, no-cache, must-revalidate");
+            header("Cache-Control: post-check=0, pre-check=0", false);
+            header("Access-Control-Allow-Origin: *");
+        }
+        echo $body;
 
         if (!$this->is_test)
-	        Yii::app()->end();
+            Yii::app()->end();
     }
 
     /** @method void sendJSON Writes JSON to output */
-    protected function sendJSON($data, $status = 200) {
+    protected function sendJSON($data, $status = 200)
+    {
         $this->_sendResponse($status, CJSON::encode($data));
     }
 }
