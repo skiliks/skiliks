42c01168a781336843b55e5a276fbdf3a603a56c
slavkaa <nikoolin@ukr.net>
2012-12-03 18:37:13 +0200

    SKILIKS-34. Code for flag rules reimport.

--- protected/models/FlagsRulesModel.php
+++ protected/models/FlagsRulesModel.php
@@ -67,6 +67,100 @@ class FlagsRulesModel extends CActiveRecord{
         ));
         return $this;
     }
+    
+    /**
+     * Выбрать по номеру записи
+     * @param int $recordId
+     * @return FlagsRulesModel 
+     */
+    public function byRecordId($recordId)
+    {
+        if (null !== $recordId) {
+            $this->getDbCriteria()->mergeWith(array(
+                'condition' => "rec_id = {$recordId}"
+            ));
+        } else {
+            $this->getDbCriteria()->mergeWith(array(
+                'condition' => "rec_id is null"
+            ));
+        }
+        return $this;
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    public function getEventCode()
+    {
+        return $this->rule_name;
+    }
+    
+    /**
+     * @param string $eventCode
+     * 
+     * @return \FlagsRulesModel
+     */
+    public function setEventCode($eventCode)
+    {
+        $this->rule_name = $eventCode;
+        return $this;
+    }
+    
+    public function getRecordId()
+    {
+        return $this->rec_id;
+    }
+    
+    /**
+     * @param null|integer $recordId
+     * 
+     * @return \FlagsRulesModel
+     */
+    public function setRecordId($recordId)
+    {
+        $this->rec_id = $recordId;
+        return $this;
+    }
+    
+    public function getStepNo()
+    {
+        return $this->step_number;
+    }
+    
+    /**
+     * @param integer $stepNo
+     * 
+     * @return \FlagsRulesModel
+     */
+    public function setStepNo($stepNo)
+    {
+        $this->step_number = $stepNo;
+        return $this;
+    } 
+    
+    public function getReplicaNo()
+    {
+        return $this->step_number;
+    }
+    
+    /**
+     * @param integer $replicaCode
+     * 
+     * @return \FlagsRulesModel
+     */
+    public function setReplicaNo($replicaNo)
+    {
+        $this->replica_number = $replicaNo;
+        return $this;
+    }
+    
+    /**
+     * @param integer $id
+     * 
+     * @return \FlagsRulesModel
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
 
-?>
