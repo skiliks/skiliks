e22d5837cbcb72e77a8070b13ba9fb673f30da9a
Ivan Tugay <listepo@ya.ru>
2012-12-05 17:51:04 +0200

    SK - 6

--- protected/controllers/AdminController.php
+++ protected/controllers/AdminController.php
@@ -6,6 +6,7 @@ class AdminController extends AjaxController
     public function actionLog()
     {
         //$action = Yii::app()->request->getParam('action', false);
+        $send_json = true;
         $action = array(
             'type' => Yii::app()->request->getParam('type','DialogDetail'),
             'data' => (string)Yii::app()->request->getParam('data','json'),
@@ -18,7 +19,14 @@ class AdminController extends AjaxController
             if(method_exists('LogHelper', $method)) {
                 if(isset($action['data'])) {
                     if(isset($action['params']) AND is_array($action['params'])) {
-                        $result += (array)LogHelper::$method($action['data']);
+
+                        $db_data = LogHelper::$method($action['data']);
+                        if(is_array($db_data)){
+                            $result += $db_data;
+                        }else{
+                            $send_json = false;
+                        }
+                        
                     } else {
                         throw new Exception("Не указаны параметры!");
                     }
@@ -37,7 +45,8 @@ class AdminController extends AjaxController
     catch (Exception $e) {
         $result = array('result'=>0, 'message'=>$e->getMessage(), 'data'=>null);
     }
-
-    $this->sendJSON($result);
+    if($send_json){
+        $this->sendJSON($result);
+    }
     }
 }
