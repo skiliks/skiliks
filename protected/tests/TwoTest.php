909710d23cc456895a2e321aed792b247fd5262c
saf <saf@skiliks.com>
2012-12-07 01:37:56 +0200

    Обновил тест 2 в соответствиями с требованиями

--- protected/tests/TwoTest.php
+++ protected/tests/TwoTest.php
@@ -5,11 +5,11 @@ class TwoTest extends SeleniumTestCase
     {
         # Login
         $session = $this->webdriver->session('firefox');
-        $session->open($this->browser_url);
+        $session->open($this->browser_url . 'site.php');
         # раскрыть окно на весь экран
         $session->window()->maximize();
         # из-за черной полосы загрузки, пришлось добавить временное ожидание
-        sleep(2);
+        sleep(3);
         # вводится текст
         $this->waitForElement($session, "id", "login")->value(
             array("value" => str_split("kaaaaav@gmail.com"))
@@ -34,49 +34,51 @@ class TwoTest extends SeleniumTestCase
         $session->element("xpath", "//input[@value='Создать']")->click();
         #ждем исчезновения черной полосы загрузки и в появившемся окне тригера кликаем ОК
         sleep(3);
-        $session->element('css selector', '.alert a.btn')->click();
-
+        $session->element('css selector','.alert a.btn')->click();     
+        sleep(5);
+        
         #2 ответа на диалог с Денежной
-        sleep(15);
-        $session->element("xpath", "//p[@onclick=\"phone.getSelect('5')\"]")->click();
+       
+        $this->waitForElement($session, 'xpath', "//p[text()=\"- Раиса Романовна, ну что вы так волнуетесь?! Я уже несколько дней только бюджетом и занимаюсь, до отпуска точно успею.\"]")->click();
+        $this->waitForElement($session, 'xpath', "//p[text()=\"- Хорошо, за три часа управлюсь.\"]")->click();
         sleep(3);
-        $session->element("xpath", "//p[@onclick=\"phone.getSelect('11')\"]")->click();
-        sleep(2);
+        
 
         #обнуляем поле ввода названия события, вписываем новое, кликаем создать
         $session->element("id", "addTriggerSelect")->clear();
         $this->waitForElement($session, "id", "addTriggerSelect")->value(array("value" => str_split("E8.3")));
         $session->element("xpath", "//input[@value='Создать']")->click();
-        #подтвеждение тригера
+        #подтвеждение тригера, вейт почему то не работает
         sleep(3);
-        $session->element('css selector', '.alert a.btn')->click();
-        sleep(15);
-
+        $session->element('css selector','.alert a.btn')->click();     
+        sleep(5);
+        
+        
         #3 ответа на диалог с начальником АйТи отдела за обедом
-        $session->element("xpath", "//p[@onclick=\"dialogController.getSelect('332')\"]")->click();
-        sleep(3);
-        $session->element("xpath", "//p[@onclick=\"dialogController.getSelect('337')\"]")->click();
-        sleep(3);
-        $session->element("xpath", "//p[@onclick=\"dialogController.getSelect('340')\"]")->click();
+        $this->waitForElement($session, 'xpath', "//p[text()=\"- Нет, прости, Мирон. Сегодня просто сумасшедший день. Собирался почту разбирать как только со срочными вопросами разберусь. А им конца и края нет.\"]")->click();
+        $this->waitForElement($session, 'xpath', "//p[text()=\"- Семен, а у тебя наверняка в бюджете статейка есть на непредвиденные расходы. Это ведь форс-мажор?\"]")->click();
+        $this->waitForElement($session, 'xpath', "//p[text()=\"- Хорошо, сейчас вернусь и напишу служебку. Спасибо за информацию!\"]")->click();
+        
         sleep(10);
+
 
         #обнуляем поле ввода названия события, вписываем новое, кликаем создать
         $session->element("id", "addTriggerSelect")->clear();
         $this->waitForElement($session, "id", "addTriggerSelect")->value(array("value" => str_split("E12.1")));
         $session->element("xpath", "//input[@value='Создать']")->click();
-        #подтвеждение тригера
+        #подтвеждение тригера, вейт почему то не работает
         sleep(3);
-        $session->element('css selector', '.alert a.btn')->click();
-        sleep(15);
-
+        $session->element('css selector','.alert a.btn')->click();     
+        sleep(5);
+        
+        
         #3 ответа на диалог
-        $session->element("xpath", "//p[@onclick=\"phone.getSelect('442')\"]")->click();
-        sleep(3);
-        $session->element("xpath", "//p[@onclick=\"phone.getSelect('444')\"]")->click();
-        sleep(3);
-        $session->element("xpath", "//p[@onclick=\"phone.getSelect('448')\"]")->click();
-        sleep(10);
+        $this->waitForElement($session, 'xpath', "//p[text()=\"- Может мой аналитик подойти вместо меня?\"]")->click();
+        $this->waitForElement($session, 'xpath', "//p[text()=\"- Хорошо, буду в 18.00\"]")->click();
+        $this->waitForElement($session, 'xpath', "//p[text()=\"- Действительно, повезло! Уже бегу!\"]")->click();
+ 
 
+        sleep(8);
         #проверка конечного результата
         $this->assertEquals("Сумма оценок: 4.5", $session->element("css selector", ".result-total")->text());
         $this->assertEquals("Сумма оценок 6x: 10", $session->element("css selector", ".result-total-6x")->text());
