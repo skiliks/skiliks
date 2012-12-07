d6f64c2f36dcfa56a4d1d7523709750f4741fdca
slavkaa <nikoolin@ukr.net>
2012-12-07 16:36:52 +0200

    Experiments with zoho save. v7

--- tmp/zoho_save.php
+++ tmp/zoho_save.php
@@ -3,5 +3,5 @@ $f = fopen('Log.txt', 'a+');
 fwrite($f, "*** \n");
 fwrite($f, serialize($_FILES));
 fclose($f);
-move_uploaded_file($_FILES['content']['tmp_name'], '/var/www/live/backend/tmp/responses/1.xls');
+move_uploaded_file($_FILES['content']['tmp_name'], '/var/www/live/backend/tmp/responses/'.$_FILES['content']['name'] );
 echo 'RESPONSE: saved!';
