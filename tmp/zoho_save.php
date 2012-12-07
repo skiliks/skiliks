15f4e0c356ee7e90fadbdccf5f594790b86145c3
slavkaa <nikoolin@ukr.net>
2012-12-07 16:10:51 +0200

    Experiments with zoho save. v1

--- tmp/zoho_save.php
+++ tmp/zoho_save.php
@@ -1,3 +1,3 @@
 <?php
-move_uploaded_file($_FILES['content']['tmp_name'], '/tmp/'.$_FILES['content']['tmp_name']);
+move_uploaded_file($_FILES['content']['tmp_name'], '/var/www/live/backend/tmp/responces/'.$_FILES['content']['tmp_name']);
 echo 'RESPONSE: saved!';
