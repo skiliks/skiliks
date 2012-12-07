9e1a549b56894dec0f4b2815c9c855cde6dff358
slavkaa <nikoolin@ukr.net>
2012-12-07 18:59:55 +0200

    SKILIKS-791. Encoding problem, 9.

--- protected/controllers/ZohoController.php
+++ protected/controllers/ZohoController.php
@@ -3,7 +3,8 @@ class ZohoController extends CController
 {
     public function actionSaveExcel()
     {
-        /*$name = explode('-', $_FILES['content']['name']);
+        // @todo: add nginx rule to make authentification exeption for api/zoho/saveExcel
+        $name = explode('-', $_FILES['content']['name']);
         
         $simId = $name[0];
         $documentID = $name[1];
@@ -19,7 +20,7 @@ class ZohoController extends CController
             $realFileName
         );
 
-        move_uploaded_file($_FILES['content']['tmp_name'], $pathToUserFile);*/
+        move_uploaded_file($_FILES['content']['tmp_name'], $pathToUserFile);
         
         echo 'RESPONSE: Файл успешно сохранён.';
         die;
