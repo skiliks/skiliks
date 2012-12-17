f4372c0d7ae4c7928269446f2d603211bd735e11
Andrii Kostenko <andrey@kostenko.name>
2012-12-17 16:01:13 +0400

    fix

--- protected/migrations/m121217_115709_add_reply_all.php
+++ protected/migrations/m121217_115709_add_reply_all.php
@@ -4,7 +4,7 @@ class m121217_115709_add_reply_all extends CDbMigration
 {
 	public function up()
 	{
-        $this->addColumn(
+        $this->alterColumn(
             'mail_template',
             'type_of_importance',
             "ENUM('none','2_min','plan','info','first_category', 'reply_all') DEFAULT 'none' COMMENT 'Is it spam, is it impotrant etc. None - not desided by game autor jet.'");
