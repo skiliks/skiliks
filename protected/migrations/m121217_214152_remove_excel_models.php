f119a7ea94d089eb158d62840585058ee6615413
slavkaa <nikoolin@ukr.net>
2012-12-19 00:22:17 +0200

    hot fix;

--- protected/migrations/m121217_214152_remove_excel_models.php
+++ protected/migrations/m121217_214152_remove_excel_models.php
@@ -8,8 +8,8 @@ class m121217_214152_remove_excel_models extends CDbMigration
 {
 	public function up()
 	{
-//        $this->dropTable('excel_worksheet_template_cells');
-//        $this->dropTable('excel_worksheet_cells');
+        $this->dropTable('excel_worksheet_template_cells');
+        $this->dropTable('excel_worksheet_cells');
         $this->dropTable('excel_clipboard');
         $this->dropTable('excel_worksheet');
         $this->dropTable('excel_worksheet_template');
