e0a439db70c69dabacf6669827e0bfec4372b859
slavkaa <nikoolin@ukr.net>
2012-12-19 12:25:23 +0200

    SK-851

---
+++ protected/models/LogDialogs.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @property integer id
+ * @property integer sim_id
+ * @property integer mail_id
+ * @property integer window
+ * @property datetime start_time
+ * @property datetime end_time
+ */
+class Logmail extends CActiveRecord 
+{
+    public $id;
+    
+    public $sim_id;
+    
+    public $mail_id;
+    
+    public $window;
+    
+    public $start_time;
+    
+    public $end_time;
+    
+    public $mail_task_id;
+    
+    /**
+     * @var string, '-' or mail_template.code
+     */
+    public $full_coinsidence;
+    
+    /**
+     * @var string, '-' or mail_template.code
+     */
+    public $part1_coinsidence;
+    
+    /**
+     * @var string, '-' or mail_template.code
+     */    
+    public $part2_coinsidence;
+    
+    /**
+     * @var bool
+     */
+    public $is_coinsidence;
+    
+    /** ------------------------------------------------------------------------------------------------------------ **/
+    
+    /**
+     *
+     * @param type $className
+     * @return Characters
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+    
+    public function bySimId($simId)
+    {
+        $this->getDbCriteria()->mergeWith(array(
+            'condition' => "sim_id = {$simId}"
+        ));
+        return $this;
+    }
+    
+    public function byMailTaskId($mailTaskId)
+    {
+        $this->getDbCriteria()->mergeWith(array(
+            'condition' => "mail_task_id = {$mailTaskId}"
+        ));
+        return $this;
+    }
+    
+    public function byWindow($v)
+    {
+        $this->getDbCriteria()->mergeWith(array(
+            'condition' => "window = {$v}"
+        ));
+        return $this;
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'log_mail';
+    }
+}
