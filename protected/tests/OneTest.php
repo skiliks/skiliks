6340261749e2d549b90e8a0038ddc63526b0d333
Andrii Kostenko <andrey@kostenko.name>
2012-12-19 19:37:37 +0400

    fixed other tests

--- protected/tests/OneTest.php
+++ protected/tests/OneTest.php
@@ -21,7 +21,7 @@ class OneTest extends SeleniumTestCase
         # Ждём появления елемента и кликаем на него
     $this->waitForElement($session, "id","pass")->value(array("value"=>str_split("111")));
         # Кликаем на него
-    $session->element("xpath", "//input[@class='btn']")->click();
+      $session->element("css selector", "input.btn-primary")->click();
         # Enter Developer Mode - дождаться кнопки, кликнуть на кнопку
     $this->waitForElement($session, 'xpath', "//input[@value='Начать симуляцию developer']");
     $session->element("xpath", "//input[@value='Начать симуляцию developer']")->click();
