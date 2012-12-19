ffb2bee29fd2f30a1b4cfa0b1e6f11609d6b16b9
slavkaa <nikoolin@ukr.net>
2012-12-19 11:15:34 +0200

    Move busines logic from controllers.

--- protected/components/MyDocumentsService.php
+++ protected/components/MyDocumentsService.php
@@ -1,39 +1,40 @@
 <?php
 
-
 /**
  * Сервис моих документов
  *
  * @author Sergey Suzdaltsev <sergey.suzdaltsev@gmail.com>
  */
-class MyDocumentsService {
-    
+class MyDocumentsService
+{
+
     /**
      * Копируем все документы для симуляции пользователя
      * @param type $simId 
      */
-    public static function init($simId) {
+    public static function init($simId)
+    {
         $sql = "insert into my_documents (sim_id, fileName, template_id, hidden)
             select :simId, fileName, id, hidden from my_documents_template where type='start'";
-        
+
         $connection = Yii::app()->db;
-        $command = $connection->createCommand($sql);       
+        $command = $connection->createCommand($sql);
         $command->bindParam(":simId", $simId, PDO::PARAM_INT);
         $command->execute();
     }
-    
-    public static function copyToSimulation($simId, $fileId) {
+
+    public static function copyToSimulation($simId, $fileId)
+    {
         $sql = "insert into my_documents (sim_id, fileName, template_id, hidden)
             select :simId, fileName, id, hidden from my_documents_template where id = :fileId ";
-        
+
         $connection = Yii::app()->db;
-        $command = $connection->createCommand($sql);       
+        $command = $connection->createCommand($sql);
         $command->bindParam(":simId", $simId, PDO::PARAM_INT);
         $command->bindParam(":fileId", $fileId, PDO::PARAM_INT);
         $command->execute();
         return $connection->getLastInsertID();
     }
-
 
     /**
      * Проверяет существует ли заданный файл в рамках симуляции
@@ -41,48 +42,98 @@ class MyDocumentsService {
      * @param int $fileId 
      * @return MyDocumentsModel
      */
-    public static function existsInSim($simId, $fileId) {
+    public static function existsInSim($simId, $fileId)
+    {
         return MyDocumentsModel::model()->bySimulation($simId)->byId($fileId)->find();
     }
-    
+
     /**
      * Получить шаблон файла по его id
      * @param int $id 
      * @return int
      */
-    public static function getTemplate($id) {
+    public static function getTemplate($id)
+    {
         $document = MyDocumentsModel::model()->byId($id)->find();
-        if (!$document) return false;
+        if (!$document)
+            return false;
         return $document->template_id;
     }
-    
-    public static function getFileIdByTemplateId($simId, $templateId) {
+
+    /**
+     * @param int $simId
+     * @param int $templateId
+     * @return boolean
+     */
+    public static function getFileIdByTemplateId($simId, $templateId)
+    {
         $document = MyDocumentsModel::model()->bySimulation($simId)->byTemplateId($templateId)->find();
-        if (!$document) return false;
+        if (!$document)
+            return false;
         return $document->id;
     }
-    
+
     /**
      * Определение айди документа по его коду
      * @param string $code
      * @return int
      */
-    public static function getTemplateIdByCode($code) {
+    public static function getTemplateIdByCode($code)
+    {
         $document = MyDocumentsTemplateModel::model()->byCode($code)->find();
-        
-        if (!$document) return false;
+
+        if (!$document)
+            return false;
         return $document->id;
     }
-    
-    public static function getAllCodes() {
+
+    /**
+     * @return mixed array
+     */
+    public static function getAllCodes()
+    {
         $documents = MyDocumentsTemplateModel::model()->findAll();
-        
+
         $list = array();
-        foreach($documents as $document) {
+        foreach ($documents as $document) {
             $list[$document->code] = $document->id;
         }
         return $list;
     }
-}
 
+    /**
+     * @param integer $fileId
+     * 
+     * @throws Exception
+     * 
+     * @return array of string, paths to file pages (images)
+     */
+    public static function getDocumentPages($fileId)
+    {
+        // получить шаблон файла
+        $templateId = self::getTemplate($fileId);
+        $pages = array();
+        $fileId = 0;
+        
+        if (!$templateId) {
+            throw new Exception("Немогу определить шаблон для файла {$fileId}");
+        }
+
+        $items = ViewerTemplateModel::model()->byFile($templateId)->findAll();
+
+        foreach($items as $item) {
+            $fileId = $item->file_id;
+            $pages[] = $item->filePath;
+        }
+
+        // получим кода файлов
+        $file = MyDocumentsTemplateModel::model()->byId($fileId)->find();
+
+        foreach($pages as $index => $page) {
+            $pages[$index] = $file->code.'/'.$page;
+        }
+        
+        return $pages;
+    }
+}
 
