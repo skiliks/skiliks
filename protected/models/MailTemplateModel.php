fe5a8a1e18cbb1b21f70b82342744fcf3cb69097
slavkaa <nikoolin@ukr.net>
2012-12-19 18:24:17 +0200

    SKILIKS-862. Fix mail import.

--- protected/models/MailTemplateModel.php
+++ protected/models/MailTemplateModel.php
@@ -128,6 +128,18 @@ class MailTemplateModel extends CActiveRecord
     }
     
     /**
+     * @param string $ids
+     * @return \MailTemplateModel
+     */
+    public function byIdsNotIn($ids)
+    {
+        $this->getDbCriteria()->mergeWith(array(
+            'condition' => " `id` NOT IN ({$ids})"
+        ));
+        return $this;
+    }    
+    
+    /**
       * @param ineger $receiverId
      * @return \MailTemplateModel
      */
