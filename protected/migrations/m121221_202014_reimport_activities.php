3c14d73053ce5a823d536bffe04223774142d00d
slavkaa <nikoolin@ukr.net>
2012-12-21 23:24:26 +0200

    Fix Activities import.

--- protected/migrations/m121221_202014_reimport_activities.php
+++ protected/migrations/m121221_202014_reimport_activities.php
@@ -5,7 +5,11 @@ class m121221_202014_reimport_activities extends CDbMigration
 	public function up()
 	{
         $import = new ImportGameDataService();
-        $import->importActivity();
+        $result1 = $import->importActivity();
+        var_dump($result1);
+        
+        $result2 = $import->importActivityEfficiencyConditions();
+        var_dump($result2);
 	}
 
 	public function down()
