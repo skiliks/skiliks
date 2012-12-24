674c5c9e409f9cba3aac0062a63682fed276a616
slavkaa <nikoolin@ukr.net>
2012-12-24 17:43:32 +0200

    Debug Zoho save.

--- protected/controllers/ZohoController.php
+++ protected/controllers/ZohoController.php
@@ -3,11 +3,14 @@ class ZohoController extends CController
 {
     public function actionSaveExcel()
     {
+        Yii::log('Zoho save start. ');
         $status = ZohoDocuments::saveFile(
             Yii::app()->getRequest()->getParam('id'), 
             $_FILES['content']['tmp_name'], 
             'xls'
         );
+        
+        Yii::log('Zoho save status: '.$status);
         
         $this->_sendResponse(200, 'RESPONSE: '.$status);
     }
