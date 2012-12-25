7c07dd05fe87991e67d5468870d3680155aac77b
Andrii Kostenko <andrey@kostenko.name>
2012-12-25 17:50:10 +0400

    logging only final replicas

--- protected/components/LogHelper.php
+++ protected/components/LogHelper.php
@@ -827,7 +827,7 @@ class LogHelper {
                 } elseif( self::ACTION_CLOSE == (string)$log[2] || self::ACTION_DEACTIVATED == (string)$log[2] ) {
                     $windows = LogWindows::model()->findAllByAttributes(array('end_time' => '00:00:00', 'sim_id' => $simId));
                     if (!$windows) {
-                        continue;
+                        throw(new CException('Two active windows at one time. Achtung!'));
                     }
                     foreach ($windows as $window) {
                         $window->end_time = date("H:i:s", $log[3]);
@@ -976,7 +976,7 @@ class LogHelper {
             if (empty($log[4]['dialogId'])) continue;
             
             $lastDialogIdInMySQL = $log[4]['lastDialogId'];
-            $dialog = Dialogs::model()->findByPk($lastDialogIdInMySQL);
+            $dialog = Dialogs::model()->findByAttributes(['id' => $lastDialogIdInMySQL, 'is_final_replica' => 1]);
             $lastDialogIdAccordingExcel = (null === $dialog) ? null : $dialog->excel_id;
             
             if( self::ACTION_OPEN == (string)$log[2] || self::ACTION_ACTIVATED == (string)$log[2]) {
@@ -1055,7 +1055,7 @@ class LogHelper {
     }
     
     public static function getLegActionsDetail($return) {
-
+                # Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn :)
                 $sql = "SELECT DISTINCT l.sim_id
                               , CASE
                                 WHEN a.dialog_id THEN
