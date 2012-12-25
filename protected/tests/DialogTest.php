2bf5674f45392f69cc8496e38fa0337f7df55b03
Andrii Kostenko <andrey@kostenko.name>
2012-12-25 04:19:36 +0400

    test impromevent, single import

---
+++ protected/tests/DialogTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: gugu
+ * Date: 19.12.12
+ * Time: 18:13
+ * To change this template use File | Settings | File Templates.
+ */
+class DialogTest extends SeleniumTestCase
+{
+    /**
+     * @large
+     */
+    public function testE1()
+    {
+
+        # Login
+        $session = $this->webdriver->session('firefox');
+        $this->startSimulation($session);
+        $this->runEvent($session, "E1");
+        # one letter
+        $this->waitForElement($session, 'xpath', "//p[text()=\"- Раиса Романовна, ну что вы так волнуетесь?! Я уже несколько дней только бюджетом и занимаюсь, до отпуска точно успею.\"]")->click();
+        $this->waitForElement($session, 'xpath', "//p[text()=\"- Ну, с помощью Крутько я должен управиться в эти сроки.\"]")->click();
+        $this->waitForElement($session, 'xpath', "//p[text()=\"-          Марина, есть срочная работа.\"]")->click();
+        $this->waitForElement($session, 'xpath', '//p[text()="- А мне что делать? А ты не можешь пока отложить презентацию? Через пару часиков снова ею займешься."]')->click();
+        $this->waitForElement($session, 'xpath', '//p[text()="- Да уж, ситуация... Значит так, быстренько зови сюда Трутнева, попробую его подключить."]')->click();
+        $this->waitForElement($session, 'xpath', '//p[text()="-          Сергей, нужно сделать бюджет. Срочно. Тебе придется этим заняться."]')->click();
+        $this->waitForElement($session, 'xpath', '//p[text()="- Я тебе сейчас перешлю файл, ты посмотри, и сразу приходи, если будут вопросы, разберемся."]')->click();
+
+    }
+
+
+
+}
