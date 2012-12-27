579aeb7f48002e886aa325b82689e9466adb98c7
Andrii Kostenko <andrey@kostenko.name>
2012-12-28 02:33:52 +0400

    db configs

--- protected/config/stage_live.php
+++ protected/config/stage_live.php
@@ -3,10 +3,10 @@
 return CMap::mergeArray(require(dirname(__FILE__) . '/base.php'), array(
     'components'=>array(
         'db'=>array(
-            'connectionString' => 'mysql:host=localhost;dbname=skiliks',
+            'connectionString' => 'mysql:host=localhost;dbname=skiliks_live',
             'emulatePrepare' => true,
-            'username' => 'root',
-            'password' => 'Vtufpfdh',
+            'username' => 'skiliks_live',
+            'password' => 'dep-vep-eb-up-a',
             'charset' => 'utf8',
 
             'enableParamLogging'=>true,
