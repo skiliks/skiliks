0229248daf593baea486e6cfb8dd677ff78e13b5
slavkaa <nikoolin@ukr.net>
2012-12-27 13:21:37 +0200

    SKILIKS-897. fix zoho.

--- protected/config/stage_real.php
+++ protected/config/stage_real.php
@@ -16,7 +16,14 @@ return CMap::mergeArray(require(dirname(__FILE__) . '/base.php'), array(
     'params' => array(
         'frontendUrl' => 'http://front.skiliks.com/',
         'simulationStartTime' => '9:45',
-        'skiliksSpeedFactor' => 4
+        'skiliksSpeedFactor' => 4,
+        'zoho' => array(
+            'apiKey'              => 'b5e3f7316085c8ece12832f533c751be',
+            'saveUrl'             => 'http://skiliks.com/api/index.php/zoho/saveExcel',
+            'xlsTemplatesDirPath' => 'documents/excel',
+            'templatesDirPath'    => 'documents',
+            'sendFileUrl'         => 'https://sheet.zoho.com/remotedoc.im?apikey=%s&output=editor',
+        ),
     )
 ));
 
