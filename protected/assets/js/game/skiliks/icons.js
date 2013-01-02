6727cfc37bee82ca0e0700be28c325bc49b506a5
Andrii Kostenko <andrey@kostenko.name>
2013-01-02 17:22:15 +0200

    mail, icons disabled, img fixes

--- protected/assets/js/game/skiliks/icons.js
+++ protected/assets/js/game/skiliks/icons.js
@@ -1,142 +1,100 @@
 icons = {
-    issetDiv: false,
-    divTop: 50,
-    divLeft: 50,
+    issetDiv:false,
+    divTop:50,
+    divLeft:50,
     divZindex:50,
-    
+
     newEvents:{
         'visit':[],
         'phone':[],
-        
+
         'todo':[],
         'email':[],
         'documents':[]
     },
-    
+
     recalcCounter:0,
     recalcReq:10,
-    setDivTop: function(val)
-    {
+    setDivTop:function (val) {
         this.divTop = val;
     },
-    setDivLeft: function(val)
-    {
+    setDivLeft:function (val) {
         this.divLeft = val;
     },
-    switchDisplayMode: function(mode){
-        if(mode == 'normal'){
-            $('#iconsMainDiv').css('z-index',  this.divZindex);
-        }else if(mode == 'dialog'){
+    switchDisplayMode:function (mode) {
+        if (mode == 'normal') {
+            $('#iconsMainDiv').css('z-index', this.divZindex);
+        } else if (mode == 'dialog') {
             var zIndex = dialogController.getZindex();
-            $('#iconsMainDiv').css('z-index',  (zIndex-1));
+            $('#iconsMainDiv').css('z-index', (zIndex - 1));
         }
     },
-    createDiv: function()
-    {
+    createDiv:function () {
         var div = document.createElement('div');
-          div.setAttribute('id', 'iconsMainDiv');
-          div.setAttribute('class', 'iconsMainDiv');
-          div.style.position = "absolute";
-          div.style.zIndex = this.divZindex;
-          document.body.appendChild(div);
-          $('#iconsMainDiv').css('top', this.divTop+'px');
-          $('#iconsMainDiv').css('left',  this.divLeft+'px');
-          $('#iconsMainDiv').css('right',  this.divLeft+'px');
-          
-          this.issetDiv = true;
-    },
-    draw: function()
-    {
-        if(!this.issetDiv){
-            this.createDiv();
-        }
-        //обновляем по todo счетчик
-        sender.dayPlanTodoGetCount();
-        
-        
-        var html = this.html;
-        html = php.str_replace('{icon_phone}', this.htmlIconPhone, html);
-        html = php.str_replace('{icon_visit}', this.htmlIconVisit, html);
-        html = php.str_replace('{icon_todo}', this.htmlIconTodo, html);
-        html = php.str_replace('{icon_email}', this.htmlIconEmail, html);
-        html = php.str_replace('{icon_documents}', this.htmlIconDocuments, html);
-        $('#iconsMainDiv').html(html);
-        this.update();
-        
-        $('.main-screen-icons ul li a').hover(function(){
-		$(this).parent('li').css('background-position','0 -140px')
-	},
-	function(){
-		$(this).parent('li').css('background-position','0 0')
-	})
-    },
-    addCustomEvent: function(icon,flag,id)
-    {
-        var curUnixtime = timer.getCurUnixtime();
-        var newEvent = {
-            'flag':flag,
-            'id':id,
-            'curUnixtime':curUnixtime
-        };
-        this.newEvents[icon].push(newEvent);
-    },
-    addNewEvent: function(newEvent)
-    {
+        div.setAttribute('id', 'iconsMainDiv');
+        div.setAttribute('class', 'iconsMainDiv');
+        div.style.position = "absolute";
+        div.style.zIndex = this.divZindex;
+        document.body.appendChild(div);
+        $('#iconsMainDiv').css('top', this.divTop + 'px');
+        $('#iconsMainDiv').css('left', this.divLeft + 'px');
+        $('#iconsMainDiv').css('right', this.divLeft + 'px');
 
-        if(newEvent[0]['dialog_subtype']==1){
+        this.issetDiv = true;
+    },
+    addNewEvent:function (newEvent) {
+
+        if (newEvent[0]['dialog_subtype'] == 1) {
             var curUnixtime = timer.getCurUnixtime();
             newEvent[0]['curUnixtime'] = curUnixtime;
-         
+
             this.newEvents['phone'].push(newEvent);
-        }else if(newEvent[0]['dialog_subtype']==5){
+        } else if (newEvent[0]['dialog_subtype'] == 5) {
             var curUnixtime = timer.getCurUnixtime();
             newEvent[0]['curUnixtime'] = curUnixtime;
-            
+
             this.newEvents['visit'].push(newEvent);
-        }else{
-            if(newEvent[0]['dialog_subtype'] == 2){
+        } else {
+            if (newEvent[0]['dialog_subtype'] == 2) {
                 dialogController.draw('close');
                 phone.draw('dialog', newEvent);
                 return;
-            }else{
+            } else {
                 phone.draw('close');
                 dialogController.draw('dialog', newEvent);
                 return;
             }
         }
     },
-    update: function()
-    {
-        if(php.count(this.newEvents['phone']) > 0){
+    update:function () {
+        if (php.count(this.newEvents['phone']) > 0) {
             for (var key in this.newEvents['phone']) {
                 var event = this.newEvents['phone'][key];
                 var curUnixtime = timer.getCurUnixtime();
-                
+
                 // catch Missed phone call as Ignoder {
-                if( event[0]['curUnixtime']<(curUnixtime-12))
-                {
+                if (event[0]['curUnixtime'] < (curUnixtime - 12)) {
                     phone.missedCalls++;
                     icons.setIconCounter('phone', phone.missedCalls);
                     simulation.isRecentlyIgnoredPhone = true;
                     var my = this.newEvents['phone'].shift() // => "удаляем первый элемент"
-                        
+
                     // if ('undefined' !== typeof event[2]) - trick :(, just to prevent JS warning
                     if ('undefined' !== typeof event[2]) {
                         sender.phoneGetSelect(event[2]['id']);
-                     }  
+                    }
                     return;
                 }
                 // catch Missed phone call as Ignoder }
             }
         }
-        
-        if(php.count(this.newEvents['visit']) > 0){
+
+        if (php.count(this.newEvents['visit']) > 0) {
             for (var key in this.newEvents['visit']) {
                 var event = this.newEvents['visit'][key];
                 var curUnixtime = timer.getCurUnixtime();
-                    
-                if( event[0]['curUnixtime']<(curUnixtime-15))
-                {
+
+                if (event[0]['curUnixtime'] < (curUnixtime - 15)) {
                     var my = this.newEvents['visit'].shift() // => "удаляем первый элемент"
 
                     sender.dialogsGetSelect(event[2]['id']);
@@ -144,41 +102,38 @@ icons = {
                 }
             }
         }
-        
+
         this.animateIcons('phone');
         this.animateIcons('visit');
-        
-        if(php.count(this.newEvents['todo']) > 0){
+
+        if (php.count(this.newEvents['todo']) > 0) {
             for (var key in this.newEvents['todo']) {
                 var event = this.newEvents['todo'][key];
                 var curUnixtime = timer.getCurUnixtime();
-                
-                if( event['curUnixtime']<(curUnixtime-15))
-                {
+
+                if (event['curUnixtime'] < (curUnixtime - 15)) {
                     var my = this.newEvents['todo'].shift() // => "удаляем первый элемент"
                     return;
                 }
             }
         }
-        if(php.count(this.newEvents['email']) > 0){
+        if (php.count(this.newEvents['email']) > 0) {
             for (var key in this.newEvents['email']) {
                 var event = this.newEvents['email'][key];
                 var curUnixtime = timer.getCurUnixtime();
-                
-                if( event['curUnixtime']<(curUnixtime-15))
-                {
+
+                if (event['curUnixtime'] < (curUnixtime - 15)) {
                     var my = this.newEvents['email'].shift() // => "удаляем первый элемент"
                     return;
                 }
             }
         }
-        if(php.count(this.newEvents['documents']) > 0){
+        if (php.count(this.newEvents['documents']) > 0) {
             for (var key in this.newEvents['documents']) {
                 var event = this.newEvents['documents'][key];
                 var curUnixtime = timer.getCurUnixtime();
-                
-                if( event['curUnixtime']<(curUnixtime-15))
-                {
+
+                if (event['curUnixtime'] < (curUnixtime - 15)) {
                     var my = this.newEvents['documents'].shift() // => "удаляем первый элемент"
                     return;
                 }
@@ -188,76 +143,72 @@ icons = {
         this.animateIcons('email');
         this.animateIcons('documents');
     },
-    animateIcons: function(icon)
-    {
-        var icon_el = $('#icons_'+icon).parent();
-        if(php.count(this.newEvents[icon]) > 0){
+    animateIcons:function (icon) {
+        var icon_el = $('#icons_' + icon).parent();
+        if (php.count(this.newEvents[icon]) > 0) {
             if (!icon_el.hasClass('icon-active')) {
                 icon_el.addClass('icon-active');
                 var bounceIcon;
-                bounceIcon = function() {
+                bounceIcon = function () {
                     if (!icon_el.hasClass('icon-active')) {
                         return;
                     }
-                    icon_el.animate({'margin-left':'-20px'}, 'fast', function() {
-                        icon_el.animate({'margin-left': '20px'}, 'fast', bounceIcon)
+                    icon_el.animate({'margin-left':'-20px'}, 'fast', function () {
+                        icon_el.animate({'margin-left':'20px'}, 'fast', bounceIcon)
                     });
                 };
                 bounceIcon();
             }
             /*
-            $('#icons_'+icon).css('width',  mathematics.getRandom(50, 80)+'px');
-            $('#icons_'+icon).css('height', mathematics.getRandom(50, 80)+'px');
-            $('#icons_'+icon).css('background', 'red');
-            */
-        }else{
+             $('#icons_'+icon).css('width',  mathematics.getRandom(50, 80)+'px');
+             $('#icons_'+icon).css('height', mathematics.getRandom(50, 80)+'px');
+             $('#icons_'+icon).css('background', 'red');
+             */
+        } else {
             icon_el.removeClass('icon-active');
             icon_el.css({'margin-left':'0px'});
             /*
-            $('#icons_'+icon).css('width', '78px');
-            $('#icons_'+icon).css('height', '78px');
-            $('#icons_'+icon).css('background', '');
-            */
+             $('#icons_'+icon).css('width', '78px');
+             $('#icons_'+icon).css('height', '78px');
+             $('#icons_'+icon).css('background', '');
+             */
         }
     },
-    showEvent: function(icon)
-    {
-        if(php.count(this.newEvents[icon]) > 0){
+    showEvent:function (icon) {
+        if (php.count(this.newEvents[icon]) > 0) {
             var popped = this.newEvents[icon].pop();
-            
-            if(icon=='todo'){
+
+            if (icon == 'todo') {
                 dayPlan.draw();
                 dayPlan.taskdayPlanToSelect = popped['id'];
-            }else if(icon=='email'){
-                if(popped['flag']=='MS'){
+            } else if (icon == 'email') {
+                if (popped['flag'] == 'MS') {
                     mailEmulator.status = 0;
                     mailEmulator.draw('new');
                     mailEmulator.drawNewLetter();
-                }else{
+                } else {
                     mailEmulator.status = 0;
                     mailEmulator.draw();
                     mailEmulator.curMesageToSelect = popped['id'];
                 }
-            }else if(icon=='documents'){
+            } else if (icon == 'documents') {
                 documents.fileToSelect = popped['id'];
                 documents.draw();
-            }else if(icon=='phone'){
+            } else if (icon == 'phone') {
                 phone.draw('income', popped);
-            }else{
+            } else {
                 dialogController.draw('income', popped);
             }
-        }else{
-            if(icon=='todo'){
+        } else {
+            if (icon == 'todo') {
                 dayPlan.draw()
-            }else if(icon=='email'){
+            } else if (icon == 'email') {
                 mailEmulator.draw()
-            }else if(icon=='documents'){
+            } else if (icon == 'documents') {
                 documents.draw()
-            }else
-            
-            if(icon=='phone'){
+            } else if (icon == 'phone') {
                 phone.draw();
-            }else{
+            } else {
                 var message = 'События по личной инициативе вам на данный момент не доступны';
                 var lang_alert_title = 'События';
                 var lang_confirmed = 'Ок';
@@ -265,12 +216,10 @@ icons = {
             }
         }
     },
-    setIconCounter: function(icon, counter)
-    {
-        $('#icons_'+icon).html('<span>'+counter+'</span>');
+    setIconCounter:function (icon, counter) {
+        $('#icons_' + icon).html('<span>' + counter + '</span>');
     },
-    removeIconCounter: function(icon)
-    {
-        $('#icons_'+icon).html('');
+    removeIconCounter:function (icon) {
+        $('#icons_' + icon).html('');
     }
 }
