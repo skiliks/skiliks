e92eb21b3b9da2bf0694e4ebb93c39d043f8af10
Ivan Tugay <listepo@ya.ru>
2013-01-02 19:00:43 +0200

    SKPhoneView

---
+++ protected/assets/js/game/views/SKPhoneView.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(function () {
+    "use strict";
+
+    window.SKPhoneView = window.SKWindowView.extend({
+        'initialize':function (){
+            
+        },
+        'render':function (){
+            
+        }
+    });
+});
