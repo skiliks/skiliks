390843b6687f17f936d168099e5b83e72ee6e877
Ivan Tugay <listepo@ya.ru>
2013-01-02 16:39:22 +0200

    SKWindowView.js

---
+++ protected/assets/js/game/views/SKWindowView.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function () {
+    "use strict";
+
+    window.SKWindowView = Backbone.View.extend({
+        
+    });
+});
+
