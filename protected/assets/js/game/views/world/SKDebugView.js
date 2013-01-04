9ec1d804a681411af1ca8ef2b7c1278e42bbab03
Andrii Kostenko <andrey@kostenko.name>
2013-01-04 11:43:04 +0200

    Debug, location of replicas

--- protected/assets/js/game/views/world/SKDebugView.js
+++ protected/assets/js/game/views/world/SKDebugView.js
@@ -5,26 +5,38 @@
         'initialize':function () {
             this.render();
         },
-        'events': {
-            'click .set-time': 'doSetTime',
-            'submit .form-set-time': 'doFormSetTime'
+        'events':{
+            'click .set-time':'doSetTime',
+            'submit .form-set-time':'doFormSetTime',
+            'submit .trigger-event':'doEventTrigger'
         },
         'render':function () {
             this.$el.html(_.template($('#debug_panel').html(), {}));
         },
-        'doSetTime': function (event) {
+        'doSetTime':function (event) {
             var target = event.currentTarget;
             event.preventDefault();
             var hour = $(target).attr('data-hour');
             var minute = $(target).attr('data-minute');
             SKApp.user.simulation.setTime(hour, minute);
         },
-        'doFormSetTime': function (event) {
+        'doFormSetTime':function (event) {
             var target = event.currentTarget;
             event.preventDefault();
             var hours = target.elements.hours.value;
             var minutes = target.elements.minutes.value;
             SKApp.user.simulation.setTime(hours, minutes);
+        },
+        'doEventTrigger':function (event) {
+            var target = event.currentTarget;
+            event.preventDefault();
+            addTrigger.add(
+                target.elements.code.value,
+                target.elements.delay.value,
+                target.elements.clear_events.value,
+                target.elements.clear_assessment.value
+            );
+
         }
     });
 })();
