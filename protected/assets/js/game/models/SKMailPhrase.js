74b7f59b4182a8d8808941158d75a310ba4d3829
slavkaa <nikoolin@ukr.net>
2013-01-08 18:59:56 +0200

    SKILIKS-973. добавить отправку нового письма с темой из списка.

---
+++ protected/assets/js/game/models/SKMailPhrase.js
@@ -0,0 +1,21 @@
+/* 
+ * 
+ */
+(function() {
+    "use strict";
+    window.SKMailPhrase = Backbone.Model.extend({
+        // @var integer
+        mySqlId : undefined,
+
+        // @var string, 
+        text: undefined,
+        
+        // @var string
+        // uniqueId - to identify prases with same text in email, when user want to delete one from such phrases
+        uid: undefined, 
+        
+        initialize: function() {
+            this.uid = (new Date()).getTime() + '_' + Math.floor(Math.random()*100000);
+        }
+    });
+})();
