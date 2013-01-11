5a245aeb51b953e496084385ac6af63a4938985b
slavkaa <nikoolin@ukr.net>
2013-01-11 18:37:59 +0200

    update dialog to prevent any click exept current dialog buttons.

--- protected/assets/js/game/views/SKDialogView.js
+++ protected/assets/js/game/views/SKDialogView.js
@@ -6,32 +6,59 @@
      * @type {*}
      */
     window.SKDialogView = Backbone.View.extend({
+        
+        // dialog`s root DOM element
+        $el: undefined,
+        
+        preventOtherClicksElement: undefined,
+        
         'initialize': function () {
             this.options.buttons.forEach(function (button) {
                 button.id = _.uniqueId('button_');
             });
             this.render();
         },
+        
+        renderPreventClickElement: function() {
+            this.preventOtherClicksElement = 
+                $('<div class="preventOtherClicks" style="position: absolute; background: none repeat scroll 0 0 transparent; height: 100%;;width:100%;"></div>');
+            
+            this.preventOtherClicksElement.topZIndex();
+            
+            $('#canvas').prepend(this.preventOtherClicksElement);
+            
+            $('.preventOtherClicks').click(function(){
+                me.close();
+            });    
+        },
+        
         'render': function () {
-            var template = _.template($('#dialog_template').html(), {
+            
+            // must be first to get Z-index under dialog HTML block
+            this.renderPreventClickElement();
+            
+            this.$el = $(_.template($('#dialog_template').html(), {
                 title: this.options.message,
                 buttons: this.options.buttons
-            });
-            var el = $(template);
-            el.css({
-                'zIndex': 60000,
+            }));
+ 
+            this.$el.css({
+                //'zIndex': 60000, // topZIndex wokrs well
                 'top': '70px',
                 'position': 'absolute',
                 'width': '100%',
                 'margin': 'auto'
             });
-            el.topZIndex();
-            $('body').prepend(el);
-            this.$el = el;
+            
+            this.$el.topZIndex();
+            
+            $('body').prepend(this.$el);
         },
+        
         'events': {
             'click .mail-popup-button': 'handleClick'
         },
+        
         'handleClick': function (event) {
             var target = $(event.target).parents('*').andSelf().filter('.mail-popup-button');
             this.options.buttons.forEach(function(button) {
@@ -41,7 +68,13 @@
                     }
                 }
             });
+            
+            this.cleanUpDOM();
+        },
+        
+        cleanUpDOM: function(){
             this.$el.remove();
+            this.preventOtherClicksElement.remove();  
         }
     });
 })();
