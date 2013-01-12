5c88e2ad75682f0db830ef31c4ebbe91618661ee
Andrii Kostenko <andrey@kostenko.name>
2013-01-12 18:12:51 +0200

    window manager

--- protected/assets/js/game/views/phone/SKPhoneView.js
+++ protected/assets/js/game/views/phone/SKPhoneView.js
@@ -4,89 +4,43 @@
 $(function () {
     "use strict";
 
-    window.SKPhoneView = window.SKWindowView.extend({
-        initialize:function () {
-            if(this.countMaxView > this.getCountViews()){
-                this.open();
-            } else {
-                this.close();
-            }
-        },
-        el:null,
-        countMaxView: 1,
-        windowClass: "phoneMainDiv",
-        windowID: '',
-        SKWindow:null,
-        events:{
-            'click .btn-cl':'close',
+    window.SKPhoneView = SKWindowView.extend({
+        title: "Телефон",
+        events:_.defaults({
             'click .phone_get_contacts':'getContacts',
             'click .phone_get_history':'getHistory',
             'click .phone_get_menu':'getMenu'
-        },
-        open: function () {
-            var div = document.createElement('div');
-            this.windowID = this.cid;
-            div.setAttribute('id', this.windowID);
-            div.setAttribute('class', this.windowClass);
-            div.style.position = "absolute";
-            $(this.container).append(div);
-            $('#'+this.windowID).css('position', 'absolute');
-            var canvas = $('#canvas').width();
-            var icons = $('.main-screen-icons').width();
-            var phone = $('#'+this.windowID).width(); 
-            var left = (canvas - icons - phone) / 2;
-            $('#'+this.windowID).css('left', left+'px');
-            this.renderTPL('#'+this.windowID, '#Phone_Html', {windowID:this.windowID});
-            this.$el = $('#'+this.windowID);
-            this.SKWindow = new SKWindow({name:'phone', subname:'phoneMain'});
-            this.SKWindow.open();
-        },
-        close:function (event) {            
-            if(event !== undefined){
-                var id = $(event.toElement).attr('window_id');
-                $('#'+id).remove();
-            }else{
-                $('.'+this.windowClass).remove();
-            }
-            this.SKWindow.close();
+        }, SKWindowView.prototype.events),
+        renderContent: function (window_el) {
+            window_el.html(_.template($('#Phone_Html').html(), _.defaults({windowID:this.windowID}, SKConfig)));
         },
         getContacts: function (event) {
-            console.log(event.toElement);
             var id = $(event.toElement).attr('window_id');
             
             //$('#'+id+' .phone-screen')
             var contacts = new SKPhoneContactsCollection();
             contacts.fetch();
-            console.log('#'+id+' .phone-screen');
             var me = this;
             contacts.on('reset', function () {
-                me.renderTPL('#'+id+' .phone-screen', '#Phone_Contacts', {contacts:contacts});
-                me.doScroll(id);
+                me.renderTPL('.phone-screen','#Phone_Contacts', {contacts:contacts});
             });
         },
         getHistory: function (event) {
-            console.log(event.toElement);
             var id = $(event.toElement).attr('window_id');
             
             var history = SKApp.user.simulation.phone_history;
             history.fetch();
-            history.readHistory();
-            console.log('#'+id+' .phone-screen');
             var me = this;
             history.on('reset', function () {
                 me.renderTPL('#'+id+' .phone-screen', '#Phone_History', {history:history, types:['in','out','miss']});
-                me.doScroll(id);
-            })
+            });
         },
         getMenu: function(event){
             var id = $(event.toElement).attr('window_id');
-            this.renderTPL('#'+id+' .phone-screen', '#Phone_Menu', {windowID:id});
+            this.renderTPL('.phone-screen', '#Phone_Menu', {windowID:id});
         },
         getCountViews : function(){
             return $('.'+this.windowClass).length;
-        }, 
-        doScroll: function(id){
-            $('#'+id+' .phone-screen').mCustomScrollbar({'advanced':{'updateOnContentResize':true}});
         }
     });
 });
