89d86b4b85d38caf2298eb701d7b72a7fb2a47d2
Ivan Tugay <listepo@ya.ru>
2013-01-15 22:57:04 +0200

    SK-1022

--- protected/assets/js/game/collections/SKPhoneThemeCollection.js
+++ protected/assets/js/game/collections/SKPhoneThemeCollection.js
@@ -4,9 +4,16 @@
     "use strict";
     window.SKPhoneThemeCollection = Backbone.Collection.extend({
         model:SKPhoneTheme,
+        contact:{},
+        initialize:function(contact) {
+            this.contact = contact;
+        },
+        parse:function(data) {
+            return _.values(data.data);
+        },
         sync:function (method, collection, options) {
             if ('read' === method) {
-                SKApp.server.api('phone/getThemes', function (data) {
+                SKApp.server.api('phone/getThemes', this.contact, function (data) {
                     options.success(data);
                 });
             }
