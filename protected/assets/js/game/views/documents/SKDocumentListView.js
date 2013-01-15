717afea1bb0c576127eef121ad21c886fec7e792
Andrii Kostenko <andrey@kostenko.name>
2013-01-15 20:19:27 +0200

    initial socuments

---
+++ protected/assets/js/game/views/documents/SKDocumentListView.js
@@ -0,0 +1,40 @@
+/*global SKWindow, _, SKWindowView, SKConfig, SKApp, SKPhoneContactsCollection
+ */
+
+(function () {
+    "use strict";
+
+    window.SKDocumentsListView = SKWindowView.extend({
+        addClass: 'documents-list',
+        renderContent:function (el) {
+            el.elfinder({
+                url:'myDocuments/connector',
+                transport:{
+                    send:function (options) {
+                        return SKApp.server.api('myDocuments/connector', options.data);
+
+                    }
+                },
+                uiOptions : {
+                    // toolbar configuration
+                    toolbar : [
+                        ['back', 'forward'],
+                        // ['reload'],
+                        // ['home', 'up'],
+                        ['open', 'download', 'getfile'],
+                        ['info'],
+                        ['quicklook'],
+                        ['search'],
+                        ['view']
+                    ],
+
+                    navbar : {
+                        minWidth : 150,
+                        maxWidth : 500
+                    }
+                }
+                // lang: 'ru',             // language (OPTIONAL)
+            }).elfinder('instance');
+        }
+    });
+})();
