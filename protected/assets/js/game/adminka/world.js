8f35f6df8553f05e7c691589ad63cbf517130cba
slavkaa <nikoolin@ukr.net>
2013-01-16 16:27:01 +0200

    Clean up logging.

--- protected/assets/js/game/adminka/world.js
+++ protected/assets/js/game/adminka/world.js
@@ -6,7 +6,6 @@ world = {
     draw:function(string)
     {
         var activeFrame = frame_switcher.setToHTML();
-        console.log('frame_switcher: ', frame_switcher);
         var html='';
         html += menuMain.get();
         html += string;
