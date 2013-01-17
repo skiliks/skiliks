b307517f16e2863e7e7214122e376d107719fff4
Andrii Kostenko <andrey@kostenko.name>
2013-01-17 17:49:11 +0200

    XLS support

--- protected/controllers/ExcelDocumentController.php
+++ protected/controllers/ExcelDocumentController.php
@@ -9,31 +9,34 @@ class ExcelDocumentController extends AjaxController
     /**
      * New code!
      * @autor Slavka
-     * @return 
+     * @return
      */
-    public function actionGet() 
+    public function actionGet()
     {
         $simulation = $this->getSimulationEntity();
-        
-        $fileId = (int)Yii::app()->request->getParam('fileId', NULL);
-        
+
+        $fileName = Yii::app()->request->getParam('fileName', NULL);
+        $fileName = preg_replace('/.*\//', '', $fileName);
+        $file_template = MyDocumentsTemplateModel::model()->findByAttributes(['srcFile' => $fileName]);
+        $file = MyDocumentsModel::model()->findByAttributes(['template_id' => $file_template->id, 'sim_id' => $simulation->id]);
+        assert($file);
         $this->sendJSON(
-           ExcelFactory::getDocument()
-           ->loadByFile($simulation->id, $fileId)
-           ->populateFrontendResult($simulation, $fileId)
-       );
+            ExcelFactory::getDocument()
+                ->loadByFile($simulation->id, $file)
+                ->populateFrontendResult($simulation, $file)
+        );
     }
 
     /**
      * New code!
      */
-    public function actionGetExcelID() 
+    public function actionGetExcelID()
     {
         $simulation = $this->getSimulationEntity();
-        
+
         $this->sendJSON(
             MyDocumentsService::checkDocumentTime(
-                $simulation, 
+                $simulation,
                 Yii::app()->request->getParam('fileId', NULL)
             )
         );
