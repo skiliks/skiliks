117b4e98466da750316d15ce48aae894d2a18451
Ivan Tugay <listepo@ya.ru>
2013-01-17 16:30:37 +0200

    Sentry fix

--- protected/config/stage_live.php
+++ protected/config/stage_live.php
@@ -1,5 +1,5 @@
 <?php
-
+define(YII_DEBUG, false);
 return CMap::mergeArray(require(dirname(__FILE__) . '/base.php'), array(
     'components'=>array(
         'db'=>array(
@@ -12,16 +12,20 @@ return CMap::mergeArray(require(dirname(__FILE__) . '/base.php'), array(
             'enableParamLogging'=>true,
             'enableProfiling'=>true
         ),
+        'RSentryException'=> array(
+            'dsn'=> 'https://bfd7395024f24728afdf79e9034bca04:2f8bec2e2c40493dbf7b07db88afc94f@app.getsentry.com/4572',
+            'class' => 'application.components..yii-sentry-log.RSentryComponent',
+        ),
         'log'=>array(
             'class'=>'CLogRouter',
             'routes'=>array(
                 array(
-                    'class'=>'CFileLogRoute',
-                    'levels'=>'error, warning, info, trace, log',
+                    'class'=>'application.components.yii-sentry-log.RSentryLog',
+                    'dsn'=> 'https://bfd7395024f24728afdf79e9034bca04:2f8bec2e2c40493dbf7b07db88afc94f@app.getsentry.com/4572',
+                    'levels'=>'error, warning',
                 ),
-
             ),
-        )
+        ),
     ),
     'params'=>array(
         'frontendUrl'=>'http://live.skiliks.com/',
