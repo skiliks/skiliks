9251eabf6c4376b883dfcbdf8ba642386e175d95
Ivan Tugay <listepo@ya.ru>
2013-01-19 22:55:13 +0200

    DayPlan

--- protected/components/GameTime.php
+++ protected/components/GameTime.php
@@ -26,4 +26,14 @@ class GameTime
         $el[2] = isset($el[2])?$el[2]:0;
         return ($el[0]*60*60) + ($el[1]*60) + $el[2];
     }
+
+    public static function getTime($time) {
+        $el = explode(':', $time);
+        if(count($el) < 2) {
+            throw new CException("Не верный формат времени, нужно HH:MM:SS");
+        } elseif(count($el) == 2) {
+            throw new CException("Фармат не нуждаеться в преобразовании! с HH:MM:SS в HH:MM");
+        }
+        return $el[0].':'.$el[1];
+    }
 }
