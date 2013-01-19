7484d769996d9c70e2572f6b8aa041d9736f39e7
Andrii Kostenko <andrey@kostenko.name>
2013-01-19 10:16:45 +0200

    excel preload

--- protected/assets/js/game/views/documents/SKXLSDisplayView.js
+++ protected/assets/js/game/views/documents/SKXLSDisplayView.js
@@ -6,13 +6,41 @@
 
     window.SKXLSDisplayView = SKWindowView.extend({
         title:'Мои документы',
-        renderContent:function (el) {
+        displayZohoIframe:function (doc, el) {
+            var me = this;
+            $('#excel-preload-' + doc.id).show().css({
+                'zIndex':el.parents('.sim-window').css('zIndex') + 1,
+                'width':el.width() - 6,
+                'height':el.height(),
+                'left':me.$el[0].offsetLeft,
+                'top':me.$el[0].offsetTop + el[0].offsetTop,
+                'position':'absolute'
+            });
+        }, renderContent:function (el) {
             var me = this;
             var fn = this.options.model_instance.get('filename');
             var doc = SKApp.user.simulation.documents.where({name:fn})[0];
             el.html(_.template($('#document_xls_template').html(), {
-                'filename': doc.get('excel_url')
             }));
+            setTimeout(function() {
+                me.displayZohoIframe(doc, el);
+            }, 0);
+
+        },
+        doStartDrag: function (el) {
+            this.hideZohoIframe(doc, el);
+        },
+        doEndDrag: function (el) {
+            this.hideZohoIframe(doc, el);
+        },
+        hideZohoIframe:function () {
+            var fn = this.options.model_instance.get('filename');
+            var doc = SKApp.user.simulation.documents.where({name:fn})[0];
+            $('#excel-preload-' + doc.id).hide();
+        }, remove:function () {
+            var me = this;
+            this.hideZohoIframe();
+            SKWindowView.prototype.remove.call(this);
         }
     });
 })();
