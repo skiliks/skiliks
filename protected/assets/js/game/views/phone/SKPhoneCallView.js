4b2d706f39cd5f4a0f88d5d6287e3768fab8a658
slavkaa <nikoolin@ukr.net>
2013-01-22 18:39:32 +0200

    Phone: fix deny call. Also fix menu button.

--- protected/assets/js/game/views/phone/SKPhoneCallView.js
+++ protected/assets/js/game/views/phone/SKPhoneCallView.js
@@ -19,21 +19,21 @@ $(function () {
             window_el.html(_.template($('#Phone_Call').html(), {call: this.options.event.get('data')}));
         },
         getMenu: function(event){
-            var id = $(event.toElement).attr('window_id');
+            var id = $(event.currentTarget).attr('window_id');
             this.renderTPL('#'+id+' .phone-screen', '#Phone_Menu', {windowID:id});
         },
         getCountViews: function(){
             return $('.'+this.windowClass).length;
         },
         reply: function(event) {
-            var dialogId = $(event.toElement).attr('data-dialog-id');
+            var dialogId = $(event.currentTarget).attr('data-dialog-id');
             this.options.model_instance.close();
             SKApp.server.api('dialog/get', {'dialogId':dialogId}, function (data) {
                 SKApp.user.simulation.parseNewEvents(data.events);
             });
         },
         noReply: function(event) {
-            var dialogId = $(event.toElement).attr('data-dialog-id');
+            var dialogId = $(event.currentTarget).attr('data-dialog-id');
             this.options.model_instance.close();
             SKApp.server.api('dialog/get', {'dialogId':dialogId, 'time':SKApp.user.simulation.getGameMinutes()}, function (data) {
                 SKApp.user.simulation.parseNewEvents(data.events);
