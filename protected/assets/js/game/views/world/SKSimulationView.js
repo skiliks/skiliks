e436e9dcce2cac2f4ff5445f345f31677d915a44
Andrii Kostenko <andrey@kostenko.name>
2013-01-24 23:30:32 +0200

    merge

--- protected/assets/js/game/views/world/SKSimulationView.js
+++ protected/assets/js/game/views/world/SKSimulationView.js
@@ -7,123 +7,82 @@
          * @lends SKSimulationView
          */
         {
-        'el':'body',
-        'events':{
-            'click .btn-simulation-stop':      'doSimulationStop',
-            'click .btn-toggle-dialods-sound': 'doToggleDialodSound'
-        },
-        setupWindowEvents:function (window) {
-            if (window.get('name') === 'plan') {
-                var plan_view = new SKDayPlanView({model_instance:window});
-                plan_view.render();
-            }
-            if (window.get('name') === 'phone' && window.get('subname') === 'phoneMain') {
-                var phone_view = new SKPhoneView({model_instance:window});
-                phone_view.render();
-            }
-            if (window.get('name') === 'mailEmulator' && window.get('subname') === 'mailMain') {
-                var mail_client_view = new SKMailClientView({model_instance:window});
-                mail_client_view.render();
-                //SKApp.user.simulation.mailClient.toggleWindow();
-            }
-            if (window.get('name') === 'phone' && window.get('subname') === 'phoneCall') {
-                var call_view = new SKPhoneCallView({model_instance:window, event:window.get('sim_event')});
-                call_view.render();
-            }
-            if (window.get('name') === 'phone' && window.get('subname') === 'phoneTalk') {
-                var view = new SKPhoneDialogView({model_instance:window, 'event':window.get('params').event});
-                view.render();
-            }
-            if (window.get('name') === 'documents' && window.get('subname') === 'documents') {
-                var doc_view = new SKDocumentsListView({model_instance:window});
-                doc_view.render();
-            }
-            if (window.get('name') === 'documents' && window.get('subname') === 'documentsFiles') {
-                var file = window.get('filename');
-                var document_view;
-                if (file.match(/\.xlsx$/) || file.match(/\.xls$/)) {
-                    document_view = new SKXLSDisplayView({model_instance:window});
-                } else {
-                    document_view = new SKPDFDisplayView({model_instance:window});
+            'el':'body',
+            'events':{
+                'click .btn-simulation-stop':'doSimulationStop',
+                'click .btn-toggle-dialods-sound':'doToggleDialodSound'
+            },
+            setupWindowEvents:function (window) {
+                if (window.get('name') === 'plan') {
+                    var plan_view = new SKDayPlanView({model_instance:window});
+                    plan_view.render();
                 }
-                document_view.render();
-            }
-        }, 'initialize':function () {
-            var me = this;
-            var simulation = this.simulation = SKApp.user.simulation;
-            this.addSimulationEvents();
-            this.listenTo(simulation, 'tick', function () {
-                me.updateTime();
-            });
-            this.listenTo(simulation.window_set, 'add', function (window) {
-                me.setupWindowEvents(window);
-            });
-            this.listenTo(simulation.documents, 'reset', function () {
-                var timeout = 0;
-                simulation.documents.each(function (doc) {
+                if (window.get('name') === 'phone' && window.get('subname') === 'phoneMain') {
+                    var phone_view = new SKPhoneView({model_instance:window});
+                    phone_view.render();
+                }
+                if (window.get('name') === 'mailEmulator' && window.get('subname') === 'mailMain') {
+                    var mail_client_view = new SKMailClientView({model_instance:window});
+                    mail_client_view.render();
+                    //SKApp.user.simulation.mailClient.toggleWindow();
+                }
+                if (window.get('name') === 'phone' && window.get('subname') === 'phoneCall') {
+                    var call_view = new SKPhoneCallView({model_instance:window, event:window.get('sim_event')});
+                    call_view.render();
+                }
+                if (window.get('name') === 'phone' && window.get('subname') === 'phoneTalk') {
+                    var view = new SKPhoneDialogView({model_instance:window, 'event':window.get('params').event});
+                    view.render();
+                }
+                if (window.get('name') === 'documents' && window.get('subname') === 'documents') {
+                    var doc_view = new SKDocumentsListView({model_instance:window});
+                    doc_view.render();
+                }
+                if (window.get('name') === 'documents' && window.get('subname') === 'documentsFiles') {
+                    var file = window.get('filename');
+                    var document_view;
+                    if (file.match(/\.xlsx$/) || file.match(/\.xls$/)) {
+                        document_view = new SKXLSDisplayView({model_instance:window});
+                    } else {
+                        document_view = new SKPDFDisplayView({model_instance:window});
+                    }
+                    document_view.render();
+                }
+            },
+            'initialize':function () {
+                var me = this;
+                var simulation = this.simulation = SKApp.user.simulation;
+                this.addSimulationEvents();
+                this.listenTo(simulation, 'tick', function () {
+                    me.updateTime();
+                });
+                this.listenTo(simulation.window_set, 'add', function (window) {
+                    me.setupWindowEvents(window);
+                });
+                this.listenTo(simulation.documents, 'reset', function () {
+                    var timeout = 0;
+                    simulation.documents.each(function (doc) {
+                        me.listenTo(doc, 'change:excel_url', function () {
+                            me.preloadZoho(doc);
+                        });
+                    });
+                });
+                this.listenTo(simulation.documents, 'add', function (doc) {
                     me.listenTo(doc, 'change:excel_url', function () {
                         me.preloadZoho(doc);
                     });
                 });
-            });
-            this.listenTo(simulation.documents, 'add', function (doc) {
-                me.listenTo(doc, 'change:excel_url', function () {
-                    me.preloadZoho(doc);
-                });
-            });
-        },
-        /**
-         * Preloads excel with Zoho on simulation start
-         * @param doc
-         */
-        preloadZoho:function (doc) {
-            // Detecting time when we can start zoho (TODO: extract view)
-            var date = new Date();
-            this.zoho_end_date = this.zoho_end_date || date;
-            if (this.zoho_end_date < date) {
-                this.zoho_end_date = date;
-            }
-            setTimeout(function () {
-                this.$('.canvas').append($('<iframe />', {
-                    src:doc.get('excel_url'),
-                    id:'excel-preload-' + doc.id
-                }).css({
-                        'left':'-1000px',
-                        'position':'absolute'
-                    }));
-            }, (this.zoho_end_date - date));
-            this.zoho_end_date.setSeconds(this.zoho_end_date.getSeconds() + 10);
-        },
-        'render':function () {
-            var login_html = _.template($('#simulation_template').html(), {});
-            this.$el.html(login_html);
-            this.icon_view = new SKIconPanelView({'el':this.$('.main-screen-icons')});
-            if (this.simulation.isDebug()) {
-                this.debug_view = new SKDebugView({'el':this.$('.debug-panel')});
-                this.doToggleDialodSound();
-            }
-            var canvas = this.$('.canvas');
-            this.updateTime();
-            this.delegateEvents();
-        },
-        'updateTime':function () {
-            var parts = this.simulation.getGameTime().split(':');
-            this.$('.time .hour').text(parts[0]);
-            this.$('.time .minute').text(parts[1]);
-        },
-        'addSimulationEvents':function () {
-            SKApp.user.simulation.events.on('add', function (event) {
-                if (event.getTypeSlug() === 'immediate-visit') {
-                    if (this.visit_view === undefined) {
-                        this.visit_view = new SKVisitView({'event':event});
-                    } else {
-                        this.visit_view.options.event = event;
-                        this.visit_view.render();
-                    }
-                    event.complete();
-                } else if (event.getTypeSlug() === 'immediate-phone') {
-                    var win = SKApp.user.simulation.window_set.open('phone', 'phoneTalk', {sim_event:event});
-                    event.complete();
+            },
+            /**
+             * Preloads excel with Zoho on simulation start
+             * @param doc
+             */
+            preloadZoho:function (doc) {
+                // Detecting time when we can start zoho (TODO: extract view)
+                var date = new Date();
+                this.zoho_end_date = this.zoho_end_date || date;
+                if (this.zoho_end_date < date) {
+                    this.zoho_end_date = date;
                 }
                 setTimeout(function () {
                     this.$('.canvas').append($('<iframe />', {
@@ -136,21 +95,54 @@
                 }, (this.zoho_end_date - date));
                 this.zoho_end_date.setSeconds(this.zoho_end_date.getSeconds() + 10);
             },
-            
+            'render':function () {
+                var login_html = _.template($('#simulation_template').html(), {});
+                this.$el.html(login_html);
+                this.icon_view = new SKIconPanelView({'el':this.$('.main-screen-icons')});
+                if (this.simulation.isDebug()) {
+                    this.debug_view = new SKDebugView({'el':this.$('.debug-panel')});
+                    this.doToggleDialodSound();
+                }
+                var canvas = this.$('.canvas');
+                this.updateTime();
+                this.delegateEvents();
+            },
+            'updateTime':function () {
+                var parts = this.simulation.getGameTime().split(':');
+                this.$('.time .hour').text(parts[0]);
+                this.$('.time .minute').text(parts[1]);
+            },
+            'addSimulationEvents':function () {
+                SKApp.user.simulation.events.on('add', function (event) {
+                    if (event.getTypeSlug() === 'immediate-visit') {
+                        if (this.visit_view === undefined) {
+                            this.visit_view = new SKVisitView({'event':event});
+                        } else {
+                            this.visit_view.options.event = event;
+                            this.visit_view.render();
+                        }
+                        event.complete();
+                    } else if (event.getTypeSlug() === 'immediate-phone') {
+                        var win = SKApp.user.simulation.window_set.open('phone', 'phoneTalk', {sim_event:event});
+                        event.complete();
+                    }
+                });
+            },
 
-        'doSimulationStop':function () {
-            SKApp.user.stopSimulation();
-        },
-        doToggleDialodSound: function() {
-             if (SKApp.user.simulation.config.isMuteVideo === false) {
-                SKApp.user.simulation.config.isMuteVideo = true;
-                $('.btn-toggle-dialods-sound i').removeClass('icon-volume-up');
-                $('.btn-toggle-dialods-sound i').addClass('icon-volume-off');
-            } else {
-                SKApp.user.simulation.config.isMuteVideo = false;
-                $('.btn-toggle-dialods-sound i').addClass('icon-volume-up');
-                $('.btn-toggle-dialods-sound i').removeClass('icon-volume-off');
+
+            'doSimulationStop':function () {
+                SKApp.user.stopSimulation();
+            },
+            doToggleDialodSound:function () {
+                if (SKApp.user.simulation.config.isMuteVideo === false) {
+                    SKApp.user.simulation.config.isMuteVideo = true;
+                    this.$('.btn-toggle-dialods-sound i').removeClass('icon-volume-up');
+                    this.$('.btn-toggle-dialods-sound i').addClass('icon-volume-off');
+                } else {
+                    SKApp.user.simulation.config.isMuteVideo = false;
+                    this.$('.btn-toggle-dialods-sound i').addClass('icon-volume-up');
+                    this.$('.btn-toggle-dialods-sound i').removeClass('icon-volume-off');
+                }
             }
-        }
-    });
+        });
 })();
