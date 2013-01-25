b3d27736b97dc22edcd0d90172cccf5aed023dd3
slavkaa <nikoolin@ukr.net>
2013-01-25 15:58:05 +0200

    Clean-up.

--- protected/components/Import/ImportGameDataService.php
+++ protected/components/Import/ImportGameDataService.php
@@ -1162,50 +1162,8 @@ class ImportGameDataService
         return array(
             'imported_documents' => $importedRows, 
             'errors' => false, 
-        );
-        
-        // ****
-        
-        while (($row = fgetcsv($handle, 5000, ";")) !== FALSE) {
-            $index++;
-            if ($index <= 2) continue;
-            
-            if ($index > 29) {
-                echo('all done'); die();
-            }
-            
-            // Определим код документа
-            $code       = $row[0]; // A
-            // Определим тип документа
-            $type       = $row[1]; // B
-            // Имя файла в системе
-            $fileName   = iconv("Windows-1251", "UTF-8", $row[2]); // C
-            // Исходный файл
-            $srcFile    = iconv("Windows-1251", "UTF-8", $row[3]); // D
-            // Расширение файла
-            $format     = $row[4]; // E
-            
-            //if ($type == '-') continue;
-            
-            $document = MyDocumentsTemplateModel::model()->byCode($code)->find();
-            if (!$document) {
-                $document = new MyDocumentsTemplateModel();
-                $document->code         = $code;
-            }
-            
-            $document->fileName     = $fileName.'.'.$format;
-            $document->srcFile      = $srcFile;
-            $document->format       = $format;
-            $document->type         = $type;
-            $document->save();
-        }
-        fclose($handle);
-        
-        return array(
-            'status' => true,
-            'text'   => sprintf('%s document records have been imported.', $index),
-        );   
-    }
+        );        
+     }
 
     /**
      * Get unique import ID
