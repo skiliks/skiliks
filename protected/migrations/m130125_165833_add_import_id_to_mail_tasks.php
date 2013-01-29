60f4a1b0ce6decdcd0738ca8ca9732d087bf6eb5
Kirill Kostko <kirill@skiliks.com>
2013-01-29 15:19:55 +0100

    SK-1014

--- protected/migrations/m130125_165833_add_import_id_to_mail_tasks.php
+++ protected/migrations/m130125_165833_add_import_id_to_mail_tasks.php
@@ -6,8 +6,6 @@ class m130125_165833_add_import_id_to_mail_tasks extends CDbMigration
 	{
         $this->addColumn('mail_tasks', 'import_id', 'VARCHAR(14) DEFAULT NULL COMMENT \'setvice value,used to remove old data after reimport.\';');
         
-        $importService = new ImportGameDataService();
-        $importService->importAll();
 	}
 
 	public function down()
