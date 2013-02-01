1649f528a96ce9244f0d163c63ea0c3bc1deb62e
slavkaa <nikoolin@ukr.net>
2013-02-01 19:12:15 +0200

    Add loffing to reselv 1114.

--- protected/components/Excel/CheckConsolidatedBudget.php
+++ protected/components/Excel/CheckConsolidatedBudget.php
@@ -332,7 +332,7 @@ class CheckConsolidatedBudget
         }
         
         // 'wh' - worksheet
-        Yii::log(serialize(array_keys($worksheetNames['logistics'])), 'warning');
+        throw new Exception(serialize(array_keys($worksheetNames['logistics'])));
         $whLogistic     = $objPHPExcel->getSheetByName($worksheetNames['logistics']);
         $whProduction   = $objPHPExcel->getSheetByName($worksheetNames['production']);
         $whConsolidated = $objPHPExcel->getSheetByName($worksheetNames['consolidated budget']);
