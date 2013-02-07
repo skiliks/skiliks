109adaba7a33f72b80027a4dae0ae963cd7398dd
Andrii Kostenko <andrey@kostenko.name>
2013-02-07 11:07:42 +0200

    migration

---
+++ protected/migrations/m130206_175751_yii_activity_id_length.php
@@ -0,0 +1,28 @@
+<?php
+
+class m130206_175751_yii_activity_id_length extends CDbMigration
+{
+	public function up()
+	{
+        #$this->alterColumn('activity', 'id', 'VARCHAR(60) CHARACTER SET utf8 NOT NULL');
+        #$this->alterColumn('activity_action', 'activity_id', 'VARCHAR(60) CHARACTER SET utf8 NOT NULL');
+        $this->addForeignKey('activity_action_action_id', 'activity_action', 'activity_id', 'activity', 'id', 'CASCADE', 'CASCADE');
+	}
+
+	public function down()
+	{
+		echo "m130206_175751_yii_activity_id_length does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
