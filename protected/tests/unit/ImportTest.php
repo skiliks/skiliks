3e7d75c53b4a1cac942e4c9a5cbf01f794140b4c
slavkaa <nikoolin@ukr.net>
2013-02-08 17:01:18 +0200

    Try to make import test supersonic.

--- protected/tests/unit/ImportTest.php
+++ protected/tests/unit/ImportTest.php
@@ -18,62 +18,32 @@ class ImportTest extends CDbTestCase
             $import->importLearningGoals();
             $import->importDialogReplicas();
             $import->importEmailSubjects();
-            $this->assertEquals(CommunicationTheme::model()->countByAttributes(['character_id' => null]), 41);
-            $this->assertEquals(CommunicationTheme::model()->countByAttributes(['phone' => 1]), 67);
-            $this->assertEquals(CommunicationTheme::model()->countByAttributes(['mail' => 1]), 4083);
             $import->importEmails();
             $import->importMailTasks();
             $import->importEventSamples();
             $import->importTasks();
             $import->importMyDocuments();
             $import->importActivity();
-            $this->assertEquals(CommunicationTheme::model()->countByAttributes(['text' => '!проблема с сервером!']),43);
-            $transaction->rollback();
-        } catch (Exception $e) {
-            $transaction->rollback();
-            throw $e;
-        }
-    }
-
-    public function test_events_samples() {
-        $transaction = Yii::app()->db->beginTransaction();
-        try {
-            $import = new ImportGameDataService();
-            $import->importEventSamples();
+            
             $this->assertNotNull(EventsSamples::model()->findByAttributes([
                 'code' => 'P5'
             ]));
-            $transaction->rollback();
-        } catch (Exception $e) {
-            $transaction->rollback();
-            throw $e;
-        }
-    }
-
-    public function test_subject_import() {
-        $transaction = Yii::app()->db->beginTransaction();
-        try {
-            $import = new ImportGameDataService();
-            $import->importEmailSubjects();
-            $transaction->rollback();
-        } catch (Exception $e) {
-            $transaction->rollback();
-            throw $e;
-        }
-    }
-
-    public function test_dialog_import() {
-        $transaction = Yii::app()->db->beginTransaction();
-        try {
-            $import = new ImportGameDataService();
-            $import->importDialogReplicas();
-            $this->assertEquals(Dialogs::model()->findByAttributes([
-                'code' => 'E1',
-                'is_final_replica' => 1,
-                'excel_id' => 12
-            ])->next_event_code, 'E1.2');
+            
+            $this->assertEquals(CommunicationTheme::model()->countByAttributes(['character_id' => null]), 41);
+            $this->assertEquals(CommunicationTheme::model()->countByAttributes(['phone' => 1]), 67);
+            $this->assertEquals(CommunicationTheme::model()->countByAttributes(['mail' => 1]), 4083);
+            $this->assertEquals(CommunicationTheme::model()->countByAttributes(['text' => '!проблема с сервером!']),43);
+            
+            $this->assertEquals(
+                Dialogs::model()->findByAttributes([
+                    'code'             => 'E1',
+                    'is_final_replica' => 1,
+                    'excel_id'         => 12
+                ])->next_event_code, 
+                'E1.2');
             $this->assertEquals(Dialogs::model()->count(), 821);
             $this->assertNotNull(Dialogs::model()->findByAttributes(['code' => 'S12.3']));
+            
             $transaction->rollback();
         } catch (Exception $e) {
             $transaction->rollback();
