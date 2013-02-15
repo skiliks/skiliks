52290db485f8a728fa96fe1c9c7aace3ca8272bd
slavkaa <nikoolin@ukr.net>
2013-02-15 15:45:04 +0200

    Another fix for import :)

---
+++ protected/tests/unit/Flags_2_Test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: root
+ * Date: 2/15/13
+ * Time: 11:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class Flags_2_Test
+{
+
+}
