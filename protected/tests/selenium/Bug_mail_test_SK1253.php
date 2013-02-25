f3d8bb96e9758b16dfd1f3c6a643759f197480b0
Vadim Logunov <vadim.logunov@skiliks.com>
2013-02-25 06:12:12 +0200

    Тест по задаче SK-1253 (баг с отправкой письма новому адресату, в случае если первый адресат удален)

---
+++ protected/tests/selenium/Bug_mail_test_SK1253.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: vad
+ * Date: 2/24/13
+ * Time: 5:26 PM
+ */
+class MailTest extends CWebTestCase
+{
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl(Yii::app()->params['frontendUrl']);
+        parent::setUp();
+    }
+
+    public function test_Mail_new_first_to_deleted() {
+
+        $this->deleteAllVisibleCookies();
+        $this->open('/site/');
+        $this->setSpeed("1000");
+        $this->waitForVisible('id=login');
+        $this->type("id=login", "asd");
+        $this->type("id=pass", "123");
+        $this->click("css=input.btn.btn-primary");
+        for ($second = 0; ; $second++) {
+            if ($second >= 60) $this->fail("timeout");
+            try {
+                if ($this->isVisible("xpath=//input[@value='Начать симуляцию developer']")) break;
+            } catch (Exception $e) {}
+            sleep(1);
+        }
+
+        $this->click("xpath=//input[@value='Начать симуляцию developer']");
+        for ($second = 0; ; $second++) {
+            if ($second >= 60) $this->fail("timeout");
+            try {
+                if ($this->isVisible("id=addTriggerSelect")) break;
+            } catch (Exception $e) {}
+            sleep(1);
+        }
+
+        //маппинги Трудякина и Крутько в выпадающем списке адресатов
+        $trudyakin="//ul[contains(@class,'ui-autocomplete')]/li[13]/a";
+        $krutko="//ul[contains(@class,'ui-autocomplete')]/li[15]/a";
+
+        $this->click("id=icons_email");
+        $this->click("link=новое письмо");
+        $this->click("id=MailClient_RecipientsList");
+        $this->waitForElementPresent($trudyakin);
+        $this->mouseOver($trudyakin);
+        $this->click($trudyakin);
+	    $this->click("//input[@type='text']");
+        $this->mouseOver($krutko);
+        $this->click($krutko);
+        $this->select("css=select.origin", "Срочно жду бюджет логистики");
+        $this->click("css=li.tagItem");
+        $this->click("//div[@class='mail-popup']//td[1]/div['Продолжить']");
+        $this->select("css=select.origin", "Срочно жду бюджет логистики");
+
+        if ($this->isTextPresent('Срочно жду бюджет логистики') == true)
+            echo "Беда, для Крутько все еще отображаются темы как для Трудякина";
+
+        $this->click("css=input.btn.btn-simulation-stop");
+        sleep(15);
+        $this->click("css=input.btn.logout");
+    }
+}
+
