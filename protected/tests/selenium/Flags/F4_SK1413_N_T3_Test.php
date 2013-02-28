67882c2ce5fcb03fa2ba3c25ba7c8370c8cd122f
tania <tetyana.grybok@skiliks.com>
2013-02-28 19:43:16 +0200

    add all test

---
+++ protected/tests/selenium/Flags/F4_SK1413_N_T3_Test.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: tania
+ * Date: 2/28/13
+ * Time: 7:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class F4_SK1413_N_T3_Test extends SeleniumTestHelper
+{
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl(Yii::app()->params['frontendUrl']);
+        parent::setUp();
+    }
+
+    public function testSK1413_NT3() {
+        // next line for not running the test
+        //$this->markTestIncomplete();
+        $this->start_simulation();
+
+        $this->type("id=addTriggerSelect", "E1.3");
+        $this->waitForVisible("xpath=//form/fieldset/div[2]/div/input[2]");
+        $this->click("xpath=//form/fieldset/div[2]/div/input[2]");
+
+        $this->waitForVisible("xpath=(//*[contains(text(),'Ты не мог бы мне помочь?')])");
+        $this->click("xpath=(//*[contains(text(),'Ты не мог бы мне помочь?')])");
+        $this->waitForVisible("xpath=(//*[contains(text(),'Тебе же все равно рано или')])");
+        $this->click("xpath=(//*[contains(text(),'Тебе же все равно рано или')])");
+        $this->waitForVisible("xpath=(//*[contains(text(),'Я знаю, что ты справишься')])");
+        $this->click("xpath=(//*[contains(text(),'Я знаю, что ты справишься')])");
+        $this->waitForVisible("xpath=(//*[contains(text(),'Ладно. Я понял. Сделаю сам.')])");
+        $this->click("xpath=(//*[contains(text(),'Ладно. Я понял. Сделаю сам.')])");
+
+        $this->waitForVisible(Yii::app()->params['test_mappings']['send_message_quickly']['MS22']);
+        $this->click(Yii::app()->params['test_mappings']['send_message_quickly']['MS22']);
+
+        sleep(5);
+        $this->assertText("xpath=//div[1]/div[2]/div/div/div[4]/form[1]/fieldset/table[2]/tbody/tr/td[8]","0");
+
+        // to make changes with time
+        $this->type(Yii::app()->params['test_mappings']['set_time']['set_hours'], "09");
+        $this->type(Yii::app()->params['test_mappings']['set_time']['set_minutes'], "20");
+        $this->click(Yii::app()->params['test_mappings']['set_time']['submit_time']);
+
+        $this->type("id=addTriggerSelect", "E1.3.3");
+        $this->waitForVisible("xpath=//form/fieldset/div[2]/div/input[2]");
+        $this->click("xpath=//form/fieldset/div[2]/div/input[2]");
+
+        $this->verifyVisible(Yii::app()->params['test_mappings']['icons']['phone']);
+
+        $this->click("css=input.btn.btn-simulation-stop");
+    }
+}
---
+++ protected/tests/selenium/Flags/F4_SK1413_P_T4_Test.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: tania
+ * Date: 2/28/13
+ * Time: 6:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class F4_SK1413_P_T4_Test extends SeleniumTestHelper
+{
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl(Yii::app()->params['frontendUrl']);
+        parent::setUp();
+    }
+
+    public function testSK1413_PT4() {
+        // next line for not running the test
+        //$this->markTestIncomplete();
+        $this->start_simulation();
+
+        $this->type("id=addTriggerSelect", "E1.3");
+        $this->waitForVisible("xpath=//form/fieldset/div[2]/div/input[2]");
+        $this->click("xpath=//form/fieldset/div[2]/div/input[2]");
+
+        $this->waitForVisible("xpath=(//*[contains(text(),'Ты не мог бы мне помочь?')])");
+        $this->click("xpath=(//*[contains(text(),'Ты не мог бы мне помочь?')])");
+        $this->waitForVisible("xpath=(//*[contains(text(),'Тебе же все равно рано или')])");
+        $this->click("xpath=(//*[contains(text(),'Тебе же все равно рано или')])");
+        $this->waitForVisible("xpath=(//*[contains(text(),'Я знаю, что ты справишься')])");
+        $this->click("xpath=(//*[contains(text(),'Я знаю, что ты справишься')])");
+        $this->waitForVisible("xpath=(//*[contains(text(),'Однако тебе все-таки')])");
+        $this->click("xpath=(//*[contains(text(),'Однако тебе все-таки')])");
+
+        $this->waitForVisible(Yii::app()->params['test_mappings']['send_message_quickly']['MS22']);
+        $this->click(Yii::app()->params['test_mappings']['send_message_quickly']['MS22']);
+
+        sleep(5);
+        $this->assertText("xpath=//div[1]/div[2]/div/div/div[4]/form[1]/fieldset/table[2]/tbody/tr/td[8]","1");
+
+        // to make changes with time
+        $this->type(Yii::app()->params['test_mappings']['set_time']['set_hours'], "12");
+        $this->type(Yii::app()->params['test_mappings']['set_time']['set_minutes'], "15");
+        $this->click(Yii::app()->params['test_mappings']['set_time']['submit_time']);
+
+        $this->type("id=addTriggerSelect", "ET1.3.2");
+        $this->waitForVisible("xpath=//form/fieldset/div[2]/div/input[2]");
+        $this->click("xpath=//form/fieldset/div[2]/div/input[2]");
+
+        $this->optimal_click("css=li.icon-active.phone a");
+        $this->waitForVisible(Yii::app()->params['test_mappings']['phone']['reply']);
+        $this->click(Yii::app()->params['test_mappings']['phone']['reply']);
+
+        $this->waitForVisible("xpath=(//*[contains(text(),'Господи, и что же мне теперь делать')])");
+        $this->assertTextPresent("Господи, и что же мне теперь делать");
+
+        $this->click("css=input.btn.btn-simulation-stop");
+    }
+}
+
