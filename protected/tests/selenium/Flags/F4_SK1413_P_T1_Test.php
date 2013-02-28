5287f59a12bc2ae11e04e2c3bdd22e9d47212c9e
tania <tetyana.grybok@skiliks.com>
2013-02-28 16:05:50 +0200

    1) changed_maps 2) add methods to reply and no reply for phone call 3) new test for f4

--- protected/tests/selenium/Flags/F4_SK1413_P_T1_Test.php
+++ protected/tests/selenium/Flags/F4_SK1413_P_T1_Test.php
@@ -21,8 +21,6 @@ class F4_SK1413_P_T1_Test extends SeleniumTestHelper
         //$this->markTestIncomplete();
         $this->start_simulation();
 
-        $trudyakin = Yii::app()->params['test_mappings']['mail_contacts']['trudyakin'];
-
         $this->type("id=addTriggerSelect", "E1.3");
         $this->waitForVisible("xpath=//form/fieldset/div[2]/div/input[2]");
         $this->click("xpath=//form/fieldset/div[2]/div/input[2]");
@@ -35,24 +33,40 @@ class F4_SK1413_P_T1_Test extends SeleniumTestHelper
         $this->waitForVisible(Yii::app()->params['test_mappings']['send_message_quickly']['MS22']);
         $this->click(Yii::app()->params['test_mappings']['send_message_quickly']['MS22']);
 
-        sleep(2);
-        //$this->assertText("xpath=//div[1]/div[2]/div/div/div[4]/form[1]/fieldset/table[2]/tbody/tr/td[8]","1");
-        //$this->assertEquals("no","1",$this->getFixtureData("xpath=//div[1]/div[2]/div/div/div[4]/form[1]/fieldset/table[2]/tbody/tr/td[8]"));
+        sleep(5);
+        $this->assertText("xpath=//div[1]/div[2]/div/div/div[4]/form[1]/fieldset/table[2]/tbody/tr/td[8]","1");
+
+       // to make changes with time
+        $this->type(Yii::app()->params['test_mappings']['set_time']['set_hours'], "11");
+        $this->type(Yii::app()->params['test_mappings']['set_time']['set_minutes'], "50");
+        $this->click(Yii::app()->params['test_mappings']['set_time']['submit_time']);
 
         $this->type("id=addTriggerSelect", "ET1.3.1");
         $this->waitForVisible("xpath=//form/fieldset/div[2]/div/input[2]");
         $this->click("xpath=//form/fieldset/div[2]/div/input[2]");
 
-       // to make changes with time
-        $this->type(Yii::app()->params['test_mappings']['set_time']['set_hours'], "11");
-        $this->type(Yii::app()->params['test_mappings']['set_time']['set_minutes'], "49");
-        $this->click(Yii::app()->params['test_mappings']['set_time']['submit_time']);
+        //$this->reply_call();
+        sleep(5);
+        $this->waitForVisible(Yii::app()->params['test_mappings']['icons']['phone']);
+        sleep(2);
+        $this->click(Yii::app()->params['test_mappings']['icons']['phone']);
+        sleep(2);
+        if ($this->verifyTextPresent("Трутнев"))
+        {
+            $this->waitForVisible(Yii::app()->params['test_mappings']['phone']['reply']);
+            sleep(2);
+            $this->click(Yii::app()->params['test_mappings']['phone']['reply']);
+            sleep(2);
+        }
+        else
+        {
+            $this->waitForVisible(Yii::app()->params['test_mappings']['phone']['no_reply']);
+            sleep(2);
+            $this->click(Yii::app()->params['test_mappings']['phone']['no_reply']);
+            sleep(2);
+        }
 
-        $theme = "xpath=//div[@id='phoneCallThemesDiv']/ul/li[2]";
 
-        $this->call_phone(Yii::app()->params['test_mappings']['phone_contacts']['trutnev'], $theme);
-
-        // some replics from dialog ET1.3.1
         $this->waitForVisible("xpath=(//*[contains(text(),'Господи, да ведь там в вашем бюджете')])");
         $this->assertTextPresent("Господи, да ведь там в вашем бюджете");
 
