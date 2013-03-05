9e185cf8f23fe1c734cad5e9a1d22396db53c53e
tania <tetyana.grybok@skiliks.com>
2013-03-05 17:21:02 +0200

    new map

--- protected/config/test_mappings.php
+++ protected/config/test_mappings.php
@@ -12,6 +12,11 @@ return [
         'documents' => 'id=icons_documents'
     ],
 
+    'time' => [
+        'hour' => "//*[@id='canvas']/ul/li[1]/span[1]",
+        'minute' => "//*[@id='canvas']/ul/li[1]/span[3]"
+    ],
+
     'mail' => [
         'new_letter' => "link=новое письмо",
         'to_whom' => "id=MailClient_RecipientsList",
@@ -148,16 +153,16 @@ return [
         'set_hours' => "//*[@id='setTimeHours']",
         'set_minutes' => "//div[1]/div[2]/div/div/div[3]/form/fieldset/div[1]/div/input[2]",
         'submit_time' => "//div[1]/div[2]/div/div/div[3]/form/fieldset/div[1]/div/input[3]",
-        '0h' => '/div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[1]',
-        '10h' => '/div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[2]',
-        '11h' => '/div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[3]',
-        '12h' => '/div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[4]',
-        '13h' => '/div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[5]',
-        '14h' => '/div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[6]',
-        '15h' => '/div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[7]',
-        '16h' => '/div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[8]',
-        '17h' => '/div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[9]',
-        '17h50m' => '/div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[10]'
+        '0h' => '//div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[1]',
+        '10h' => '//div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[2]',
+        '11h' => '//div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[3]',
+        '12h' => '//div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[4]',
+        '13h' => '//div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[5]',
+        '14h' => '//div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[6]',
+        '15h' => '//div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[7]',
+        '16h' => '//div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[8]',
+        '17h' => '//div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[9]',
+        '17h50m' => '//div[1]/div[2]/div/div/div[3]/form/fieldset/div[2]/button[10]'
     ],
 
 
