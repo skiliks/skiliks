7abd63ce4075ad9e273fc3b794deba14c140a0e5
slavkaa <nikoolin@ukr.net>
2013-03-11 17:16:14 +0200

    1557. Fix registration.

--- protected/controllers/static/SiteController.php
+++ protected/controllers/static/SiteController.php
@@ -137,10 +137,6 @@ class SiteController extends AjaxController
         $cs->registerScriptFile($assetsUrl . "/js/game/models/SKMailSubject.js");
         $cs->registerScriptFile($assetsUrl . "/js/game/models/SKMailFolder.js");
 
-        // $cs->registerScriptFile($assetsUrl . "/js/game/views/world/SKIconPanelView.js");
-        // $cs->registerScriptFile($assetsUrl . "/js/game/views/world/SKSettingsView.js");
-        // $cs->registerScriptFile($assetsUrl . "/js/game/views/develop_mode/SKFlagStateView.js");
-
         $cs->registerScriptFile($assetsUrl . "/js/raven-0.7.1.js");
 
         //плагины
