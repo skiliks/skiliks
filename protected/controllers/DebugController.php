e7e08ba192172d5f851b1c2435982bee5fe0237f
slavkaa <nikoolin@ukr.net>
2013-03-14 16:10:56 +0200

    Firtstpart of 1596. IS corporate email validation.

--- protected/controllers/DebugController.php
+++ protected/controllers/DebugController.php
@@ -5,11 +5,11 @@
  *
  *
  */
-class DebugController extends AjaxController{
-
+class DebugController extends AjaxController
+{
     public function actionIndex()
     {
-        echo Replica::model()->findByAttributes(['code' => 'ET1.1', 'replica_number'=>2])->id;
+
     }
 }
 
