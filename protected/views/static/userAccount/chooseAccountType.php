5a5b2921c47bdcc511113ff3061304911757a922
Kirill Kostko <kirill@skiliks.com>
2013-03-14 15:43:51 +0100

    sk-1496-1588

--- protected/views/static/userAccount/chooseAccountType.php
+++ protected/views/static/userAccount/chooseAccountType.php
@@ -16,136 +16,131 @@
 </style>
 
 
-<br/>
-<br/>
-<br/>
-
-<div class="form form-account-personal" style="width: 450px; float: left;">
-
-    <h1><?php echo Yii::t('site', 'Personal account') ?></h1>
-    <br/>
-
-    <?php $form = $this->beginWidget('CActiveForm', array(
-        'id'                   => 'user-account-personal-form',
-        'enableAjaxValidation' => false,
-    )); ?>
-
-    <?php // echo $form->errorSummary($accountPersonal); ?>
-
-    <div class="row">
-        <?php echo $form->hiddenField($accountPersonal, 'user_id'); ?>
-        <?php echo $form->error($accountPersonal      , 'user_id'); ?>
-
-        <?php echo $form->hiddenField($profile, 'email'); ?>
-        <?php echo $form->error($profile      , 'email'); ?>
-    </div>
-
-    <div class="row">
-        <?php echo $form->labelEx($profile  , 'firstname'); ?>
-        <?php echo $form->textField($profile, 'firstname'); ?>
-        <?php if ($isPersonalSubmitted): ?>
-            <?php echo $form->error($profile    , 'firstname'); ?>
-        <?php endif; ?>
-    </div>
-    <br/>
-
-    <div class="row">
-        <?php echo $form->labelEx($profile  , 'lastname'); ?>
-        <?php echo $form->textField($profile, 'lastname'); ?>
-        <?php if ($isPersonalSubmitted): ?>
-            <?php echo $form->error($profile    , 'lastname'); ?>
-        <?php endif; ?>
-    </div>
-    <br/>
-
-    <div class="row">
-        <?php echo $form->labelEx($accountPersonal     ,'industry_id'); ?>
-        <?php echo $form->dropDownList($accountPersonal,'industry_id', $industries); ?>
-        <?php echo $form->error($accountPersonal       ,'industry_id'); ?>
-    </div>
-    <br/>
-
-    <div class="row">
-        <?php echo $form->labelEx($accountPersonal     ,'position_id'); ?>
-        <?php echo $form->dropDownList($accountPersonal,'position_id', $positions); ?>
-        <?php echo $form->error($accountPersonal       ,'position_id'); ?>
-    </div>
-    <br/>
-
-    <div class="row buttons">
-        <?php echo CHtml::submitButton(Yii::t('site', 'Submit'), ['name' => 'personal']); ?>
-    </div>
-    <br/>
-
-    <?php $this->endWidget(); ?>
-
-</div>
-
-<!-- --------------------------------------------------------------------------------------------------------- -->
-
-<div class="form form-account-corporate" style="width: 450px; float: right;">
-
-    <h1><?php echo Yii::t('site', 'Corporate account') ?></h1>
-    <br/>
-
-    <?php $form = $this->beginWidget('CActiveForm', array(
-        'id'                   => 'user-account-corporate-form',
-        'enableAjaxValidation' => false,
-    )); ?>
-
-    <?php // echo $form->errorSummary($accountCorporate); ?>
-
-    <div class="row">
-        <?php echo $form->hiddenField($accountCorporate,'user_id'); ?>
-        <?php echo $form->error($accountCorporate,'user_id'); ?>
-
-        <?php echo $form->hiddenField($profile,'email'); ?>
-        <?php echo $form->error($profile      , 'email'); ?>
-    </div>
-
-    <div class="row">
-        <?php echo $form->labelEx($profile  , 'firstname'); ?>
-        <?php echo $form->textField($profile, 'firstname'); ?>
-        <?php if ($isCorporateSubmitted): ?>
-            <?php echo $form->error($profile    , 'firstname'); ?>
-        <?php endif; ?>
-    </div>
-    <br/>
-
-    <div class="row">
-        <?php echo $form->labelEx($profile  , 'lastname'); ?>
-        <?php echo $form->textField($profile, 'lastname'); ?>
-        <?php if ($isCorporateSubmitted): ?>
-            <?php echo $form->error($profile    , 'lastname'); ?>
-        <?php endif; ?>
-    </div>
-    <br/>
-
-    <div class="row">
-        <?php echo $form->labelEx($accountCorporate     , 'industry_id'); ?>
-        <?php echo $form->dropDownList($accountCorporate, 'industry_id', $industries); ?>
-        <?php echo $form->error($accountCorporate       , 'industry_id'); ?>
-    </div>
-    <br/>
-
-    <div class="row">
-        <?php echo $form->labelEx($accountCorporate  , 'corporate_email'); ?>
-        <?php echo $form->textField($accountCorporate, 'corporate_email'); ?>
-        <?php echo $form->error($accountCorporate    , 'corporate_email'); ?>
-    </div>
-    <br/>
-
-    <div class="row buttons">
-        <?php echo CHtml::submitButton(Yii::t('site', 'Submit'), ['name' => 'corporate']); ?>
-    </div>
-    <br/>
-
-    <?php $this->endWidget(); ?>
-
-</div>
-
-<!-- --------------------------------------------------------------------------------------------------------- -->
-
-<p class="note" style="float: none; clear: both;">
-    <?php echo Yii::t('site', 'Fields with * are required.') ?>
-</p>
+<section class="registration">
+	<h2>Sign-up using your preferred option and get the results</h2>
+	
+	<div class="form form-account-personal">
+	
+	    <h1><?php echo Yii::t('site', 'Personal account') ?></h1>
+	    
+	    <ul>
+			<li>Free full assesmnet of skills</li>
+			<li>Skills comparison with others*</li>
+			<li>Updates for free</li>
+		</ul>
+	
+	    <?php $form = $this->beginWidget('CActiveForm', array(
+	        'id'                   => 'user-account-personal-form',
+	        'enableAjaxValidation' => false,
+	    )); ?>
+	
+	    <?php // echo $form->errorSummary($accountPersonal); ?>
+	
+	    <div class="row">
+	        <?php echo $form->hiddenField($accountPersonal, 'user_id'); ?>
+	        <?php echo $form->error($accountPersonal      , 'user_id'); ?>
+	
+	        <?php echo $form->hiddenField($profile, 'email'); ?>
+	        <?php echo $form->error($profile      , 'email'); ?>
+	    </div>
+	
+	    <div class="row">
+	        <?php echo $form->labelEx($profile  , 'Name'); ?>
+	        <?php echo $form->textField($profile, 'firstname'); ?>
+	        <?php if ($isPersonalSubmitted): ?>
+	            <?php echo $form->error($profile    , 'firstname'); ?>
+	        <?php endif; ?>
+	        <?php echo $form->textField($profile, 'lastname'); ?>
+	        <?php if ($isPersonalSubmitted): ?>
+	            <?php echo $form->error($profile    , 'lastname'); ?>
+	        <?php endif; ?>
+	    </div>
+	
+	    <div class="row">
+	        <?php echo $form->labelEx($accountPersonal     ,'industry_id'); ?>
+	        <?php echo $form->dropDownList($accountPersonal,'industry_id', $industries); ?>
+	        <?php echo $form->error($accountPersonal       ,'industry_id'); ?>
+	    </div>
+	
+	    <div class="row wide">
+	        <?php echo $form->labelEx($accountPersonal     ,'position_id'); ?>
+	        <?php echo $form->dropDownList($accountPersonal,'position_id', $positions); ?>
+	        <?php echo $form->error($accountPersonal       ,'position_id'); ?>
+	    </div>
+	
+	    <div class="row buttons">
+	        <?php echo CHtml::submitButton(Yii::t('site', 'Start and get the report'), ['name' => 'personal']); ?>
+	    </div>
+	
+	    <?php $this->endWidget(); ?>
+	
+	</div>
+	
+	<!-- --------------------------------------------------------------------------------------------------------- -->
+	
+	<div class="form form-account-corporate">
+	
+	    <h1><?php echo Yii::t('site', 'Corporate account') ?></h1>
+	    
+	    <ul>
+			<li>Package of simulations to assess others</li>
+			<li>Simple but powerful tool for assessment process</li>
+			<li>Comprehensive statistics on people and skills*</li>
+			<li>Updates for free</li>
+		</ul>
+	
+	    <?php $form = $this->beginWidget('CActiveForm', array(
+	        'id'                   => 'user-account-corporate-form',
+	        'enableAjaxValidation' => false,
+	    )); ?>
+	
+	    <?php // echo $form->errorSummary($accountCorporate); ?>
+	
+	    <div class="row">
+	        <?php echo $form->hiddenField($accountCorporate,'user_id'); ?>
+	        <?php echo $form->error($accountCorporate,'user_id'); ?>
+	
+	        <?php echo $form->hiddenField($profile,'email'); ?>
+	        <?php echo $form->error($profile      , 'email'); ?>
+	    </div>
+	
+	    <div class="row">
+	        <?php echo $form->labelEx($profile  , 'Name'); ?>
+	        <?php echo $form->textField($profile, 'firstname'); ?>
+	        <?php if ($isCorporateSubmitted): ?>
+	            <?php echo $form->error($profile    , 'firstname'); ?>
+	        <?php endif; ?>
+	        <?php echo $form->textField($profile, 'lastname'); ?>
+	        <?php if ($isCorporateSubmitted): ?>
+	            <?php echo $form->error($profile    , 'lastname'); ?>
+	        <?php endif; ?>
+	    </div>
+	
+	    <div class="row">
+	        <?php echo $form->labelEx($accountCorporate     , 'industry_id'); ?>
+	        <?php echo $form->dropDownList($accountCorporate, 'industry_id', $industries); ?>
+	        <?php echo $form->error($accountCorporate       , 'industry_id'); ?>
+	    </div>
+	
+	    <div class="row wide">
+	        <?php echo $form->labelEx($accountCorporate  , 'corporate_email'); ?>
+	        <?php echo $form->textField($accountCorporate, 'corporate_email'); ?>
+	        <?php echo $form->error($accountCorporate    , 'corporate_email'); ?>
+	    </div>
+	
+	    <div class="row buttons">
+	        <?php echo CHtml::submitButton(Yii::t('site', 'Start and get the report'), ['name' => 'corporate']); ?>
+	    </div>
+	
+	    <?php $this->endWidget(); ?>
+	
+	</div>
+	
+	<!-- --------------------------------------------------------------------------------------------------------- -->
+	
+	<div style="clear:both;"></div>
+	
+	<p class="note" style="float: none; clear: both;">
+	    <?php echo Yii::t('site', '* Currently not available and will be added in the next release') ?>
+	</p>
+</section>
