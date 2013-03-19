61647f4e7295ebb4a35b17fde1a2e9c65815c882
slavkaa <nikoolin@ukr.net>
2013-03-19 21:07:54 +0200

    1582. Reload docs when zoho 500.

--- protected/assets/js/game/views/documents/SKXLSDisplayView.js
+++ protected/assets/js/game/views/documents/SKXLSDisplayView.js
@@ -80,7 +80,8 @@ define([
                                 me.remove();
 
                                 // clean array of not handled zoho 500 {
-                                delete SKApp.simulation.documents.zoho_500[doc.get('id')];
+                                var i = SKApp.simulation.documents.zoho_500.indexOf(doc.get('id')
+                                delete SKApp.simulation.documents.zoho_500[i];
                                 // clean array of not handled zoho 500 }
 
                                 delete me.message_window;
