61e778c1a031c8a5d049b993266e444cd50aee5a
tania <tetyana.grybok@skiliks.com>
2013-03-19 03:24:45 +0200

    after start simulation wait

--- protected/tests/selenium/Flags/F1_SK1403_Test.php
+++ protected/tests/selenium/Flags/F1_SK1403_Test.php
@@ -21,7 +21,7 @@ class F1_SK1403_Test extends SeleniumTestHelper
     {
         //$this->markTestIncomplete();
         $this->start_simulation();
-
+        sleep(2);
         $this->run_event('S9');
         $this->optimal_click("css=li.icon-active.phone a");
         $this->optimal_click(Yii::app()->params['test_mappings']['phone']['no_reply']);
@@ -40,7 +40,7 @@ class F1_SK1403_Test extends SeleniumTestHelper
     {
         //$this->markTestIncomplete();
         $this->start_simulation();
-
+        sleep(2);
         $this->run_event('S9');
         $this->optimal_click("css=li.icon-active.phone a");
         $this->optimal_click(Yii::app()->params['test_mappings']['phone']['reply']);
