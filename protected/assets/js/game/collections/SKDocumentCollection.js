3f0eb9f5c7ad353e57105e6f24ebc15fec4b2dc8
slavkaa <nikoolin@ukr.net>
2013-03-19 11:28:15 +0200

    Fix xls for import test.

--- protected/assets/js/game/collections/SKDocumentCollection.js
+++ protected/assets/js/game/collections/SKDocumentCollection.js
@@ -21,9 +21,6 @@ define(["game/models/SKDocument"], function () {
          */
         initialize: function() {
             var me = this;
-            console.log('window: ', window);
-            //window.addEventListener(" ", me.handlePostMessage(), false);
-
             if (window.addEventListener){
                 window.addEventListener("message", me.handlePostMessage,false);
             } else {
@@ -38,14 +35,29 @@ define(["game/models/SKDocument"], function () {
          * @return void
          */
         handlePostMessage: function(event) {
+            var me = this;
+
             console.log('handlePostMessage');
-            console.log('event!');
-
-
             if (undefined != typeof event && event.origin !== "*") {
-                console.log('event!');
-                console.log('event', event.origin);
-                return;
+                console.log('event', event);
+                me.message_window = new SKDialogView({
+                    'message': 'Excel выполнил недопустимую операцию. <br/> Необходимо закрыть и заново открыть документ.',
+                    'buttons': [
+                        {
+                            'value': 'Подтвердить',
+                            'onclick': function () {
+                                AKApp.user.simulation.documents = new SKDocumentCollection();
+                                delete me.message_window;
+                            }
+                        },
+                        {
+                            'value': 'Отмена',
+                            'onclick': function () {
+                                delete me.message_window;
+                            }
+                        }
+                    ]
+                });
             }
         },
 
