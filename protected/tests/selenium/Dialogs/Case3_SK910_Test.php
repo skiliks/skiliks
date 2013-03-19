e0851f8aeee1a08a69498ab47c4472165d2d734f
Vadim Logunov <vadim.logunov@skiliks.com>
2013-03-19 11:18:25 +0200

    sel

---
+++ protected/tests/selenium/Dialogs/Case3_SK910_Test.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * \addtogroup Selenium
+ * @{
+ */
+/**
+ * Тест на диалоги, Case 3.
+ * Пошагово запускаем диалоги из /Users/Tony/Dropbox/projectx - development/1. Documentation/1.1 Scenario/1.1.1 Оценка/Тесты/Расчет оценки_тест3_final.xls
+ * После чего в Simulation points сверяем суммы оценок поведений positive, negative & personal по mail matrix и all dialogs
+ */
+class Case3_SK910_Test extends SeleniumTestHelper
+{
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl(Yii::app()->params['frontendUrl']);
+        parent::setUp();
+    }
+
+    public function testSK910()
+    {
+        $this->markTestIncomplete();
+        $this->start_simulation();
+
+        $this->run_event('E2.4');
+        $this->optimal_click("xpath=(//*[contains(text(),'Марина, срочно пересылай мне презентацию для Генерального')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'Отлично, одной проблемой меньше. Жду в 15.30')])");
+
+        $this->run_event('E12.1');
+        $this->optimal_click("xpath=(//*[contains(text(),'Может мой аналитик подойти вместо меня?')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'В понедельник, скажем в 10.00, будет моя сотрудница Марина Крутько')])");
+
+        $this->run_event('E12.5');
+        $this->optimal_click("xpath=(//*[contains(text(),'Действительно, повезло! Уже бегу!')])");
+
+        $this->optimal_click("xpath=(//*[contains(text(),'Кхе-кхе…')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'Да, доволен')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'Ваша презентация была не единственным его промахом')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'Это наши корпоративные цвета')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'Мы вместе с сотрудниками. Они готовили – я проверял.')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'Хорошего вам выступления, Валерий Семенович!')])");
+
+        $this->run_event('MS27');
+        $this->run_event('MS48');
+        $this->run_event('MS68');
+        $this->run_event('MS70');
+
+        $this->optimal_click(Yii::app()->params['test_mappings']['dev']['show_logs']);
+        $this->optimal_click(Yii::app()->params['test_mappings']['dev']['sim_points']);
+        $this->assertText(Yii::app()->params['test_mappings']['dev']['admm_positive'],"8.083");
+        $this->assertText(Yii::app()->params['test_mappings']['dev']['admm_negative'],"-7");
+        $this->assertText(Yii::app()->params['test_mappings']['dev']['admm_personal'],"6.5");
+
+
+    }
+}
