78761cb411d4a5ecae956d28a753b2aafd7b31cb
slavkaa <nikoolin@ukr.net>
2013-03-19 13:09:05 +0200

    1582. Reload docs when zoho 500.

--- protected/assets/js/game/collections/SKDocumentCollection.js
+++ protected/assets/js/game/collections/SKDocumentCollection.js
@@ -44,11 +44,10 @@ define(["game/models/SKDocument"], function () {
             var me = this;
 
             console.log('handlePostMessage');
-            if (undefined != typeof event && event.origin !== "*") {
+            if ((undefined != typeof event && event.origin !== "*") || true === me.excelErrorHappened) {
                 console.log('event', event);
                 me.excelErrorHappened = true;
 
-                // to display pop-up when 500 happened crash
                 me.trigger('zoho-500');
             }
         },
