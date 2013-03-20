e7eaa080de5d38af6ce8c3f06d5030761589b29f
tania <tetyana.grybok@skiliks.com>
2013-03-20 16:53:37 +0200

    new_test_for_1273

--- protected/tests/selenium/Mail/SK1273_Case1_2_Test.php
+++ protected/tests/selenium/Mail/SK1273_Case1_2_Test.php
@@ -4,7 +4,7 @@
  * @{
  */
 /**
- * Тесты (негативные) на тестирование флага F4 (для SK1413)
+ * Тесты на тестирование флага F4 (для SK1273)
  */
 class SK1273_Case1_2_Test extends SeleniumTestHelper
 {
@@ -16,14 +16,14 @@ class SK1273_Case1_2_Test extends SeleniumTestHelper
     }
 
     /**
-     * testSK1413_N_Case1() тестирует задачу SKILIKS-1413
+     * testSK1413_N_Case1() тестирует задачу SKILIKS-1273
      *
      * 1.
      */
     public function testSK1273_Case1() {
-        $this->markTestIncomplete();
+        //$this->markTestIncomplete();
         $this->start_simulation();
-        sleep(5);
+        sleep(10);
         $this->run_event('MS30');
         $this->assertTrue($this->incoming_counter(1));
         $this->optimal_click("css=li.icon-active.mail a");
@@ -46,15 +46,63 @@ class SK1273_Case1_2_Test extends SeleniumTestHelper
         sleep(5);
         $this->waitForVisible("xpath=(//*[contains(text(),'новое письмо')])");
 
+        $this->optimal_click(Yii::app()->params['test_mappings']['mail_main']['outbox']);
+        $this->waitForVisible("xpath=(//*[contains(text(),'Отчет для Правления')])");
+        $this->assertTrue($this->mail_comes("Re: Re: Срочно жду бюджет логистики"));
+
         $this->click("css=.btn-close button");
 
         $this->assertTrue($this->incoming_counter(1));
-        $this->optimal_click("css=li.icon-active.mail a");
+        $this->optimal_click(Yii::app()->params['test_mappings']['icons']['mail']);
         $this->optimal_click(Yii::app()->params['test_mappings']['mail_main']['inbox']);
-        $this->waitForVisible("xpath=(//*[contains(text(),'Отчет для Правления')])");
+        $this->waitForVisible("xpath=(//*[contains(text(),'По ценовой политике')])");
         $this->assertTrue($this->mail_comes("Re: Re: Re: Срочно жду бюджет логистики"));
 
-        $this->mail_open("Re: Срочно жду бюджет логистики");
+        $this->mail_open("Re: Re: Re: Срочно жду бюджет логистики");
+
+        $this->mouseOver(Yii::app()->params['test_mappings']['mail_main']['reply_email']);
+        $this->click(Yii::app()->params['test_mappings']['mail_main']['reply_email']);
+
+        $this->waitForTextPresent("Re: Re: Re: Re: Срочно жду бюджет логистики");
+        $this->optimal_click(Yii::app()->params['test_mappings']['mail']['send']);
+        sleep(5);
+        $this->waitForVisible("xpath=(//*[contains(text(),'новое письмо')])");
+
+        $this->optimal_click(Yii::app()->params['test_mappings']['mail_main']['outbox']);
+        $this->waitForVisible("xpath=(//*[contains(text(),'Отчет для Правления')])");
+        $this->assertTrue($this->mail_comes("Re: Re: Re: Re: Срочно жду бюджет логистики"));
+
+        $this->click("css=.btn-close button");
+
+        if ($this->is_it_done("css=li.icon-active.mail a"))
+        {
+            print ("The test crashed! This action couldn't be active in such situation!");
+        }
+
+        $this->optimal_click(Yii::app()->params['test_mappings']['icons']['mail']);
+        $this->optimal_click(Yii::app()->params['test_mappings']['mail_main']['inbox']);
+        $this->waitForVisible("xpath=(//*[contains(text(),'По ценовой политике')])");
+        $this->assertTrue($this->mail_comes("Re: Re: Re: Срочно жду бюджет логистики"));
+
+        $this->mail_open("Re: Re: Re: Срочно жду бюджет логистики");
+
+        $this->mouseOver(Yii::app()->params['test_mappings']['mail_main']['forward_email']);
+        $this->click(Yii::app()->params['test_mappings']['mail_main']['forward_email']);
+
+        $this->waitForTextPresent("Fwd: Re: Re: Re: Срочно жду бюджет логистики");
+
+        $this->optimal_click(Yii::app()->params['test_mappings']['mail']['to_whom']);
+        $this->waitForElementPresent(Yii::app()->params['test_mappings']['mail_contacts']['trudyakin']);
+        $this->mouseOver(Yii::app()->params['test_mappings']['mail_contacts']['trudyakin']);
+        $this->optimal_click(Yii::app()->params['test_mappings']['mail_contacts']['trudyakin']);
+
+        $this->optimal_click(Yii::app()->params['test_mappings']['mail']['send']);
+        sleep(5);
+        $this->waitForVisible("xpath=(//*[contains(text(),'новое письмо')])");
+
+        $this->optimal_click(Yii::app()->params['test_mappings']['mail_main']['outbox']);
+        $this->waitForVisible("xpath=(//*[contains(text(),'Отчет для Правления')])");
+        $this->assertTrue($this->mail_comes("Fwd: Re: Re: Re: Срочно жду бюджет логистики"));
 
         $this->click("css=input.btn.btn-simulation-stop");
     }
