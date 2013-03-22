456c026f56378b8a16c1d1866a547eacfbbb4b51
slavkaa <nikoolin@ukr.net>
2013-03-22 10:46:24 +0200

    1745. Make controllers/URLs structure.

--- protected/views/layouts/_language_switcher.php
+++ protected/views/layouts/_language_switcher.php
@@ -1,6 +1,7 @@
 <div class="language">
-    <?php if (in_array(Yii::app()->request->getPathInfo(), ['', 'team', 'product'])): ?>
-        <a href="?_lang=<?php echo Yii::t('site', 'ru')?>">
+    <?php $path = Yii::app()->request->getPathInfo() ?>
+    <?php if (in_array($path, ['', 'ru', 'en']) || -1 < strpos($path, 'static')|| -1 < strpos($path, 'team')|| -1 < strpos($path, 'product')): ?>
+        <a href="<?php echo Yii::t('site', 'ru')?>">
             <?php echo Yii::t('site', 'Русский') ?>
         </a>
     <?php endif ?>
