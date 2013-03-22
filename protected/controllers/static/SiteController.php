119ca942ac284015a5e650465a778af855665fd0
Dmitry Alekseev <pernifin@gmail.com>
2013-03-22 17:46:33 +0200

    SKILIKS-1665 Check browser and speed

--- protected/controllers/static/SiteController.php
+++ protected/controllers/static/SiteController.php
@@ -36,73 +36,20 @@ class SiteController extends AjaxController
             $this->redirect('/');
         }
 
-        $cs = Yii::app()->clientScript;
-
         $assetsUrl = $this->getAssetsUrl();
-
         $config = array_merge(
             Yii::app()->params['public'],
             Yii::app()->params['simulation'][Simulation::$typeLabel[$type]],
-            ['assetsUrl' => $assetsUrl, 'mode' => $mode, 'type' => $type]
+            [
+                'assetsUrl' => $assetsUrl,
+                'mode' => $mode,
+                'type' => $type,
+                'badBrowserUrl' => '/bad-browser',
+                'oldBrowserUrl' => '/old-browser',
+                'dummyFilePath' => $assetsUrl . '/img/kotik.jpg'
+            ]
         );
 
-        $cs->registerCssFile($assetsUrl . "/js/jquery/jquery-ui.css");
-        $cs->registerCssFile($assetsUrl . "/js/bootstrap/css/bootstrap.css");
-        $cs->registerCssFile($assetsUrl . "/js/jquery/jquery-ui-1.8.23.slider.css");
-        $cs->registerCssFile($assetsUrl . "/js/jquery/jquery.mCustomScrollbar.css");
-        $cs->registerCssFile($assetsUrl . "/js/elfinder-2.0-rc1/css/elfinder.min.css");
-        $cs->registerCssFile($assetsUrl . "/js/elfinder-2.0-rc1/css/theme.css");
-        $cs->registerCssFile($assetsUrl . "/css/tag-handler.css");
-        $cs->registerCssFile($assetsUrl . "/css/main.css");
-        $cs->registerLinkTag('stylesheet/less', 'text/css', $assetsUrl . "/css/simulation.less");
-        $cs->registerLinkTag('stylesheet/less', 'text/css', $assetsUrl . "/css/plan.less");
-        $cs->registerLinkTag('stylesheet/less', 'text/css', $assetsUrl . "/css/documents.less");
-        $cs->registerLinkTag('stylesheet/less', 'text/css', $assetsUrl . "/css/ddSlick.css");
-
-        $cs->registerScriptFile($assetsUrl . '/js/jquery/jquery-1.7.2.min.js');
-        $cs->registerScriptFile($assetsUrl . "/js/jquery/jquery-ui-1.8.24.custom.js");
-        $cs->registerScriptFile($assetsUrl . "/js/jquery/jquery.hotkeys.js");
-        $cs->registerScriptFile($assetsUrl . "/js/jquery/jquery.balloon.js");
-        $cs->registerScriptFile($assetsUrl . "/js/jquery/jquery.topzindex.min.js");
-        $cs->registerScriptFile($assetsUrl . "/js/jquery/jquery.cookies.js");
-        $cs->registerScriptFile($assetsUrl . "/js/jquery/jquery-skiliks.js");
-        $cs->registerScriptFile($assetsUrl . "/js/jquery/jquery.mCustomScrollbar.js");
-        $cs->registerScriptFile($assetsUrl . "/js/jquery/jquery.mousewheel.min.js");
-        $cs->registerScriptFile($assetsUrl . "/js/jquery/jquery.tablesorter.js");
-
-        $cs->registerScriptFile($assetsUrl . "/js/bootstrap/js/bootstrap.js");
-        $cs->registerScriptFile($assetsUrl . "/js/game/lib/pdf.js");
-        $cs->registerScriptFile($assetsUrl . "/js/game/lib/hyphenate.js");
-        $cs->registerScriptFile($assetsUrl . "/js/underscore.js");
-        $cs->registerScriptFile($assetsUrl . "/js/prefixfree.min.js");
-        $cs->registerScriptFile($assetsUrl . "/js/jquery.ddslick.min.js");
-        $cs->registerScriptFile($assetsUrl . '/js/require.js');
-
-        $cs->registerScriptFile($assetsUrl . "/js/backbone.js");
-        $cs->registerScriptFile($assetsUrl . "/js/less-1.3.3.min.js");
-        $cs->registerScriptFile($assetsUrl . "/js/elfinder-2.0-rc1/js/elfinder.min.js");
-
-        $cs->registerScriptFile($assetsUrl . "/js/tag-handler/jquery.taghandler.min.js");
-
-        $cs->registerScriptFile($assetsUrl . "/js/game/models/SKTodoTask.js");
-        $cs->registerScriptFile($assetsUrl . "/js/game/models/SKDayTask.js");
-        $cs->registerScriptFile($assetsUrl . "/js/game/models/SKPhoneContact.js");
-        $cs->registerScriptFile($assetsUrl . "/js/game/models/SKPhoneTheme.js");
-
-        $cs->registerScriptFile($assetsUrl . "/js/game/collections/SKPhoneContactsCollection.js");
-        $cs->registerScriptFile($assetsUrl . "/js/game/collections/SKPhoneThemeCollection.js");
-
-        $cs->registerScriptFile($assetsUrl . "/js/game/models/SKMailTask.js");
-        $cs->registerScriptFile($assetsUrl . "/js/game/models/SKMailPhrase.js");
-        $cs->registerScriptFile($assetsUrl . "/js/game/models/SKMailSubject.js");
-        $cs->registerScriptFile($assetsUrl . "/js/game/models/SKMailFolder.js");
-
-        $cs->registerScriptFile($assetsUrl . "/js/game/models/SKMailFolder.js");
-
-        $cs->registerScriptFile($assetsUrl . "/js/raven-0.7.1.js");
-
-        //плагины
-        //$cs->registerScriptFile($assetsUrl . "/js/jquery-ui-1.10.2.custom.js");
         $this->layout = false;
         $this->render('site', [
             'config'    => CJSON::encode($config),
