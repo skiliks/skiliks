0cea07610bc39f2ca48d6422bc2a3103da8c8d60
Dmitry Alekseev <pernifin@gmail.com>
2013-03-22 19:50:02 +0200

    SKILIKS-1662 Pause availability

--- protected/components/SimulationService.php
+++ protected/components/SimulationService.php
@@ -486,6 +486,10 @@ class SimulationService
      */
     public static function pause($simulation)
     {
+        if ($simulation->type != Simulation::TYPE_LITE) {
+            throw new Exception('Pause feature is not available in non-lite simulation');
+        }
+
         if (empty($simulation->paused)) {
             $simulation->paused = GameTime::setNowDateTime();
             $simulation->save();
