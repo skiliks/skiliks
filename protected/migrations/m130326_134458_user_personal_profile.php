620806ae869d1f8738633eeddb956684a5874840
slavkaa <nikoolin@ukr.net>
2013-03-27 03:17:39 +0200

    fix migration.

--- protected/migrations/m130326_134458_user_personal_profile.php
+++ protected/migrations/m130326_134458_user_personal_profile.php
@@ -4,10 +4,14 @@ class m130326_134458_user_personal_profile extends CDbMigration
 {
 	public function up()
 	{
-        $this->addColumn('user_account_personal', 'birthday', 'date');
-        $this->addColumn('user_account_personal', 'location', 'VARCHAR(255)');
+        //$this->addColumn('user_account_personal', 'birthday', 'date');
+        //$this->addColumn('user_account_personal', 'location', 'VARCHAR(255)');
 
-        $this->truncateTable('professional_statuses');
+        $this->delete('professional_statuses');
+
+        // Cannot truncate a table referenced in a foreign key constraint
+        // (`skiliks_11`.`user_account_personal`, CONSTRAINT `fk_user_account_personal_professional_status_id`
+        // FOREIGN KEY (`professional_status_id`) REFERENCES `skiliks_11`.`professional_statuses` (`id`)).
 
         $this->insert('professional_statuses', ['label' => 'Собственник']);
         $this->insert('professional_statuses', ['label' => 'Высшее руководство']);
