eb80fa0918fce021b3637b306851ddc995bc5b41
Svetlana Morozova <svetlana@skiliks.com>
2013-03-27 10:20:48 +0100

    edited changes

--- protected/views/static/dashboard/dashboard_personal.php
+++ protected/views/static/dashboard/dashboard_personal.php
@@ -1,13 +1,17 @@
 
 <section class="dashboard">
-
-
     <!-- private-invitations-list-box -->
     <div class="narrow-contnt">
-        <h1 class="thetitle">Received invites</h1>
-        <div id="private-invitations-list-box" class="nice-border backgroud-light-yellow wideblock">
-            <?php $this->renderPartial('_private_invitations_list_box', []) ?>
+        <div class="searchform">
+            <input type="text" class="inputtext" placeholder="Search"/>
+            <input type="submit"/>
+        </div>
 
+        <h1 class="thetitle">Received invites</h1>
+
+        <div id="private-invitations-list-box" class="transparent-boder wideblock">
+            <?php $this->renderPartial('_private_invitations_list_box', []) ?>
+            <p>&nbsp;</p>
             <div class="grid-view">
                 <table class="items">
                     <thead>
@@ -20,6 +24,13 @@
                     </tr>
                     </thead>
                     <tbody>
+                        <tr>
+                            <td>Eksmo</td>
+                            <td>Head of IT</td>
+                            <td>Core management</td>
+                            <td>1/09/12  3:02</td>
+                            <td><a href="#" class="blue-btn">Accept</a> or <a href="#">Decline</a></td>
+                        </tr>
                         <tr>
                             <td>Eksmo</td>
                             <td>Head of IT</td>
@@ -40,32 +51,51 @@
             <?php $this->renderPartial('_dashboard_skills_box', []) ?>
             <p>Core management
                 <span class="ratingwrap radiusthree">
-                    <span class="ratebg">
-                        <span class="rating" style="width: 80%"></span>
-                    </span>
-                    80%
+                    <span class="ratebg"><span class="rating" style="width: 80%"></span></span>
+                    <sup>80%</sup>
                 </span>
                 <a href="#" class="link-go"></a>
             </p>
             <div><a href="#" class="light-btn">Compare</a><a href="#" class="light-btn">Apply to position</a></div>
         </div>
 
+        <div class="sidefeedback"><a href="#" class="light-btn">Send feedback</a></div>
+
         <!-- companies-you-follow-box -->
         <div id="companies-you-follow-box" class="backgroud-bue-bordered sideblock">
             <?php $this->renderPartial('_companies_you_follow_box', []) ?>
             <ul class="nodesign-list">
-                <li><a href="#">Skiliks <span>1</span></a></li>
-                <li><a href="#">Googgle Russia <span>5</span></a></li>
+                <li><a href="#">Skiliks <span class="noticeblock">1</span></a></li>
+                <li><a href="#">Googgle Russia <span class="noticeblock">5</span></a></li>
                 <li><a href="#">Headhunter</a></li>
                 <li><a href="#">Yandex</a></li>
                 <li><a href="#">Sberbank</a></li>
             </ul>
+            <p><a href="#" class="whitelink">More</a></p>
+            <a href="#" class="light-btn">Add company</a>
         </div>
 
         <!-- job-recomendations-box -->
         <div id="job-recomendations-box" class="backgroud-bue-bordered sideblock">
             <?php $this->renderPartial('_job_recomendations_box', []) ?>
+            <ul class="nodesign-list jobs-list">
+                <li><a href="#">Head of IT at Sberbank</a></li>
+                <li><a href="#">Head of IT at Yandex</a></li>
+                <li class="job-active"><a href="#">Head of IT at Alfa Sberbank bank</a>
+                    <div class="sublist">
+                        <p><a href="#">Don’t show me this job...</a></p>
+                        <p><a href="#">Not relevant</a></p>
+                        <p><a href="#">Relevant but I’m not interested</a></p>
+                    </div>
+                </li>
+                <li><a href="#">Head of IT at Mail</a></li>
+            </ul>
         </div>
+
+        <div class="pager">
+            <a href="#">Prev</a>
+        </div>
+
     </aside>
 </section>
 
