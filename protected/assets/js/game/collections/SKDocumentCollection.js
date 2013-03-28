0c698330a02d23466022ba64fe7bdb6e49b1c4d4
slavkaa <nikoolin@ukr.net>
2013-03-28 12:33:06 +0200

    FIx ave log as XML.

--- protected/assets/js/game/collections/SKDocumentCollection.js
+++ protected/assets/js/game/collections/SKDocumentCollection.js
@@ -28,10 +28,49 @@ define(["game/models/SKDocument"], function () {
          * @param options
          */
         sync: function (method, collection, options) {
+            var me = this;
+
             if ('read' === method) {
                 SKApp.server.api('myDocuments/getList', {}, function (data) {
+                    //console.log('1');
+                    //alert('1*');
                     options.success(data.data);
+                    //console.log('2');
+
+                    //me.waitForDocumentsInitialization();
                 });
+            }
+        },
+
+        isDocumentsInitialized: function() {
+            var me = this;
+
+            var isDocumentsInitialized = true;
+
+            for (var key in this.models) {
+                console.log(this.models[key].get('isInitialized'));
+                if (false == this.models[key].get('isInitialized')) {
+                    isDocumentsInitialized = false;
+                    //alert('false!');
+                }
+            }
+
+            if (true == isDocumentsInitialized) {
+                this.trigger('documents-initialized');
+                //console.log('documents-initialized!');
+                //alert('documents-initialized!');
+            }
+
+            return isDocumentsInitialized;
+        },
+
+        waitForDocumentsInitialization: function() {
+            var me = this;
+
+            //console.log('waitForDocumentsInitialization RUN ... ');
+
+            if (false == me.waitForDocumentsInitialization()) {
+                setTimeout(me.waitForDocumentsInitialization(), 2000);
             }
         }
     });
