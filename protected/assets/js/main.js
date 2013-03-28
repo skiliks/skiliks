67cb56ae5d867c80976cbf7b9197fea1e7f90d92
Andrii Kostenko <andrey@kostenko.name>
2013-03-28 23:43:09 +0200

    JS code cleanliness

--- protected/assets/js/main.js
+++ protected/assets/js/main.js
@@ -1,27 +1,27 @@
-$(document).ready(function(){
-
-    $('a.feedback').click(function(){
+$(document).ready(function () {
+    "use strict";
+    $('a.feedback').click(function () {
         $('.feedback_dialog').dialog({ width: 600});
         return false;
     });
-        $('.dashboard').append($('<div id="simulation-details-pop-up"></div>'));
-        $('#simulation-details-pop-up').dialog({
-            modal: true,
-            width: 940,
-            minHeight: 600
+    $('.dashboard').append($('<div id="simulation-details-pop-up"></div>'));
+    var simulation_popup = $('#simulation-details-pop-up');
+    simulation_popup.dialog({
+        modal:     true,
+        width:     940,
+        minHeight: 600,
+        autoOpen: false
+    });
+
+    $(".view-simulation-details-pop-up").click(function (event) {
+        event.preventDefault();
+        $.ajax({
+            url:     $(this).attr('data-simulation'),
+            success: function (data) {
+
+                simulation_popup.html(data);
+                simulation_popup.dialog('open');
+            }
         });
-
-        $('#simulation-details-pop-up').dialog('close');
-
-        $(".view-simulation-details-pop-up").click(function(event){
-            event.preventDefault();
-            $.ajax({
-                url: $(this).attr('data-simulation'),
-                success: function(data) {
-
-                    $('#simulation-details-pop-up').html(data);
-                    $('#simulation-details-pop-up').dialog('open');
-                }
-            });
-        });
+    });
 });
