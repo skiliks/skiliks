2a911f8719546e249095d002d5c3afa2e70ee547
slavkaa <nikoolin@ukr.net>
2013-03-28 17:20:43 +0200

    Presonal dashboard - fix for Sunny.

--- protected/views/static/dashboard/_private_invitations_list_box.php
+++ protected/views/static/dashboard/_private_invitations_list_box.php
@@ -18,8 +18,7 @@
         'columns' => [
             ['header' => Yii::t('site', Yii::t('site', 'Company'))    , 'value' => 'Yii::t("site", $data->ownerUser->getAccount()->ownership_type." ".$data->ownerUser->getAccount()->company_name)'],
             ['header' => Yii::t('site', Yii::t('site', 'Vacancy'))    , 'value' => 'Yii::t("site", $data->vacancy->label)'],
-            ['header' => Yii::t('site', Yii::t('site', 'Assessment')) , 'value' => 'Yii::t("site", "Менеджмент"'],
-            ['header' => Yii::t('site', Yii::t('site', 'Status'))     , 'name' => 'status'      , 'value' => 'Yii::t("site", $data->getStatusText())'],
+            ['header' => Yii::t('site', Yii::t('site', 'Assessment')) , 'value' => '"Core managament"'],
             ['header' => Yii::t('site', Yii::t('site', 'Date / time')), 'name' => 'sent_time'   , 'value' => '$data->getSentTime()->format("j/m/y G:i")'],
             ['header' => Yii::t('site', Yii::t('site', 'Score')) , 'value' => '$data->getSimulationResultsTag()', 'type' => 'html'],
         ]
