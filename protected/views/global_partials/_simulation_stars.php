112f2486f3e37d1af4b05a4dd0234cbda42ce80f
Malysh <svetlana@skiliks.com>
2013-03-28 11:06:53 +0000

    personal profile

--- protected/views/global_partials/_simulation_stars.php
+++ protected/views/global_partials/_simulation_stars.php
@@ -4,7 +4,7 @@
     $label = (isset($label)) ? 'Менеджмент' : '--' ;
 ?>
 
-<p>Менеджмент
+<p><span class="skillstitle">Базовый менеджмент</span>
     <span class="ratingwrap radiusthree">
         <span class="ratebg"><span class="rating" style="width: 80%"></span></span>
         <sup>80%</sup>
