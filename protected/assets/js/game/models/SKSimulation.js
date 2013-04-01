e8497c4fe16911b01cd8c68ca9289ed0302a54a9
Ivan Tugay <listepo@ya.ru>
2013-04-01 18:38:14 +0300

    Zoho test

--- protected/assets/js/game/models/SKSimulation.js
+++ protected/assets/js/game/models/SKSimulation.js
@@ -158,7 +158,7 @@ define([
                 }
 
                 this.documents = new SKDocumentCollection();
-                this.documents.bind('add', this.onAddDocument, this);
+                this.documents.bind('reset', this.onAddDocument, this);
                 this.windowLog = new SKWindowLog();
                 this.skipped_seconds = 0;
                 this.mailClient = new SKMailClient();
