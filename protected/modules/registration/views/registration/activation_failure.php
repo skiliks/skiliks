8a9faecb51b06aa9308ff8a8741ca84043759215
slavkaa <nikoolin@ukr.net>
2013-04-02 17:48:04 +0300

    TEst strange error message behaviour.

--- protected/modules/registration/views/registration/activation_failure.php
+++ protected/modules/registration/views/registration/activation_failure.php
@@ -19,7 +19,7 @@
 */ ?>
 
 <h3 class="midtitle">
-    <?php var_dump($error) ?>
+    <?php if(false === $error) echo Yii::t('site','The user is already activated'); ?>
     <?php if($error == -1) echo Yii::t('site','The user is already activated'); ?>
     <?php if($error == -2) echo Yii::t('site','Wrong activation Key'); ?>
     <?php if($error == -3) echo Yii::t('site','Profile found, but no associated user. Possible database inconsistency. Please contact the System administrator with this error message, thank you'); ?>
