1a0c27f124e91455b87b9f9c8508f8355feacea3
Malysh <svetlana@skiliks.com>
2013-04-03 20:40:24 +0100

    personal profile

--- protected/views/static/pages/tariffs.php
+++ protected/views/static/pages/tariffs.php
@@ -26,7 +26,7 @@
 <?php endforeach ?>
     <p class="text-right text16"><sup>*</sup> <strong>Свяжитесь с нами,</strong> чтобы приобрести</p>
     <div class="contwrap"><a href="#" class="light-btn feedback">Обратная связь</a>
-        <span class="socicons">!!<a href="#" class="iconfb"></a><a href="#" class="icontw"></a>
+        <span class="socicons"><a href="#" class="iconfb"></a><a href="#" class="icontw"></a>
             <a href="#" class="icongplus"></a><a href="#" class="iconin"></a></span>
     </div>
 </div>
