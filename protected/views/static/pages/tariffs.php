171d19296b267de593b07bbd19d27e6020b25245
Malysh <svetlana@skiliks.com>
2013-04-04 14:34:47 +0100

    personal profile

--- protected/views/static/pages/tariffs.php
+++ protected/views/static/pages/tariffs.php
@@ -33,8 +33,20 @@
 <?php endforeach ?>
     <p class="text-right text16"><sup>*</sup> <strong>Свяжитесь с нами,</strong> чтобы приобрести</p>
     <div class="contwrap"><a href="#" class="light-btn feedback">Обратная связь</a>
-        <span class="socicons"><a href="#" class="iconfb"></a><a href="#" class="icontw"></a>
-            <a href="#" class="icongplus"></a><a href="#" class="iconin"></a></span>
+    <span class="social_networks">
+
+        <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+            <a class="addthis_button_vk"></a>
+            <a class="addthis_button_facebook"></a>
+            <a class="addthis_button_twitter"></a>
+            <a class="addthis_button_google_plusone"  g:plusone:count="false"></a>
+            <a class="addthis_button_linkedin"></a>
+        </div>
+<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5158c9c22198d938"></script>
+<!-- AddThis Button END -->
+
+    </span>
     </div>
 </div>
 
