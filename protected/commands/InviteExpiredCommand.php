cbeba831b6a0caac87ed4a8d4f24ee2211794c60
Ivan Tugay <listepo@hotmail.com>
2013-04-05 12:45:36 +0300

    Tariff

--- protected/commands/InviteExpiredCommand.php
+++ protected/commands/InviteExpiredCommand.php
@@ -11,6 +11,7 @@ class InviteExpiredCommand extends CConsoleCommand
 {
     public function actionIndex() // 7 days
     {
+        //Invites
         $time = time() - Yii::app()->params['cron']['InviteExpired'];
         /** @var $invites Invite[] */
         $invites = Invite::model()->findAll("status = ".Invite::STATUS_PENDING." AND sent_time <= ".$time);
@@ -19,6 +20,15 @@ class InviteExpiredCommand extends CConsoleCommand
             $invite->inviteExpired();
         }
 
+        /* @var $users UserAccountCorporate[] */
+        $users = UserAccountCorporate::model()->findAll("tariff_expired_at <= '".(new DateTime())->format("Y-m-d H:i:s")."'");
+        if(null !== $users){
+            /* @var $user UserAccountCorporate */
+            foreach($users as $user){
+                $user->invites_limit = 0;
+                $user->update();
+            }
+        }
 
     }
 }
