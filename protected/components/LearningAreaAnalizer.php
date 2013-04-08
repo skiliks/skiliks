3c73334252579ae4b0d8e72daf4254e65fc57955
Ivan Tugay <listepo@hotmail.com>
2013-04-08 16:01:55 +0300

    SKILIKS-2011 #resolved

--- protected/components/LearningAreaAnalizer.php
+++ protected/components/LearningAreaAnalizer.php
@@ -15,6 +15,8 @@ class LearningAreaAnalizer {
         $this->stability();
         $this->responsibility();
         $this->resultOrientation();
+        $this->constructibility();
+        $this->flexibility();
     }
 
     /*
@@ -197,6 +199,37 @@ class LearningAreaAnalizer {
         $value = round(($value / $max_rate->rate) * 100, 2);
 
         $learning_area = $game_type->getLearningArea(['code' => 15]);//Конструктивность
+        $sim_learning_area = new SimulationLearningArea();
+        $sim_learning_area->learning_area_id = $learning_area->id;
+        $sim_learning_area->value = ($value > 100)?100:$value;
+        $sim_learning_area->sim_id = $simulation->id;
+        $sim_learning_area->save();
+
+    }
+
+    /*
+     * Гибкость
+     */
+    public function flexibility(){
+
+        /* @var $simulation Simulation */
+        /* @var $game_type Scenario */
+        $simulation = $this->simulation;
+        $game_type = $simulation->game_type;
+        $point = $game_type->getHeroBehaviour(['code' => 8391]);
+        if (null === $point) {
+            return;
+        }
+        $value = AssessmentAggregated::model()->findByAttributes(['sim_id'=>$simulation->id, 'point_id'=>$point->id]);
+        if(null === $value){
+            $value = 0;
+        }
+
+        $max_rate = $game_type->getMaxRate(['hero_behaviour_id' => $point->id]);
+
+        $value = round(($value / $max_rate->rate) * 100, 2);
+
+        $learning_area = $game_type->getLearningArea(['code' => 16]);//Гибкость
         $sim_learning_area = new SimulationLearningArea();
         $sim_learning_area->learning_area_id = $learning_area->id;
         $sim_learning_area->value = ($value > 100)?100:$value;
