94e87a130441863d1a7d379b74bb5f7039bd5334
Ivan Tugay <listepo@ya.ru>
2013-04-09 00:56:53 +0300

    SKILIKS-2019 #resolved SKILIKS-2086 #resolved

---
+++ protected/tests/unit/LearningAreaAnalizerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+class LearningAreaAnalizerTest extends PHPUnit_Framework_TestCase {
+
+    public function addValueByCode($simulation, $code, $value) {
+
+        /* @var $simulation Simulation */
+        /* @var $point HeroBehaviour */
+        /* @var $assessment AssessmentAggregated */
+        $point = $simulation->game_type->getHeroBehaviour(['code'=>$code]);
+        $assessment = AssessmentAggregated::model()->findByAttributes(['sim_id'=>$simulation->id, 'point_id'=> $point->id]);
+        if(null === $assessment){
+            $assessment = new AssessmentAggregated();
+            $assessment->point_id = $point->id;
+            $assessment->sim_id = $simulation->id;
+        }
+            $assessment->value = $value;
+            $assessment->save();
+
+    }
+
+    public function testAdoptionOfDecisions() {
+
+        $user = YumUser::model()->findByAttributes(['username' => 'asd']);
+        $invite = new Invite();
+        $invite->scenario = new Scenario();
+        $invite->receiverUser = $user;
+        $invite->scenario->slug = Scenario::TYPE_FULL;
+        $simulation = SimulationService::simulationStart($invite, Simulation::MODE_PROMO_LABEL);
+
+        $this->addValueByCode($simulation, 8311, 1);
+        //$this->addValueByCode($simulation, 8321, 0);
+        //$this->addValueByCode($simulation, 8331, 0);
+        //$this->addValueByCode($simulation, 8341, 0);
+        $this->addValueByCode($simulation, 8351, 1);
+        //$this->addValueByCode($simulation, 8361, 0);
+
+        $learn = new LearningAreaAnalizer($simulation);
+        $learn->adoptionOfDecisions();
+
+        $code = $simulation->game_type->getLearningArea(['code'=>13]);
+        $assessment = SimulationLearningArea::model()->findByAttributes(['sim_id' => $simulation->id, 'learning_area_id' => $code->id]);
+        /* @var $assessment SimulationLearningArea */
+        $this->assertEquals('2.18', $assessment->value);
+    }
+
+}
