c6eb878ef3fc731f94f04d04a4320b9a5d2a9097
Dmitry Alekseev <pernifin@gmail.com>
2013-04-09 16:17:10 +0300

    Infographic navigation

--- protected/views/static/simulations/simulation_details.php
+++ protected/views/static/simulations/simulation_details.php
@@ -1,44 +1,151 @@
 <?php /** @var Simulation $simulation */ ?>
+<style>
+    .sections > div {
+        display: none;
+    }
+    .sections > div.active {
+        display: block;
+    }
+
+    .prev {
+        float: left;
+    }
+
+    .next {
+        float: right;
+    }
+</style>
 
 <h1><?php echo $simulation->user->profile->firstname ?> <?php echo $simulation->user->profile->lastname ?></h1>
 
-<div id="simulation-details-tabs">
+<div class="simulation-details">
     <script type="text/javascript">
         var assessmentResult = <?= json_encode($simulation->getAssessmentDetails()); ?>;
     </script>
 
-    <ul id="simulation-details">
-        <li><a href="#tab-main"><?php echo Yii::t('site', 'Main') ?></a></li>
-        <li><a href="#tab-productivity"><?php echo Yii::t('site', 'Productivity') ?></a></li>
-        <li><a href="#tab-time-management"><?php echo Yii::t('site', 'Time management') ?></a></li>
-        <li><a href="#tab-managerial-skills"><?php echo Yii::t('site', 'Managerial skills') ?></a></li>
-        <li><a href="#tab-personal-qualities"><?php echo Yii::t('site', 'Personal qualities') ?></a></li>
+    <ul class="navigation">
+        <li><a href="#main"><?php echo Yii::t('site', 'Main') ?></a></li>
+        <li><a href="#managerial-skills"><?php echo Yii::t('site', 'Managerial skills') ?></a></li>
+        <li><a href="#productivity"><?php echo Yii::t('site', 'Productivity') ?></a></li>
+        <li><a href="#time-management"><?php echo Yii::t('site', 'Time management') ?></a></li>
+        <li><a href="#personal-qualities"><?php echo Yii::t('site', 'Personal qualities') ?></a></li>
     </ul>
 
-    <div id="tab-main">
-        <?php $this->renderPartial('partials/tab_main', ['simulation' => $simulation]) ?>
+    <div class="sections">
+        <div id="main">
+            <?php $this->renderPartial('partials/tab_main', ['simulation' => $simulation]) ?>
+        </div>
+
+        <div id="managerial-skills">
+            <?php $this->renderPartial('partials/tab_managerial_skills', []) ?>
+        </div>
+
+        <div id="productivity">
+            <?php $this->renderPartial('partials/tab_productivity', ['simulation' => $simulation]) ?>
+        </div>
+
+        <div id="time-management">
+            <?php $this->renderPartial('partials/tab_time_management', []) ?>
+        </div>
+
+        <div id="personal-qualities">
+            <?php $this->renderPartial('partials/tab_personal_skills', ['simulation' => $simulation, 'learning_areas'=>$learning_areas]) ?>
+        </div>
     </div>
 
-    <div id="tab-productivity">
-        <?php $this->renderPartial('partials/tab_productivity', ['simulation' => $simulation]) ?>
-    </div>
-
-    <div id="tab-time-management">
-        <?php $this->renderPartial('partials/tab_time_management', []) ?>
-    </div>
-
-    <div id="tab-managerial-skills">
-        <?php $this->renderPartial('partials/tab_managerial_skills', []) ?>
-    </div>
-
-    <div id="tab-personal-qualities">
-        <?php $this->renderPartial('partials/tab_personal_skills', ['simulation' => $simulation, 'learning_areas'=>$learning_areas]) ?>
+    <div class="footer">
+        <a class="prev" href="#prev"><?php echo Yii::t('site', 'Previous') ?></a>
+        <a class="next" href="#next"><?php echo Yii::t('site', 'Next') ?></a>
     </div>
 </div>
 
 <script>
+    function DetailsNavigator(container, options) {
+        $.extend(this.options, options || {});
+
+        this.$container = $(container);
+        this.$sections = this.$container.find(options.sectionSelector);
+        this.idList = $.map($.makeArray(this.$sections), function(section) {
+            return section.id
+        });
+
+        this.init();
+    }
+
+    $.extend(DetailsNavigator.prototype, {
+        sectionSelector: '.section',
+        prevSelector: '.prev',
+        nextSelector: '.next',
+
+        activeClass: 'active',
+        changeHash: true,
+
+        init: function() {
+            var me = this;
+
+            $(this.$container).find('a').on('click.Navigator', function(e) {
+                var id = $(this).attr('href').slice(1);
+
+                if ($(this).is(me.prevSelector)) {
+                    me.prev();
+                } else if ($(this).is(me.nextSelector)) {
+                    me.next();
+                } else {
+                    history.pushState({id: id}, '', me.changeHash ? this.href : null);
+                    me.open(id);
+                }
+
+                e.preventDefault();
+            });
+
+            $(window).on('popstate.Navigator', function(e) {
+                var state = e.originalEvent.state,
+                    id = state ? state.id : me.idList[0];
+
+                if (id) {
+                    me.open(id);
+                }
+            });
+
+            if (me.idList.length) {
+                me.open(me.idList[0]);
+            }
+        },
+
+        destroy: function() {
+            $(this.$container).find('a').off('.Navigator');
+            $(window).off('.Navigator');
+        },
+
+        open: function(id) {
+            if (this.idList.indexOf(id) > -1) {
+                this.$sections
+                    .removeClass(this.activeClass)
+                    .filter('#' + id)
+                    .addClass(this.activeClass);
+
+                this.current = id;
+            }
+        },
+
+        prev: function() {
+            var index = (this.idList.indexOf(this.current) || this.idList.length) - 1;
+            this.open(this.idList[index]);
+        },
+
+        next: function() {
+            var index = this.idList.indexOf(this.current) + 1;
+            if (index == this.idList.length) {
+                index = 0;
+            }
+            this.open(this.idList[index]);
+        }
+    });
+
     $(function() {
-        $( "#simulation-details-tabs" ).tabs();
+        new DetailsNavigator('.simulation-details', {
+            sectionSelector: '.sections > div'
+        });
     });
 </script>
 
