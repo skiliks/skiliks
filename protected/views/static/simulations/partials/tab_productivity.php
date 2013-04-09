e3d9f8c9c345c196ea80a191eea970c01f5f21ad
Malysh <svetlana@skiliks.com>
2013-04-09 21:00:55 +0100

    personal profile

--- protected/views/static/simulations/partials/tab_productivity.php
+++ protected/views/static/simulations/partials/tab_productivity.php
@@ -5,19 +5,30 @@
 
 
     <p class="barstitle resultlabeltitle">Уровень выполнения задач</p>
-    <div class="labels">
-        <div class="row"><h3 class="resulttitele">Срочно</h3><a href="#" class="questn">?</a></div>
-        <div class="row"><h3 class="resulttitele">Высокий приоритет</h3><a href="#" class="questn">?</a></div>
-        <div class="row"><h3 class="resulttitele">Средний приоритет</h3><a href="#" class="questn">?</a></div>
-        <div class="row"><h3 class="resulttitele">Прочее</h3><a href="#" class="questn">?</a></div>
+    <div class="clearfix">
+        <div class="labels">
+            <div class="row"><h3 class="resulttitele">Срочно</h3><a href="#" class="questn">?</a></div>
+            <div class="row"><h3 class="resulttitele">Высокий приоритет</h3><a href="#" class="questn">?</a></div>
+            <div class="row"><h3 class="resulttitele">Средний приоритет</h3><a href="#" class="questn">?</a></div>
+            <div class="row"><h3 class="resulttitele">Прочее</h3><a href="#" class="questn">?</a></div>
+        </div>
+
+        <div class="bars barswrap">
+
+        </div>
+    </div>
+    <div class="legendwrap resultslegend">
+        <div class="legend">
+            <p class="barstitle">Обозначения</p>
+            <div class="legendvalue"><span class="legendcolor colormax"></span><span class="legendtitle">Максимальный уровень выполнения задач</span></div>
+            <div class="legendvalue"><span class="legendcolor colordone"></span><span class="legendtitle">Проявленный уровень выполнения задач</span></div>
+        </div>
     </div>
 
-    <div class="bars barswrap">
-
-    </div>
-
-   <div class="legend"></div>
 </div>
+
+
+
 
 <script type="text/javascript">
     $(function() {
