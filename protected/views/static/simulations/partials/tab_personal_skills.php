76abf101bea6d9b5e9a26d0a37be5bda39f2daa9
Ivan Tugay <listepo@hotmail.com>
2013-04-09 11:34:28 +0300

    SKILIKS-2018 #resolved

--- protected/views/static/simulations/partials/tab_personal_skills.php
+++ protected/views/static/simulations/partials/tab_personal_skills.php
@@ -25,10 +25,23 @@
 
 <div class="page nice-border backgroud-light-blue">
     <h2>Личностные характеристики</h2>
-</div>
-
-<script type="text/javascript">
+    <div>Ориентация на результат<div class="resultOrientation"></div></div>
+    <div>Внимательность<div class="attentiveness"></div></div>
+    <div>Ответственность<div class="responsibility"></div></div>
+    <div>Устойчивость к манипуляциям и давлению<div class="stability"></div></div>
+    <div>Конструктивность<div class="constructibility"></div></div>
+    <div>Гибкость<div class="flexibility"></div></div>
+    <div>Принятие решений<div class="adoptionOfDecisions"></div></div>
+    <div>Стрессоустойчивость<div class="stressResistance"></div></div>
+</div><script type="text/javascript">
     $(function() {
-        new charts.Bullet('.page', 70, {class: 'small', displayValue: true});
+        new charts.Bullet('.resultOrientation', 10, {class: 'small', displayValue: true});
+        new charts.Bullet('.attentiveness', 20, {class: 'small', displayValue: true});
+        new charts.Bullet('.responsibility', 30, {class: 'small', displayValue: true});
+        new charts.Bullet('.stability', 40, {class: 'small', displayValue: true});
+        new charts.Bullet('.constructibility', 50, {class: 'small', displayValue: true});
+        new charts.Bullet('.flexibility', 60, {class: 'small', displayValue: true});
+        new charts.Bullet('.adoptionOfDecisions', 70, {class: 'small', displayValue: true});
+        new charts.Bullet('.stressResistance', 80, {class: 'small', displayValue: true});
     });
 </script>
