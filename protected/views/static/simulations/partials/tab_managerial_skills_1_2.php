fb436c069ad33be14bc8c605a3fbf4e70b06a0bb
Malysh <svetlana@skiliks.com>
2013-04-10 21:37:20 +0100

    personal profile

--- protected/views/static/simulations/partials/tab_managerial_skills_1_2.php
+++ protected/views/static/simulations/partials/tab_managerial_skills_1_2.php
@@ -1,1 +1,73 @@
-_tab_managerial_skills_1_2
+<div class="extrasidepads">
+    <div class="textcener"><h2 class="total">Управленческие навыки</h2></div>
+    <h3 class="resulttitele"><span class="valuetitle">60%</span>1. Следование приоритетам</h3>
+
+    <div class="twobarstitles resultlabeltitle">
+        <span class="barstitle">Уровень владения навыком</span>
+        <span class="barstitle">Уровень проблем</span>
+    </div>
+
+    <div class="clearfix mangrlresults">
+        <div class="labels">
+            <p class="labelwrap"><span class="thelabel">1.1 Следование целям подразделения</span></p>
+            <p class="labelwrap"><span class="thelabel"><a href="#">1.2 Следование личным приоритетам</a></span></p>
+        </div>
+        <div class="barswrap">
+            <div class="twocharts">
+                <div class="chartbar"><span class="chart-value" style="width: 20%">&nbsp; 20%</span></div>
+                <div class="chartbar chartproblem"><span class="chart-value" style="width: 0%"></span></div>
+            </div>
+            <div class="twocharts">
+                <div class="chartbar"><span class="chart-value" style="width: 50%">&nbsp; 50%</span></div>
+                <div class="chartbar chartproblem"><span class="chart-value" style="width: 0%"></span></div>
+            </div>
+        </div>
+    </div>
+
+    <h3 class="resulttitele"><span class="valuetitle">45%</span>2. Управление задачами</h3>
+
+    <div class="twobarstitles resultlabeltitle">
+        <span class="barstitle">Уровень владения навыком</span>
+        <span class="barstitle">Уровень проблем</span>
+    </div>
+
+    <div class="clearfix mangrlresults">
+        <div class="labels">
+            <p class="labelwrap"><span class="thelabel">2.1 Использование планирования в течение дня</span></p>
+            <p class="labelwrap"><span class="thelabel">2.2 Правильное определение приоритетов задач при планировании</span></p>
+            <p class="labelwrap"><span class="thelabel">2.3 Выполнение задач в соответствии с приоритетами</span></p>
+            <p class="labelwrap"><span class="thelabel">2.4 Завершение начатых задач <span class="helpbuble">Описание навыка</span></span></p>
+        </div>
+        <div class="barswrap">
+            <div class="twocharts">
+                <div class="chartbar"><span class="chart-value" style="width: 20%">&nbsp; 20%</span></div>
+                <div class="chartbar chartproblem"><span class="chart-value" style="width: 0%"></span></div>
+            </div>
+            <div class="twocharts">
+                <div class="chartbar"><span class="chart-value" style="width: 50%">&nbsp; 50%</span></div>
+                <div class="chartbar chartproblem">
+                    <span class="helpbuble">Перечень продемонстрированных ошибочных поведений</span>
+                    <span class="chart-value" style="width: 40%">40%</span>
+                </div>
+            </div>
+            <div class="twocharts">
+                <div class="chartbar"><span class="chart-value" style="width: 60%">&nbsp; 60%</span></div>
+                <div class="chartbar chartproblem"><span class="chart-value" style="width: 70%">&nbsp; 70%</span></div>
+            </div>
+            <div class="twocharts">
+                <div class="chartbar"><span class="chart-value" style="width: 50%">&nbsp; 50%</span></div>
+                <div class="chartbar chartproblem"><span class="chart-value" style="width: 40%">&nbsp; 40%</span></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="legendwrap legendmargin">
+        <div class="legend">
+            <p class="barstitle">Обозначения</p>
+            <div class="legendvalue"><span class="legendcolor colormax"></span><span class="legendtitle">Максимальный уровень владения навыком</span></div>
+            <div class="legendvalue"><span class="legendcolor colordone"></span><span class="legendtitle">Проявленный уровень владения навыком</span></div>
+            <div class="legendvalue"><span class="legendcolor colorwarn"></span><span class="legendtitle">Уровень продемонстрированного ошибочного поведения</span></div>
+        </div>
+    </div>
+
+</div>
