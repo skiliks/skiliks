0bd49ef55cb0dfead0cbf873fd66dbf6482b393d
Cherous <r.kilimov@gmail.com>
2013-04-10 19:54:08 +0300

    commit

--- protected/views/static/userAuth/chooseAccountType.php
+++ protected/views/static/userAuth/chooseAccountType.php
@@ -1,31 +1,22 @@
 <section class="registration">
 	<h2><?php echo Yii::t('site', 'Sign-up using your preferred option and get the results') ?></h2>
-	
 	<div class="form form-account-personal">
-	
 	    <h1><?php echo Yii::t('site', 'Personal account') ?></h1>
-	    
 	    <ul>
 			<li><?php echo Yii::t('site', 'Free full assessment of skills') ?></li>
 			<li><?php echo Yii::t('site', 'Skills comparison with others') ?>*</li>
 			<li><?php echo Yii::t('site', 'Updates for free') ?></li>
 		</ul>
-	
 	    <?php $form = $this->beginWidget('CActiveForm', array(
 	        'id'                   => 'user-account-personal-form',
 	        'enableAjaxValidation' => false,
 	    )); ?>
-	
-	    <?php // echo $form->errorSummary($accountPersonal); ?>
-	
 	    <div class="row">
 	        <?php echo $form->hiddenField($accountPersonal, 'user_id'); ?>
 	        <?php echo $form->error($accountPersonal      , 'user_id'); ?>
-	
 	        <?php echo $form->hiddenField($profile, 'email'); ?>
 	        <?php echo $form->error($profile      , 'email'); ?>
 	    </div>
-	
 	    <div class="row">
 	        <?php echo $form->labelEx($profile  , 'Name'); ?>
 	        <?php echo $form->textField($profile, 'firstname',['placeholder' => Yii::t('site', 'First name')]); ?>
@@ -37,55 +28,40 @@
 	            <?php echo $form->error($profile    , 'lastname'); ?>
 	        <?php endif; ?>
 	    </div>
-	
 	    <div class="row">
 	        <?php echo $form->labelEx($accountPersonal     ,'industry_id'); ?>
 	        <?php echo $form->dropDownList($accountPersonal,'industry_id', $industries); ?>
 	        <?php echo $form->error($accountPersonal       ,'industry_id'); ?>
 	    </div>
-	
 	    <div class="row wide">
 	        <?php echo $form->labelEx($accountPersonal     ,'professional_status_id'); ?>
 	        <?php echo $form->dropDownList($accountPersonal,'professional_status_id', $statuses); ?>
 	        <?php echo $form->error($accountPersonal       ,'professional_status_id'); ?>
 	    </div>
-	
 	    <div class="row buttons">
 	        <?php echo CHtml::submitButton(Yii::t('site', 'Start and get the report'), ['name' => 'personal']); ?>
 	    </div>
-	
 	    <?php $this->endWidget(); ?>
-	
 	</div>
-	
 	<!-- --------------------------------------------------------------------------------------------------------- -->
-	
 	<div class="form form-account-corporate">
-	
 	    <h1><?php echo Yii::t('site', 'Corporate account') ?></h1>
-	    
 	    <ul>
 			<li><?php echo Yii::t('site', 'Package of simulations to assess others') ?></li>
 			<li><?php echo Yii::t('site', 'Simple but powerful tool for assessment process') ?></li>
 			<li><?php echo Yii::t('site', 'Comprehensive statistics on people and skills') ?>*</li>
 			<li><?php echo Yii::t('site', 'Updates for free') ?></li>
 		</ul>
-	
 	    <?php $form = $this->beginWidget('CActiveForm', array(
 	        'id'                   => 'user-account-corporate-form',
 	        'enableAjaxValidation' => false,
 	    )); ?>
-	
-	    <?php // echo $form->errorSummary($accountCorporate); ?>
-	
 	    <div class="row">
 	        <?php echo $form->hiddenField($accountCorporate,'user_id'); ?>
 	        <?php echo $form->error($accountCorporate,'user_id'); ?>
-	
 	        <?php echo $form->hiddenField($profile,'email'); ?>
 	        <?php echo $form->error($profile      , 'email'); ?>
 	    </div>
-	
 	    <div class="row">
 	        <?php echo $form->labelEx($profile  , 'Name'); ?>
 	        <?php echo $form->textField($profile, 'firstname',['placeholder' => Yii::t('site', 'First name')]); ?>
@@ -104,7 +80,6 @@
 	        <?php echo $form->dropDownList($accountCorporate, 'industry_id', $industries); ?>
 	        <?php echo $form->error($accountCorporate       , 'industry_id'); ?>
 	    </div>
-	
 	    <div class="row wide">
             <?php if (UserService::isCorporateEmail($profile->email)): ?>
                 <?php echo $form->hiddenField($accountCorporate, 'corporate_email'); ?>
@@ -114,19 +89,13 @@
                 <?php echo $form->error($accountCorporate    , 'corporate_email'); ?>
             <?php endif ?>
 	    </div>
-	
 	    <div class="row buttons">
 	        <?php echo CHtml::submitButton(Yii::t('site', 'Start and get the report'), ['name' => 'corporate']); ?>
 	    </div>
-	
 	    <?php $this->endWidget(); ?>
-	
 	</div>
-	
 	<!-- --------------------------------------------------------------------------------------------------------- -->
-	
 	<div style="clear:both;"></div>
-	
 	<p class="note" style="float: none; clear: both;">
 	    * <?php echo Yii::t('site', 'Currently not available and will be added in the next release') ?>
 	</p>
@@ -144,7 +113,7 @@ function setModalSize() {
 	var w=$('body').width();
 	var cw=$('.content').width()/2;
 	var l=w/2-cw;
-	
+
 	$('.register-by-link-wrap').each(function(){
 		$(this).css({
 			'width':w,
