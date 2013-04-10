1ae7b93aae4a758a9c10f33993af256134cb7341
Malysh <svetlana@skiliks.com>
2013-04-10 12:58:48 +0100

    personal profile

--- protected/views/static/simulations/partials/tab_time_management.php
+++ protected/views/static/simulations/partials/tab_time_management.php
@@ -1,1 +1,12 @@
-time managament
+<div class="textcener"><h2 class="total totalwithresult">Эффективность использования времени <span class="value blockvalue">0%</span></h2></div>
+<div class="timeblock niceborder">
+
+    <div class="legendwrap">
+        <div class="legend">
+            <p class="barstitle">Обозначения</p>
+            <div class="legendvalue"><span class="legendcolor colormax"></span><span class="legendtitle">Действия, относящиеся к задачам 1 приоритета</span></div>
+            <div class="legendvalue"><span class="legendcolor colorwarn"></span><span class="legendtitle">Действия, не относящиеся к задачам 1 приоритета</span></div>
+            <div class="legendvalue"><span class="legendcolor colordone"></span><span class="legendtitle">Время ожидания</span></div>
+        </div>
+    </div>
+</div>
