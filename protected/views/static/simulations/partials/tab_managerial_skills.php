4127ab02fd724d374ee4967c44765d1a1872c3c0
Malysh <svetlana@skiliks.com>
2013-04-10 17:18:01 +0100

    personal profile

--- protected/views/static/simulations/partials/tab_managerial_skills.php
+++ protected/views/static/simulations/partials/tab_managerial_skills.php
@@ -1,1 +1,49 @@
 _tab_managerial_skills
+<div class="textcener"><h2 class="total totalwithresult">Управленческие навыки <span class="value blockvalue">0%</span></h2></div>
+<div class="clearfix">
+    <div class="labeltitles">
+        <h3>По группам навыков</h3>
+        <h4>Критичные навыки</h4>
+    </div>
+    <p class="barstitle mangrlresultstitle">Уровень владения навыком</p>
+</div>
+
+<div class="clearfix mangrlresults">
+    <div class="labels">
+        <p><a href="#">1. Следование приоритетам <span class="signmore"></span></a></p>
+        <p><a href="#">2. Управление задачами <span class="signmore"></span></a></p>
+        <p><a href="#">3. Управление людьми <span class="signmore"></span></a></p>
+        <p><a href="#">4. Оптимальный выбор каналов коммуникации<span class="signmore"></span></a></p>
+    </div>
+    <div class="barswrap">
+        <div class="chartbar" style="width: 100%;"><span class="chart-value" style="width: 20%">&nbsp; 20%</span></div>
+        <div class="chartbar" style="width: 100%;"><span class="chart-value" style="width: 50%">&nbsp; 50%</span></div>
+        <div class="chartbar" style="width: 100%;"><span class="chart-value" style="width: 60%">&nbsp; 60%</span></div>
+        <div class="chartbar" style="width: 100%;"><span class="chart-value" style="width: 40%">&nbsp; 40%</span></div>
+    </div>
+</div>
+<div class="clearfix">
+    <div class="labeltitles">
+        <h4>Другие важные навыки</h4>
+    </div>
+</div>
+<div class="clearfix mangrlresults">
+    <div class="labels">
+        <p><a href="#">5. Эффективная работа с почтой <span class="signmore"></span></a></p>
+        <p><a href="#">6. Эффективное управление звонками <span class="signmore"></span></a></p>
+        <p><a href="#">7. Эффективное управление встречами <span class="signmore"></span></a></p>
+    </div>
+    <div class="barswrap">
+        <div class="chartbar" style="width: 100%;"><span class="chart-value" style="width: 20%">&nbsp; 20%</span></div>
+        <div class="chartbar" style="width: 100%;"><span class="chart-value" style="width: 50%">&nbsp; 50%</span></div>
+        <div class="chartbar" style="width: 100%;"><span class="chart-value" style="width: 60%">&nbsp; 60%</span></div>
+    </div>
+</div>
+
+<div class="legendwrap resultslegend">
+    <div class="legend">
+        <p class="barstitle">Обозначения</p>
+        <div class="legendvalue"><span class="legendcolor colormax"></span><span class="legendtitle">Максимальный уровень владения навыком</span></div>
+        <div class="legendvalue"><span class="legendcolor colordone"></span><span class="legendtitle">Проявленный уровень владения навыком</span></div>
+    </div>
+</div>
--- protected/views/static/simulations/partials/tab_time_management.php
+++ protected/views/static/simulations/partials/tab_time_management.php
@@ -1,12 +1,34 @@
 <div class="textcener"><h2 class="total totalwithresult">Эффективность использования времени <span class="value blockvalue">0%</span></h2></div>
-<div class="timeblock niceborder">
-
-    <div class="legendwrap">
-        <div class="legend">
-            <p class="barstitle">Обозначения</p>
-            <div class="legendvalue"><span class="legendcolor colormax"></span><span class="legendtitle">Действия, относящиеся к задачам 1 приоритета</span></div>
-            <div class="legendvalue"><span class="legendcolor colorwarn"></span><span class="legendtitle">Действия, не относящиеся к задачам 1 приоритета</span></div>
-            <div class="legendvalue"><span class="legendcolor colordone"></span><span class="legendtitle">Время ожидания</span></div>
+<div class="extrasidepads">
+    <div class="timeblock">
+        <h2 class="resulttitele"><a href="#">Распределение времени <span class="signmore"></span></a></h2>
+        <div class="timewrap">
+            <div class="tziferblat">
+                <div class="timepart timecritical"></div>
+                <div class="timepart timeinactv"></div>
+                <div class="timepart timeactv"></div>
+                <div class="timevalue criticalvalue">60%</div>
+                <div class="timevalue inactvalue">10%</div>
+                <div class="timevalue actvalue">30%</div>
+                <div class="timeimage"></div>
+            </div><!-- /tziferblat -->
+            <div class="helpbuble criticalbuble">Время, потраченное на не приоритетные задачи, ошибочные действия, отвлечения</div>
+            <div class="helpbuble actvbuble">Продуктивное время. Время, потраченное на задачи первого приоритета</div>
+            <div class="helpbuble inactvbuble">Бездействие</div>
         </div>
+
+        <div class="legendwrap">
+            <div class="legend">
+                <p class="barstitle">Обозначения</p>
+                <div class="legendvalue"><span class="legendcolor colormax"></span><span class="legendtitle">Действия, относящиеся к задачам 1 приоритета</span></div>
+                <div class="legendvalue"><span class="legendcolor colorwarn"></span><span class="legendtitle">Действия, не относящиеся к задачам 1 приоритета</span></div>
+                <div class="legendvalue"><span class="legendcolor colordone"></span><span class="legendtitle">Время ожидания</span></div>
+            </div>
+        </div>
+    </div>
+    <div class="blockseprt"></div>
+    <div class="timeblock">
+        <h2 class="resulttitele">Сверхурочное время</h2>
+        <div class="extrahourswrap"><div class="extrahours">45</div></div>
     </div>
 </div>
