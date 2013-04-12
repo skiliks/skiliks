1bf5cf7ce3e9703058782dd327361add900c3b8d
Ivan Tugay <listepo@hotmail.com>
2013-04-12 11:36:27 +0300

    Fix

--- protected/assets/js/game/views/SKIntroView.js
+++ protected/assets/js/game/views/SKIntroView.js
@@ -34,7 +34,7 @@ define([
                 //console.log("window.height: " +window.outerHeight);
                 //console.log("x: " + e.pageX);
             });
-
+            $.cookie('intro_is_watched', 'yes');
         },
         eventHandler: function() {
             window.SKApp = new SKApplication(window.gameConfig);
