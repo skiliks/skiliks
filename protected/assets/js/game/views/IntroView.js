70dbf60c6944f15cefa0d0a65d9dd503d4e5c7da
Ivan Tugay <listepo@ya.ru>
2013-04-12 04:18:54 +0300

    SKILIKS-2003 #resolved SKILIKS-2007 #resolved

--- protected/assets/js/game/views/IntroView.js
+++ protected/assets/js/game/views/IntroView.js
@@ -14,16 +14,17 @@ define([
     IntroView = Backbone.View.extend({
         initialize:function () {
 
-            $('body').html('<video id="skiliks_intro" src="http://storage.skiliks.com/v1/videos/skiliks_intro_1280.webm" controls="controls" autoplay=""></video>');
+            $('body').html('<video id="skiliks_intro" src="http://storage.skiliks.com/v1/videos/skiliks_intro_1280.webm" autoplay="autoplay"></video>');
             $('body').append('<div class="intro-top-icons">Пропустить видео <button class="pass-video"/></div>');
             $('#skiliks_intro').bind('ended', function () {
+                $(window).unbind("mousemove");
                 window.IntroView.trigger('simulationStart');
             });
             $('.pass-video').bind('click', function () {
                 $('#skiliks_intro').trigger('ended');
             });
             $(window).mousemove( function(e) {
-                console.log("window.height / 3 >= e.pageX : "+parseInt(window.outerHeight) / 3);
+                //console.log("window.height / 3 >= e.pageX : "+parseInt(window.outerHeight) / 3);
                 if(window.outerHeight / 3 >= e.pageY){
                     $('.intro-top-icons').css('display', 'block');
                 }else{
