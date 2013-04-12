7b3d042431c7d4a78b70f61818a2f3bbbb266a10
Ivan Tugay <listepo@hotmail.com>
2013-04-12 13:23:55 +0300

    Fix

--- protected/views/global_partials/_simulation_stars.php
+++ protected/views/global_partials/_simulation_stars.php
@@ -4,14 +4,15 @@
     if (false == isset($isDisplayScaleIfSimulationNull)) { $isDisplayScaleIfSimulationNull = true; }
 
     if (false == $isDisplayScaleIfSimulationNull && null == $simulation) { return; };
+/* @var $simulation Simulation */
 ?><p><?php
     if($isDisplayTitle):
         ?><span class="skillstitle">Базовый менеджмент</span><?php
     endif
     ?><span <?php if (null!==$simulation) { echo 'data-simulation="/simulations/details/'.$simulation->id.'"';} ?>
-        class="ratingwrap radiusthree <?php if(null!==$simulation) { echo "view-simulation-details-pop-up";} ?>">
-        <span class="ratebg"><span class="rating" style="width: <?php if(null!==$simulation){ echo $simulation->getCategoryAssessment(); }else{ echo "0"; } ?>%"></span></span><sup><?php
-            if (null !== $simulation ):
+        class="ratingwrap radiusthree <?php if(null!==$simulation AND $simulation->end !== null) { echo "view-simulation-details-pop-up";} ?>">
+        <span class="ratebg"><span class="rating" style="width: <?php if(null!==$simulation AND $simulation->end !== null) { echo $simulation->getCategoryAssessment(); }else{ echo "0"; } ?>%"></span></span><sup><?php
+            if (null !== $simulation AND $simulation->end !== null):
                 ?><?= (float)$simulation->getCategoryAssessment() ?>%<?php
             else:
                 ?>0%<?php
