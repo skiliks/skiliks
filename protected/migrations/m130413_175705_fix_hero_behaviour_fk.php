ef134c7c512065885956c0f474646a71c93a8f16
slavkaa <nikoolin@ukr.net>
2013-04-13 21:06:47 +0300

    SKILIKS-2160. Fix import.

--- protected/migrations/m130413_175705_fix_hero_behaviour_fk.php
+++ protected/migrations/m130413_175705_fix_hero_behaviour_fk.php
@@ -4,17 +4,17 @@ class m130413_175705_fix_hero_behaviour_fk extends CDbMigration
 {
 	public function up()
 	{
-//        $this->dropForeignKey('hero_behaviour_learning_goal', 'hero_behaviour');
-//
-//        $this->addForeignKey(
-//            'hero_behaviour_learning_goal',
-//            'hero_behaviour',
-//            'learning_goal_id',
-//            'learning_goal',
-//            'id',
-//            'CASCADE',
-//            'CASCADE'
-//        );
+        $this->dropForeignKey('hero_behaviour_learning_goal', 'hero_behaviour');
+
+        $this->addForeignKey(
+            'hero_behaviour_learning_goal',
+            'hero_behaviour',
+            'learning_goal_id',
+            'learning_goal',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
 	}
 
 	public function down()
