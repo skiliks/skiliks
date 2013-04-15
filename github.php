523245e3db336db28f1813c6d67d50ed936f0b2e
Andrii Kostenko <andrey@kostenko.name>
2013-04-15 17:29:18 +0300

    removed flash message

--- github.php
+++ github.php
@@ -2,4 +2,4 @@
 if ($_GET['paranoia'] !== 'erb-yan-oj-al-c')
     die();
 
-exec("sudo -u skiliks /usr/local/bin/git-update-backend &");
+exec("sudo -u skiliks nohup /usr/local/bin/git-update-backend");
