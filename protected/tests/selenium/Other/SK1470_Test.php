57c3970fb15c0ddc3a632728b27e3b9d4c1ec16b
Vadim Logunov <vadim.logunov@skiliks.com>
2013-04-17 01:20:28 +0300

    seleniumsht

--- protected/tests/selenium/Other/SK1470_Test.php
+++ protected/tests/selenium/Other/SK1470_Test.php
@@ -20,11 +20,9 @@ class SK1470_Test extends SeleniumTestHelper
 
     public function testSK1470_Case1()
     {
-        //$this->markTestIncomplete();
         $this->start_simulation();
         $this->run_event('ET2.4',"css=li.icon-active.phone a",'click');
-        //$this->optimal_click("css=li.icon-active.phone a");
-        $this->assertTrue($this->isVisible(Yii::app()->params['test_mappings']['phone']['reply']));
+        $this->waitForVisible(Yii::app()->params['test_mappings']['phone']['reply']);
         $this->assertFalse($this->isElementPresent(Yii::app()->params['test_mappings']['phone']['no_reply']));
         $this->optimal_click(Yii::app()->params['test_mappings']['phone']['reply']);
         $this->optimal_click("xpath=(//*[contains(text(),'Конечно, Валерий Семенович! Буду у Вас в 16.00 с готовой презентаций')])");
@@ -32,10 +30,9 @@ class SK1470_Test extends SeleniumTestHelper
 
     public function testSK1470_Case2()
     {
-        //$this->markTestIncomplete();
         $this->start_simulation();
         $this->run_event('ET2.4',Yii::app()->params['test_mappings']['phone']['reply'],'-');
-        //$this->waitForVisible(Yii::app()->params['test_mappings']['phone']['reply']);
+        $this->waitForVisible(Yii::app()->params['test_mappings']['phone']['reply']);
         $this->assertFalse($this->isElementPresent(Yii::app()->params['test_mappings']['phone']['no_reply']));
         $this->optimal_click("xpath=(//*[contains(text(),'Конечно, Валерий Семенович! Буду у Вас в 16.00 с готовой презентаций')])");
     }
