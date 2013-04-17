2214beec6f889a1480d8321c98ff1311e47fdcfd
slavkaa <nikoolin@ukr.net>
2013-04-17 16:56:33 +0300

    SKILIKS-2229. Fix activation link handle for currently logged in user.

--- protected/messages/ru/site.php
+++ protected/messages/ru/site.php
@@ -319,5 +319,6 @@ return array(
     'min' => 'мин.',
     'Share' => 'Рекомендовать',
 
-    'Available simulation' => 'Доступные симуляции'
+    'Available simulation' => 'Доступные симуляции',
+    'You are already logged in (%s), please log out to activate your account %s' => 'В данный момент вы уже вошли в систему как %s. <br/><br/> Выйдите, чтобы активировать новый аккаунт %s.'
 );
