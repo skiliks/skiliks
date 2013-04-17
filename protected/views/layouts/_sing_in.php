75933c11ce76a5d8d4932387d5b333619d413ce9
Cherous <r.kilimov@gmail.com>
2013-04-17 20:39:28 +0300

    1957

--- protected/views/layouts/_sing_in.php
+++ protected/views/layouts/_sing_in.php
@@ -107,3 +107,13 @@
 
     });
 </script>
+<script>
+    $(document).ready(function(){
+        var errors = $(".errorMessage");
+        for (var i=0;i<errors.length;i++) {
+            var inp = $(errors[i]).prev("input.error");
+            $(inp).css({"border":"2px solid #bd2929"});
+            $(errors[i]).addClass($(inp).attr("id"));
+        }
+    });
+</script>
