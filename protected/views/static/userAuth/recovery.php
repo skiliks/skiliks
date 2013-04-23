40c2376155f315e37e5b2d53e0937c6452e7e147
Cherous <r.kilimov@gmail.com>
2013-04-23 11:13:18 +0300

    2153

--- protected/views/static/userAuth/recovery.php
+++ protected/views/static/userAuth/recovery.php
@@ -19,3 +19,14 @@
 
     <?php $this->endWidget(); ?>
 </div>
+
+<script>
+    $(document).ready(function(){
+        var errors = $(".errorMessage");
+        for (var i=0; i < errors.length;i++) {
+            var inp = $(errors[i]).prev("input.error");
+            $(inp).css({"border":"2px solid #bd2929"});
+            $(errors[i]).addClass($(inp).attr("id"));
+        }
+    });
+</script>
