a6ae5ab98fcb89d019690afd1e60f0d6c9c204ff
Andrii Kostenko <andrey@kostenko.name>
2013-04-24 19:37:43 +0300

    logwindow command

---
+++ protected/commands/LogWindowCommand.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: gugu
+ * Date: 24.04.13
+ * Time: 19:25
+ * To change this template use File | Settings | File Templates.
+ */
+
+class LogWindowCommand extends CConsoleCommand {
+    public function actionIndex($sim_id){
+        /** @var Simulation $simulation */
+        $simulation = Simulation::model()->findByPk($sim_id);
+        foreach ($simulation->log_windows as $log) {
+            echo '$lw = new LogWindow();' . "\n";
+            echo '$lw->sim_id = ' . $log->primaryKey . ';'. "\n";
+            echo '$lw->window = ' . $log->window . ';'. "\n";
+            echo '$lw->start_time = ' . $log->start_time . ';'. "\n";
+            echo '$lw->end_time = ' . $log->end_time . ';'. "\n";
+            echo '$lw->window_uid = ' . $log->window_uid . ';'. "\n";
+            echo '$lw->save()' . "\n";
+        }
+    }
+}
