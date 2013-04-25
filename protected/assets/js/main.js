bc13a9f5195274f27879e4ffa02c3b76c7d8d441
Ivan Tugay <listepo@hotmail.com>
2013-04-25 18:56:38 +0300

    SKILIKS-2343 #resolved Fix

--- protected/assets/js/main.js
+++ protected/assets/js/main.js
@@ -169,7 +169,7 @@ Cufon.replace('.invite-people-form input[type="submit"], .brightblock, .lightblo
     '#password-recovery-form input[type="submit"], #simulation-details-pop-up h1, .estmtileswrap h2, .estmtileswrap h2 a, .product .estmtileswrap h2, .simulation-result-popup h3,' +
     '.levellabels h3, .resulttitele, .resulttitele a, .barstitle, .total, .labeltitles h3, .labeltitles h4, .valuetitle, .resulttitele  small, .timedetail .thelabel,' +
     '.feedback #input_2, .profileform input[type="submit"], .pager ul.yiiPager .next a, .pager ul.yiiPager .previous a, .product .ratepercnt, .light-btn' +
-    '.value, .tarifform .value',
+    '.value, .tarifform .value, .light-btn',
     {hover: true}
 );
 Cufon.replace('.main-article article ul li, .container>header nav a, .features ul li, .sbHolder a, #simulation-details label, .container>header nav a, .features .error span, ' +
