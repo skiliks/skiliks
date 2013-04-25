40489f01817d70e9dbfa4e14a9c094e83386e6ce
Andrii Kostenko <andrey@kostenko.name>
2013-04-25 14:54:43 +0300

    Revert "selenium, check my code!"
    This reverts commit 5bb80e08ae1fd6eb7441c2506611f223e2a72931.

--- protected/assets/js/game/models/SKMailClient.js
+++ protected/assets/js/game/models/SKMailClient.js
@@ -215,7 +215,7 @@ define(["game/models/SKMailFolder", "game/models/SKMailSubject","game/models/SKC
 
                 this.folders[this.aliasFolderTrash] = new SKMailFolder();
                 this.folders[this.aliasFolderTrash].alias = this.aliasFolderTrash;
-                this.emailUIDs = {};
+
                 // init folder names
                 this.getInboxFolder().name  = 'Входящие';
                 this.getDraftsFolder().name = 'Черновики';
@@ -709,21 +709,19 @@ define(["game/models/SKMailFolder", "game/models/SKMailSubject","game/models/SKC
              */
             setWindowsLog:function (newSubscreen, emailId) {
                 var window = this.getSimulationMailClientWindow();
-                var oldMailId = window.get('params') && window.get('params').mailId;
                 window.setOnTop();
-                if (oldMailId && !this.emailUIDs[oldMailId]) {
-                        this.emailUIDs[oldMailId] = window.window_uid;
-                }
                 SKApp.simulation.windowLog.deactivate(window);
-                if (emailId) {
-                    if (this.emailUIDs[emailId]) {
-                        window.window_uid = this.emailUIDs[emailId];
-                    } else {
-                        window.updateUid();
-                        this.emailUIDs[emailId] = window.window_uid;
-                    }
-                } else {
+
+                if ((window.get('subname') === 'mailMain' && 'mailNew' === newSubscreen) ||
+                    (window.get('subname') === 'mailMain' && 'mailPlan' === newSubscreen)) {
+                    this.window_uid = parseInt(window.window_uid, 10);
                     window.updateUid();
+                } else if ((window.get('subname') === 'mailNew' && 'mailMain' === newSubscreen) ||
+                    (window.get('subname') === 'mailPlan' && 'mailMain' === newSubscreen)) {
+                    if (this.window_uid === undefined) {
+                        throw 'Window UID is undefined';
+                    }
+                    window.window_uid = parseInt(this.window_uid, 10);
                 }
 
                 window.set('id', newSubscreen);
--- protected/commands/LogWindowCommand.php
+++ protected/commands/LogWindowCommand.php
@@ -11,43 +11,14 @@ class LogWindowCommand extends CConsoleCommand {
     public function actionIndex($sim_id){
         /** @var Simulation $simulation */
         $simulation = Simulation::model()->findByPk($sim_id);
-        $data = [];
         foreach ($simulation->log_windows as $log) {
-            if (!isset($data[$log->start_time])) {
-                $data[$log->start_time] = '';
-            }
-            $data[$log->start_time] .=
-                '$lw = new LogWindow();' . "\n" .
-                '$lw->sim_id = $sim_id;'. "\n" .
-                '$lw->window = "' . $log->window . '";'. "\n" .
-                '$lw->start_time = "' . $log->start_time . '";'. "\n" .
-                '$lw->window_uid = "' . $log->window_uid . '";'. "\n" .
-                '$lw->save();' . "\n";
-            $data[$log->end_time] =
-                '$lw = LogWindow::model()->findByAttributes(["end_time" => "00:00:00", "sim_id" => $sim_id, "window_uid" => ' . $log->window_uid . ']);' . "\n" .
-                '$lw->end_time = "' . $log->end_time . '";'. "\n" .
-                '$lw->save();' . "\n";
-        }
-        foreach ($simulation->log_mail as $log) {
-            if (!isset($data[$log->start_time])) {
-                $data[$log->start_time] = '';
-            }
-            $data[$log->start_time] .=
-                '$lw = new LogMail();' . "\n" .
-                    '$lw->sim_id = $sim_id;'. "\n" .
-                    '$lw->window = "' . $log->window . '";'. "\n" .
-                    '$lw->start_time = "' . $log->start_time . '";'. "\n" .
-                    '$lw->window_uid = "' . $log->window_uid . '";'. "\n" .
-                    '$lw->mail_id = "' . $log->mail_id . '";'. "\n" .
-                    '$lw->save();' . "\n";
-            echo $log->getPrimaryKey() . "\n";
-            $data[$log->end_time] .=
-                '$lw = LogMail::model()->findByAttributes(["end_time" => "00:00:00", "sim_id" => $sim_id, "window_uid" => ' . $log->window_uid . ']);' . "\n" .
-                    '$lw->end_time = "' . $log->end_time . '";'. "\n" .
-                    '$lw->save();' . "\n";
-        }
-        foreach ($data as $time => $v) {
-            echo $v;
+            echo '$lw = new LogWindow();' . "\n";
+            echo '$lw->sim_id = $sim_id;'. "\n";
+            echo '$lw->window = "' . $log->window . '";'. "\n";
+            echo '$lw->start_time = "' . $log->start_time . '";'. "\n";
+            echo '$lw->end_time = "' . $log->end_time . '";'. "\n";
+            echo '$lw->window_uid = "' . $log->window_uid . '";'. "\n";
+            echo '$lw->save();' . "\n";
         }
     }
 }
