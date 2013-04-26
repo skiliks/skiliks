8d10c04782c610bfa2cfb522e703979fec3cd426
slavkaa <nikoolin@ukr.net>
2013-04-26 15:41:21 +0300

    Test 1-2-3 level assesments.

--- protected/tests/unit/Assessment_Goals_Areas_Overals.php
+++ protected/tests/unit/Assessment_Goals_Areas_Overals.php
@@ -12,29 +12,176 @@ class Assessment_Goals_Areas_Overals extends CDbTestCase
 
     public function testAssessment_Goals_Areas_Overals_case1()
     {
-        /*$user = YumUser::model()->findByAttributes(['username' => 'asd']);
-        $invite = new Invite();
-        $invite->scenario = new Scenario();
-        $invite->receiverUser = $user;
-        $invite->scenario->slug = Scenario::TYPE_FULL;
-        $simulation = SimulationService::simulationStart($invite, Simulation::MODE_PROMO_LABEL);
+        $user = YumUser::model()->findByAttributes(['username' => 'asd']);
+//        $invite = new Invite();
+//        $invite->scenario = new Scenario();
+//        $invite->receiverUser = $user;
+//        $invite->scenario->slug = Scenario::TYPE_FULL;
+//        $simulation = SimulationService::simulationStart($invite, Simulation::MODE_PROMO_LABEL);
+        $simulation = Simulation::model()->findByPk(1501);
 
-        SimulationService::simulationStop($simulation);
+        AssessmentAggregated::model()->deleteAllByAttributes(['sim_id' => $simulation->id]);
+        AssessmentOverall::model()->deleteAllByAttributes(['sim_id' => $simulation->id]);
+        StressPoint::model()->deleteAllByAttributes(['sim_id' => $simulation->id]);
 
         $this->addAssessmentAggregated($simulation, '214d0');
         $this->addAssessmentAggregated($simulation, '214d1');
         $this->addAssessmentAggregated($simulation, '214d2');
         $this->addAssessmentAggregated($simulation, '214d3');
+        $this->addAssessmentAggregated($simulation, '214d4');
 
-        SimulationLearningArea::model()->findByAttributes(['sim_id'=>$simulation->id]);*/
+        $this->addAssessmentAggregated($simulation, '341b5');
+        $this->addAssessmentAggregated($simulation, '341b7');
+        $this->addAssessmentAggregated($simulation, '4121');
+        $this->addAssessmentAggregated($simulation, '4124');
+        $this->addAssessmentAggregated($simulation, '3216');
+        $this->addAssessmentAggregated($simulation, '4122');
+        $this->addAssessmentAggregated($simulation, '341b1');
+        $this->addAssessmentAggregated($simulation, '351b3');
+        $this->addAssessmentAggregated($simulation, '4125');
+        $this->addAssessmentAggregated($simulation, '4141');
+        $this->addAssessmentAggregated($simulation, '4143');
+        $this->addAssessmentAggregated($simulation, '4153');
+        $this->addAssessmentAggregated($simulation, '4127');
+        $this->addAssessmentAggregated($simulation, '3214');
+        $this->addAssessmentAggregated($simulation, '351a2');
+        $this->addAssessmentAggregated($simulation, '1122');
+        $this->addAssessmentAggregated($simulation, '1232'); // 1
+        $this->addAssessmentAggregated($simulation, '3218'); // 1
+        $this->addAssessmentAggregated($simulation, '351b2');
+        $this->addAssessmentAggregated($simulation, '351b1');
+        $this->addAssessmentAggregated($simulation, '351c1');
+        $this->addAssessmentAggregated($simulation, '351c3');
+        $this->addAssessmentAggregated($simulation, '3322');
+        $this->addAssessmentAggregated($simulation, '3323');
+        $this->addAssessmentAggregated($simulation, '3313');
+        $this->addAssessmentAggregated($simulation, '3333');
+        $this->addAssessmentAggregated($simulation, '3326');
+        $this->addAssessmentAggregated($simulation, '3311');
+        $this->addAssessmentAggregated($simulation, '3332');
+        $this->addAssessmentAggregated($simulation, '214a1');
+        $this->addAssessmentAggregated($simulation, '214a3');
+        $this->addAssessmentAggregated($simulation, '214a4');
+        $this->addAssessmentAggregated($simulation, '214a5');
+        $this->addAssessmentAggregated($simulation, '214b0');
+        $this->addAssessmentAggregated($simulation, '214b1');
+        $this->addAssessmentAggregated($simulation, '214b2');
+        $this->addAssessmentAggregated($simulation, '214b3');
+        $this->addAssessmentAggregated($simulation, '214b4');
+        $this->addAssessmentAggregated($simulation, '214b9');
+
+//        $this->addAssessmentAggregated($simulation, '3324');
+//        $this->addAssessmentAggregated($simulation, '3325');
+//        $this->addAssessmentAggregated($simulation, '214a8');
+//        $this->addAssessmentAggregated($simulation, '214b5');
+//        $this->addAssessmentAggregated($simulation, '214b6');
+//        $this->addAssessmentAggregated($simulation, '214b8');
+        $this->addAssessmentAggregated($simulation, '8311');
+        $this->addAssessmentAggregated($simulation, '8351');
+        $this->addAssessmentAggregated($simulation, '8331');
+        $this->addAssessmentAggregated($simulation, '8381');
+        $this->addAssessmentAggregated($simulation, '8211');
+        $this->addAssessmentAggregated($simulation, '8341');
+        $this->addAssessmentAggregated($simulation, '8371');
+        $this->addAssessmentAggregated($simulation, '7211');
+        $this->addAssessmentAggregated($simulation, '7141'); // Stress resistance
+
+        // -------------------------------
+
+        $learningGoalAnalyzer = new LearningGoalAnalyzer($simulation);
+        $learningGoalAnalyzer->run();
+
+        $learning_area = new LearningAreaAnalyzer($simulation);
+        $learning_area->run();
+
+        $evaluation = new Evaluation($simulation);
+        $evaluation->run();
+
+        $goals   = SimulationLearningArea::model()->findAllByAttributes(['sim_id' => $simulation->id]);
+        $areas   = SimulationLearningArea::model()->findAllByAttributes(['sim_id' => $simulation->id]);
+        $overall = AssessmentOverall::model()->findAllByAttributes(['sim_id' => $simulation->id]);
+
+        echo 'Sim id = '.$simulation->id."\n\n";
+        echo "\n Goals: \n";
+
+        foreach ($goals as $listItem) {
+            echo sprintf(
+                "%s %s : %s \n",
+                $listItem->learningArea->code,
+                $listItem->learningArea->title,
+                round($listItem->value, 2)
+            );
+        }
+
+        echo "\n Areas: \n";
+        foreach ($areas as $listItem) {
+            echo sprintf(
+                "%s %s : %s \n",
+                $listItem->learningArea->code,
+                $listItem->learningArea->title,
+                round($listItem->value, 2)
+            );
+        }
+
+        echo "\n Overals: \n";
+        foreach ($overall as $listItem) {
+            echo sprintf(
+                "%s : %s \n",
+                $listItem->assessment_category_code,
+                round($listItem->value, 2)
+            );
+        }
     }
 
     // -----------------------------------------------------
 
-    private function addAssessmentAggregated(Simulation $simulation, $code, $k = 1 )
+    private function addStressPoint(Simulation $simulation, $code )
+    {
+        if (is_integer($code)) {
+            $stresRule = $simulation->game_type->getStressRule(['code' => $code]);
+        } elseif ($code instanceof HeroBehaviour) {
+            $stresRule = $code;
+        }
+
+        if (null == $stresRule) {
+            return false;
+        }
+
+        $item = new StressPoint();
+        $item->sim_id         = $simulation->id;
+        $item->stress_rule_id = $stresRule->id;
+        $item->save();
+    }
+
+    private function addAssessmentAggregated(Simulation $simulation, $code, $value = null, $k = 1 )
     {
         if (is_string($code)) {
-            $behaviour = $simulation->game_type->getHeroBehavour(['code' => $code]);
+            $behaviour = $simulation->game_type->getHeroBehaviour(['code' => $code]);
+        } elseif ($code instanceof HeroBehaviour) {
+            $behaviour = $code;
+        }
+
+        if (null == $behaviour) {
+            return false;
+        }
+        $value = (null === $value) ? $behaviour->scale : $value;
+        var_dump($value);
+        var_dump($code);
+
+        $item = new AssessmentAggregated();
+        $item->sim_id      = $simulation->id;
+        $item->point_id    = $behaviour->id;
+        $item->value       = $k * $value;
+        $item->fixed_value = $k * $value;
+        $item->save();
+
+        return $item;
+    }
+
+    private function addAssessmentPoints(Simulation $simulation, $code, $k = 1 )
+    {
+        if (is_string($code)) {
+            $behaviour = $simulation->game_type->getHeroBehaviour(['code' => $code]);
         } elseif ($code instanceof HeroBehaviour) {
             $behaviour = $code;
         }
@@ -43,10 +190,29 @@ class Assessment_Goals_Areas_Overals extends CDbTestCase
             return false;
         }
 
-        $item = new AssessmentAggregated();
-        $item->sim_id = $simulation->id;
+        $item = new AssessmentPoint();
+        $item->sim_id   = $simulation->id;
         $item->point_id = $behaviour->id;
-        $item->value = $k * $behaviour->scale;
+        $item->value    = $k * $behaviour->scale;
+        $item->save(false);
+    }
+
+    private function addAssessmentCalculation(Simulation $simulation, $code, $k = 1 )
+    {
+        if (is_string($code)) {
+            $behaviour = $simulation->game_type->getHeroBehaviour(['code' => $code]);
+        } elseif ($code instanceof HeroBehaviour) {
+            $behaviour = $code;
+        }
+
+        if (null == $behaviour) {
+            return false;
+        }
+
+        $item = new AssessmentCalculation();
+        $item->sim_id   = $simulation->id;
+        $item->point_id = $behaviour->id;
+        $item->value    = $k * $behaviour->scale;
         $item->save();
     }
 }
