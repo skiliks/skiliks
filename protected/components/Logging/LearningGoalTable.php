9fc9af5e72d2acd7726e2fc6d425e0c5df83b3b5
Dmitry Alekseev <pernifin@gmail.com>
2013-04-26 15:34:37 +0300

    Admin

---
+++ protected/components/Logging/LearningGoalTable.php
@@ -0,0 +1,64 @@
+<?php
+namespace application\components\Logging;
+
+    /**
+     * \addtogroup Logging
+     * @{
+     */
+/**
+ * Детально расписанные поведения по матрицам диалогов и писем. Присутствует гребаный ад из LogHelper-a
+ */
+class LearningGoalTable extends LogTable
+{
+    public function getId()
+    {
+        return 'learning-goal';
+    }
+
+    public function getTitle()
+    {
+        return 'Learning Goal';
+    }
+
+    public function getHeaders()
+    {
+        return [
+            'Код',
+            'Наименование области обучения',
+            'Набранные положительные очки',
+            'Оценка (0-100%)',
+            'Уровень проблем (0-100%)',
+        ];
+    }
+
+    /**
+     * @param \SimulationLearningGoal $row
+     * @return array
+     * @throws \Exception
+     */
+    protected function getRow($row)
+    {
+        $value = round($row->value, 2);
+        $value = (string)$value === '0' ? '0.00':$value;
+
+        return [
+            $row->learningGoal->code,
+            $row->learningGoal->title,
+            $row->value,
+            $row->percent,
+            $row->problem
+        ];
+    }
+
+
+    public function getRowId($row)
+    {
+        return sprintf(
+            'learning-goal-code-%s ',
+            $row[0]
+        );
+    }
+}
+/**
+ * @}
+ */
