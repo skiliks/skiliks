c33de519e08d02e22c13e64baea32fca98a54f28
slavkaa <nikoolin@ukr.net>
2013-04-29 19:37:01 +0300

    Global assessment - code review.

--- protected/components/LearningAreaAnalyzer.php
+++ protected/components/LearningAreaAnalyzer.php
@@ -145,11 +145,7 @@ class LearningAreaAnalyzer {
         $maxRate = 0;
         $ids = [];
 
-        $except = [
-            '214g1', '214g2', '214g3', '214g4', '32110', '32112', '32113',
-            '32114', '3312', '3335', '341a1', '341a8', '341c1', '341c2', '371a1',
-            '371a2', '371a3', '371a4', '371a5', '371b1', '371b2', '371b3', '8211'
-        ];
+        $except = HeroBehaviour::getExcludedFromAssessmentBehavioursCodes();
 
         $area = $scenario->getLearningArea(['code' => $learningAreaCode]);
         if ($area) {
