df2478f13def3e0cea2733594169d01f71a14bcf
slavkaa <nikoolin@ukr.net>
2013-04-29 17:47:07 +0300

    SKILIKS-2272. Fix work with cookies.

--- protected/components/SimulationService.php
+++ protected/components/SimulationService.php
@@ -664,7 +664,7 @@ class SimulationService
         $simulation->end = GameTime::setNowDateTime();
         $simulation->save();
 
-        Yii::app()->request->cookies =
+        Yii::app()->request->cookies['display_result_for_simulation_id'] =
             new CHttpCookie('display_result_for_simulation_id', $simulation->id);
     }
 
