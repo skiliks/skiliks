7965ce3794ff33d1c52dca48128cd601f57f837d
slavkaa <nikoolin@ukr.net>
2013-05-02 14:15:16 +0300

    SKILKS-2433. Make email images embeded into email.

--- protected/views/global_partials/mails/invite.php
+++ protected/views/global_partials/mails/invite.php
@@ -14,7 +14,7 @@
         <table cellpadding="0" cellspacing="0">
             <tr>
                 <td>
-                    <img src="http://kilimov.ho.ua/img/mail-top.png" style="display:block"/>
+                    <img src="cid:mail-top" style="display:block"/>
                 </td>
             </tr>
             <tr>
@@ -22,7 +22,7 @@
                     <table cellpadding="0" cellspacing="0">
                         <tr>
                             <td width="41">
-                                <img src="http://kilimov.ho.ua/img/mail-left.png" style="display:block"/>
+                                <img src="cid:mail-left" style="display:block"/>
                             </td>
                             <td bgcolor="fdfbc6" width="489" height="344" valign="top">
                                 <h1 style="color:#626250; font: 'ProximaNova-Bold'; font-size:30px; margin:0 0 10px 0; padding:0;">
@@ -30,7 +30,8 @@
                                 </h1>
 
                                 <p style="margin:0 0 10px 0; color:#555545; font-family:'ProximaNova-Regular';font-size:16px;">
-                                       <?= $invite->ownerUser->account_corporate->company_name ?: 'Компания' ?> предлагает вам пройти тест «Базовый менеджмент» для участия в конкурсе на вакансию
+                                       <?= $invite->ownerUser->account_corporate->company_name ?: 'Компания' ?>
+                                        предлагает вам пройти тест «Базовый менеджмент» для участия в конкурсе на вакансию
                                         <a style="text-decoration:none; color:#147b99;font-family: 'ProximaNova-Regular'; font-size: 18px;" href="<?= $invite->vacancy->link ?: '#' ?>">
                                                 <?= $invite->getVacancyLabel() ?>
                                         </a>.
@@ -40,7 +41,8 @@
                                     <?php if (empty($invite->receiverUser)): ?>
                                         <a style="text-decoration:none; color:#147b99;font-family: 'ProximaNova-Regular'; font-size: 18px;" href="<?= $this->createAbsoluteUrl('static/pages/product') ?>">
                                                 «Базовый менеджмент»
-                                        </a> - это деловая симуляция, позволяющая оценить менеджерские навыки в форме увлекательной игры.
+                                        </a> - это деловая симуляция, позволяющая оценить менеджерские навыки в форме
+                                        увлекательной игры.
                                     <?php endif; ?>
                                 </p>
 
@@ -53,12 +55,14 @@
                                         Пожалуйста,
                                         <a style="text-decoration:none; color:#147b99;font-family: 'ProximaNova-Regular'; font-size: 16px;" href="<?= $this->createAbsoluteUrl('dashboard') ?>">
                                              зайдите
-                                        </a> в свой кабинет и примите приглашение на тестирование для прохождения симуляции.
+                                        </a> в свой кабинет и примите приглашение на
+                                        тестирование для прохождения симуляции.
                                     <?php else: ?>
                                         Пожалуйста,
                                         <a style="text-decoration:none; color:#147b99;font-family: 'ProximaNova-Regular'; font-size: 16px;" href="<?= $invite->getInviteLink() ?>">
                                             зарегистрируйтесь
-                                        </a> и в своем кабинете примите приглашение на тестирование для прохождения симуляции.
+                                        </a> и в своем кабинете примите приглашение на
+                                        тестирование для прохождения симуляции.
                                     <?php endif; ?>
                                 </p>
 
@@ -67,7 +71,7 @@
                                 </a>
                             </td>
                             <td width="340">
-                                <img src="http://kilimov.ho.ua/img/mail-right.png" style="display:block"/>
+                                <img src="cid:mail-right" style="display:block"/>
                             </td>
                         </tr>
                     </table>
@@ -75,7 +79,7 @@
             </tr>
             <tr>
                 <td>
-                    <img src="http://kilimov.ho.ua/img/mail-bottom.png" style="display:block"/>
+                    <img src="cid:mail-bottom" style="display:block"/>
                 </td>
             </tr>
         </table>
