e5fd93a3708c5796383f3761d095f1a03f9c22ea
slavkaa <nikoolin@ukr.net>
2013-05-02 16:00:28 +0300

    SKILIKS-2233. Fix string source.

--- protected/views/static/dashboard/dashboard_corporate.php
+++ protected/views/static/dashboard/dashboard_corporate.php
@@ -1,5 +1,5 @@
 <section class="dashboard corpdashboard">
-    <h2 class="thetitle bigtitle"><?php echo Yii::t('site', 'Corporate dashboard') ?></h2>
+    <h2 class="thetitle bigtitle"><?php echo Yii::t('site', 'Work dashboard') ?></h2>
     <aside>
     <!-- invite-people-box -->
         <div id="invite-people-box" class="nice-border backgroud-rich-blue sideblock">
