9229034d21d8ca3a2c11b92d75a2d8f3707393b4
Cherous <r.kilimov@gmail.com>
2013-05-06 10:39:09 +0300

    2199

--- protected/views/global_partials/mails/invite.php
+++ protected/views/global_partials/mails/invite.php
@@ -4,74 +4,106 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Ruseller Email Newsletter</title>
         <style type="text/css">
-            img {display: block;}
-            h1 {color:#626250; font: 'ProximaNova-Bold'; font-size:30px; margin:0 0 10px 0; padding:0;}
-            p {margin:0 0 10px 0; color:#555545; font-family:'ProximaNova-Regular';font-size:16px;}
-            a {text-decoration:none; color:#147b99;font-family: 'ProximaNova-Regular'; font-size: 18px;}
+            img {
+                display: block;
+            }
+            h1 {
+                color:#626250;
+                font-family: 'ProximaNova-Bold', Arial;
+                font-size:30px;
+                margin:0 0 10px 0;
+                padding:0;}
+            p {
+                margin:0 0 10px 0;
+                color:#555545;
+                font-family:'ProximaNova-Regular', Arial;
+                font-size:16px;
+                text-align:justify;
+                line-height:22px;
+            }
+            a {
+                text-decoration:none;
+                color:#147b99;
+                font-family: 'ProximaNova-Regular', Arial;
+                font-size: 16px;
+            }
         </style>
     </head>
     <body>
         <table cellpadding="0" cellspacing="0">
             <tr>
                 <td>
-                    <img src="cid:mail-top" style="display:block"/>
+                    <img src="cid:mail-top">
                 </td>
             </tr>
             <tr>
                 <td>
                     <table cellpadding="0" cellspacing="0">
-                        <tr>
-                            <td width="41">
-                                <img src="cid:mail-left" style="display:block"/>
+                        <tr style="vertical-align: top;">
+                            <td width="31" bgcolor="f2f2f2" style="padding-left:10px;">
+                                <table bgcolor="fdfbc6" width="100%" height="100%">
+                                    <tr><td></td></tr>
+                                </table>
+                                <!--<img src="cid:mail-left"/>-->
                             </td>
                             <td bgcolor="fdfbc6" width="489" height="344" valign="top">
-                                <h1 style="color:#626250; font: 'ProximaNova-Bold'; font-size:30px; margin:0 0 10px 0; padding:0;">
+                                <h1>
                                         <?= $invite->getReceiverUserName() ?>, приветствуем Вас!
                                 </h1>
 
-                                <p style="margin:0 0 10px 0; color:#555545; font-family:'ProximaNova-Regular';font-size:16px;">
+                                <p>
                                        <?= $invite->ownerUser->account_corporate->company_name ?: 'Компания' ?>
                                         предлагает вам пройти тест «Базовый менеджмент» для участия в конкурсе на вакансию
-                                        <a style="text-decoration:none; color:#147b99;font-family: 'ProximaNova-Regular'; font-size: 18px;" href="<?= $invite->vacancy->link ?: '#' ?>">
+                                        <a href="<?= $invite->vacancy->link ?: '#' ?>">
                                                 <?= $invite->getVacancyLabel() ?>
                                         </a>.
                                 </p>
 
-                                <p style="margin:0 0 10px 0; color:#555545; font-family:'ProximaNova-Regular';font-size:16px;">
-                                    <?php if (empty($invite->receiverUser)): ?>
-                                        <a style="text-decoration:none; color:#147b99;font-family: 'ProximaNova-Regular'; font-size: 18px;" href="<?= $this->createAbsoluteUrl('static/pages/product') ?>">
+                                <?php if (empty($invite->receiverUser)): ?>
+                                    <p>
+                                        <a href="<?= $this->createAbsoluteUrl('static/pages/product') ?>">
                                                 «Базовый менеджмент»
                                         </a> - это деловая симуляция, позволяющая оценить менеджерские навыки в форме
                                         увлекательной игры.
-                                    <?php endif; ?>
-                                </p>
+                                    </p>
+                                <?php endif; ?>
 
-                                <p style="margin:0 0 10px 0; color:#555545; font-family:'ProximaNova-Regular';font-size:16px;">
+                                <p>
                                     <?= $invite->message ?>
                                 </p>
 
-                                <p style="margin:0 0 10px 0; color:#555545; font-family:'ProximaNova-Regular';font-size:16px;">
+                                <p>
                                     <?php if ($invite->receiverUser): ?>
                                         Пожалуйста,
-                                        <a style="text-decoration:none; color:#147b99;font-family: 'ProximaNova-Regular'; font-size: 16px;" href="<?= $this->createAbsoluteUrl('dashboard') ?>">
+                                        <a href="<?= $this->createAbsoluteUrl('dashboard') ?>">
                                              зайдите
                                         </a> в свой кабинет и примите приглашение на
                                         тестирование для прохождения симуляции.
                                     <?php else: ?>
                                         Пожалуйста,
-                                        <a style="text-decoration:none; color:#147b99;font-family: 'ProximaNova-Regular'; font-size: 16px;" href="<?= $invite->getInviteLink() ?>">
+                                        <a href="<?= $invite->getInviteLink() ?>">
                                             зарегистрируйтесь
                                         </a> и в своем кабинете примите приглашение на
                                         тестирование для прохождения симуляции.
                                     <?php endif; ?>
                                 </p>
-
-                                <a style="text-decoration:none; color:#147b99; font-family:'ProximaNova-Regular'; font-size: 16px;" href="http://www.skiliks.com">
+                                <p>
+                                    <b>Ваш Skiliks</b>
+                                </p>
+                                <br/>
+                                <a href="http://www.skiliks.com">
                                         www.skiliks.com
                                 </a>
                             </td>
-                            <td width="340">
-                                <img src="cid:mail-right" style="display:block"/>
+                            <td width="300" style="padding-right:30px;">
+                                <table bgcolor="f2f2f2" width="100%" height="100%" cellspacing="0" cellpadding="0" style="padding-right:10px;">
+                                    <tr style="vertical-align: top;">
+                                        <td bgcolor="fdfbc6">
+                                            <img src="cid:mail-right" style="margin:0 -40px 0 0;"/>
+                                        </td>
+                                    </tr>
+                                </table>
+
                             </td>
                         </tr>
                     </table>
@@ -79,7 +111,7 @@
             </tr>
             <tr>
                 <td>
-                    <img src="cid:mail-bottom" style="display:block"/>
+                    <img src="cid:mail-bottom" />
                 </td>
             </tr>
         </table>
