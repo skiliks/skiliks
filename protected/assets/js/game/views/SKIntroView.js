3afe89bd137924aa8be67704f597fabe6387abfd
Dmitry Alekseev <pernifin@gmail.com>
2013-05-07 21:03:02 +0300

    Revert SKILIKS-2516

--- protected/assets/js/game/views/SKIntroView.js
+++ protected/assets/js/game/views/SKIntroView.js
@@ -25,7 +25,8 @@ define([
             this.$el.find('#skiliks_intro').bind('ended', function () {
                 this.pause();
                 this.src = '';
-                me.$el.empty().removeClass('loading').unbind("mousemove");
+                me.$(this).remove();
+                me.$el.unbind("mousemove");
                 me.appLaunch();
             });
 
@@ -39,25 +40,20 @@ define([
         },
 
         appLaunch: function() {
-            var me = this;
-
             window.SKApp = new SKApplication(window.gameConfig);
             window.AppView = new SKApplicationView();
 
-            window.SKApp.simulation.start(function() {
-                window.AppView.drawDesktop();
-                me.tutorial = new SKDialogView({
-                    class: 'tutorial-popup',
-                    content: _.template(tutorial_template, {}),
-                    buttons: [{
-                        id: 'ok',
-                        value: 'Начать',
-                        onclick: function() {
-                            $('.time').removeClass('paused');
-                            window.SKApp.simulation.run();
-                        }
-                    }]
-                });
+            this.tutorial = new SKDialogView({
+                class: 'tutorial-popup',
+                content: _.template(tutorial_template, {}),
+                buttons: [{
+                    id: 'ok',
+                    value: 'Начать',
+                    onclick: function() {
+                        $('.time').removeClass('paused');
+                        window.SKApp.run();
+                    }
+                }]
             });
         },
         handleClick: function(){
