f7575c2a4699d4db35c969f1df3354375377569d
slavkaa <nikoolin@ukr.net>
2013-05-08 19:04:40 +0300

    SKILIKS-2517. Fix.

--- protected/assets/js/game/models/SKSimulation.js
+++ protected/assets/js/game/models/SKSimulation.js
@@ -154,11 +154,11 @@ define([
 
                     SKApp.server.api('myDocuments/isDocumentSaved', {id: doc_id}, function(result) {
                         console.log('result: ', result);
-                        if ('true' === SKApp.get('isLocalPc') || '1' === result.status.toString()) {
+                        if (('true' === SKApp.get('isLocalPc') || '1' === result.status.toString()) &&
+                            false === SKApp.simulation.get('isZohoDocumentSuccessfullySaved')) {
                             console.log('saved!');
                             SKApp.simulation.set('isZohoDocumentSuccessfullySaved', true);
                             SKApp.simulation.tryCloseLoadDocsDialog();
-                            return;
                         }
                     });
                 }
