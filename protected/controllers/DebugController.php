9e6f818c37000ca65a8044c996a67fefff2e8dab
Andrii Kostenko <andrey@kostenko.name>
2013-05-08 15:14:17 +0300

    Revert "Revert "SKILIKS-2546 #resolved""
    This reverts commit ec2b21247b78cc10b61f238c61752710cd20989e.

--- protected/controllers/DebugController.php
+++ protected/controllers/DebugController.php
@@ -6,9 +6,7 @@ class DebugController extends AjaxController
     public function actionIndex()
     {
         //1367911963
-        $datetime = new DateTime('now', new DateTimeZone('Europe/Moscow'));
-        $datetime->setTimestamp(1367911963);
-        echo $datetime->format("H:i");
+       echo strtotime('10:02:10') - strtotime('10:02:00');
     }
 }
 
