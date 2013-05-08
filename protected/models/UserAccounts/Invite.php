fd9f4b5071b6a2362d72513ebc74a5970f88bcf5
Andrii Kostenko <andrey@kostenko.name>
2013-05-08 17:36:32 +0300

    sorting on vacancy

--- protected/models/UserAccounts/Invite.php
+++ protected/models/UserAccounts/Invite.php
@@ -500,7 +500,7 @@ class Invite extends CActiveRecord
 		$criteria->compare('status', $this->status);
         $criteria->compare('scenario_id', $this->scenario_id);
 		$criteria->compare('sent_time', $this->sent_time);
-
+        $criteria->join = 'LEFT JOIN vacancy ON vacancy.id=vacancy_id';
         $criteria->mergeWith($criteria2);
 
 		return new CActiveDataProvider($this, [
