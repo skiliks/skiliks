e4b9faedbe4f617f5adb1a70a1d0e6f8212d745e
Andrii Kostenko <andrey@kostenko.name>
2013-05-14 17:41:27 +0300

    test fix

--- protected/assets/js/game/test/new-mail-m1-test.js
+++ protected/assets/js/game/test/new-mail-m1-test.js
@@ -102,7 +102,7 @@ define([
                 "subject":"Re: \u0441\u0440\u043e\u0447\u043d\u043e! \u041e\u0442\u0447\u0435\u0442\u043d\u043e\u0441\u0442\u044c",
                 "receiver":"\u0421\u043a\u043e\u0440\u043e\u0431\u0435\u0439 \u0410.\u041c. <skorobey.am@skiliks.com>",
                 "receiver_id":"50",
-                "copiesIds":"2,11,12",
+                "copiesIds":"44,42,43",
                 "copies":"\u0414\u0435\u043d\u0435\u0436\u043d\u0430\u044f \u0420.\u0420. <denezhnaya.rr@skiliks.com>,\u0412\u0430\u0441\u0438\u043b\u0438\u0439 \u0411\u043e\u0431\u0440 <bobr.vs@skiliks.com>,\u0415\u0433\u043e\u0440 \u0422\u0440\u0443\u0434\u044f\u043a\u0438\u043d <trudyakin.ek@skiliks.com>",
                 "phrases":{
                     "message":"",
@@ -633,7 +633,7 @@ define([
                 // check sendMessage request to server
                 server.respondWith("POST", "/index.php/mail/sendMessage",
                     function (xhr) {
-                        var data = {}
+                        var data = {};
                         decodeURIComponent(xhr.requestBody).split('&')
                             .forEach(function (val) {
                                 var vals = val.split('=');
