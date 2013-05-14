b16e1592b83bda99ba1c42da994569341d99ece9
Ivan Tugay <listepo@hotmail.com>
2013-05-14 17:42:00 +0300

    SKILIKS-2510 #resolved

--- protected/assets/js/game/models/SKMailClient.js
+++ protected/assets/js/game/models/SKMailClient.js
@@ -859,7 +859,7 @@ define(["game/models/SKMailFolder", "game/models/SKMailSubject","game/models/SKC
                 var list = [];
                 for (var i in this.defaultRecipients) {
                     // non strict "!=" is important!
-                    if(this.activeScreen === 'SCREEN_WRITE_NEW_EMAIL'){
+                    if(this.activeScreen !== 'SCREEN_WRITE_NEW_EMAIL'){
                         if ('' !== this.defaultRecipients[i].get('fio') && '' !== this.defaultRecipients[i].get('email')) {
                             list.push(this.defaultRecipients[i].getFormatedForMailToName());
                         }
