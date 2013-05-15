be3ccbea8c446dab4aacbcf71a825cf6612eaab6
Andrii Kostenko <andrey@kostenko.name>
2013-05-15 19:31:13 +0300

    stage fix

--- protected/config/stage_test.php
+++ protected/config/stage_test.php
@@ -15,6 +15,16 @@ return CMap::mergeArray(require(dirname(__FILE__) . '/base.php'), array(
     ),
     'params'=>array(
         'frontendUrl'=>'http://test.skiliks.com/',
+        'zoho' => array(
+            'apiKey'              => 'c076746cd578f7e9287ff1234d3faf2f',
+            'saveUrl'             => 'http://test.skiliks.com/zoho/saveExcel',
+            'xlsTemplatesDirPath' => 'documents/templates',
+            'templatesDirPath'    => 'documents/zoho',
+            //'sendFileUrl'         => 'https://presheet.zoho.com/remotedoc.im?apikey=%s&output=editor',
+            'sendFileUrl'         => 'http://zoho.skiliks.com/remotedoc.im?apikey=%s&output=editor',
+            'extExcel'            => 'xls'
+
+        ),
     )
 ));
 
