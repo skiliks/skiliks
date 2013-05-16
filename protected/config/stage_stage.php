156a58825e9cde67231b3c6cf7b3e0fb597a9566
Andrii Kostenko <andrey@kostenko.name>
2013-05-16 12:06:53 +0300

    stage config

---
+++ protected/config/stage_stage.php
@@ -0,0 +1,36 @@
+<?php
+define(YII_DEBUG, false);
+return CMap::mergeArray(require(dirname(__FILE__) . '/base.php'), array(
+    'preload'=> array('log', 'RSentryException'),
+    'components'=>array(
+        'db'=>array(
+            'connectionString' => 'mysql:host=localhost;dbname=skiliks_stage',
+            'emulatePrepare' => true,
+            'username' => 'skiliks_live',
+            'password' => 'dep-vep-eb-up-a',
+            'charset' => 'utf8',
+
+            'enableParamLogging'=>true,
+            'enableProfiling'=>true
+        ),
+        'RSentryException'=> array(
+            'dsn'=> 'https://bfd7395024f24728afdf79e9034bca04:2f8bec2e2c40493dbf7b07db88afc94f@app.getsentry.com/4572',
+            'class' => 'application.components..yii-sentry-log.RSentryComponent',
+        ),
+        'log'=>array(
+            'class'=>'CLogRouter',
+            'routes'=>array(
+                array(
+                    'class'=>'application.components.yii-sentry-log.RSentryLog',
+                    'dsn'=> 'https://bfd7395024f24728afdf79e9034bca04:2f8bec2e2c40493dbf7b07db88afc94f@app.getsentry.com/4572',
+                    'levels'=>'error, warning',
+                ),
+            ),
+        ),
+    ),
+    'params'=>array(
+        'frontendUrl'=>'http://stage.dev.skiliks.com/',
+    )
+));
+
+
