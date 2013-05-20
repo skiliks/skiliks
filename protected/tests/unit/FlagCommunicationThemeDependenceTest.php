8a507c605098b502fce28c1eb0237e643fb2ef3e
Ivan Tugay <listepo@hotmail.com>
2013-05-20 11:39:14 +0300

    SKILIKS-2638 #resolved

--- protected/tests/unit/FlagCommunicationThemeDependenceTest.php
+++ protected/tests/unit/FlagCommunicationThemeDependenceTest.php
@@ -108,5 +108,28 @@ class FlagCommunicationThemeDependenceTest extends PHPUnit_Framework_TestCase {
         }
     }
 
+    public function testMailBoxFantasticNotBlocking() {
+        /** @var $user YumUser */
+        $user = YumUser::model()->findByAttributes(['username' => 'asd']);
+        $invite = new Invite();
+        $invite->scenario = new Scenario();
+        $invite->receiverUser = $user;
+        $invite->scenario->slug = Scenario::TYPE_FULL;
+        $simulation = SimulationService::simulationStart($invite, Simulation::MODE_DEVELOPER_LABEL);
+        $replicas = $simulation->game_type->getReplicas(['fantastic_result' => 1]);
+        $ms = [];
+        foreach($replicas as $replica){
+            /* @var $replica Replica */
+            if(substr($replica->next_event_code, 0, 2) === "MS" && false === in_array($replica->next_event_code, $ms)){
+                $ms[] = $replica->next_event_code;
+                /* @var $theme CommunicationTheme */
+                $theme = $simulation->game_type->getCommunicationTheme(['letter_number'=>$replica->next_event_code]);
+                $this->assertNotNull($theme);
+                $this->assertFalse($theme->isBlockedByFlags($simulation));
+            }
+        }
+
+    }
+
 
 }
