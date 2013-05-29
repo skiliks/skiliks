ddd14f8aa967c2fdbdecdea182923521367cfa3d
Ivan <listepo@hotmail.com>
2013-05-29 11:28:09 +0300

    Debug test

--- protected/components/FlagsService.php
+++ protected/components/FlagsService.php
@@ -257,6 +257,7 @@ class FlagsService
         $flags = SimulationFlagQueue::model()->findAll("sim_id = :sim_id and is_processed = :is_processed and switch_time >= :switch_time", [
             'sim_id' => $simulation->id, 'switch_time' => $simulation->getGameTime(), 'is_processed' => SimulationFlagQueue::NONE
         ]);
+        var_dump(empty($flags));
         /* @var SimulationFlagQueue $flag */
         foreach($flags as $flag) {
             var_dump($flag->flag_code);
