c6e27e74c2b6f7c40fe7f3443d9ce66322389946
Ivan <ivan@skiliks.com>
2013-05-29 10:47:27 +0300

    Fix unit test

--- protected/tests/unit/ImportUnitTest.php
+++ protected/tests/unit/ImportUnitTest.php
@@ -92,8 +92,8 @@ class ImportUnitTest extends CDbTestCase
             $this->assertGreaterThan(0, FlagRunMail::model()->count('scenario_id = '.$fullScenario->id));
 
             $this->assertEquals(11, FlagBlockReplica::model()->count('scenario_id = '.$fullScenario->id), 'block replica');
-            $this->assertEquals(12, FlagBlockDialog::model()->count('scenario_id = '.$fullScenario->id), 'block dialog');
-            $this->assertEquals(22, Flag::model()->count('scenario_id = '.$fullScenario->id), 'flags');
+            $this->assertEquals(20, FlagBlockDialog::model()->count('scenario_id = '.$fullScenario->id), 'block dialog');
+            $this->assertEquals(31, Flag::model()->count('scenario_id = '.$fullScenario->id), 'flags');
 
             $this->assertEquals(5, FlagBlockMail::model()->count('scenario_id = '.$fullScenario->id), 'block mail');
 
