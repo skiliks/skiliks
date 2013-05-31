691d4b812297889344f3b38704aa5184c14384c7
tania <tetyana.grybok@skiliks.com>
2013-05-31 12:38:58 +0300

    zoho Get Usage Test

--- protected/tests/zoho/ZohoGetUsagesTest.php
+++ protected/tests/zoho/ZohoGetUsagesTest.php
@@ -27,25 +27,25 @@ class ZohoGetUsagesTest extends SeleniumTestHelper
 
     public function testZohoGetUsages() {
         $this->markTestIncomplete();
-//        $this->setUp();
-//        $this->deleteAllVisibleCookies();
-//        $this->windowMaximize();
-//        $this->open('https://zapi.zoho.com/login.do');
-//
-//        $this->waitForVisible("id=lid");
-//
-//        $this->type('id=lid','zosksk');
-//        $this->type('name=pwd','zoho531');
-//
-//        $this->click("name=submit");
-//
-//        // $this->click("//form[@id='login']/table/tr[0]/td[0]/table/tr[7]/td[1]/input");
-//
-//        sleep(2);
-//
-//        $value = $this->getText("//div[.usageinner_topdiv']/table/td[1]");
-//
-//        $this->open('http://test.skiliks.com/cheat/zoho/saveUsageValue/'.urlencode($value));
+        $this->setUp();
+        $this->deleteAllVisibleCookies();
+        $this->windowMaximize();
+        $this->open('https://zapi.zoho.com/login.do');
+
+        $this->selectFrame('zohoiam');
+
+        $this->type('id=lid','zosksk');
+        $this->type('name=pwd','zoho531');
+
+        $this->click("name=submit");
+
+        // $this->click("//form[@id='login']/table/tr[0]/td[0]/table/tr[7]/td[1]/input");
+
+        sleep(2);
+
+        $value = $this->getText("//div[.usageinner_topdiv']/table/td[1]");
+
+        $this->open('http://test.skiliks.com/cheat/zoho/saveUsageValue/'.urlencode($value));
 
         $this->close();
     }
