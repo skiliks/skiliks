634c8979581ecf71db30804c8677ae7fa3e6b9d1
slavkaa <nikoolin@ukr.net>
2013-05-31 15:29:12 +0300

    SKILIKS-2873. Fix output forflafs in dev mode.

--- protected/assets/js/game/views/develop_mode/SKFlagStateView.js
+++ protected/assets/js/game/views/develop_mode/SKFlagStateView.js
@@ -54,11 +54,24 @@ define([],
                 },
                 function (response) {
                     if (response.result) {
-                        $('body form.trigger-event').append('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Флаг ' + flagName + ' переключён в "' + flagValue + '"!</div>');
+                        $('body form.trigger-event').append(
+                            '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Флаг '
+                            + flagName
+                            + ' переключён в "'
+                            + flagValue
+                            + '"!</div>'
+                        );
+
                         me.updateValues(response.flags);
                         window.scrollTo(0, 0);
                     } else {
-                        $('body form.trigger-event').append('<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button>Флаг ' + flagName + ' не переключён в "' + flagValue + '"!</div>');
+                        $('body form.trigger-event').append(
+                            '<div class="alert alert-error "><button type="button" class="close" data-dismiss="alert">&times;</button>Флаг '
+                            + flagName
+                            + ' не переключён в "'
+                            + flagValue
+                            + '"!</div>'
+                        );
                     }
                     $('body form.trigger-event .alert').fadeOut(4000);
                 });
@@ -74,7 +87,7 @@ define([],
             $('.form-flags table').remove();
             // clean old data AND init base structure,
             // may be will be better to put this code to template, but it so short and use once! :)
-            $('.form-flags fieldset').append($('<table class="table table-bordered"><thead><tr></tr></thead><tbody><tr></tr></tbody></table>'));
+            $('.form-flags fieldset').append($('<table class="table table-bordered"><thead><tr></tr></thead><tbody><tr></tr><tr></tr></tbody></table>'));
 
             var j = 0;
             var n = 0;
@@ -83,7 +96,7 @@ define([],
                 if (9 < n) {
                     j++;
                     n = 0;
-                    $('.form-flags fieldset').append($('<table class="table table-bordered"><thead><tr></tr></thead><tbody><tr></tr></tbody></table>'));
+                    $('.form-flags fieldset').append($('<table class="table table-bordered"><thead><tr></tr></thead><tbody><tr></tr><tr></tr></tbody></table>'));
                 }
 
 
@@ -93,14 +106,26 @@ define([],
                 el1.attr('title', flagsState[i].name); // add title
                 el1.attr('data-flag-code',  i); // add title
                 el1.attr('data-flag-value', flagsState[i].value); // add title
-                el1.attr('data-flag-time', '00:00:00'); // add title
+                el1.attr('data-flag-time',  '00:00:00'); // add title
                 // generate th
-                var el = $('<th/>', {});
-                el.append(el1);
-                $('.form-flags table:eq('+j+') thead tr').append(el);
+                var el_th = $('<th/>', {});
+                el_th.append(el1);
+                $('.form-flags table:eq('+j+') thead tr').append(el_th);
 
-                el = $('<td/>', {text: this.getFlagData(flagsState[i])});
-                $('.form-flags table:eq('+j+') tbody tr').append(el);
+                var el_td = $('<td/>', {text: flagsState[i].value});
+                el_td.addClass(flagsState[i].name + '-value');
+                $('.form-flags table:eq('+j+') tbody tr:eq(0)').append(el_td);
+
+                // flag queue time
+
+                var flagTime = '';
+                if (null !== flagsState[i].time) {
+                    flagTime = flagsState[i].time;
+                }
+
+                var el_td2 = $('<td/>', {text: flagTime});
+                el_td2.addClass(flagsState[i].name + '-time');
+                $('.form-flags table:eq('+j+') tbody tr:eq(1)').append(el_td2);
 
                 n++;
             }
