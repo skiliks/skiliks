72bb1254abe698ba356010c4ea531cff82784c12
Andrii Kostenko <andrey@kostenko.name>
2013-06-05 01:43:45 +0300

    SKILIKS-2769

--- protected/tests/unit/DocumentsUnitTest.php
+++ protected/tests/unit/DocumentsUnitTest.php
@@ -10,4 +10,16 @@ class DocumentsUnitTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($file->getSheetList()[0]['name'], 'Сводный');
         $file->setSheetContent('Сводный', 'жопа');
     }
+    public function testSaveExcel(){
+        $file = new MyDocument();
+        $file->setPrimaryKey(2);
+        $scenario = Scenario::model()->findByAttributes(['slug' => Scenario::TYPE_FULL]);
+        $file->template = $scenario->getDocumentTemplate(['code' => 'D1']);
+        $this->assertStringEndsWith('.sc' , $file->template->srcFile);
+        $this->assertEquals($file->getSheetList()[0]['name'], 'Сводный');
+
+        $file->setSheetContent('Логистика', 'Что-то');
+        $file->setSheetContent('Сводный', 'жопа');
+        $this->assertEquals($file->getSheetList()[3]['content'], 'Что-то');
+    }
 }
