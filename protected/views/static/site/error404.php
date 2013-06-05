7b5edaec010dac9ab08ee7498686b521439c2044
Cherous <r.kilimov@gmail.com>
2013-06-05 06:05:49 +0300

    2690

--- protected/views/static/site/error404.php
+++ protected/views/static/site/error404.php
@@ -8,7 +8,7 @@
     <h1>Error 404. Запрошенная вами страница не найдена.</h1>
     <br/>
     <br/>
-    <a href="/" class="bigbtnsubmt freeacess"><?php echo Yii::t('site','Back to homepage');?></a>
+    <a href="/" class="bigbtnsubmt"><?php echo Yii::t('site','Back to homepage');?></a>
     <br/>
     <br/>
 </div>
