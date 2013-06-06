7d3f061992cd9c178bf913e287d7da73029971f6
slavkaa <nikoolin@ukr.net>
2013-06-06 14:00:11 +0300

    Add more console.logs to load docs.

--- protected/assets/js/game/models/SKSimulation.js
+++ protected/assets/js/game/models/SKSimulation.js
@@ -238,6 +238,8 @@ define([
             tryCloseLoadDocsDialog: function() {
                 console.log(SKApp.simulation.isAllExcelDocsInitialized(), SKApp.simulation.get('isZohoDocumentSuccessfullySaved'));
 
+                console.log('isAllExcelDocsInitialized: ', SKApp.simulation.isAllExcelDocsInitialized());
+
                 if (SKApp.simulation.isAllExcelDocsInitialized() &&
                     true === SKApp.simulation.get('isZohoDocumentSuccessfullySaved')) {
                     SKApp.simulation.closeLoadDocsDialog();
