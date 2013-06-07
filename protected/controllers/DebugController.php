25752d2d001c778e7b88dccdcbd26057d633ca5c
Ivan <listepo@hotmail.com>
2013-06-07 15:48:32 +0300

    SKILIKS-2496

--- protected/controllers/DebugController.php
+++ protected/controllers/DebugController.php
@@ -5,7 +5,8 @@ class DebugController extends AjaxController
 {
     public function actionIndex()
     {
-        TestUserHelper::addUser("personal");
+        //TestUserHelper::addUser("personal");
+        echo TestUserHelper::getActivationUrl("ivan@skiliks.com");
     }
 }
 
