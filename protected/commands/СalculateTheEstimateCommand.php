40076d83e5c905e8a4b16309a75d4c4427c3e1d5
Ivan <listepo@hotmail.com>
2013-06-07 13:52:42 +0300

    SKILIKS-2925

---
+++ protected/commands/СalculateTheEstimateCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+class СalculateTheEstimateCommand extends CConsoleCommand {
+
+    public function actionIndex($email, $simId)
+    {
+        /** @var  $simulation Simulation */
+        $simulation = Simulation::model()->findByPk($simId);
+        if(null === $simulation){
+            throw new Exception("Simulation by id = {$simId} not found.");
+        }
+        /* @var $profile YumProfile */
+        $profile = YumProfile::model()->findByAttributes(['email'=>$email]);
+        if(null === $profile){
+            throw new Exception("Profile by email = {$email} not found.");
+        }
+
+        if($profile->user_id !== $simulation->user_id){
+            throw new Exception("This simulation does not belong to this user.");
+        }
+
+        LogActivityActionAgregated::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        TimeManagementAggregated::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        AssessmentCalculation::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        DayPlanLog::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        LogActivityActionAgregated214d::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        AssessmentPlaningPoint::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        SimulationExcelPoint::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        PerformancePoint::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        PerformanceAggregated::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        StressPoint::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        AssessmentAggregated::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        SimulationLearningGoal::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        SimulationLearningArea::model()->deleteAllByAttributes(['sim_id' => $simId]);
+        AssessmentOverall::model()->deleteAllByAttributes(['sim_id' => $simId]);
+
+        SimulationService::simulationStop($simulation);
+
+        echo "Сalculate the estimate for email = {$email} and simId = {$simId} - done. \r\n";
+    }
+
+}
