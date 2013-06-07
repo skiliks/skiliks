2112cf43a70efa2c2fc558278a5299799d4bf165
Ivan <listepo@hotmail.com>
2013-06-07 18:28:13 +0300

    SKILIKS-2934

--- protected/controllers/statistics/StatisticsController.php
+++ protected/controllers/statistics/StatisticsController.php
@@ -5,13 +5,13 @@ class StatisticsController extends AjaxController
     /**
      *
      */
-    public function actionIndex()
+    public function actionCiTests()
     {
         $this->checkUserDeveloper();
 
-        $this->layout = 'statistics';
+        $this->layout = false;
 
-        $this->render('index');
+        $this->render('ci_tests');
     }
 
     /**
@@ -21,8 +21,37 @@ class StatisticsController extends AjaxController
     {
         $this->checkUserDeveloper();
 
-        $this->layout = 'statistics';
+        $this->layout = false;
 
         $this->render('php_unit_tests');
+    }
+
+    public function actionSeleniumTests()
+    {
+        $this->checkUserDeveloper();
+
+        //$this->layout = 'statistics';
+        $this->layout = false;
+        $this->render('selenium_tests');
+    }
+
+    public function actionSeleniumTestsAuth()
+    {
+        $this->layout = false;
+        $this->checkUserDeveloper();
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://ci.dev.skiliks.com' . $_GET['params']);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Basic aW5kaWNhdG9yOmluZGljYXRvcg==']);
+        curl_exec($ch);
+    }
+
+    public function actionZoho500()
+    {
+        $this->checkUserDeveloper();
+
+        //$this->layout = 'statistics';
+        $this->layout = false;
+        $this->render('zoho_500');
     }
 }
