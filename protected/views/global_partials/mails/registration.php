15f9c0da93214bcb4605a177eef3fd30c70d85ee
Svitlana Morozova <svetaswork@gmail.com>
2013-06-11 14:40:48 +0300

    SKILIKS-2906

--- protected/views/global_partials/mails/registration.php
+++ protected/views/global_partials/mails/registration.php
@@ -25,13 +25,13 @@
                         <table cellspacing="0" cellpadding="15"><tbody><tr><td></td></tr></tbody></table>
                     </td>
                     <td>
-                        <table cellpadding="0" cellspacing="0" width="800">
+                        <table cellpadding="0" cellspacing="0" width="800" border="1">
                             <tr>
                                 <td valign="top" bgcolor="fdfbc6">
                                     <img src="cid:mail-right-1" align="right" style="display:block;"/>
                                     <h1 style="color:#626250;font-family:Tahoma, Geneva, sans-serif;font-size:28px;margin:0 0 15px 0;padding:0;">
 Благодарим вас за выбор skiliks!</h1>
-                                    <p style="margin:0 0 15px 0;color:#555545;font-family:Tahoma, Geneva, sans-serif;font-size:14px;text-align:justify;line-height:20px;">Пожалуйста, <a href="<?= $link ?>">активируйте</a> ваш аккаунт.</p>
+                                    <p style="margin:0 0 15px 0;color:#555545;font-family:Tahoma, Geneva, sans-serif;font-size:14px;text-align:justify;line-height:20px;">Пожалуйста, <a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;" href="<?= $link ?>">активируйте</a> ваш аккаунт.</p>
                                     <a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;" href="http://www.skiliks.com">
                                         www.skiliks.com
                                     </a>
