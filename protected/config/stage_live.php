1ee2009af5352a7791f9137076fc801d040c24ad
slavkaa <nikoolin@ukr.net>
2013-06-11 18:12:32 +0300

    Disable proxing at live.

--- protected/config/stage_live.php
+++ protected/config/stage_live.php
@@ -32,7 +32,17 @@ return CMap::mergeArray(require(dirname(__FILE__) . '/base.php'), array(
         'frontendUrl'=>'http://live.skiliks.com/',
         'public' => [
             'storageURL' => 'http://storage.dev.skiliks.com/v1',
-        ]
+        ],
+        'zoho' => array(
+            'apiKey'              => 'e52059ce3aeff6dd2c71afb9499bdcf7',
+            'saveUrl'             => 'http://live.skiliks.com/zoho/saveExcel',
+            'xlsTemplatesDirPath' => 'documents/templates',
+            'templatesDirPath'    => 'documents/zoho',
+            'sendFileUrl'         => 'https://presheet.zoho.com/remotedoc.im?apikey=%s&output=editor',
+            //'sendFileUrl'         => 'http://zoho.skiliks.com/remotedoc.im?apikey=%s&output=editor',
+            'extExcel'            => 'xls'
+
+        ),
     )
 ));
 
