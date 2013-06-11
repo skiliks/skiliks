0ef410425a43925e65871bda15225b0bc855a82b
Svitlana Morozova <svetaswork@gmail.com>
2013-06-11 16:54:33 +0300

    SKILIKS-2969

--- protected/views/global_partials/mails/verification.php
+++ protected/views/global_partials/mails/verification.php
@@ -1,17 +1,74 @@
-Приветствуем, <?= $name ?>!
-<br/>
-<br/>
-Благодарим за доверие skiliks!
-<br/>
-<br/>
-Пожалуйста, зайдите в ваш <a href="<?= $link ?>">корпоративный кабинет</a> для запуска его работы.
-<br/>
-Из кабинета вы можете:
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Ruseller Email Newsletter</title>
+</head>
+<body>
+
+<table width="856" border="0" cellpadding="0" cellspacing="0">
+    <tr>
+        <td>
+            <img src="cid:mail-top-angela" style="display:block;" />
+        </td>
+    </tr>
+    <tr>
+        <td valign="top">
+            <table cellpadding="0" cellspacing="0">
+                <tr>
+                    <td bgcolor="f2f2f2">
+                        <table cellspacing="0" cellpadding="5"><tbody><tr><td></td></tr></tbody></table>
+                    </td>
+                    <td bgcolor="fdfbc6">
+                        <table cellspacing="0" cellpadding="15"><tbody><tr><td></td></tr></tbody></table>
+                    </td>
+                    <td>
+                        <table cellpadding="0" cellspacing="0" width="842" border="0">
+                            <tr>
+                                <td valign="top" bgcolor="fdfbc6">
+                                    <h1 style="color:#626250;font-family:Tahoma, Geneva, sans-serif;font-size:28px;margin:0 0 15px 0;padding:0;">
+
+Приветствуем, <?= $name ?>!</h1>
+<p style="margin:0 0 15px 0;color:#555545;font-family:Tahoma, Geneva, sans-serif;font-size:14px;text-align:justify;line-height:20px;">
+Благодарим за доверие skiliks!</p>
+<p style="margin:0 0 15px 0;color:#555545;font-family:Tahoma, Geneva, sans-serif;font-size:14px;text-align:justify;line-height:20px;">Пожалуйста, зайдите в ваш <a href="<?= $link ?>">корпоративный кабинет</a> для запуска его работы.</p>
+<p style="margin:0 0 15px 0;color:#555545;font-family:Tahoma, Geneva, sans-serif;font-size:14px;text-align:justify;line-height:20px;">Из кабинета вы можете:</p>
 <ul>
-    <li><a href="<?= $link.'&redirect=profile' ?>">обновить данные,</a></li>
-    <li><a href="<?= $link.'&redirect=dashboard' ?>">пригласить кандидата к оценке,</a></li>
-    <li><a href="<?= $link.'&redirect=profile/corporate/tariff' ?>">выбрать тариф,</a></li>
-    <li><a href="<?= $link.'&redirect=simulations' ?>">пройти симуляцию самостоятельно.</a></li>
+    <li><a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;"  href="<?= $link.'&redirect=profile' ?>">обновить данные,</a></li>
+    <li><a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;"  href="<?= $link.'&redirect=dashboard' ?>">пригласить кандидата к оценке,</a></li>
+    <li><a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;"  href="<?= $link.'&redirect=profile/corporate/tariff' ?>">выбрать тариф,</a></li>
+    <li><a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;"  href="<?= $link.'&redirect=simulations' ?>">пройти симуляцию самостоятельно.</a></li>
 </ul>
-<br/>
-Ваш skiliks
+<a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;" href="http://www.skiliks.com">
+www.skiliks.com
+</a>
+                                </td>
+                                <td><img src="cid:mail-bottom-angela" style="display:block;" /></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <img src="cid:mail-bottom" style="display:block;" />
+        </td>
+    </tr>
+</table>
+<style type="text/css">
+    img {
+        display: block;
+    }
+    h1 {
+        color:#626250;font-family:Tahoma, Geneva, sans-serif;font-size:28px;margin:0 0 15px 0;padding:0;
+    }
+    p {
+        margin:0 0 15px 0;color:#555545;font-family:Tahoma, Geneva, sans-serif;font-size:14px;text-align:justify;line-height:20px;
+    }
+    a {
+        text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;
+    }
+</style>
+</body>
+</html>
