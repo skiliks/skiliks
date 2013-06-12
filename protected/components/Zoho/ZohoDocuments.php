2bc219c18605db7e06fec974562339be3762aaf7
slavkaa <nikoolin@ukr.net>
2013-06-12 15:27:20 +0300

    Add logging to test zoho save.

--- protected/components/Zoho/ZohoDocuments.php
+++ protected/components/Zoho/ZohoDocuments.php
@@ -185,6 +185,10 @@ class ZohoDocuments
      */
     public static function saveFile($returnedId, $tmpFileName, $extention)
     {
+        $f = fopen(__DIR__.'/../../../documents/data.txt', 'w+');
+        fwrite($f, $returnedId."\n");
+        fclose($f);
+
         $path = explode('-', $returnedId);
 
         if (2 !== count($path)) {
