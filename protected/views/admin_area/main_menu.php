bcfb9497c3f66fa11605cf7601fa16a5a55ca532
Ivan <listepo@hotmail.com>
2013-06-12 13:05:06 +0300

    SKILIKS-2964

---
+++ protected/views/admin_area/main_menu.php
@@ -0,0 +1,10 @@
+<div class="nav-collapse collapse">
+    <p class="navbar-text pull-right">
+        Logged in as <a href="#" class="navbar-link">Username</a>
+    </p>
+    <ul class="nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
+</div>
