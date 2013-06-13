eeebd8c7b76258e4dc3ed92ec3eb698dd5d672eb
Ivan <listepo@hotmail.com>
2013-06-13 15:34:36 +0300

    SKILIKS-3031

--- protected/views/debug/style_css.php
+++ protected/views/debug/style_css.php
@@ -1,36 +1,15 @@
 <?php
 
-/**
- * Created by JetBrains PhpStorm.
- * User: gugu
- * Date: 05.02.13
- * Time: 12:14
- * To change this template use File | Settings | File Templates.
- */
 $cs = Yii::app()->clientScript;
 $assetsUrl = $this->getAssetsUrl();
-$cs->registerCssFile($assetsUrl . "/css/style_new.css");
+$cs->registerCssFile($assetsUrl . "/css/styles_new.css");
 ?>
 
 <!DOCTYPE html>
-<html lang="<?php echo Yii::t('site', 'en') ?>">
+<html lang="en">
 <head>
-    <meta property="og:image" content="<?php echo $assetsUrl?>/img/skiliks-fb.png"/>
-    <meta charset="utf-8" />
-    <meta name="description" content="Самый простой и надежный способ проверить навыки менеджеров:
-        деловая онлайн симуляция, имитирующая реальный рабочий день с типичными управленческими задачами
-        и ситуациями принятия решений">
-    <meta property="og:description" content="Самый простой и надежный способ проверить навыки менеджеров:
-        деловая онлайн симуляция, имитирующая реальный рабочий день с типичными управленческими задачами
-        и ситуациями принятия решений">
-    <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-    <title>Skiliks - game the skills</title>
-
-    <!--[if IE]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
 </head>
-
 <body>
+
 </body>
 </html>
