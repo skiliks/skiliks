d93c940daf52c31426ba6e83472fa433f3743e63
Ivan <listepo@hotmail.com>
2013-06-13 18:47:06 +0300

    SKILIKS-3012

--- protected/views/admin_area/admin_area.php
+++ protected/views/admin_area/admin_area.php
@@ -4,9 +4,9 @@
     <base href="<? Yii::app()->request->pathInfo ?>">
     <meta charset="utf-8">
     <title>Template &middot; Bootstrap</title>
-    <link href="/public/admin_area/css/main.css" rel="stylesheet">
     <link href="/public/admin_area/bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="/public/admin_area/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="/public/admin_area/css/main.css" rel="stylesheet">
     <script src="/public/admin_area/js/jquery-2.0.2.js"></script>
     <script src="/public/admin_area/bootstrap/js/bootstrap.js"></script>
     <script src="/public/admin_area/js/main.js"></script>
