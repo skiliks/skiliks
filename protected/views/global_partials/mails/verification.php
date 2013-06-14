296266ace8576b8884c7970c45fbb8bf0571c844
Svitlana Morozova <svetaswork@gmail.com>
2013-06-14 17:52:43 +0300

    SKILIKS-2969

--- protected/views/global_partials/mails/verification.php
+++ protected/views/global_partials/mails/verification.php
@@ -37,7 +37,7 @@
     <li><a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;"  href="<?= $link.'&redirect=profile' ?>">обновить данные,</a></li>
     <li><a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;"  href="<?= $link.'&redirect=dashboard' ?>">пригласить кандидата к оценке,</a></li>
     <li><a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;"  href="<?= $link.'&redirect=profile/corporate/tariff' ?>">выбрать тариф,</a></li>
-    <li><a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;"  href="<?= $link.'&redirect=simulations' ?>">пройти симуляцию самостоятельно.</a></li>
+    <li><a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;"  href="<?= $link.'&redirect=simulations' ?>">пройти симуляцию</a> самостоятельно.</li>
 </ul>
                                     <p style="margin:0 0 15px 0;color:#555545;font-family:Tahoma, Geneva, sans-serif;font-size:14px;text-align:justify;line-height:20px;">Ваш <a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;" href="http://www.skiliks.com">skiliks</a></p>
                                 </td>
