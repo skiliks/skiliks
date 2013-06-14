11ca77ca3dfca6dc5c7f1ddc640283ed495177db
Svitlana Morozova <svetaswork@gmail.com>
2013-06-14 16:54:34 +0300

    SKILIKS-2903

--- protected/views/global_partials/mails/registration.php
+++ protected/views/global_partials/mails/registration.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 
-<table width="850" border="0" cellpadding="0" cellspacing="0">
+<table width="880" border="0" cellpadding="0" cellspacing="0">
     <tr>
         <td>
             <img src="cid:mail-top-angela" style="display:block;" />
@@ -13,7 +13,7 @@
     </tr>
     <tr>
         <td valign="top">
-            <table cellpadding="0" cellspacing="0" width="840">
+            <table cellpadding="0" cellspacing="0" width="880">
                 <tr>
                     <td bgcolor="f2f2f2">
                         <table cellspacing="0" cellpadding="5"><tbody><tr><td></td></tr></tbody></table>
@@ -21,8 +21,8 @@
                     <td bgcolor="fdfbc6">
                         <table cellspacing="0" cellpadding="15"><tbody><tr><td></td></tr></tbody></table>
                     </td>
-                    <td>
-                        <table cellpadding="0" cellspacing="0" width="840" border="0">
+                    <td bgcolor="f2f2f2" alight="right">
+                        <table cellpadding="0" cellspacing="0" width="830" border="0" bgcolor="f2f2f2">
                             <tr>
                                 <td valign="top" bgcolor="fdfbc6">
                                     <h1 style="color:#626250;font-family:Tahoma, Geneva, sans-serif;font-size:28px;margin:0 0 15px 0;padding:0;">
@@ -30,7 +30,7 @@
                                     <p style="margin:0 0 15px 0;color:#555545;font-family:Tahoma, Geneva, sans-serif;font-size:14px;text-align:justify;line-height:20px;">Пожалуйста, <a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;" href="<?= $link ?>">активируйте</a> ваш аккаунт.</p>
                                     <p style="margin:0 0 15px 0;color:#555545;font-family:Tahoma, Geneva, sans-serif;font-size:14px;text-align:justify;line-height:20px;">Ваш <a style="text-decoration:none;color:#147b99;font-family:Tahoma, Geneva, sans-serif;font-size:14px;" href="http://www.skiliks.com">skiliks</a></p>
                                 </td>
-                                <td><img src="cid:mail-bottom-angela" style="display:block;" /></td>
+                                <td bgcolor="f2f2f2" alight="right"><img src="cid:mail-bottom-angela" style="display:block;" /></td>
                             </tr>
                         </table>
                     </td>
