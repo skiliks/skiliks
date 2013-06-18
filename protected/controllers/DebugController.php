246e9a86a46712d9bc91ac155043a9d6eaf47174
slavkaa <nikoolin@ukr.net>
2013-06-18 18:05:43 +0300

    SKILIKS-3102. Add page fot pop-ups styles to statdards.

--- protected/controllers/DebugController.php
+++ protected/controllers/DebugController.php
@@ -3,16 +3,31 @@
 
 class DebugController extends AjaxController
 {
+    /**
+     *
+     */
     public function actionIndex()
     {
         //TestUserHelper::addUser("personal");
         echo TestUserHelper::getActivationUrl("ivan@skiliks.com");
     }
 
+    /**
+     *
+     */
     public function actionStyleCss()
     {
         $this->layout = false;
         $this->render('style_css');
+    }
+
+    /**
+     *
+     */
+    public function actionStyleForPopupCss()
+    {
+        $this->layout = false;
+        $this->render('style_for_popup_css');
     }
 }
 
---
+++ protected/views/debug/style_for_popup_css.php
@@ -0,0 +1,50 @@
+<?php
+
+$cs = Yii::app()->clientScript;
+$assetsUrl = $this->getAssetsUrl();
+
+$cs->scriptMap=array(
+    'jquery.js'=>$assetsUrl . '/js/jquery/jquery-1.9.1.min.js',
+    'jquery.min.js'=>$assetsUrl . '/js/jquery/jquery-1.9.1.min.js',
+    'jquery.ba-bbq.js'=>$assetsUrl . '/js/jquery/jquery.ba-bbq.js',
+);
+
+$cs->registerCoreScript('jquery');
+$cs->registerScriptFile($assetsUrl . '/js/cufon-yui.js');
+$cs->registerScriptFile($assetsUrl . '/js/ProximaNova.font.js');
+
+$cs->registerCssFile($assetsUrl . "/css/styles_new.css");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+
+body {
+    background:#7cb8c2;
+}
+.blockfortest, .testblocks div {
+    display: inline-block;
+    height:20px;
+    margin: 0px 20px;
+    width:30px;
+}
+.razdelitel {
+    clear:both;
+    height: 30px;
+}
+</style>
+
+</head>
+<body style="padding: 100px;">
+
+</body>
+
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+        Cufon.replace('.btn', {fontFamily:"ProximaNova-Bold", hover: true});
+    });
+</script>
+
+</html>
