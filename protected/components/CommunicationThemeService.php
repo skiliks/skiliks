a23a823eacad7b302ce579d1a831df555bcd02aa
Ivan <listepo@hotmail.com>
2013-06-19 11:09:27 +0300

    SKILIKS-3051

--- protected/components/CommunicationThemeService.php
+++ protected/components/CommunicationThemeService.php
@@ -3,12 +3,17 @@
 class CommunicationThemeService {
 
     public function addToTheLogUsed(Simulation $simulation, $theme_id) {
-        $log = new LogCommunicationThemeUsage();
-        $log->sim_id = $simulation->id;
-        $log->communication_theme_id = $theme_id;
-        if(false === $log->save(false)){
-            throw new Exception("Not save");
+
+        $log = LogCommunicationThemeUsage::model()->findByAttributes(['sim_id'=>$simulation->id, 'communication_theme_id'=>$theme_id]);
+        if(null === $log){
+            $log = new LogCommunicationThemeUsage();
+            $log->sim_id = $simulation->id;
+            $log->communication_theme_id = $theme_id;
+            if(false === $log->save(false)){
+                throw new Exception("Not save");
+            }
         }
+
     }
 
 }
