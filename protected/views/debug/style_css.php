1513e5a14f41bd86d52f44d5c0111a060f2ebdbc
Malysh <svetlana@skiliks.com>
2013-06-20 09:43:39 +0100

    SKILIKS-3007

--- protected/views/debug/style_css.php
+++ protected/views/debug/style_css.php
@@ -226,6 +226,14 @@ body {
 </section>
 
 <div class="razdelitel"></div><div class="razdelitel"></div>
+    <div class="controls-border">
+         <div class="block-border bg-yellow grid2 border-primary">
+             <div class="pad20">AAA</div>
+         </div>
+    </div>
+<div class="razdelitel"></div>
+
+
 </div>
 </body>
 
