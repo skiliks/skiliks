6486bd0433151c9839b13a705a11ebf201e64af6
Ivan <listepo@hotmail.com>
2013-06-20 14:27:18 +0300

    SKILIKS-3132

--- protected/controllers/DebugController.php
+++ protected/controllers/DebugController.php
@@ -29,5 +29,23 @@ class DebugController extends AjaxController
         $this->layout = false;
         $this->render('style_for_popup_css');
     }
+
+    public function actionStyleGrid()
+    {
+        $this->layout = false;
+        $this->render('style_grid');
+    }
+
+    public function actionStyleGridResults()
+    {
+        $this->layout = false;
+        $this->render('style_grid_results');
+    }
+
+    public function actionStyleBlocks()
+    {
+        $this->layout = false;
+        $this->render('style_blocks');
+    }
 }
 
