c333054e8d9b966f36b86bfbf427b42d83abe3a0
Malysh <svetlana@skiliks.com>
2013-06-20 13:24:48 +0100

    SKILIKS-3007

--- protected/views/debug/style_for_popup_css.php
+++ protected/views/debug/style_for_popup_css.php
@@ -101,16 +101,16 @@ body {
     <div class="razdelitel"></div>
 
     <div class="container-4">
-        <div class="grid1 text-center">
+        <div class="grid1">
             <h2 class="font-white font-xlarge text-center">Уровень владения навыками</h2>
         </div>
-        <div class="grid1 text-center">
+        <div class="grid1">
             <h2 class="font-white font-xlarge text-center">Уровень достижения результатов: количество и значимость выполненных задач</h2>
         </div>
-        <div class="grid1 text-center">
+        <div class="grid1">
             <h2 class="font-white font-xlarge text-center">Скорость достижения результатов</h2>
         </div>
-        <div class="grid1 text-center">
+        <div class="grid1">
             <h2 class="font-white font-xlarge text-center">Личностные качества, проявленные в симуляции</h2>
         </div>
     </div>
@@ -130,22 +130,31 @@ body {
         </div>
         <div class="razdelitel"></div>
 
-        <h2 class="font-dark">Три колонки</h2>
+        <h2 class="font-dark">Три колонки (1 +2 )</h2>
 
         <div class="container-results-3">
-            <div class="grid1">!!!</div>
-            <div class="grid2 bg-blue-block border-primary"><div class="pad16">AAA</div></div>
+            <div class="grid1">AAA</div>
+            <div class="grid2 bg-blue-block border-primary"><div class="pad16">BBB</div></div>
         </div>
         <div class="razdelitel"></div>
 
         <h2 class="font-dark">Четыре колонки</h2>
 
         <div class="container-results-4">
-            <div class="grid1">!!!</div>
-            <div class="grid1">!!!</div>
-            <div class="grid1">!!!</div>
-            <div class="grid1">!!!</div>
+            <div class="grid1">
+                <h2 class="font-xlarge text-center">Уровень владения навыками</h2>
+            </div>
+            <div class="grid1">
+                <h2 class="font-xlarge text-center">Уровень достижения результатов: количество и значимость выполненных задач</h2>
+            </div>
+            <div class="grid1">
+                <h2 class="font-xlarge text-center">Скорость достижения результатов</h2>
+            </div>
+            <div class="grid1">
+                <h2 class="font-xlarge text-center">Личностные качества, проявленные в симуляции</h2>
+            </div>
         </div>
+        <div class="razdelitel"></div>
     </div>
 </div>
 
