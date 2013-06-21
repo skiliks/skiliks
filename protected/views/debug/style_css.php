96b0235df7ccf676f31a705fb2f435dba332868d
Malysh <svetlana@skiliks.com>
2013-06-21 16:37:54 +0100

    SKILIKS-3113

--- protected/views/debug/style_css.php
+++ protected/views/debug/style_css.php
@@ -242,7 +242,7 @@ body {
 
 <script type="text/javascript">
     jQuery(document).ready(function(){
-        Cufon.replace('.btn', {fontFamily:"ProximaNova-Bold", hover: true});
+        Cufon.replace('.btn, .proxima-bold, h1, h2, h3, h4, h5, .dark-labels label, .list-dark li', {fontFamily:"ProximaNova-Bold", hover: true});
     });
 </script>
 
