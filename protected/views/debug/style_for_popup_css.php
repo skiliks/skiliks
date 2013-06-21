7664fd59855bd842dd92afe0d3435004e53794a1
Malysh <svetlana@skiliks.com>
2013-06-21 11:02:55 +0100

    SKILIKS-3112

--- protected/views/debug/style_for_popup_css.php
+++ protected/views/debug/style_for_popup_css.php
@@ -62,7 +62,8 @@ body {
             <a href="#" class="ui-dialog-titlebar-close ui-corner-all" role="button"><span class="ui-icon ui-icon-closethick">close</span></a>
         </div>
         <div class="ui-dialog-content ui-widget-content">
-            Content
+            <div class="popup-primary-title"></div>
+            <div>Content</div>
         </div>
     </div>
 
