96e5f48ef23d29452436cd42f7a7bb621259bf65
Tetyana Grybok <tetyana.grybok@skiliks.com>
2013-06-22 21:28:59 +0300

    test fix

--- protected/tests/selenium/Other/WindowHasCogwheel_SK3063_Test.php
+++ protected/tests/selenium/Other/WindowHasCogwheel_SK3063_Test.php
@@ -40,14 +40,16 @@ class WindowHasCogwheel_SK3063_Test extends SeleniumTestHelper
     private function has_settings($window)
     {
         $this->optimal_click($window);
-        $this->assertTrue($this->verifyVisible(Yii::app()->params['test_mappings']['icons']['settings']));
+        sleep(2);
+        $this->assertTrue($this->isVisible(Yii::app()->params['test_mappings']['icons']['settings']));
         $this->optimal_click(Yii::app()->params['test_mappings']['icons']['close']);
     }
 
     private function has_not_settings($window)
     {
         $this->optimal_click($window);
-        $this->assertFalse($this->verifyVisible(Yii::app()->params['test_mappings']['icons']['settings']));
+        sleep(2);
+        $this->assertTrue($this->isNotVisible(Yii::app()->params['test_mappings']['icons']['settings']));
         $this->optimal_click(Yii::app()->params['test_mappings']['icons']['close']);
     }
 
