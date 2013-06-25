c5b5b4e0f70a07f4343b8d2e35509d4dd9f9b950
Malysh <svetlana@skiliks.com>
2013-06-25 09:19:41 +0100

    SKILIKS-3113

--- protected/views/debug/style_css.php
+++ protected/views/debug/style_css.php
@@ -242,7 +242,7 @@ body {
 
 <script type="text/javascript">
     jQuery(document).ready(function(){
-        //Cufon.replace('li, p, label, input, select', {fontFamily:"ProximaNova-Regular", hover: true});
+        Cufon.replace('li, p, label, input, select', {fontFamily:"ProximaNova-Regular", hover: true});
         Cufon.replace('.btn, .proxima-bold, h1, h2, h3, h4, h5, .dark-labels label, .list-dark li', {fontFamily:"ProximaNova-Bold", hover: true});
         Cufon.replace('.yiiPager .previous a, .yiiPager .next a', {fontFamily:"Conv_ProximaNova-Semibold", hover: true});
     });
