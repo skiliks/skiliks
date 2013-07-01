baa9a62907e3c7fcf22c827a96e2952823e521f1
Malysh <svetlana@skiliks.com>
2013-07-01 16:49:35 +0100

    SKILIKS-3191

--- protected/views/layouts/static.php
+++ protected/views/layouts/static.php
@@ -85,7 +85,6 @@ $cs->registerCssFile($assetsUrl . "/css/static.css");
 
 			<!--content-->
 			<div class="content">
-                DDD
                 <!-- flash-messages { -->
                 <?php $flashes = Yii::app()->user->getFlashes(); ?>
                 <?php if (0 < count($flashes)): ?>
