f2851b6310e36acb8628419fa0d7b564f3c8e298
Malysh <svetlana@skiliks.com>
2013-07-03 13:12:07 +0100

    SKILIKS-3229

--- protected/assets/js/main.js
+++ protected/assets/js/main.js
@@ -273,7 +273,7 @@
                 resizable: false,
                 draggable: false,
                 title: '',
-                width: 584,
+                width: 590,
                 position: {
                     my: "left top",
                     at: "left top",
