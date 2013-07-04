811c7b37e9d5063187c80bf373beacbd5f643d2b
slavkaa <nikoolin@ukr.net>
2013-07-04 18:30:41 +0300

    Fix cufon ProximaNova to include cyrilic letters.

--- protected/views/layouts/site_standard.php
+++ protected/views/layouts/site_standard.php
@@ -195,8 +195,9 @@ $cs->registerCssFile($assetsUrl . "/css/static.css");
     <script type="text/javascript">
         Cufon.replace('.menu-site li, .unstyled li, p, label, input, select, .proxima-reg, .sbHolder a, .errorMessage, .feedback-dialog-title, .to-top', {fontFamily:"ProximaNova-Regular", hover: true});
         Cufon.replace('.btn, .proxima-bold, h1, h2, h3, h4, h5, strong, .dark-labels label, .list-dark li, .items th, .items td, .add-vacancy-popup h1, .ui-dialog-title', {fontFamily:"ProximaNova-Bold", hover: true});
-        Cufon.replace('.feedback-dialog-title', {fontFamily:"ProximaNova-Regular", hover: true});
-        Cufon.replace('.semi, .yiiPager, .yiiPager li, .yiiPager a, .yiiPager .next a, .yiiPager .next a', {fontFamily:"ProximaNova-Semibold", hover: true});
+        Cufon.replace('.semi, .yiiPager', {fontFamily:"ProximaNova-Semibold", hover: true});
+        Cufon.replace('.feedback-dialog-title, .yiiPager', {fontFamily:"ProximaNova-Regular", hover: true});
+
 
         Cufon.now();
     </script>
