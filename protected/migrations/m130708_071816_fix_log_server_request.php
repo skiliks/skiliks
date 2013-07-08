2dc59d6ba21197dd84f8fb45a57daceeaf65c262
slavkaa <nikoolin@ukr.net>
2013-07-08 10:25:20 +0300

    Fix log_server_request.

--- protected/migrations/m130708_071816_fix_log_server_request.php
+++ protected/migrations/m130708_071816_fix_log_server_request.php
@@ -4,13 +4,11 @@ class m130708_071816_fix_log_server_request extends CDbMigration
 {
 	public function up()
 	{
-        $this->alterColumn('log_server_request', 'frontend_game_time', 'time default null');
-        $this->alterColumn('log_server_request', 'frontend_game_time', 'time default null');
+        $this->alterColumn('log_server_request', 'backend_game_time', 'time default null');
 	}
 
 	public function down()
 	{
-        $this->alterColumn('log_server_request', 'frontend_game_time', 'time not null');
-        $this->alterColumn('log_server_request', 'frontend_game_time', 'time not null');
+        $this->alterColumn('log_server_request', 'backend_game_time', 'time not null');
 	}
 }
