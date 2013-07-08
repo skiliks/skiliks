7717cc7e3e19769ad0d1ee6432ab8d49742ccb80
Malysh <svetlana@skiliks.com>
2013-07-08 16:40:54 +0100

    SKILIKS-3239

--- protected/views/layouts/site_standard.php
+++ protected/views/layouts/site_standard.php
@@ -195,7 +195,7 @@ $cs->registerCssFile($assetsUrl . "/css/static.css");
     <script type="text/javascript">
         Cufon.replace('.menu-site li, .unstyled li, p, label, input, select, .proxima-reg, .sbHolder a, .errorMessage, .feedback-dialog-title, .to-top, small', {fontFamily:"ProximaNova-Regular", hover: true});
         Cufon.replace('.btn, .proxima-bold, h1, h2, h3, h4, h5, strong, .dark-labels label, .list-dark li, .items th, .items td, .add-vacancy-popup h1, .ui-dialog-title', {fontFamily:"ProximaNova-Bold", hover: true});
-        Cufon.replace('.semi, .yiiPager', {fontFamily:"ProximaNova-Semibold", hover: true});
+        Cufon.replace('.semi, .yiiPager', {fontFamily:"ProximaNova-Semibold, .side-menu .active", hover: true});
         Cufon.replace('.feedback-dialog-title', {fontFamily:"ProximaNova-Regular", hover: true});
 
 
