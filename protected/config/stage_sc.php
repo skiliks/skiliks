4c337d6ef9757adebd5e64535f2087f169bd6901
slavkaa <nikoolin@ukr.net>
2013-07-08 15:18:32 +0300

    Add separeted config for social cals server.

---
+++ protected/config/stage_sc.php
@@ -0,0 +1,29 @@
+<?php
+
+// for SocialCalc
+
+define(YII_DEBUG, false);
+return CMap::mergeArray(require(dirname(__FILE__) . '/base.php'), array(
+    'preload'=> array('log', 'RSentryException'),
+    'components'=>array(
+        'db'=>array(
+            'connectionString' => 'mysql:host=localhost;dbname=skiliks_sc',
+            'emulatePrepare' => true,
+            'username' => 'skiliks_sc',
+            'password' => 'dep-vep-eb-up-a',
+            'charset' => 'utf8',
+
+            'enableParamLogging'=>true,
+            'enableProfiling'=>true
+        )
+    ),
+    'public' => [
+        'useSentryForJsLog' => false,
+    ],
+    'params'=>array(
+        'frontendUrl' => 'http://socialcalc.skiliks.com/',
+        'zoho'        => [],
+    )
+));
+
+
