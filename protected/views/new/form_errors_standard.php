4a199c3ae09da81dd618f8aa638956b3caff104e
slavkaa <nikoolin@ukr.net>
2013-07-08 10:23:15 +0300

    Add files for form standarts .

--- protected/views/new/form_errors_standard.php
+++ protected/views/new/form_errors_standard.php
@@ -1,8 +1,122 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: root
- * Date: 7/8/13
- * Time: 1:55 AM
- * To change this template use File | Settings | File Templates.
- */
+<div class="form invite-people-form sideform darkblueplacehld">
+
+    <h2>Отправить приглашение</h2>
+
+    <?php $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'invite-form'
+    )); ?>
+
+    <span class="form-global-errors">
+        <?php echo $form->error($invite, 'invitations'); // You has no available invites! ?>
+    </span>
+
+    <div class="row <?php echo ($form->error($invite, 'firstname') || $form->error($invite, 'lastname')) ? 'error' : ''; ?>">
+        <?php echo $form->labelEx($invite, 'full_name'); ?>
+        <?php echo $form->textField($invite, 'firstname', ['placeholder' => Yii::t('site','First name')]); ?>
+        <?php echo $form->error($invite, 'firstname'); ?>
+        <?php echo $form->textField($invite, 'lastname', ['placeholder'  => Yii::t('site','Last Name')]); ?>
+        <?php echo $form->error($invite, 'lastname'); ?>
+    </div>
+
+    <div class="row <?php echo ($form->error($invite, 'email')) ? 'error' : ''; ?>">
+        <?php echo $form->labelEx($invite, 'email'); ?>
+        <?php echo $form->textField($invite, 'email', ['placeholder' => Yii::t('site','Enter Email address')]); ?>
+        <?php echo $form->error($invite, 'email'); ?>
+    </div>
+
+    <div class="row wide <?php echo (0 == count($vacancies) ? 'no-border' : '') ?> <?php echo ($form->error($invite, 'vacancy_id')) ? 'error' : ''; ?>v">
+        <?php echo $form->labelEx($invite, 'vacancy_id'); ?>
+        <?php echo $form->dropDownList($invite, 'vacancy_id', $vacancies); ?>
+        <?php echo $form->error($invite, 'vacancy_id'); ?>
+    </div>
+
+    <?php $this->endWidget(); ?>
+</div>
+
+<div class="form profileform radiusthree">
+
+    <?php $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'account-corporate-password-form'
+    )); ?>
+
+    <div class="row">
+        <?php echo $form->labelEx($passwordForm, 'currentPassword'); ?>
+        <?php echo $form->passwordField($passwordForm, 'currentPassword'); ?>
+        <?php echo $form->error($passwordForm, 'currentPassword'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($passwordForm, 'password'); ?>
+        <?php echo $form->passwordField($passwordForm, 'password'); ?>
+        <?php echo $form->error($passwordForm, 'password'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($passwordForm, 'verifyPassword'); ?>
+        <?php echo $form->passwordField($passwordForm, 'verifyPassword'); ?>
+        <?php echo $form->error($passwordForm, 'verifyPassword'); ?>
+    </div>
+
+    <?php $this->endWidget(); ?>
+</div>
+
+<div class="form profileform radiusthree" style="margin-top: 20px;">
+
+    <?php $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'account-corporate-password-form'
+    )); ?>
+
+    <div class="row">
+        <?php echo $form->labelEx($passwordForm2, 'currentPassword'); ?>
+        <?php echo $form->passwordField($passwordForm2, 'currentPassword'); ?>
+        <?php echo $form->error($passwordForm2, 'currentPassword'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($passwordForm2, 'password'); ?>
+        <?php echo $form->passwordField($passwordForm2, 'password'); ?>
+        <?php echo $form->error($passwordForm2, 'password'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($passwordForm2, 'verifyPassword'); ?>
+        <?php echo $form->passwordField($passwordForm2, 'verifyPassword'); ?>
+        <?php echo $form->error($passwordForm2, 'verifyPassword'); ?>
+    </div>
+
+    <?php $this->endWidget(); ?>
+</div>
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
