382c933384b265810a252b209bf80ef34a2fbba2
slavkaa <nikoolin@ukr.net>
2013-07-09 11:02:15 +0300

    Add pages for layout standartization.

---
+++ protected/views/new/password_corporate.php
@@ -0,0 +1,52 @@
+
+<h2 class="thetitle"><?php echo Yii::t('site', 'Profile') ?></h2>
+
+<div class="transparent-boder profilewrap">
+
+<?php $this->renderPartial('//new/_menu_corporate', ['active' => ['password' => true]]) ?>
+
+    <div class="form profileform radiusthree">
+
+        <?php $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'account-corporate-password-form'
+        )); ?>
+
+        <div class="row">
+            <p class="text16"><?php if($is_done) { echo Yii::t('site', 'Ваш новый пароль сохранён'); }else{ echo Yii::t('site', 'Вы можете изменить пароль'); } ?></p>
+        </div>
+
+        <div class="row">
+            <?php echo $form->labelEx($passwordForm, 'currentPassword'); ?>
+            <?php echo $form->passwordField($passwordForm, 'currentPassword'); ?><?php echo $form->error($passwordForm, 'currentPassword'); ?>
+        </div>
+
+        <div class="row">
+            <?php echo $form->labelEx($passwordForm, 'password'); ?>
+            <?php echo $form->passwordField($passwordForm, 'password'); ?><?php echo $form->error($passwordForm, 'password'); ?>
+        </div>
+
+        <div class="row">
+            <?php echo $form->labelEx($passwordForm, 'verifyPassword'); ?>
+            <?php echo $form->passwordField($passwordForm, 'verifyPassword'); ?><?php echo $form->error($passwordForm, 'verifyPassword'); ?>
+        </div>
+        <?php if($is_done) { ?>
+            <div class="done-password-change"></div>
+        <?php }else{ ?>
+        <div class="row buttons">
+            <?php echo CHtml::submitButton(Yii::t('site', 'Сохранить изменения'), ['name' => 'save']); ?>
+        </div>
+        <?php } ?>
+
+        <?php $this->endWidget(); ?>
+    </div>
+</div>
+<script>
+    $(document).ready(function(){
+        var errors = $(".errorMessage");
+        for (var i=0; i < errors.length;i++) {
+            var inp = $(errors[i]).prev("input.error");
+            $(inp).css({"border":"2px solid #bd2929"});
+            $(errors[i]).addClass($(inp).attr("id"));
+        }
+    });
+</script>
