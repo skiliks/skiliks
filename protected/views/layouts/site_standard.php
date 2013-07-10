e520b370066a10e39651bcd47c2fc54d9bbb8c74
Malysh <svetlana@skiliks.com>
2013-07-10 09:29:53 +0100

    SKILIKS-3239

--- protected/views/layouts/site_standard.php
+++ protected/views/layouts/site_standard.php
@@ -198,9 +198,6 @@ $cs->registerCssFile($assetsUrl . "/css/static.css");
             '.side-menu .active a', {fontFamily:"ProximaNova-Bold", hover: true});
         Cufon.replace('.semi, .yiiPager', {fontFamily:"ProximaNova-Semibold", hover: true});
         Cufon.replace('.feedback-dialog-title', {fontFamily:"ProximaNova-Regular", hover: true});
-
-        Cufon.replace('.side-menu .active a', {fontFamily:"ProximaNova-Bold", hover: true});
-
         Cufon.now();
     </script>
     </body>
