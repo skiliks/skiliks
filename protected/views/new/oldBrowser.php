b515aed12dd26c2a058089829ab23bdb9944fe43
Malysh <svetlana@skiliks.com>
2013-07-12 12:36:39 +0100

    SKILIKS-3284

--- protected/views/new/oldBrowser.php
+++ protected/views/new/oldBrowser.php
@@ -1,6 +1,6 @@
 <div class="">
     <h1 class="page-header">К сожалению, в данный момент мы не поддерживаем ваш браузер</h1>
-    <span class="font-xxlarge">Предлагаем установить</span>
+    <!--<span class="font-xxlarge">Предлагаем установить</span>
     <div class="container-3-short">
         <div class="grid2">
             <h3 class="proxima-reg">Google Chrome 27</h3>
@@ -11,5 +11,5 @@
             <h3 class="proxima-reg">Mozilla Firefox 21</h3>
             <div class="ico-firefox"></div><a class="btn btn-site btn-green" href="http://www.mozilla.org/firefox/new/" target="_blank">Установить</a>
         </div>
-    </div>
+    </div>-->
 </div>
