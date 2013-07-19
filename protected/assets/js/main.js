4615391f111e18b0e7233704feab5f8576e25adf
Malysh <svetlana@skiliks.com>
2013-07-19 09:26:07 +0100

    SKILIKS-3394

--- protected/assets/js/main.js
+++ protected/assets/js/main.js
@@ -116,7 +116,7 @@ var fixLogotypes = function() {
                 autoOpen:    true,
                 resizable:   false,
                 draggable:   false,
-                width:       600,
+                width:       940,
                 open: function( event, ui ) {
                     $(this).find('.accept-requirements').attr('href', link);
                 }
