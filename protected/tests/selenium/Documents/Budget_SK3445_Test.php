92a3888471504c45f7fc088636c5fa9f1fe893b0
Tetyana Grybok <tetyana.grybok@skiliks.com>
2013-07-22 17:16:08 +0300

    zindex SK3361

---
+++ protected/tests/selenium/Documents/Budget_SK3445_Test.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * \addtogroup Selenium
+ * @{
+ */
+/**
+ * Тест для проверки возможности посчитать сводный бюджет и набрать 55 баллов по результативности (связанной с подсчетом сводного бюджета) (для SK3445))
+ */
+class ZIndexOfWindows_SK3361_Test extends SeleniumTestHelper
+{
+    public function test_ZIndexOfWindows_SK3361 ()
+    {
+        $this->start_simulation();
+
+        /*$this->clearEventQueueBeforeEleven('RST1');
+
+        $this->run_event('T3.1',"xpath=(//*[contains(text(),'Егор, приветствую')])",'click');
+        $this->optimal_click("xpath=(//*[contains(text(),'Отлично, но у тебя не больше пяти минут')])");
+
+        $this->run_event('ET9', "css=li.icon-active.phone a", 'click');
+        $this->optimal_click(Yii::app()->params['test_mappings']['phone']['reply']);
+        $this->optimal_click("xpath=(//*[contains(text(),'Василий, вопрос в чем?')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'Василий, давайте ближе к делу!')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'Василий, так какое у вас ко мне дело?')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'Василий, прошу прощения, вы к чему все это рассказываете?')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'Та-а-ак, и что же в вашем бюджете изменилось?')])");
+        $this->optimal_click("xpath=(//*[contains(text(),'Принимаю, что же с вами делать. Сейчас посмотрю и внесу изменения. ')])");
+        sleep(30);
+
+        $this->optimal_click(Yii::app()->params['test_mappings']['icons']['mail']);
+        $this->optimal_click("xpath=(//*[contains(text(), 'бюджет логистики')])");
+        $this->optimal_click("css=.save-attachment-icon");
+        $this->optimal_click("css=.mail-popup-button");
+        $this->optimal_click("xpath=(//*[contains(text(), 'новый бюджет по производству')])");
+        $this->optimal_click("css=.save-attachment-icon");
+        $this->optimal_click("css=.mail-popup-button");
+        $this->optimal_click(Yii::app()->params['test_mappings']['icons']['documents']);
+        sleep(5);
+
+        //начинаем работу с документами
+        $this->doubleClick("xpath=//*[@id='Бюджет логистики_2014_план.xls']/div[2]");
+        $this->waitForElementPresent("css=#cell_B7");
+        $this->optimal_click("css=#cell_B7");
+        $this->keyDownNative("16");
+        $this->optimal_click("css=#cell_M14");
+
+        $this->optimal_click();
+
+        $this->doubleClick("xpath=//*[@id='Бюджет логистики_2014_план.xls']/div[2]");
+
+        $this->doubleClick("xpath=//*[@id='Бюджет логистики_2014_план.xls']/div[2]");
+
+        */
+        $this->simulation_stop();
+    }
+}
--- protected/tests/selenium/Other/ZIndexOfWindows_SK3361_Test.php
+++ protected/tests/selenium/Other/ZIndexOfWindows_SK3361_Test.php
@@ -4,7 +4,7 @@
  * @{
  */
 /**
- * Тест для проверки наличия шестеренок для настройки звука в окнах (для SK3361))
+ * Тест для проверки открытия каждого нового окна поверх предыдущего (для SK3361))
  */
 class ZIndexOfWindows_SK3361_Test extends SeleniumTestHelper
 {
@@ -15,24 +15,15 @@ class ZIndexOfWindows_SK3361_Test extends SeleniumTestHelper
         $this->optimal_click(Yii::app()->params['test_mappings']['icons']['todo']);
         sleep(5);
         $this->assertTrue($this->isVisible("css=.plan-todo-tit-hide"));
-        //printf($this->getEval("$('.sim-window.mail-window').css('z-index')"));
-        printf($this->getEval("window.document.getElementById('plannerBookAfterVacation').style.zIndex"));
-        //printf($this->getEval("$(this.browserbot.getCurrentWindow().document).find('body')"));
-        //$(this.browserbot.getCurrentWindow().document).find('body');
-        /*$this->optimal_click(Yii::app()->params['test_mappings']['icons']['mail']);
-        sleep(4);
+        $a_zindex=$this->getEval("window.document.getElementById('plan-window').style.z-index");
+
+        $this->optimal_click(Yii::app()->params['test_mappings']['icons']['mail']);
+        sleep(5);
         $this->assertTrue($this->isVisible("css=.MOVE_TO_TRASH"));
-        printf($this->getEval("this.browserbot.getCurrentWindow().document.getElementByClassName('sim-window mail-window ui-draggable').style.z-index"));
+        $b_zindex=$this->getEval("window.document.getElementById('mail-window').style.z-index");
 
-        $this->optimal_click(Yii::app()->params['test_mappings']['icons']['phone']);
-        sleep(4);
-        $this->assertTrue( $this->isVisible("css=.phone-menu-btn.phone_get_menu"));
-        printf($this->getEval("this.browserbot.getCurrentWindow().document.getElementByClassName('sim-window ui-draggable').style.z-index"));
+        $this->assertTrue($a_zindex<$b_zindex);
 
-        $this->optimal_click(Yii::app()->params['test_mappings']['icons']['documents']);
-        sleep(4);
-        $this->assertTrue($this->isVisible("css=.elfinder-cwd-filename.ui-draggable"));
-        printf($this->getEval("this.browserbot.getCurrentWindow().document.getElementByClassName('sim-window ui-draggable').style.z-index"));
-        $this->simulation_stop();*/
+        $this->simulation_stop();
     }
 }
