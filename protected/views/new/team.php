1eab97daafd97178a7a14d4ab1f86334dd3480a9
Malysh <svetlana@skiliks.com>
2013-07-23 09:48:33 +0100

    SKILIKS-3348

--- protected/views/new/team.php
+++ protected/views/new/team.php
@@ -1,6 +1,6 @@
 <div class="team">
     <h1 class="page-header text-center"><?php echo Yii::t('site', 'Meet the Team') ?></h1>
-	<p class="font-white text-center font-xlarge text-about"><?php echo Yii::t('site', 'As managers we know the issue of hiring and developing professionals. As consultants and continuous improvement practitioners we know the way this issue can be solved. So we decided to create the product that would help managers of any level in any business to build their teams with much more confidence and without time-consuming assessment process.') ?></p>
+	<p class="font-white text-center font-large text-about"><?php echo Yii::t('site', 'As managers we know the issue of hiring and developing professionals. As consultants and continuous improvement practitioners we know the way this issue can be solved. So we decided to create the product that would help managers of any level in any business to build their teams with much more confidence and without time-consuming assessment process.') ?></p>
 	
 	<ul class="team-list">
 		<li class="maria">
@@ -26,7 +26,7 @@
 	</ul>
 	
 	<div class="team-values">
-        <h2 class="font-blue-dark text-center"><?php echo Yii::t('site', 'Our Values') ?></h2><ul class="font-xlarge">
+        <h2 class="font-blue-dark text-center"><?php echo Yii::t('site', 'Our Values') ?></h2><ul class="font-large">
 			<li><?php echo Yii::t('site', 'We love people and we are eager to help them in realising their aims, we are happy to see them doing it in a smart way and with inspiration') ?></li>
 			<li><?php echo Yii::t('site', 'We love creating simple things that solve complicated issues') ?></li>
 			<li><?php echo Yii::t('site', 'We adore good design and facilitating technology') ?></li>
