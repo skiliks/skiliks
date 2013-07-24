a67bd6ccb5665bf28c68b61f0363c43e54cd7500
Tetyana Grybok <tetyana.grybok@skiliks.com>
2013-07-24 19:29:55 +0300

    documents test

--- protected/tests/selenium/Other/TwoDocumentsOpened_SK3362_Test.php
+++ protected/tests/selenium/Other/TwoDocumentsOpened_SK3362_Test.php
@@ -11,14 +11,24 @@ class TwoDocumentsOpened_SK3362_Test extends SeleniumTestHelper
     public function test_TwoDocumentsOpened_SK3362 ()
     {
         $this->start_simulation();
-        //$this->clearEventQueueBeforeEleven('RST1');
-        sleep(160);
+
+        $this->clearEventQueueBeforeEleven('RST1');
+
         $this->optimal_click(Yii::app()->params['test_mappings']['icons']['documents']);
         sleep(10);
-        $this->doubleClick("xpath=//*[@id='Бюджет АО_2013.xls']");
+        $this->mouseDown("xpath=//*[@id='Бюджет АО_2013.xls']/div[1]");
+        $this->click("xpath=//*[@id='Бюджет АО_2013.xls']/div[1]");
+        $this->doubleClick("xpath=//*[@id='Бюджет АО_2013.xls']/div[1]");
         sleep(5);
         $this->assertTrue($this->isVisible("css=#cell_B4"));
-        //$this->click(Yii::app()->params['test_mappings']['icons']['close1']);
+
+        $this->optimal_click(Yii::app()->params['test_mappings']['icons']['documents']);
+        $this->mouseDown("xpath=//*[@id='Бюджет производства_2013_утв.xls']/div[1]");
+        $this->click("xpath=//*[@id='Бюджет производства_2013_утв.xls']/div[1]");
+        $this->doubleClick("xpath=//*[@id='Бюджет производства_2013_утв.xls']/div[1]");
+        sleep(5);
+        $this->assertTrue($this->isVisible("css=#cell_B7"));
+
         $this->simulation_stop();
     }
 }
