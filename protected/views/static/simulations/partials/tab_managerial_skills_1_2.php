881a9753a09788550d3dbabb738a6a6f8360f8a5
Malysh <svetlana@skiliks.com>
2013-07-25 13:04:28 +0100

    SKILIKS-3481

--- protected/views/static/simulations/partials/tab_managerial_skills_1_2.php
+++ protected/views/static/simulations/partials/tab_managerial_skills_1_2.php
@@ -1,16 +1,21 @@
 <div class="extrasidepads">
     <div class="textcener"><h2 class="total">Управленческие навыки</h2></div>
-    <h3 class="resulttitele resulttitelevalue"><span class="valuetitle followPriorities"></span>1. Следование приоритетам</h3>
+    <h3 class="resulttitele resulttitelevalue"><span class="valuetitle followPriorities"></span>1. Управление задачами с учётом приоритетов</h3>
 
     <div class="twobarstitles resultlabeltitle">
         <span class="barstitle">Уровень владения навыком</span>
         <span class="barstitle">Уровень проблем</span>
     </div>
 
+    <h3 class="resulttitele resulttitelevalue"><span class="valuetitle taskManagement"></span>1. Управление задачами с учётом приоритетов</h3>
+
     <div class="clearfix mangrlresults">
         <div class="labels">
-            <p class="labelwrap"><span class="thelabel">1.1 Следование целям подразделения</span></p>
-            <p class="labelwrap"><span class="thelabel"><a href="#">1.2 Следование личным приоритетам</a></span></p>
+            <p class="labelwrap"><span class="thelabel"><a href="#">1.1 Определение приоритетов</a></span></p>
+            <p class="labelwrap"><span class="thelabel"><a href="#">1.2 Использование планирования в течение дня</a></span></p>
+            <p class="labelwrap"><span class="thelabel"><a href="#">1.3 Правильное определение приоритетов задач при планировании</a></span></p>
+            <p class="labelwrap"><span class="thelabel"><a href="#">1.4 Выполнение задач в соответствии с приоритетами</a></span></p>
+            <p class="labelwrap"><span class="thelabel"><a href="#">1.5 Завершение начатых задач</a></span></p>
         </div>
         <div class="barswrap">
             <div class="twocharts followPriorities-1">
@@ -21,43 +26,20 @@
                 <div class="chartbar"></div>
                 <div class="chartproblem"></div>
             </div>
-        </div>
-    </div>
-
-    <h3 class="resulttitele resulttitelevalue"><span class="valuetitle taskManagement"></span>2. Управление задачами</h3>
-
-    <div class="twobarstitles resultlabeltitle">
-        <span class="barstitle">Уровень владения навыком</span>
-        <span class="barstitle">Уровень проблем</span>
-    </div>
-
-    <div class="clearfix mangrlresults">
-        <div class="labels">
-            <p class="labelwrap"><span class="thelabel">2.1 Использование планирования в течение дня</span></p>
-            <p class="labelwrap"><span class="thelabel">2.2 Правильное определение приоритетов задач при планировании</span></p>
-            <p class="labelwrap"><span class="thelabel">2.3 Выполнение задач в соответствии с приоритетами</span></p>
-            <p class="labelwrap"><span class="thelabel">2.4 Завершение начатых задач <span class="helpbuble">Описание навыка</span></span></p>
-        </div>
-        <div class="barswrap">
-            <div class="twocharts taskManagement-1">
+            <div class="twocharts followPriorities-3">
                 <div class="chartbar"></div>
                 <div class="chartproblem"></div>
             </div>
-            <div class="twocharts taskManagement-2">
+            <div class="twocharts followPriorities-4">
                 <div class="chartbar"></div>
                 <div class="chartproblem"></div>
             </div>
-            <div class="twocharts taskManagement-3">
-                <div class="chartbar"></div>
-                <div class="chartproblem"></div>
-            </div>
-            <div class="twocharts taskManagement-4">
+            <div class="twocharts followPriorities-5">
                 <div class="chartbar"></div>
                 <div class="chartproblem"></div>
             </div>
         </div>
     </div>
-
     <div class="legendwrap legendmargin lessmargintop">
         <div class="legend">
             <p class="barstitle">Обозначения</p>
