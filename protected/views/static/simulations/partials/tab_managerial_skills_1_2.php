06cf24d54ab1893038542b1b5d1076845bf3f0c7
Malysh <svetlana@skiliks.com>
2013-07-25 14:31:29 +0100

    SKILIKS-3481

--- protected/views/static/simulations/partials/tab_managerial_skills_1_2.php
+++ protected/views/static/simulations/partials/tab_managerial_skills_1_2.php
@@ -24,18 +24,6 @@
                 <div class="chartbar"></div>
                 <div class="chartproblem"></div>
             </div>
-            <div class="twocharts followPriorities-3">
-                <div class="chartbar"><div class="chart-bar"><span class="chart-value">30%</span></div></div>
-                <div class="chartproblem"><div class="chart-bar redbar"><span class="chart-value">10%</span></div></div>
-            </div>
-            <div class="twocharts followPriorities-4">
-                <div class="chartbar"><div class="chart-bar"><span class="chart-value">30%</span></div></div>
-                <div class="chartproblem"><div class="chart-bar redbar"><span class="chart-value">10%</span></div></div>
-            </div>
-            <div class="twocharts followPriorities-5">
-                <div class="chartbar"><div class="chart-bar"><span class="chart-value">30%</span></div></div>
-                <div class="chartproblem"><div class="chart-bar redbar"><span class="chart-value">10%</span></div></div>
-            </div>
         </div>
     </div>
 
