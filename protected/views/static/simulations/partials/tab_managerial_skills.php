a913d77f17697d8eca9f1fed7cbd5f6b211013ad
Malysh <svetlana@skiliks.com>
2013-07-26 14:24:44 +0100

    SKILIKS-3483

--- protected/views/static/simulations/partials/tab_managerial_skills.php
+++ protected/views/static/simulations/partials/tab_managerial_skills.php
@@ -9,8 +9,8 @@
 
     <div class="clearfix mangrlresults">
         <div class="labels high-labels">
-            <div class="labelwrap"><span class="thelabel"><div><span class="list-counter">1.</span><a href="#managerial-skills-1-2" class="list-text"><span>Управление задачами с учётом приоритетов</span></a><a href="#managerial-skills-1-2" class="signmore"></a></div></span></div>
-            <div class="labelwrap"><span class="thelabel"><div><span class="list-counter">2.</span><a href="#managerial-skills-3-4" class="list-text">Управление людьми</a><ahref="#managerial-skills-3-4" class="signmore"></a></div></span></div>
+            <div class="labelwrap"><span class="thelabel"><div><span class="list-counter">1.</span><a href="#managerial-skills-1-2" class="list-text">Управление задачами с учётом приоритетов</a><a href="#managerial-skills-1-2" class="signmore"></a></div></span></div>
+            <div class="labelwrap"><span class="thelabel"><div><span class="list-counter">2.</span><a href="#managerial-skills-3-4" class="list-text">Управление людьми</a><a href="#managerial-skills-3-4" class="signmore"></a></div></span></div>
             <div class="labelwrap"><span class="thelabel"><div><span class="list-counter">3.</span><a href="#managerial-skills-3-4" class="list-text">Управление коммуникациями</a><a href="#managerial-skills-3-4" class="signmore"></a></div></span></div>
         </div>
         <div class="bars barswrap main-skills">
