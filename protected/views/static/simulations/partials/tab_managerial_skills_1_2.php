d3dda9f52d0f0ee79c21c9c5160a42bb16b91360
Malysh <svetlana@skiliks.com>
2013-07-26 10:38:27 +0100

    SKILIKS-3482

--- protected/views/static/simulations/partials/tab_managerial_skills_1_2.php
+++ protected/views/static/simulations/partials/tab_managerial_skills_1_2.php
@@ -9,11 +9,11 @@
 
     <div class="clearfix mangrlresults">
         <div class="labels labels1">
-            <div class="labelwrap"><span class="thelabel"><p><span class="list-counter">1.1 </span><a href="#" class="list-text">Определение приоритетов</a></p></span></div>
-            <div class="labelwrap"><span class="thelabel"><p><span class="list-counter">1.2 </span><a href="#" class="list-text">Использование планирования в течение дня</a></p></span></div>
-            <div class="labelwrap"><span class="thelabel"><p><span class="list-counter">1.3 </span><a href="#" class="list-text">Правильное определение приоритетов задач при планировании</a></p></span></div>
-            <div class="labelwrap"><span class="thelabel"><p><span class="list-counter">1.4 </span><a href="#" class="list-text">Выполнение задач в соответствии с приоритетами</a></p></span></div>
-            <div class="labelwrap"><span class="thelabel"><p><span class="list-counter">1.5 </span><a href="#" class="list-text">Завершение начатых задач</a></p></span></div>
+            <div class="labelwrap"><span class="thelabel"><div><span class="list-counter">1.1 </span><a href="#" class="list-text">Определение приоритетов</a></div></span></div>
+            <div class="labelwrap"><span class="thelabel"><div><span class="list-counter">1.2 </span><a href="#" class="list-text">Использование планирования в течение дня</a></div></span></div>
+            <div class="labelwrap"><span class="thelabel"><div><span class="list-counter">1.3 </span><a href="#" class="list-text">Правильное определение приоритетов задач при планировании</a></div></span></div>
+            <div class="labelwrap"><span class="thelabel"><div><span class="list-counter">1.4 </span><a href="#" class="list-text">Выполнение задач в соответствии с приоритетами</a></div></span></div>
+            <div class="labelwrap"><span class="thelabel"><div><span class="list-counter">1.5 </span><a href="#" class="list-text">Завершение начатых задач</a></div></span></div>
         </div>
         <div class="barswrap">
             <div class="twocharts followPriorities-1">
