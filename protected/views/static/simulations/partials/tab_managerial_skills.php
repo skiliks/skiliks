480d7300a8ca8a5027da698d204bfbf5aa993b10
Ivan <listepo@ya.ru>
2013-07-27 02:39:40 +0300

    Update popup

--- protected/views/static/simulations/partials/tab_managerial_skills.php
+++ protected/views/static/simulations/partials/tab_managerial_skills.php
@@ -17,28 +17,9 @@
             <div class="chartbar management-1"></div>
             <div class="chartbar management-2"></div>
             <div class="chartbar management-3"></div>
-            <!--<div class="chartbar management-4"></div>-->
         </div>
     </div>
-    <!--
-    <div class="clearfix">
-        <div class="labeltitles toplabelmargin">
-            <h4>Другие важные навыки</h4>
-        </div>
-    </div>
-    <div class="clearfix mangrlresults">
-        <div class="labels">
-            <p class="labelwrap"><span class="thelabel"><a href="#managerial-skills-5-6">5. Эффективная работа с почтой <span class="signmore"></span></a></span></p>
-            <p class="labelwrap"><span class="thelabel"><a href="#managerial-skills-5-6">6. Эффективное управление звонками <span class="signmore"></span></a></span></p>
-            <p class="labelwrap"><span class="thelabel"><a href="#managerial-skills-7">7. Эффективное управление встречами <span class="signmore"></span></a></span></p>
-        </div>
-        <div class="barswrap otherskills">
-            <div class="chartbar management-5"></div>
-            <div class="chartbar management-6"></div>
-            <div class="chartbar management-7"></div>
-        </div>
-    </div>
--->
+
     <div class="legendwrap resultslegend">
         <div class="legend">
             <p class="barstitle">Обозначения</p>
@@ -54,6 +35,7 @@
             renderer = function(v) { return v + '%'; };
 
         for (var i = 1; i < 4; i++) { //8
+            console.log(result[i]);
             new charts.Bar(
                 '.management-' + i,
                 r(result[i] && result[i].total ? result[i].total : 0),
