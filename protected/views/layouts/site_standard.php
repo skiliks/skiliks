d80faf4ec13f882f46b29172ebb9b87fe5620ff0
Malysh <svetlana@skiliks.com>
2013-07-31 12:31:24 +0100

    SKILIKS-3431

--- protected/views/layouts/site_standard.php
+++ protected/views/layouts/site_standard.php
@@ -188,7 +188,7 @@ $cs->registerCssFile($assetsUrl . "/css/static.css");
     <?php $this->renderPartial('//global_partials/_feedback', []) ?>
     <script type="text/javascript">
         Cufon.replace('.menu-site, .unstyled, .inner .site-main p, span, label, input, select, .proxima-reg, .sbHolder a, .errorMessage, .feedback-dialog-title, .to-top, small, .team-values, ' +
-            '.action-productNew p, ' +
+            '.action-productNew p, .action-tariffsNew p' +
             '.action-teamNew p', {fontFamily:"ProximaNova-Regular", hover: true});
         Cufon.replace('.btn, .proxima-bold, h1, h2, h3, h4, h5, h6, strong, .dark-labels label, .list-dark li, .items th, .items td, .add-vacancy-popup h1, .ui-dialog-title, ' +
             '.side-menu .active a', {fontFamily:"ProximaNova-Bold", hover: true});
