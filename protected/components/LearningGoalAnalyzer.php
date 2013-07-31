c10b80c2164f5d7c72bcd3e4f77868c5f5594c03
Ivan <listepo@hotmail.com>
2013-07-31 19:05:25 +0300

    SKILIKS-3580 Evaluation SimulationServiceUnitTest

--- protected/components/LearningGoalAnalyzer.php
+++ protected/components/LearningGoalAnalyzer.php
@@ -111,7 +111,7 @@ class LearningGoalAnalyzer
     public static function getReducingCoefficient($problem)
     {
         if (0 <= $problem && $problem <= 10) {
-            return 1;
+            return (float)1;
         } elseif (10 < $problem && $problem <= 20) {
             return 0.8;
         }elseif (20 < $problem && $problem <= 50) {
