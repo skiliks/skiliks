324bb47c38f05b32d417785795268b914a5c7b55
slavkaa <nikoolin@ukr.net>
2013-08-01 22:00:53 +0300

    SKILIKS-3634. Fix CS cache system.

---
+++ protected/commands/UpdateScDocsCacheCommand.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Class UpdateScDocsCache
+ */
+class UpdateScDocsCacheCommand extends CConsoleCommand {
+
+    public function actionIndex()
+    {
+        echo "Начинаем кеширование: \n";
+
+        $templates = DocumentTemplate::model()->findAll();
+        foreach ($templates as $template) {
+            if (in_array($template->format, ['xls', 'xlsx'])) {
+                echo "{$template->fileName} \n ({$template->getCacheFilePath()}): \n";
+
+                if (file_exists($template->getCacheFilePath())) {
+                    unlink($template->getCacheFilePath());
+                }
+
+                echo "удалён, ";
+
+                $scData = ScXlsConverter::xls2sc($template->getFilePath());
+                file_put_contents($template->getCacheFilePath(), serialize($scData));
+                echo "cache обновлён. \n\n";
+            }
+        }
+
+        echo "Готово! \n";
+    }
+}
