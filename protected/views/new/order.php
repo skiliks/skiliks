93eb16bc80a2ea90df36ef5efe56b6aee23b9653
Malysh <svetlana@skiliks.com>
2013-08-02 13:01:26 +0100

    SKILIKS-3432

--- protected/views/new/order.php
+++ protected/views/new/order.php
@@ -14,7 +14,7 @@
                 <header class="font-white font-xxlarge text-center">Ваш заказ</header>
 
                 <h4 class="font-4xlarge proxima-bold text-center font-brown"><?= $tariff->label ?></h4>
-                <div class="font-slarge text-center">1 Месяц</div>
+                <p class="font-slarge text-center">1 Месяц</p>
 
                 <div class="item-price font-6xlarge proxima-bold text-center">
                     <?= $tariff->getFormattedPrice() ?>
