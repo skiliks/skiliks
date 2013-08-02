986043c745b5fb801a47a49376bc0cca91334b06
Ivan <listepo@ya.ru>
2013-08-02 23:46:21 +0300

    SKILIKS-3526 fix 214g

--- protected/views/admin_area/pages/statistics.php
+++ protected/views/admin_area/pages/statistics.php
@@ -8,24 +8,24 @@
 <div class="row-fluid">
 <table class="table-statistics">
    <tr>
-       <td class="statistic-php-unit">
+       <td class="td-statistic statistic-php-unit">
            <div>Develop</div>
            <div class="status"></div>
            <div class="author"></div>
        </td>
-       <td class="statistic-selenium-site">
+       <td class="td-statistic statistic-selenium-site">
            <div>Selenium Engine & Site</div>
            <div class="status"></div>
            <div class="author"></div>
        </td>
    </tr>
    <tr>
-       <td class="statistic-selenium-assessment">
+       <td class="td-statistic statistic-selenium-assessment">
            <div>Selenium Assessment</div>
            <div class="status"></div>
            <div class="author"></div>
        </td>
-       <td class="statistic-free-disk-space">
+       <td class="td-statistic statistic-free-disk-space">
            <div>Free disk space</div>
            <div class="status"></div>
            <div class="author"></div>
