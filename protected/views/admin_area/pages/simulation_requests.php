8e342a7f86a0065a68e89390b6c19ca68355cc98
slavkaa <nikoolin@ukr.net>
2013-08-05 12:44:57 +0300

    Add advanced log view.

--- protected/views/admin_area/pages/simulation_requests.php
+++ protected/views/admin_area/pages/simulation_requests.php
@@ -1,8 +1,47 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: root
- * Date: 8/4/13
- * Time: 4:49 PM
- * To change this template use File | Settings | File Templates.
- */
+
+<h1>
+    Симуляция <?= $simulation->id ?>,
+    <small><?= $simulation->start ?> ~ <?= $simulation->end ?></small>
+</h1>
+
+<?= $simulation->user->profile->firstname ?> <?=$simulation->user->profile->lastname ?>,
+<?=$simulation->user->profile->email ?>
+
+<br/>
+    <?php if (null !== $simulation->invite): ?>
+        <a href="/admin_area/invite/<?= $simulation->invite->id?>/site-logs">Смотреть логи приглашения</a>,
+    <?php endif ?>
+    <a href="/admin_area/simulation/<?= $simulation->id?>/site-logs">Смотреть логи симуляции</a>
+<br/>
+<br/>
+
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Реальное время (UID) <strong>Игровое время</strong> , URL</th>
+            <th>Запрос</th>
+            <th>Ответ</th>
+        </tr>
+    </thead>
+    <?php foreach ($simulationLogs as $simulationLog) : ?>
+        <tr style="height: 120px;">
+            <td>
+                <?= $simulationLog->real_time ?> (<?= $simulationLog->request_uid?>)
+                <strong><?= $simulationLog->frontend_game_time ?></strong>,
+
+                </br>
+                <?= str_replace('/index.php/', '', $simulationLog->request_url) ?>
+            </td>
+            <td style="overflow: scroll;">
+                <div style="width: 500px; height: 120px;">
+                    <?= str_replace(',', ', ', $simulationLog->request_body) ?>
+                </div>
+            </td>
+            <td style="overflow: scroll;">
+                <div style="width: 500px; height: 120px;">
+                    <?= str_replace(',', ', ', $simulationLog->response_body) ?>
+                </div>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+</table>
