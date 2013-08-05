26c4e3ca561691c1de49c2cf965eb486c56c5c13
Ivan <listepo@ya.ru>
2013-08-06 01:22:23 +0300

    SKILIKS-3658

---
+++ protected/assets/js/game/views/SKCrashOptionsPanelView.js
@@ -0,0 +1,18 @@
+/*global Backbone, _, define */
+
+var SKCrashOptionsPanelView;
+define([
+    "game/views/SKDialogView"
+], function (
+    ) {
+    "use strict";
+    /**
+     * List of user's phrases added to letter
+     * @class SKCrachOptionsPanelView
+     * @augments Backbone.View
+     */
+    SKCrashOptionsPanelView = SKDialogView.extend({
+
+    });
+    return SKCrashOptionsPanelView; //SKDialogView;
+});
