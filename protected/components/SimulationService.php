a4c911fd7de6e3b247fb129cc557284e7ef7956e
Dmitry Alekseev <pernifin@gmail.com>
2013-08-07 10:59:35 +0300

    SKILIKS-3661

--- protected/components/SimulationService.php
+++ protected/components/SimulationService.php
@@ -722,9 +722,11 @@ class SimulationService
 
         self::logAboutSim($simulation, 'sim stop: assessment calculated');
 
-        $simulation->invite->can_be_reloaded = false;
-        $simulation->invite->save(false);
-        InviteService::logAboutInviteStatus($simulation->invite, 'invite : updated : can be reloaded set to false');
+        if ($simulation->isFull()) {
+            $simulation->invite->can_be_reloaded = false;
+            $simulation->invite->save(false);
+            InviteService::logAboutInviteStatus($simulation->invite, 'invite : updated : can be reloaded set to false');
+        }
 
         // remove all files except D1 - Сводный бюджет 2013 {
         $docs = MyDocument::model()->findAllByAttributes([
