1411b557655698a09aadbb75a5a09bc3c51f5d5e
slavkaa <nikoolin@ukr.net>
2013-08-15 16:39:16 +0300

    Check cache system.

--- protected/assets/js/game/models/SKSimulation.js
+++ protected/assets/js/game/models/SKSimulation.js
@@ -62,7 +62,7 @@ define([
              */
             'initialize':function () {
                 try {
-                    console.log('5');
+                    alert('6');
                     var me = this;
 
                     /**
