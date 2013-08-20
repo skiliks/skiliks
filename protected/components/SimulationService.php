df14347e2fcc0e71d1f162f97414df3c12554e61
Ivan Tugay <listepo@ya.ru>
2013-08-20 10:45:42 +0300

    SKILIKS-3734 new leg action logging

--- protected/components/SimulationService.php
+++ protected/components/SimulationService.php
@@ -649,7 +649,11 @@ class SimulationService
                     throw $e;
                 }
             }
-
+            if(Yii::app()->params['disableOldLogging']){
+                LogHelper::updateUniversalLog($simulation);
+                $analyzer = new ActivityActionAnalyzer($simulation);
+                $analyzer->run();
+            }
             // Make aggregated activity log
             LogHelper::combineLogActivityAgregated($simulation);
 
