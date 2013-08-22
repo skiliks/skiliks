0272b5ac13924417f3199f847f6f0b1ff8119a2c
slavka <nikoolin@ukr.net>
2013-08-22 18:26:24 +0300

    SKILIKS-3794. Clean up migration folder. Fix initDb command.

--- protected/migrations/m130815_123110_universal_log.php
+++ protected/migrations/m130815_123110_universal_log.php
@@ -4,7 +4,12 @@ class m130815_123110_universal_log extends CDbMigration
 {
 	public function up()
 	{
-        $this->dropForeignKey('universal_log_dialog_id', 'universal_log');
+        try {
+            $this->dropForeignKey('universal_log_dialog_id', 'universal_log');
+        } catch (CDbException $e) {
+            // just to run migration
+        }
+
         $this->renameColumn('universal_log', 'dialog_id', 'replica_id');
         $this->addForeignKey('fk_universal_log_replica_id', 'universal_log', 'replica_id', 'replica', 'id', 'CASCADE', 'CASCADE');
 	}
