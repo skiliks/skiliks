a8a89461043108fdd1e8ea85cce8264b32a65c10
slavka <nikoolin@ukr.net>
2013-08-23 19:47:18 +0300

    Fix SiteUnitTest.php.

--- protected/tests/unit/SiteUnitTest.php
+++ protected/tests/unit/SiteUnitTest.php
@@ -13,8 +13,6 @@ class SiteUnitTest extends CDbTestCase
         curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($handler);
 
-        $this->assertFalse(true, $url."\n".$response);
-        $this->assertFalse(empty($response));
         $this->assertContains("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');", $response);
         $this->assertContains("ga('create', 'UA-42053049-1', 'skiliks.com')", $response);
         $this->assertContains("ga('send', 'pageview');", $response);
