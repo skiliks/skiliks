58660aefd0141e6762b2078a7c22a6aeb84685ca
qzenchik <qzeni.4@gmail.com>
2013-08-29 13:00:02 +0300

    SKILIKS-3243 fixed

--- protected/views/static/dashboard/dashboard_corporate.php
+++ protected/views/static/dashboard/dashboard_corporate.php
@@ -1,76 +1,7 @@
-<?php if (true === $validPrevalidate): ?>
-    <div class="form form-invite-message message_window" title="Сообщение">
-
-        <?php $form = $this->beginWidget('CActiveForm', array(
-            'id' => 'send-invite-message-form',
-        )); ?>
-
-        <?php echo $form->hiddenField($invite, 'firstname'); ?>
-        <?php echo $form->hiddenField($invite, 'lastname'); ?>
-        <?php echo $form->hiddenField($invite, 'email'); ?>
-        <?php echo $form->hiddenField($invite, 'vacancy_id'); ?>
-
-        <div class="block-form">
-            <p><?php echo $form->textField($invite, 'fullname'); ?></p>
-
-            <?php if (Yii::app()->params['emails']['isDisplayStandardInvitationMailTopText']): ?>
-                <p class="font-green-dark"><?= $invite->ownerUser->account_corporate->company_name ?: 'Компания' ?> предлагает вам пройти тест "Базовый менеджмент" для участия в конкурсе на вакансию <a href="<?= $invite->vacancy->link ?: '#' ?>"><?= $invite->getVacancyLabel() ?></a>.</p>
-                <?php if (empty($invite->receiverUser)): ?>
-                    <p class="font-green-dark"><a href="<?= $this->createAbsoluteUrl('static/pages/product') ?>">"Базовый менеджмент"</a> - это деловая симуляция, позволяющая оценить менеджерские навыки в форме увлекательной игры</p>
-                <?php endif; ?>
-            <?php endif; ?>
-
-            <p><?php echo $form->textArea($invite, 'message', ['rows' => 10, 'cols' => 60]); ?><?php echo $form->error($invite, 'message'); ?></p>
-            <p class="font-green-dark">
-                <?php if ($invite->receiverUser && $invite->receiverUser->isPersonal()): ?>
-                    Пожалуйста, <a href="<?= $this->createAbsoluteUrl('dashboard') ?>">зайдите</a> в свой кабинет и примите приглашение на тестирование для прохождения симуляции.
-                <?php elseif ($invite->receiverUser && $invite->receiverUser->isCorporate()): ?>
-                    Пожалуйста, <a href="<?= $this->createAbsoluteUrl('/registration') ?>">создайте личный профиль</a> или
-                    <a href="<?= $this->createAbsoluteUrl('static/dashboard/personal') ?>">войдите в личный кабинет</a> и примите приглашение на тестирование для прохождения симуляции.
-                <?php else: ?>
-                    Пожалуйста, <a href="<?= $this->createAbsoluteUrl('/registration') ?>">зарегистрируйтесь</a> или <a href="<?= $this->createAbsoluteUrl('/user/auth') ?>">войдите</a> в свой кабинет и примите приглашение на тестирование для прохождения симуляции.
-                <?php endif; ?>
-            </p>
-            <p class="font-green-dark">Ваш skiliks</p>
-        </div>
-
-        <?php // echo $form->labelEx($invite, 'signature'); ?>
-        <?php // echo $form->textField($invite, 'signature'); ?>
-        <?php // echo $form->error($invite, 'signature'); ?>
-        <div class="row buttons no-margin-left">
-            <?php echo CHtml::submitButton('Отправить', ['name' => 'send']); ?>
-        </div>
-
-        <?php $this->endWidget(); ?>
-    </div>
-
-
-    <script type="text/javascript">
-        $(function() {
-            // @link: http://jqueryui.com/dialog/
-            $( ".message_window" ).dialog({
-                modal: true,
-                resizable: false,
-                draggable: false,
-                width: 590,
-                height: 500,
-                position: {
-                    my: "left top",
-                    at: "left top",
-                    of: $('#corporate-invitations-list-box .items')
-                },
-                open: function( event, ui ) { Cufon.refresh(); }
-            });
-
-            $( ".message_window").parent().addClass('nice-border cabmessage');
-            $( ".message_window").dialog('open');
-        });
-    </script>
-<?php endif; ?>
 <section class="dashboard corpdashboard">
     <h2 class="thetitle bigtitle"><?php echo Yii::t('site', 'Work dashboard') ?></h2>
     <aside>
-    <!-- invite-people-box -->
+        <!-- invite-people-box -->
         <div id="invite-people-box" class="nice-border backgroud-rich-blue sideblock">
             <?php $this->renderPartial('_invite_people_box', [
                 'invite'    => $invite,
@@ -78,13 +9,13 @@
             ]) ?>
         </div>
 
-<?php if ($display_results_for): ?>
-    <script type="text/javascript">
-        $(function() {
-            showSimulationDetails('/simulations/details/<?= $display_results_for->id ?>');
-        });
-    </script>
-<?php endif; ?>
+        <?php if ($display_results_for): ?>
+            <script type="text/javascript">
+                $(function() {
+                    showSimulationDetails('/simulations/details/<?= $display_results_for->id ?>');
+                });
+            </script>
+        <?php endif; ?>
 
         <!-- simulations-counter-box -->
         <div id="simulations-counter-box" class="nice-border backgroud-light-blue">
@@ -95,17 +26,84 @@
     </aside>
     <div class="narrow-contnt">
         <!-- corporate-invitations-list-box -->
+        <!-- hack for taking position -->
+        <div id="corporate-invitations-list-box-position"></div>
         <div id="corporate-invitations-list-box" class="transparent-boder wideblock">
             <?php $this->renderPartial('_corporate_invitations_list_box', [
                 'inviteToEdit'    => $inviteToEdit,
                 'vacancies'       => $vacancies,
             ]) ?>
         </div>
-    <script>
-        $(document).ready(function() {
-            $("#corporate-invitations-list-box").show();
-        });
-    </script>
+        <?php if (true === $validPrevalidate): ?>
+            <div class="form form-invite-message message_window" title="Сообщение">
+
+                <?php $form = $this->beginWidget('CActiveForm', array(
+                    'id' => 'send-invite-message-form',
+                )); ?>
+
+                <?php echo $form->hiddenField($invite, 'firstname'); ?>
+                <?php echo $form->hiddenField($invite, 'lastname'); ?>
+                <?php echo $form->hiddenField($invite, 'email'); ?>
+                <?php echo $form->hiddenField($invite, 'vacancy_id'); ?>
+
+                <div class="block-form">
+                    <p><?php echo $form->textField($invite, 'fullname'); ?></p>
+
+                    <?php if (Yii::app()->params['emails']['isDisplayStandardInvitationMailTopText']): ?>
+                        <p class="font-green-dark"><?= $invite->ownerUser->account_corporate->company_name ?: 'Компания' ?> предлагает вам пройти тест "Базовый менеджмент" для участия в конкурсе на вакансию <a href="<?= $invite->vacancy->link ?: '#' ?>"><?= $invite->getVacancyLabel() ?></a>.</p>
+                        <?php if (empty($invite->receiverUser)): ?>
+                            <p class="font-green-dark"><a href="<?= $this->createAbsoluteUrl('static/pages/product') ?>">"Базовый менеджмент"</a> - это деловая симуляция, позволяющая оценить менеджерские навыки в форме увлекательной игры</p>
+                        <?php endif; ?>
+                    <?php endif; ?>
+
+                    <p><?php echo $form->textArea($invite, 'message', ['rows' => 10, 'cols' => 60]); ?><?php echo $form->error($invite, 'message'); ?></p>
+                    <p class="font-green-dark">
+                        <?php if ($invite->receiverUser && $invite->receiverUser->isPersonal()): ?>
+                            Пожалуйста, <a href="<?= $this->createAbsoluteUrl('dashboard') ?>">зайдите</a> в свой кабинет и примите приглашение на тестирование для прохождения симуляции.
+                        <?php elseif ($invite->receiverUser && $invite->receiverUser->isCorporate()): ?>
+                            Пожалуйста, <a href="<?= $this->createAbsoluteUrl('/registration') ?>">создайте личный профиль</a> или
+                            <a href="<?= $this->createAbsoluteUrl('static/dashboard/personal') ?>">войдите в личный кабинет</a> и примите приглашение на тестирование для прохождения симуляции.
+                        <?php else: ?>
+                            Пожалуйста, <a href="<?= $this->createAbsoluteUrl('/registration') ?>">зарегистрируйтесь</a> или <a href="<?= $this->createAbsoluteUrl('/user/auth') ?>">войдите</a> в свой кабинет и примите приглашение на тестирование для прохождения симуляции.
+                        <?php endif; ?>
+                    </p>
+                    <p class="font-green-dark">Ваш skiliks</p>
+                </div>
+
+                <?php // echo $form->labelEx($invite, 'signature'); ?>
+                <?php // echo $form->textField($invite, 'signature'); ?>
+                <?php // echo $form->error($invite, 'signature'); ?>
+                <div class="row buttons no-margin-left">
+                    <?php echo CHtml::submitButton('Отправить', ['name' => 'send']); ?>
+                </div>
+
+                <?php $this->endWidget(); ?>
+            </div>
+
+
+            <script type="text/javascript">
+                $(function() {
+                    // @link: http://jqueryui.com/dialog/
+                    $( ".message_window" ).dialog({
+                        modal: true,
+                        resizable: false,
+                        draggable: false,
+                        width: 590,
+                        height: 500,
+                        position: {
+                            my: "left top",
+                            at: "left top",
+                            of: $('#corporate-invitations-list-box-position')
+                        },
+                        open: function( event, ui ) { Cufon.refresh(); }
+                    });
+
+                    $( ".message_window").parent().addClass('nice-border cabmessage');
+                    $( ".message_window").dialog('open');
+                });
+            </script>
+        <?php endif; ?>
+        <script>$("#corporate-invitations-list-box").show();</script>
 
     </div>
 
