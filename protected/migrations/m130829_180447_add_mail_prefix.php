b1fb561c694a1ab6692b6640f11fb8143560755b
slavka <nikoolin@ukr.net>
2013-08-30 02:23:16 +0300

    SKILIKS-3632. Fix migration.

--- protected/migrations/m130829_180447_add_mail_prefix.php
+++ protected/migrations/m130829_180447_add_mail_prefix.php
@@ -4,7 +4,7 @@ class m130829_180447_add_mail_prefix extends CDbMigration
 {
 	public function up()
 	{
-        $this->insert('mail_prefix', ['code' => 're:re:fwd:', 'title' => 'Re: Re: Fwd:']);
+        $this->insert('mail_prefix', ['code' => 'rerefwd', 'title' => 'Re: Re: Fwd:']);
 	}
 
 	public function down()
