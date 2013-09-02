9f9892a54a46bf8b23b7cc6924fb98b3f8a724a4
qzenchik <qzeni.4@gmail.com>
2013-09-02 18:00:56 +0300

    SKILIKS-3946 simulation list in dashboard

--- protected/views/static/dashboard/_private_invitations_list_box.php
+++ protected/views/static/dashboard/_private_invitations_list_box.php
@@ -4,26 +4,39 @@
     <?php
 
     $scoreRender = function(Invite $invite) {
-        if ($invite->status == Invite::STATUS_PENDING) {
-            return (string)$invite->getAcceptActionTag().' '.$invite->getDeclineActionTag();
-        }
+        if(!$invite->scenario->isLite()) {
+            if ($invite->status == Invite::STATUS_PENDING) {
+                return (string)$invite->getAcceptActionTag().' '.$invite->getDeclineActionTag();
+            }
 
-        if (null !== $invite && false === $invite->isAllowedToSeeResults(Yii::app()->user->data())) {
-            return '<div style="line-height: 30px;">Результаты скрыты<div>';
-        }
+            if (null !== $invite && $invite->status == Invite::STATUS_COMPLETED && false === $invite->isAllowedToSeeResults(Yii::app()->user->data())) {
+                return '<div style="line-height: 30px;">Результаты скрыты<div>';
+            }
 
-        return $this->renderPartial('//global_partials/_simulation_stars', [
-            'simulation'     => $invite->simulation,
-            'isDisplayTitle' => false,
-            'isDisplayArrow' => false,
-            'isDisplayScaleIfSimulationNull' => false,
-        ],false);
+            if (null !== $invite && Invite::STATUS_ACCEPTED !== $invite->status && $invite->scenario->isFull()) {
+                //$return = '<a data-href="'.$invite->id.'" class="start-full-simulation start-full-simulation-button" href="#">Начать</a>';
+                //echo $return;
+                return sprintf(
+                    "<a class=\"start-full-simulation start-full-simulation-button\" data-href=\"/simulation/promo/%s/%s\" href=\"#\">Начать</a>",
+                    $invite->scenario->slug,
+                    $invite->id
+                );
+            }
+
+            return $this->renderPartial('//global_partials/_simulation_stars', [
+                'simulation'     => $invite->simulation,
+                'isDisplayTitle' => false,
+                'isDisplayArrow' => false,
+                'isDisplayScaleIfSimulationNull' => false,
+            ],false);
+        }
+        else return false;
     };
 
     $this->widget('zii.widgets.grid.CGridView', [
         'dataProvider' => Invite::model()->searchByInvitedUserEmail(
             strtolower(Yii::app()->user->data()->profile->email),
-            [Invite::STATUS_PENDING, Invite::STATUS_COMPLETED]
+            false
         ),
         'summaryText' => '',
         'emptyText' => '',
@@ -36,7 +49,7 @@
         ],
         'columns' => [
             ['header' => Yii::t('site', Yii::t('site', 'Компания')), 'name' => "company", 'value' => 'Yii::t("site", $data->getCompanyOwnershipType()." ".$data->getCompanyName())'],
-            ['header' => Yii::t('site', Yii::t('site', 'Вакансия')), 'name' =>'vacancy_id', 'value' => 'Yii::t("site", $data->getVacancyLabel())'],
+            ['header' => Yii::t('site', Yii::t('site', 'Вакансия')), 'name' =>'vacancy_id', 'value' => '(Yii::t("site", $data->getVacancyLabel()) !== null) ? Yii::t("site", $data->getVacancyLabel()) : "-"'],
             ['header' => Yii::t('site', Yii::t('site', 'Оценка')) , 'value' => '"Базовый менеджмент"'],
             [
                 'header' => Yii::t('site', Yii::t('site', 'Дата / Время')),
@@ -44,7 +57,7 @@
                 'value' => '$data->getUpdatedTime()->format("j/m/y") . " <time>" . $data->getUpdatedTime()->format("G:i") . "</time>"',
                 'type' => 'raw'
             ],
-            ['header' => Yii::t('site', Yii::t('site', 'Статус')) , 'value' => $scoreRender, 'type' => 'html'],
+            ['header' => Yii::t('site', Yii::t('site', 'Статус')) , 'value' => $scoreRender, 'type' => 'raw'],
         ]
     ]);
     ?>
