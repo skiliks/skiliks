74779f7e00ef28d79eedd969f214005f04d0a365
= <=>
2013-09-05 19:40:38 +0300

    SKILIKS-3639. Устранить возможность циклического пересчета данных в ячейке при вставке данных МЕЖДУ ЛИСТАМИ.

---
+++ protected/assets/js/game/models/SKDocumentsManager.js
@@ -0,0 +1,48 @@
+/*
+ *
+ */
+var SKDocumentsManager;
+(function() {
+    "use strict";
+    /**
+     * @class SKMailTask
+     * @augments Backbone.Model
+     */
+    SKDocumentsManager = Backbone.Model.extend({
+        initialize: function() {
+
+        },
+
+        checkIsPasteOperationAllowedInExcel: function() {
+            var me = this;
+            if (false === me.isPasteOperationAllowedInExcel()) {
+                me.warning = new SKDialogView({
+                    'message':'Обнаружены циклические ссылки между листами в формулах. </br> Вставка данных отменена.',
+                    'buttons':[
+                        {
+                            'value':'Ок',
+                            'onclick':function () {
+                                delete SKApp.simulation.documentsManager.warning;
+                            }
+                        }
+                    ]
+                });
+            }
+        },
+
+        isPasteOperationAllowedInExcel: function() {
+            var pastedPiece = SocialCalc.Clipboard.clipboard;
+            var currentSheetName = $('.sim-window-id-'
+                + SKApp.simulation.window_set.getActiveWindow().window_uid).find(".sheet-tabs .active").text();
+
+            console.log('currentSheetName: ', currentSheetName);
+            console.log('pastedPiece: ', pastedPiece);
+            console.log(pastedPiece.indexOf(currentSheetName+'!'));
+            if (pastedPiece.indexOf(currentSheetName+'!') > -1) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    });
+})();
