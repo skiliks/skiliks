3d7feda382fe202d5ecd5409934a8bba01d4046a
qzenchik <qzeni.4@gmail.com>
2013-09-06 18:51:45 +0300

    SKILIKS-3927 adding new filters into invites

--- protected/views/admin_area/pages/invites.php
+++ protected/views/admin_area/pages/invites.php
@@ -31,14 +31,12 @@ $titles = [
     <br/>
     <br/>
 
-    <form action="" method="post" style="display: inline-block;">
+    <form action="/admin_area/invites" method="post" style="display: inline-block;">
         <i class="icon-filter"></i> &nbsp; email соискателя:
         <input name="receiver-email-for-filtration" value="<?= $receiverEmailForFiltration ?>"/>
         <input type="submit" value="фильтровать" class="btn btn-warning"/>
-    </form>
-
-    <form action="" method="post" style="display: inline-block;">
-        <input type="submit" value="Сбросить фильтр" class="btn btn-warning"/>
+        <input type="submit" value="Сбросить фильтр"  data-form-name="admin_filter_form" class="btn btn-warning clear_filter_button"/>
+        <input type="hidden" name="clear_form" class="clear_form_field" value="">
     </form>
 
     <br/>
