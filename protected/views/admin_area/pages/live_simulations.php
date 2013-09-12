eedb5e09abb658a79ab73d233d2c77e3fbf7d359
slavkaa <nikoolin@ukr.net>
2013-09-12 16:17:03 +0300

    Merge,

--- protected/views/admin_area/pages/live_simulations.php
+++ protected/views/admin_area/pages/live_simulations.php
@@ -3,15 +3,15 @@
 </script>
 
 <?php
-    $titles = [
-        'Игрок <br/> Тип аккаунта',
-        'ID Симуляции <br/> Тип симуляции',
-        'Статус приглашения <br/>ID Приглашения',
-        'Статус симуляции',
-        'Текущее игровое время',
-        'Время старта <br/> Время окончания',
-        'Запросы',
-    ];
+$titles = [
+    'Игрок <br/> Тип аккаунта',
+    'ID Симуляции <br/> Тип симуляции',
+    'Статус приглашения <br/>ID Приглашения',
+    'Статус симуляции',
+    'Текущее игровое время',
+    'Время старта <br/> Время окончания',
+    'Запросы',
+];
 ?>
 
 <div>
@@ -26,85 +26,85 @@
                 <?php endforeach ?>
             </tr>
 
-        <?php $allSimulation = [$full_simulations, $lite_simulations, $tutorial_simulations]; ?>
+            <?php $allSimulation = [$full_simulations, $lite_simulations, $tutorial_simulations]; ?>
 
-        <?php $i = 0 ?>
+            <?php $i = 0 ?>
 
-        <? foreach($allSimulation as $simulations) : ?>
-            <? foreach($simulations as $simulation) : ?>
-                <?php $i++; ?>
-                <tr>
-                    <td>
-                        <a href="/admin_area/user/<?=$simulation->user->id ?>/details">
-                            <?= $simulation->user->profile->firstname." ".$simulation->user->profile->lastname ?>
-                        </a>
-                        <br/>
-                        <?php
+            <? foreach($allSimulation as $simulations) : ?>
+                <? foreach($simulations as $simulation) : ?>
+                    <?php $i++; ?>
+                    <tr>
+                        <td>
+                            <a href="/admin_area/user/<?=$simulation->user->id ?>/details">
+                                <?= $simulation->user->profile->firstname." ".$simulation->user->profile->lastname ?>
+                            </a>
+                            <br/>
+                            <?php
                             $class = "label-warning"; // просто оранжевый заметнее, никакого предупреждения в этом нет
                             if ($simulation->user->isCorporate()) {
                                 $class = "label-inverse";
                             }
-                        ?>
+                            ?>
 
-                        <a href="mailto: <?= $simulation->user->profile->email ?>" title="Отправить письмо">
-                            <i class="icon-envelope "></i>
-                        </a>
-                        &nbsp;
+                            <a href="mailto: <?= $simulation->user->profile->email ?>" title="Отправить письмо">
+                                <i class="icon-envelope "></i>
+                            </a>
+                            &nbsp;
                         <span class="label <?= $class ?>">
                             <?= $simulation->user->getAccountName() ?>
                         </span>
-                    </td>
-                    <td>
+                        </td>
+                        <td>
                         <span class="label <?= $simulation->game_type->getSlugCss() ?>" style="margin: 0 auto;">
                             <?= $simulation->game_type->slug?>
                         </span>
-                        &nbsp;
-                        <a href="/admin_area/simulation/<?=$simulation->id ?>/site-logs" target="_blank">
-                            <?=$simulation->id ?>
-                        </a>
-                    </td>
-                    <td>
-                        <? if(isset($simulation->invite->id)) : ?>
-                            <span class="label <?= $simulation->invite->getStatusCssClass() ?>">
+                            &nbsp;
+                            <a href="/admin_area/simulation/<?=$simulation->id ?>/site-logs" target="_blank">
+                                <?=$simulation->id ?>
+                            </a>
+                        </td>
+                        <td>
+                            <? if(isset($simulation->invite->id)) : ?>
+                                <span class="label <?= $simulation->invite->getStatusCssClass() ?>">
                                 <?= $simulation->invite->getStatusText() ?>
                             </span>
-                            &nbsp;
-                            <a href="/admin_area/invite/<?=$simulation->invite->id ?>/site-logs" target="_blank">
-                                <?=$simulation->invite->id ?>
-                            </a>
-                        <? endif; ?>
-                    </td>
-                    <td>
+                                &nbsp;
+                                <a href="/admin_area/invite/<?=$simulation->invite->id ?>/site-logs" target="_blank">
+                                    <?=$simulation->invite->id ?>
+                                </a>
+                            <? endif; ?>
+                        </td>
+                        <td>
                         <span class="label <?= $simulation->getStatusCss() ?>">
                             <?= $simulation->status ?>
                         </span>
-                    </td>
-                    <td><? echo $simulation->getCurrentGameTime() ?></td>
-                    <td>
-                        <? if(isset($simulation->start)) echo $simulation->start ?>
-                        <br/>
-                        <? if(isset($simulation->end)) echo $simulation->end ?>
-                    </td>
-                    <td>
-                        <a target="_blank" class="btn btn-info"
-                           href="/admin_area/simulation/<?=$simulation->id ?>/requests" target="_blank">
-                            Логи запросов
-                        </a>
-                    </td>
-                </tr>
-
-                <?php // проставляем заголовки таблицы в каждой 12й строке - для удобства чтения данных ?>
-                <?php if ($i == 13) : ?>
-                    <tr>
-                        <?php foreach ($titles as $title) : ?>
-                            <th><?= $title ?></th>
-                        <?php endforeach ?>
+                        </td>
+                        <td><? echo $simulation->getCurrentGameTime() ?></td>
+                        <td>
+                            <? if(isset($simulation->start)) echo $simulation->start ?>
+                            <br/>
+                            <? if(isset($simulation->end)) echo $simulation->end ?>
+                        </td>
+                        <td>
+                            <a target="_blank" class="btn btn-info"
+                               href="/admin_area/simulation/<?=$simulation->id ?>/requests" target="_blank">
+                                Логи запросов
+                            </a>
+                        </td>
                     </tr>
-                    <?php $i = 0 ?>
-                <?php endif ?>
 
+                    <?php // проставляем заголовки таблицы в каждой 12й строке - для удобства чтения данных ?>
+                    <?php if ($i == 13) : ?>
+                        <tr>
+                            <?php foreach ($titles as $title) : ?>
+                                <th><?= $title ?></th>
+                            <?php endforeach ?>
+                        </tr>
+                        <?php $i = 0 ?>
+                    <?php endif ?>
+
+                <? endforeach; ?>
             <? endforeach; ?>
-        <? endforeach; ?>
 
         </table>
     <? else : ?>
