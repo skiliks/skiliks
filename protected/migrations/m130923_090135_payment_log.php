2bf8a89b57e38e89b8645dc9096d7fb069653e6f
qzenchik <qzeni.4@gmail.com>
2013-09-23 12:08:21 +0300

    Created migration for lag payment method

---
+++ protected/migrations/m130923_090135_payment_log.php
@@ -0,0 +1,33 @@
+<?php
+
+class m130923_090135_payment_log extends CDbMigration
+{
+	public function up()
+	{
+        $this->execute('
+                         DROP TABLE `log_payment`;
+                         CREATE TABLE `log_payment` (id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT);
+                         ALTER TABLE `log_payment` ADD `invoice_id` INT  NULL  DEFAULT NULL  AFTER `id`;
+                         ALTER TABLE `log_payment` ADD `text` TEXT  NULL  AFTER `invoice_id`;
+                         ALTER TABLE `log_payment` ADD `created_at` DATETIME  NULL  AFTER `invoice_id`;
+                         ALTER TABLE `invoice` CHANGE `create_at` `created_at` DATETIME  NULL  DEFAULT NULL;
+        ');
+	}
+
+	public function down()
+	{
+		echo "m130923_090135_payment_log does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
