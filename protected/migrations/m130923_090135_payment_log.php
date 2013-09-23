04117c3239fa1d655290be8c8daa9b94677be431
qzenchik <qzeni.4@gmail.com>
2013-09-23 12:23:07 +0300

    changing migration to drop table if exists

--- protected/migrations/m130923_090135_payment_log.php
+++ protected/migrations/m130923_090135_payment_log.php
@@ -5,7 +5,7 @@ class m130923_090135_payment_log extends CDbMigration
 	public function up()
 	{
         $this->execute('
-                         DROP TABLE `log_payment`;
+                         DROP TABLE IF EXISTS `log_payment`;
                          CREATE TABLE `log_payment` (id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT);
                          ALTER TABLE `log_payment` ADD `invoice_id` INT  NULL  DEFAULT NULL  AFTER `id`;
                          ALTER TABLE `log_payment` ADD `text` TEXT  NULL  AFTER `invoice_id`;
