0b52e354534213bc60d021d4ff83e86d4a12e360
slavkaa <nikoolin@ukr.net>
2013-09-24 17:52:36 +0300

    SKILIKS-4096. Set invoice@skiliks.com

--- protected/config/base.php
+++ protected/config/base.php
@@ -392,7 +392,7 @@ return array(
             'completeInvoiceUserEmail' => '//global_partials/mails/completeInvoiceUserEmail',
 
 //            'bookerEmail' => 'accounter@skiliks.com',
-            'bookerEmail' => 'tetyana.grybok@skiliks.com',
+            'bookerEmail' => 'invoice@skiliks.com',
         ],
         'allowedLanguages' => [
             'en' => [
