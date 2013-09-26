c9809fda41768ea3da66d3a8047dd8578d01e001
slavkaa <nikoolin@ukr.net>
2013-09-26 20:37:35 +0300

    SKILIKS-4218. Fix lost invites.

--- protected/controllers/static/UserAuthController.php
+++ protected/controllers/static/UserAuthController.php
@@ -188,7 +188,7 @@ class UserAuthController extends YumController
                     $permission->template = 1; // magic const
                     $permission->save(false);
 
-                    UserService::assignAllNotAssignedUserInvites($YumUser);
+                    UserService::assignAllNotAssignedUserInvites(Yii::app()->user->data());
 
                     $this->redirect('/dashboard');
                 } else {
