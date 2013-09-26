b2305c21cc2223a25cce5c9785a577055b2e085a
qzenchik <qzeni.4@gmail.com>
2013-09-26 15:54:41 +0300

    SKILIKS-4160 made total invites

--- protected/views/static/profile/referrers_corporate.php
+++ protected/views/static/profile/referrers_corporate.php
@@ -5,9 +5,8 @@
     <?php $this->renderPartial('_menu_corporate', ['active' => ['referrers' => true]]) ?>
 
     <div class="profileform radiusthree">
-
+        <div class="total-rows">Всего приглашенных: <?=$totalRefers ?></div><br/>
         <?php $this->renderPartial('_reffers_list', []) ?>
-
 
     </div>
 
