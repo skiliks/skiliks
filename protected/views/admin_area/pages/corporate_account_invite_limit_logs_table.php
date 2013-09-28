c6d366975ed1de466705848f0e15e62a9437bb47
slavkaa <nikoolin@ukr.net>
2013-09-28 15:36:41 +0300

    SKILIKS-4214. Add tariff and referal invites amount to admin area.

--- protected/views/admin_area/pages/corporate_account_invite_limit_logs_table.php
+++ protected/views/admin_area/pages/corporate_account_invite_limit_logs_table.php
@@ -2,19 +2,40 @@
     'Дата',
     'Транзакция',
     'Место спровоцировавцее транзакцию (в коде проекта)',
+    'Приглашения <br/>тариф / рефералы<br/>в итоге',
 ] ?>
 <div class="row fix-top">
 
     <h2>Логи движения инвайтов <?= $account->ownership_type ?> "<?= $account->company_name ?>"</h2>
 
-    <strong>Текущий тарифный план:</strong> <?= $account->tariff->label ?><br/>
-    <strong>Текущее количество доступных приглашений:</strong> <?= $account->invites_limit ?><br/>
+    <a href="/admin_area/user/<?= $account->user_id ?>/details">
+        <i class="icon icon-arrow-left"></i> К аккаунту
+    </a>
 
     <br/>
+    <br/>
 
-    <strong>Пользователь:</strong> <?= $account->user->profile->firstname ?> <?= $account->user->profile->lastname ?><br/>
-    <strong>Личный email:</strong> <?= $account->user->profile->email ?><br/>
-    <strong>Корпоративный email:</strong> <?= $account->corporate_email ?><br/>
+    <table class="table table-bordered" style="width: 50%">
+        <tr>
+            <td>Текущий тарифный план</td>
+            <td><?= $account->tariff->label ?></td>
+        </tr>
+        <tr>
+            <td>Текущее количество доступных приглашений</td>
+            <td><?= $account->getTotalAvailableInvitesLimit() ?></td>
+        <tr>
+            <td>Пользователь</td>
+            <td><?= $account->user->profile->firstname ?> <?= $account->user->profile->lastname ?></td>
+        </tr>
+        <tr>
+            <td>Личный email</td>
+            <td><?= $account->user->profile->email ?></td>
+        </tr>
+        <tr>
+            <td>Корпоративный email</td>
+            <td><?= $account->corporate_email ?></td>
+        </tr>
+    <table>
 
     <br/>
 
@@ -43,6 +64,9 @@
                 <td><?= $log->date ?></td>
                 <td><?= $log->amount ?> <?= $log->direction ?> до <?= ($log->limit_after_transaction+$log->invites_limit_referrals) ?></td>
                 <td><?= $log->action ?></td>
+                <td>
+                    <?= $log->limit_after_transaction ?> / <?= $log->invites_limit_referrals ?>
+                </td>
             </tr>
         <?php endforeach ?>
         </tbody>
