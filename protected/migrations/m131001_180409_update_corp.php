0e86d2e9642e59c41aa615ed8296210eae1c59f8
Ivan Tugay <listepo@hotmail.com>
2013-10-02 10:45:56 +0300

    SKILIKS-4205

--- protected/migrations/m131001_180409_update_corp.php
+++ protected/migrations/m131001_180409_update_corp.php
@@ -10,7 +10,7 @@ class m131001_180409_update_corp extends CDbMigration
             $users = [];
             $ids = [135,136,127,134,105,126,123,142,144,145,146,143,147,150,148,151];
             foreach($ids as $id){
-                $tmp_user = YumUser::model()->findAllByAttributes(['id'=>$id]);
+                $tmp_user = YumUser::model()->findByAttributes(['id'=>$id]);
                 if(null !== $tmp_user){
                     $users[] = $tmp_user;
                 }
