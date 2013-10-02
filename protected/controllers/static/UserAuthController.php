bce99aea1274cfdd00431dd16cc8dd5dfdc8d739
Ivan Tugay <listepo@hotmail.com>
2013-10-02 16:59:27 +0300

    SKILIKS-4209 fix after registration

--- protected/controllers/static/UserAuthController.php
+++ protected/controllers/static/UserAuthController.php
@@ -499,6 +499,7 @@ class UserAuthController extends YumController
 
                             $this->sendRegistrationEmail($user);
                             Yii::app()->session->add("email", $profile->email);
+                            Yii::app()->session->add("user_id", $profile->user_id);
                             $this->redirect(['afterRegistration']);
                         } else {
                             throw new Exception("Registration is fail!");
@@ -761,6 +762,7 @@ class UserAuthController extends YumController
         if ($profile && !$profile->user->isActive()) {
             $this->sendRegistrationEmail($profile->user);
             Yii::app()->session->add("email", $profile->email);
+            Yii::app()->session->add("user_id", $profile->user_id);
             $this->redirect(['afterRegistration']);
         } else {
             $this->redirect('/');
