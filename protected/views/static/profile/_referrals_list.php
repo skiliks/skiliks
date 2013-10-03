75d536a8f18832b0d94df726e4988d32f1ff7be4
qzenchik <qzeni.4@gmail.com>
2013-10-03 17:16:28 +0300

    SKILIKS-4165 added invites list page in profile

--- protected/views/static/profile/_referrals_list.php
+++ protected/views/static/profile/_referrals_list.php
@@ -6,6 +6,7 @@
         'summaryText' => '',
         'emptyText'   => '',
         'nullDisplay' => '',
+        'ajaxUpdate'  => false,
         'pager' => [
             'header'         => false,
             'firstPageLabel' => '<< начало',
@@ -16,7 +17,23 @@
         'columns' => [
             ['header' => 'E-mail'            , 'name' => 'referral_email', 'value' => '$data->referral_email'],
             ['header' => 'Приглашен'         , 'name' => 'invited_at'    , 'value' => '$data->invited_at'    ],
-            ['header' => 'Зарегистрировался' , 'name' => 'registered_at' , 'value' => '($data->registered_at === null) ? "Не зарегистрирован" : "Зарегистрирован"' ],
+            ['header' => 'Зарегистрировался' ,
+             'name'   => 'registered_at' ,
+             'value' => function($data) {
+                                         if($data->status == "approved") {
+                                             $assetsUrl = $this->getAssetsUrl();
+                                             return '<div style="display:inline-block; margin-top:2px;">1 симуляция</div>' .
+                                             '<img src="'.$assetsUrl.'/img/referral-bonus.png" style="margin-left: 20px; margin-top:-4px;" />';
+                                         }
+                                         elseif($data->status == "rejected") {
+                                             return '<a class="showDialogRejected" data-domain="'.substr($data->referral_email, strpos($data->referral_email, "@")).'" href="#">Не начислено</a>';
+                                         }
+                                         elseif($data->status == "pending") {
+                                             return '<a class="showDialogPending" href="#">В ожидании</a>';
+                                         }
+            },
+             'type' => 'raw'
+            ],
         ]
     ]);
 ?>
