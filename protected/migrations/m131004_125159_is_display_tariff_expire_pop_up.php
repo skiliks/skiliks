146ae9bad6328130d2c7c1d1f80724670f0448f7
qzenchik <qzeni.4@gmail.com>
2013-10-04 18:31:42 +0300

    SKILIKS-4300 added tariff expired popup

---
+++ protected/migrations/m131004_125159_is_display_tariff_expire_pop_up.php
@@ -0,0 +1,25 @@
+<?php
+
+class m131004_125159_is_display_tariff_expire_pop_up extends CDbMigration
+{
+	public function up()
+	{
+        $this->addColumn("user_account_corporate", "is_display_tariff_expire_pop_up", "TINYINT (1) DEFAULT 1");
+	}
+
+	public function down()
+	{
+        $this->dropColumn("user_account_corporate", "is_display_tariff_expire_pop_up");
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
