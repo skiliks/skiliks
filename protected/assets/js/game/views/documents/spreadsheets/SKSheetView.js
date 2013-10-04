624aa9d2b591aa8103d011f7d0216fedce864cd3
slavkaa <nikoolin@ukr.net>
2013-10-04 13:36:34 +0300

    SKILIKS-4115. Add sentry log when SC fall down.

--- protected/assets/js/game/views/documents/spreadsheets/SKSheetView.js
+++ protected/assets/js/game/views/documents/spreadsheets/SKSheetView.js
@@ -8,26 +8,29 @@ define([], function () {
         initialize:      function () {
             try {
                 var sheet = this.options.sheet;
+
+                // SC иногда подвисает
+                //
+                sheet.set('isInitializeCalculated', false);
+                var longLoadingTimeout = setTimeout(function() {
+                    console.log('isInitializeCalculated', sheet.get('isInitializeCalculated'));
+                    if (false == sheet.get('isInitializeCalculated')) {
+                        if (window.Raven) {
+                            window.Raven.captureMessage(
+                                'Sheet ' + sheet.get('name') + ' in simulation ' + SKApp.simulation.id + ' завис.'
+                            );
+                        }
+                    }
+                }, 10000);
+
                 this.parts = sc.DecodeSpreadsheetSave(sheet.get('content'));
                 if (this.parts && this.parts.sheet) {
                     this.sheetData = sheet.get('content').substring(this.parts.sheet.start, this.parts.sheet.end);
                     SocialCalc.Formula.AddSheetToCache(sheet.collection.document.get('name'), sheet.get('name'), this.sheetData);
                 }
 
-                var me = this;
-
-                me.set('isCalculated', false);
-
-                setTimeout(function() {
-                    if (false == me.get('isCalculated')) {
-                        console.log('Sheet ' + sheet.get('name') + ' in simulation ' + SKApp.simulation.id + ' завис.');
-                        if (window.Raven) {
-//                            window.Raven.captureMessage(
-//                                'Sheet ' + sheet.get('name') + ' in simulation ' + SKApp.simulation.id + ' завис.'
-//                            );
-                        }
-                    }
-                }, 10000);
+                sheet.set('isInitializeCalculated', true);
+                clearTimeout(longLoadingTimeout);
 
                 this.listenTo(sheet, 'activate', this.activateSheet);
                 this.listenTo(sheet, 'recalc', this.recalcSheet);
@@ -57,14 +60,31 @@ define([], function () {
         recalcSheet:     function () {
             try {
                 var me = this;
+                var sheet = this.options.sheet;
+
+                sheet.set('isRecalcCalculated', false);
+                var longLoadingTimeout = setTimeout(function() {
+                    console.log('isRecalcCalculated', sheet.get('isRecalcCalculated'));
+                    if (false == sheet.get('isRecalcCalculated')) {
+                        if (window.Raven) {
+                            window.Raven.captureMessage(
+                                'Sheet ' + sheet.get('name') + ' in simulation ' + SKApp.simulation.id + ' завис.'
+                            );
+                        }
+                    }
+                }, 10000);
+
                 loadQueue.queue('fx', function () {
                     if (me.spreadsheet) {
                         me.spreadsheet.ExecuteCommand('recalc', '');
                         me.spreadsheet.ExecuteCommand('redisplay', '');
                     }
                     loadQueue.dequeue('fx');
-
                 });
+
+                sheet.set('isRecalcCalculated', true);
+                clearTimeout(longLoadingTimeout);
+
             } catch(exception) {
                 if (window.Raven) {
                     window.Raven.captureMessage(exception.message + ',' + exception.stack);
@@ -76,11 +96,27 @@ define([], function () {
                 var me = this;
                 var sheet = this.options.sheet;
 
+                sheet.set('isActivateCalculated', false);
+                var longLoadingTimeout = setTimeout(function() {
+                    console.log('isActivateCalculated', sheet.get('isActivateCalculated'));
+                    if (false == sheet.get('isActivateCalculated')) {
+                        if (window.Raven) {
+                            window.Raven.captureMessage(
+                                'Sheet ' + sheet.get('name') + ' in simulation ' + SKApp.simulation.id + ' завис.'
+                            );
+                        }
+                    }
+                }, 10000);
+
                 loadQueue.queue('fx', function () {
                     me.rootView.show();
                     me.dequeue = true;
                     me.spreadsheet.editor.SchedulePositionCalculations();
                 });
+
+                sheet.set('isActivateCalculated', true);
+                clearTimeout(longLoadingTimeout)
+
             } catch(exception) {
                 if (window.Raven) {
                     window.Raven.captureMessage(exception.message + ',' + exception.stack);
