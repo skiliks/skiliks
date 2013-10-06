164865bf619df5d85441a68252e1b9477cc34a83
slavkaa <nikoolin@ukr.net>
2013-10-06 22:14:24 +0300

    SKILIKS-4288. Core riview.

--- protected/views/static/profile/_referrals_list.php
+++ protected/views/static/profile/_referrals_list.php
@@ -20,17 +20,17 @@
             ['header' => 'Статус' ,
              'name'   => 'registered_at' ,
              'value' => function($data) {
-                                         if($data->status == "approved") {
-                                             $assetsUrl = $this->getAssetsUrl();
-                                             return '<div style="display:inline-block; margin-top:2px;">1 симуляция</div>' .
-                                             '<img src="'.$assetsUrl.'/img/referral-bonus.png" style="margin-left: 20px; margin-top:-4px;" />';
-                                         }
-                                         elseif($data->status == "rejected") {
-                                             return '<a class="showDialogRejected" data-domain="'.substr($data->referral_email, strpos($data->referral_email, "@")).'" href="#">Не начислено</a>';
-                                         }
-                                         elseif($data->status == "pending") {
-                                             return '<a class="showDialogPending" href="#">В ожидании</a>';
-                                         }
+                 if($data->isApproved()) {
+                     $assetsUrl = $this->getAssetsUrl();
+                     return '<div style="display:inline-block; margin-top:2px;">1 симуляция</div>' .
+                     '<img src="'.$assetsUrl.'/img/referral-bonus.png" style="margin-left: 20px; margin-top:-4px;" />';
+                 }
+                 elseif($data->isRejected()) {
+                     return '<a class="showDialogRejected" data-domain="'.substr($data->referral_email, strpos($data->referral_email, "@")).'" href="#">Не начислено</a>';
+                 }
+                 elseif($data->isPending()) {
+                     return '<a class="showDialogPending" href="#">В ожидании</a>';
+                 }
             },
              'type' => 'raw'
             ],
