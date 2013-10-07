0fb64fd3b98f15f80d8236988488c891e3f336ba
qzenchik <qzeni.4@gmail.com>
2013-10-07 14:11:22 +0300

    SKILIKS-4256 remaking registration popup

--- protected/views/static/help/_help_menu.php
+++ protected/views/static/help/_help_menu.php
@@ -1,15 +1,17 @@
 <div class="help-container">
-    <h1>Помощь</h1><br/>
+    <section class="dashboard corpdashboard">
 
-    <a href="/help/general" class="help-top-header <?php if (isset($active['faq'])){ echo 'active-question-header'; } ?>" id="help-top-header-general">
+    <h2 class="thetitle bigtitle">Помощь</h2><br/>
+
+    <a href="/help/general" class="help-top-header ProximaNova-Bold <?php if (isset($active['faq'])){ echo 'active-question-header'; } ?>" id="help-top-header-general">
         FAQ
     </a>
 
-    <a href="/help/corporate" class="help-top-header <?php if (isset($active['corporate'])){ echo 'active-question-header'; } ?>" id="help-top-header-corporate">
+    <a href="/help/corporate" class="help-top-header ProximaNova-Bold <?php if (isset($active['corporate'])){ echo 'active-question-header'; } ?>" id="help-top-header-corporate">
         Корпоративный<br/>профиль
     </a>
 
-    <a href="/help/personal"  class="help-top-header <?php if (isset($active['personal'])){ echo 'active-question-header'; } ?>">
+    <a href="/help/personal"  class="help-top-header ProximaNova-Bold <?php if (isset($active['personal'])){ echo 'active-question-header'; } ?>">
         Индивидуальный<br/>профиль
     </a>
 
