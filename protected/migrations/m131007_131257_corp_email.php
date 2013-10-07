68d037cff4ba3b8bf151b286cbd65f0c19fefb35
slavkaa <nikoolin@ukr.net>
2013-10-07 23:08:08 +0300

    SKILIKS-4299. Skip migration.

--- protected/migrations/m131007_131257_corp_email.php
+++ protected/migrations/m131007_131257_corp_email.php
@@ -4,11 +4,11 @@ class m131007_131257_corp_email extends CDbMigration
 {
 	public function up()
 	{
-        //$this->addColumn('user_account_corporate', 'corporate_email', 'varchar(120) DEFAULT NULL');
+        // $this->addColumn('user_account_corporate', 'corporate_email', 'varchar(120) DEFAULT NULL');
 	}
 
 	public function down()
 	{
-		$this->dropColumn('user_account_corporate', 'corporate_email');
+		// $this->dropColumn('user_account_corporate', 'corporate_email');
 	}
 }
