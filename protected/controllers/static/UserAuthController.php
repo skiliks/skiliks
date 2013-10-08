4c26c92b8c1b9ae8898ff942c6e3502faa4be0c7
Ivan <listepo@ya.ru>
2013-10-08 17:59:02 +0300

    SKILIKS-4322 fix

--- protected/controllers/static/UserAuthController.php
+++ protected/controllers/static/UserAuthController.php
@@ -431,7 +431,7 @@ class UserAuthController extends YumController
             $industries[$industry->id] = Yii::t('site', $industry->label);
         }
 
-        $statuses = [''=>'Выберите проф. статус'];
+        $statuses = [''=>'Выберите статус'];
         foreach (ProfessionalStatus::model()->findAll() as $status) {
             $statuses[$status->id] = Yii::t('site', $status->label);
         }
