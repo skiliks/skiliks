2e112ad9e421a7b23944f70ee1b3c6d13aef5bb0
qzenchik <qzeni.4@gmail.com>
2013-10-08 11:07:16 +0300

    SKILIKS-4334 remaking bugs in registration

--- protected/views/static/userAuth/registration.php
+++ protected/views/static/userAuth/registration.php
@@ -25,7 +25,7 @@
         )); ?>
         <div class="row">
             <?php echo $form->error($accountPersonal, 'professional_status_id'); ?>
-            <div class="field">
+            <div class="field registration-personal-additional-field">
                 <?php echo $form->labelEx($accountPersonal     ,'professional_status_id', ["class" => "ProximaNova-Bold"]); ?>
                 <?php echo $form->dropDownList($accountPersonal,'professional_status_id', $statuses); ?>
             </div>
@@ -41,7 +41,7 @@
         </a>
 	    <h1>Корпоративный<br>профиль</h1>
         <p class="p-chose-account-type ProximaNova-Bold">(Вы - работодатель)</p>
-	    <ul>
+	    <ul class="registration-corporate-benefits">
 			<li class="ProximaNova-Bold"><?php echo Yii::t('site', '10 симуляций бесплатно') ?></li>
             <li class="ProximaNova-Bold"><?php echo Yii::t('site', 'Пакет симуляций для оценки кандидатов и сотрудников') ?></li>
             <li class="ProximaNova-Bold"><?php echo Yii::t('site', 'Удобный инструмент для прогресса оценки') ?></li>
@@ -101,7 +101,7 @@
                 <input type="hidden" value="corporate" name="account-type">
             </div>
             <div class="row four-parts to-right submit-input">
-                <?php echo CHtml::submitButton(Yii::t('site', ' Зарегистрироваться'), ['id'=>'registration_switch']); ?>
+                <button type="submit" class="ProximaNova-Bold blue-submit-button registration-button"><?= Yii::t("site","Sign up") ?></button>
             </div>
         </div>
         <div style="clear:both;"></div>
