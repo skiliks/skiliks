29e121e256a3281ba2daad19b61e7c4b0631bf3b
Tetyana Grybok <tetyana.grybok@skiliks.com>
2013-10-09 11:59:43 +0300

    SK-4361

--- protected/components/SimulationService.php
+++ protected/components/SimulationService.php
@@ -632,7 +632,7 @@ class SimulationService
      */
     public static function simulationStop($simulation, $logs_src = array(), $manual=false)
     {
-        sleep(3000);
+        sleep(300);
         self::logAboutSim($simulation, 'sim stop: begin');
 
         // Check if simulation was already stopped
