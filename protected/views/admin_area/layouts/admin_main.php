5ca6698c3d63ca623ec3a9ca95fd8eb7dab0fa20
slavkaa <nikoolin@ukr.net>
2013-10-10 01:36:37 +0300

    Up version to 2.0.7.

--- protected/views/admin_area/layouts/admin_main.php
+++ protected/views/admin_area/layouts/admin_main.php
@@ -24,7 +24,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="brand" href="/">Skiliks: release 2.0.6</a>
+            <a class="brand" href="/">Skiliks: release 2.0.7</a>
             <?php $this->renderPartial('//admin_area/partials/_top_menu') ?>
         </div>
     </div>
