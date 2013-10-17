03e65f6ace53182b15295eb7977a44efa1038d23
qzenchik <qzeni.4@gmail.com>
2013-10-17 15:03:10 +0300

    SKILIKS-4295 SKILIKS-4175

--- protected/views/global_partials/addthis.php
+++ protected/views/global_partials/addthis.php
@@ -5,7 +5,7 @@
 ?>
 <?php if($force || in_array(Yii::app()->request->getPathInfo(), $allow)){ ?>
     <div class="social_networks"><span><?php echo Yii::t('site', "Share");?>:</span><div class="addthis_toolbox addthis_default_style addthis_32x32_style"><a class="addthis_button_vk"></a>
-        <a class="new_social_buttons facebook_share_button_big" title="Facebook" onclick="
+        <a class="new_social_buttons_footer facebook_share_button_footer" title="Facebook" onclick="
     window.open(
       'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent('<?=Yii::app()->request->hostInfo?>'),
       'facebook-share-dialog',
@@ -14,7 +14,7 @@
         <a class="addthis_button_twitter"></a>
 
 
-        <a class="new_social_buttons google_share_button_big" title="Google" onclick="
+        <a class="new_social_buttons_footer google_share_button_footer" title="Google" onclick="
             window.open(
             'https://plus.google.com/share?url=' + encodeURIComponent('<?=Yii::app()->request->hostInfo?>'),
             'facebook-share-dialog',
