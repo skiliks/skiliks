4e880e875b8eb826101e4de8882ad9e118e3ee9b
qzenchik <qzeni.4@gmail.com>
2013-10-21 20:29:44 +0300

    SKILIKS-4294

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -32,7 +32,7 @@ $cs->registerCssFile($assetsUrl . "/css/popover.css");
         <?php if(Yii::app()->controller->action->id == "watchVideo") : ?>
             <meta property="og:image" content="http://b.vimeocdn.com/ts/449/036/449036980_640.jpg"/>
         <?php else : ?>
-            <meta property="og:image" content="<?=$assetsUrl?>/img/square-logo.jpg"/>
+            <meta property="og:image" content="http://<?=$_SERVER['HTTP_HOST']; ?>/<?=$assetsUrl?>/img/square-logo.jpg"/>
         <?php endif ?>
 
         <meta property="og:title" content="Skiliks – game the skills"/>
