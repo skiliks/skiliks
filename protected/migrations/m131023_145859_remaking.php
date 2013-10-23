da03d037a2da64d3fea29635e027671e7f0dfb6c
qzenchik <qzeni.4@gmail.com>
2013-10-23 18:08:43 +0300

    SKILIKS-4422

---
+++ protected/migrations/m131023_145859_remaking.php
@@ -0,0 +1,26 @@
+<?php
+
+class m131023_145859_remaking extends CDbMigration
+{
+	public function up()
+	{
+        $this->alterColumn("invites", "sent_time", "DATETIME");
+	}
+
+	public function down()
+	{
+		echo "m131023_145859_remaking does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
