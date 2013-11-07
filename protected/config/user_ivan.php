041416512deaf9cefb7a5b26ef25fdb2cc7d1a89
slavkaa <nikoolin@ukr.net>
2013-11-07 16:15:02 +0200

    SKILIKS-4727. Исправил пароль от почты + удалил  старые конфиги.

--- protected/config/user_ivan.php
+++ protected/config/user_ivan.php
@@ -1,37 +1,47 @@
 <?php
 return CMap::mergeArray(
     require(dirname(__FILE__) . '/base.php'),
-    array('components' => array(
-        'db' => array(
-            'connectionString' => 'mysql:host=127.0.0.1;dbname=skiliks_develop',
-            'emulatePrepare' => true,
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
+    array(
+        'modules' => [
+            'gii'=>array(
+                'class'    =>'system.gii.GiiModule',
+                'password' =>'MapRofi531',
+                //'ipFilters'=>array('62.205.135.161'),
+                // 'newFileMode'=>0666,
+                // 'newDirMode'=>0777,
+            ),
+        ],
+        'components' => array(
+            'db' => array(
+                'connectionString' => 'mysql:host=127.0.0.1;dbname=skiliks_develop',
+                'emulatePrepare' => true,
+                'username' => 'root',
+                'password' => '',
+                'charset' => 'utf8',
 
-            'enableParamLogging' => true,
-            'enableProfiling' => true
-        ),
-        'log' => array(
-            'class' => 'CLogRouter',
-            'routes' => array(
-                array(
-                    'class' => 'CFileLogRoute',
-                    'levels' => 'error, warning, info, trace, log, debug',
-                ),
-                array(
-                    'class' => 'CFileLogRoute',
-                    'levels' => 'error, warning, info, trace, log, debug',
-                    'categories'=>'system.db.*',
-                    'logFile' => 'sql.log'
-                ),
+                'enableParamLogging' => true,
+                'enableProfiling' => true
+            ),
+            'log' => array(
+                'class' => 'CLogRouter',
+                'routes' => array(
+                    array(
+                        'class' => 'CFileLogRoute',
+                        'levels' => 'error, warning, info, trace, log, debug',
+                    ),
+                    array(
+                        'class' => 'CFileLogRoute',
+                        'levels' => 'error, warning, info, trace, log, debug',
+                        'categories'=>'system.db.*',
+                        'logFile' => 'sql.log'
+                    ),
 
+                ),
+            ),
+            'request' => array(
+                'baseUrl' => '',
             ),
         ),
-        'request' => array(
-            'baseUrl' => '',
-        ),
-    ),
         'params' => array(
             'frontendUrl' => 'http://loc.skiliks.com/',
             'assetsDebug' => true,
--- protected/config/user_slavka.php
+++ protected/config/user_slavka.php
@@ -1,36 +1,46 @@
 <?php
 return CMap::mergeArray(
     require(dirname(__FILE__) . '/base.php'),
-    array('components' => array(
-        'db' => array(
-            'connectionString' => 'mysql:host=127.0.0.1;dbname=production_2',
-            'emulatePrepare' => true,
-            'username' => 'root',
-            'password' => '111',
-            'charset' => 'utf8',
-            'enableParamLogging' => true,
-            'enableProfiling' => true
-        ),
-        'log' => array(
-            'class' => 'CLogRouter',
-            'routes' => array(
-                array(
-                    'class' => 'CFileLogRoute',
-                    'levels' => 'error, warning, info, trace, log, debug',
-                ),
-                array(
-                    'class' => 'CFileLogRoute',
-                    'levels' => 'error, warning, info, trace, log, debug',
-                    'categories'=>'system.db.*',
-                    'logFile' => 'sql.log'
-                ),
+    array(
+        'modules' => [
+            'gii'=>array(
+                'class'    =>'system.gii.GiiModule',
+                'password' =>'MapRofi531',
+                //'ipFilters'=>array('62.205.135.161'),
+                // 'newFileMode'=>0666,
+                // 'newDirMode'=>0777,
+            ),
+        ],
+        'components' => array(
+            'db' => array(
+                'connectionString' => 'mysql:host=127.0.0.1;dbname=production_2',
+                'emulatePrepare' => true,
+                'username' => 'root',
+                'password' => '111',
+                'charset' => 'utf8',
+                'enableParamLogging' => true,
+                'enableProfiling' => true
+            ),
+            'log' => array(
+                'class' => 'CLogRouter',
+                'routes' => array(
+                    array(
+                        'class' => 'CFileLogRoute',
+                        'levels' => 'error, warning, info, trace, log, debug',
+                    ),
+                    array(
+                        'class' => 'CFileLogRoute',
+                        'levels' => 'error, warning, info, trace, log, debug',
+                        'categories'=>'system.db.*',
+                        'logFile' => 'sql.log'
+                    ),
 
+                ),
+            ),
+            'request' => array(
+                'baseUrl' => '',
             ),
         ),
-        'request' => array(
-            'baseUrl' => '',
-        ),
-    ),
         'params' => array(
             'frontendUrl' => 'http://skiliks.loc/',
             'assetsDebug' => true,
