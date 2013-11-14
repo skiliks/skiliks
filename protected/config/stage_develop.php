bf7b0bc937b6ebd43a7ed58e0bb80108b8a216a7
slavkaa <nikoolin@ukr.net>
2013-11-14 16:54:08 +0200

    Add develop-stable config.

---
+++ protected/config/stage_develop.php
@@ -0,0 +1,46 @@
+<?php
+define(YII_DEBUG, false);
+return CMap::mergeArray(require(dirname(__FILE__) . '/base.php'), array(
+    'preload'=> array('log', 'RSentryException'),
+    'components'=>array(
+        'db'=>array(
+            'connectionString' => 'mysql:host=localhost;dbname=skiliks_live',
+            'emulatePrepare' => true,
+            'username' => 'skiliks_live',
+            'password' => 'dep-vep-eb-up-a',
+            'charset' => 'utf8',
+
+            'enableParamLogging'=>true,
+            'enableProfiling'=>true
+        ),
+        'RSentryException'=> array(
+            'dsn'=> 'https://bfd7395024f24728afdf79e9034bca04:2f8bec2e2c40493dbf7b07db88afc94f@app.getsentry.com/4572',
+            'class' => 'application.components..yii-sentry-log.RSentryComponent',
+        ),
+        'log'=>array(
+            'class'=>'CLogRouter',
+            'routes'=>array(
+                array(
+                    'class'=>'application.components.yii-sentry-log.RSentryLog',
+                    'dsn'=> 'https://bfd7395024f24728afdf79e9034bca04:2f8bec2e2c40493dbf7b07db88afc94f@app.getsentry.com/4572',
+                    'levels'=>'error, warning',
+                ),
+            ),
+        ),
+    ),
+    'params'=>array(
+        'server_name'                   => 'http://skiliks.com/',
+        'frontendUrl'=>'http://live.skiliks.com/',
+        'runMigrationOn' => 'live',
+        'disableOldLogging' => true,
+        'public' => [
+            'storageURL'           => 'http://storage.dev.skiliks.com/',
+            'isLocalPc'            => true,
+            'useSentryForJsLog'    => true,
+            'isSkipBrowserCheck'   => true,
+            'isDisplaySupportChat' => true,
+        ]
+    )
+));
+
+
