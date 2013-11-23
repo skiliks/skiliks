cc5513436cfb237364698a20b50aec56c63a9e07
Ivan <listepo@ya.ru>
2013-11-23 19:16:18 +0200

    SKILIKS-4806

--- protected/migrations/m131122_090152_assessment_version.php
+++ protected/migrations/m131122_090152_assessment_version.php
@@ -4,6 +4,18 @@ class m131122_090152_assessment_version extends CDbMigration
 {
 	public function up()
 	{
+        $simulations_v1 = Simulation::model()->findAllByAttributes(['results_popup_partials_path' => '//simulation_details_popup/v1']);
+        $simulations_v2 = Simulation::model()->findAllByAttributes(['results_popup_partials_path' => '//simulation_details_popup/v2']);
+
+        foreach($simulations_v1 as $simulation) {
+            $simulation->assessment_version = 'v1';
+            $simulation->save(false);
+        }
+
+        foreach($simulations_v2 as $simulation) {
+            $simulation->assessment_version = 'v2';
+            $simulation->save(false);
+        }
         $simulations = Simulation::model()->findAll("results_popup_partials_path is null or assessment_version is null");
         /* @var $simulation Simulation*/
         foreach($simulations as $simulation) {
