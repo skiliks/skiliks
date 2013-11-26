0689fd52b2f08a1fe1e4e7c334db488d54f1301d
Ivan <listepo@ya.ru>
2013-11-26 14:21:37 +0200

    SKILIKS-4875

--- protected/views/static/dashboard/partials/extend-tariff-popup.php
+++ protected/views/static/dashboard/partials/extend-tariff-popup.php
@@ -2,11 +2,11 @@
     <div class="more-side-pads">
         <h2 class="title">Внимание!</h2>
         <ul class="list-ordered">
-            <p class="font-xxlarge">Вы продлеваете тарифный план Lite (10 симуляций в месяц) с 20.12.2013 по 19.01.2014</br>
+            <p class="font-xxlarge">Вы продлеваете тарифный план <span class="tariff_label">Lite</span> (<span class="tariff_limits">10</span> симуляций в месяц) с <span class="tariff_start">20.12.2013</span> по <span class="tariff_end">19.01.2014</span></br>
             </p>
         </ul>
         <div class="text-center">
-            <a href="#" data-href="#" class="bigbtnsubmt">Продлить</a>
+            <a href="#" data-href="#" class="bigbtnsubmt tariff-link">Продлить</a>
             <a href="#" data-href="#" data-class="extend-tariff-popup" class="bigbtnsubmt subscribe-ti-tariff-close">Отмена</a>
         </div>
     </div>
--- protected/views/static/dashboard/partials/tariff-already-booked-popup.php
+++ protected/views/static/dashboard/partials/tariff-already-booked-popup.php
@@ -2,7 +2,7 @@
     <div class="more-side-pads">
         <h2 class="title">У вас уже заказан тариф</h2>
         <ul class="list-ordered">
-            <p class="font-xxlarge">У вас уже есть заказанный тарифный план. Он будет активирован 20 декабря 2013, тариф - Lite </br>
+            <p class="font-xxlarge">У вас уже есть заказанный тарифный план. Он будет активирован <span class="tariff_start">20 декабря 2013</span>, тариф - <span class="tariff_label">Lite</span> </br>
                 Если вам надо сменить или продлить тарифный план раньше - свяжитесь с тех поддержкой через</br>
                 <a href="malito:support@skiliks.com">support@skiliks.com</a></br></br>
             </p>
