612eff699941f8ed9a335259f73035fed1e6c5d9
Ivan <listepo@ya.ru>
2013-11-27 17:47:12 +0200

    SKILIKS-4875

---
+++ protected/views/static/dashboard/partials/downgrade-tariff-popup.php
@@ -0,0 +1,13 @@
+<div class="downgrade-tariff-popup" style="display: none;">
+    <div class="more-side-pads">
+        <h2 class="title">Внимание!</h2>
+        <ul class="list-ordered">
+            <p class="font-xxlarge">Вы продлеваете тарифный план <span class="tariff_label">Lite</span> (<span class="tariff_limits">10</span> симуляций в месяц) с <span class="tariff_start">20.12.2013</span> по <span class="tariff_end">19.01.2014</span></br>
+            </p>
+        </ul>
+        <div class="text-center">
+            <a href="#" data-href="#" class="bigbtnsubmt tariff-link">Сменить сейчас</a>
+            <a href="#" data-href="#" data-class="extend-tariff-popup" class="bigbtnsubmt subscribe-ti-tariff-close">Сменить позже</a>
+        </div>
+    </div>
+</div>
--- protected/views/static/dashboard/partials/tariff-replace-now-popup.php
+++ protected/views/static/dashboard/partials/tariff-replace-now-popup.php
@@ -3,13 +3,12 @@
     <div class="more-side-pads">
         <h2 class="title">Внимание!</h2>
         <ul class="list-ordered">
-            <p class="font-xxlarge">У вас <?php echo $account->invites_limit ?> доступные симуляций. Если вы хотите перейти на наовый тариф прямо сейчас, то эти симуляции
-                сгорят</br>
+            <p class="font-xxlarge">У вас <?php echo $account->invites_limit ?> доступных симуляций. Если вы хотите перейти на новый тариф прямо сейчас, то эти симуляции сгорят</br>
             </p>
         </ul>
         <div class="text-center">
             <a href="#" data-href="#" class="bigbtnsubmt tariff-link">Сменить сейчас</a>
-            <a href="#" data-href="#" data-class="tariff-replace-now-popup" class="bigbtnsubmt subscribe-ti-tariff-close">Отмена</a>
+            <a href="#" data-href="#" data-class="tariff-replace-now-popup" class="bigbtnsubmt subscribe-ti-tariff-close">Сменить позже</a>
         </div>
     </div>
 </div>
