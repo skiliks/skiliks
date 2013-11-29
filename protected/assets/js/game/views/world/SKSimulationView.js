b2162019879845a30ff4626cd8605392a4c733f7
slavkaa <nikoolin@ukr.net>
2013-11-29 22:34:50 +0200

    SKILIKS-4959. Fix white linws in IE.

--- protected/assets/js/game/views/world/SKSimulationView.js
+++ protected/assets/js/game/views/world/SKSimulationView.js
@@ -41,7 +41,10 @@ define([
         {
             'attributes': {
                 'style': 'width: 100%; height: 100%; float: left;'
-                    + '-moz-user-select: none; -webkit-user-select: none; -ms-user-select:none; user-select:none;'
+                    + ' -moz-user-select: none; '
+                    + ' -webkit-user-select: none;'
+                    + ' -ms-user-select:none; user-select:none;'
+                    + ' font-size: 0;'
             },
 
             'events':          {
