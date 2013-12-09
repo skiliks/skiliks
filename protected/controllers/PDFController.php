203d9bb7dec99659a56dcd2af52436a246ee7995
Ivan <listepo@ya.ru>
2013-12-09 18:28:23 +0200

    SKILIKS-4770

--- protected/controllers/PDFController.php
+++ protected/controllers/PDFController.php
@@ -91,7 +91,7 @@ class PDFController extends SiteBaseController {
 
         $pdf = new AssessmentPDF();
         $username = "Иван Иванов";
-        /*$pdf->addPage();
+        $pdf->addPage();
         $pdf->writeTextBold($username, 3.5, 3.5, 21);
         $pdf->addRatingPercentile(92.4, 37.6, 4);
         $pdf->addRatingOverall(85, 48, 100);
@@ -181,23 +181,22 @@ class PDFController extends SiteBaseController {
         $pdf->addUniversalBar(152, 60, 26, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.1 negative
         $pdf->addUniversalBar(152, 70.6, 0, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.2 negative
         $pdf->addUniversalBar(152, 81.2, 2, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.3 negative
-        */
-        $pdf->page_number = 7;
+
         $pdf->addPage();
 
         $pdf->writeTextBold($username, 3.5, 3.5, 21);
         $pdf->writeTextBold('100%', 148.7, 28, 10, [255,255,255]);//1
         $pdf->writeTextBold('100%', 2.8, 36.8, 18);
 
-        $pdf->addUniversalBar(77, 60, 3, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//2.1 positive
-        $pdf->addUniversalBar(77, 70.6, 30, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//2.2 positive
-        $pdf->addUniversalBar(77, 81.2, 93, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//2.3 positive
-        $pdf->addUniversalBar(77, 91.8, 93, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//2.3 positive
+        $pdf->addUniversalBar(77, 60, 3, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.1 positive
+        $pdf->addUniversalBar(77, 70.6, 30, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.2 positive
+        $pdf->addUniversalBar(77, 81.2, 93, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.3 positive
+        $pdf->addUniversalBar(77, 91.8, 93, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.4 positive
 
-        $pdf->addUniversalBar(152, 60, 26, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.1 negative
-        $pdf->addUniversalBar(152, 70.6, 0, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.2 negative
-        $pdf->addUniversalBar(152, 81.2, 2, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.3 negative
-        $pdf->addUniversalBar(152, 91.8, 2, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.3 negative
+        $pdf->addUniversalBar(152, 60, 26, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.1 negative
+        $pdf->addUniversalBar(152, 70.6, 0, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.2 negative
+        $pdf->addUniversalBar(152, 81.2, 2, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.3 negative
+        $pdf->addUniversalBar(152, 91.8, 2, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.4 negative
 
         $pdf->renderOnBrowser('Assessment_v2');
     }
