e97c81b62297f6e5874fde5fdf8e1f4d34815ad2
Ivan <listepo@ya.ru>
2013-12-10 01:20:31 +0200

    SKILIKS-4770

--- protected/controllers/DebugController.php
+++ protected/controllers/DebugController.php
@@ -1572,7 +1572,7 @@ class DebugController extends SiteBaseController
        /* @var $simulation Simulation */
         $simulation = Simulation::model()->findByPk(1);
        echo '<pre>';
-        $data = json_decode($simulation->getAssessmentDetails(),true)['performance'];
+        $data = json_decode($simulation->getAssessmentDetails(),true)['management'][1];
         var_dump($data);
         echo '</pre>';
     }
--- protected/controllers/PDFController.php
+++ protected/controllers/PDFController.php
@@ -91,56 +91,56 @@ class PDFController extends SiteBaseController {
         $pdf->addPage();
 
         $pdf->writeTextBold($username, 3.5, 3.5, 21);
-        $pdf->writeTextBold('100%', 149.3, 28.3, 10, [255,255,255]);//Управленческие навыки
+        $pdf->writeTextBold(round($data['management']['total']).'%', 149.3, 28.3, 10, [255,255,255]);//Управленческие навыки
 
-        $pdf->addUniversalBar(77.7, 48.9, 23, 128.7, AssessmentPDF::ROUNDED_BOTH, AssessmentPDF::BAR_POSITIVE);//1
-        $pdf->addUniversalBar(77.7, 59.5, 98, 128.7, AssessmentPDF::ROUNDED_BOTH, AssessmentPDF::BAR_POSITIVE);//2
-        $pdf->addUniversalBar(77.7, 70.1, 43, 128.7, AssessmentPDF::ROUNDED_BOTH, AssessmentPDF::BAR_POSITIVE);//3
+        $pdf->addUniversalBar(77.7, 48.9, $data['management'][1]['total'], 128.7, AssessmentPDF::ROUNDED_BOTH, AssessmentPDF::BAR_POSITIVE);//1
+        $pdf->addUniversalBar(77.7, 59.5, $data['management'][2]['total'], 128.7, AssessmentPDF::ROUNDED_BOTH, AssessmentPDF::BAR_POSITIVE);//2
+        $pdf->addUniversalBar(77.7, 70.1, $data['management'][3]['total'], 128.7, AssessmentPDF::ROUNDED_BOTH, AssessmentPDF::BAR_POSITIVE);//3
 
 
         $pdf->addPage();
         $pdf->writeTextBold($username, 3.5, 3.5, 21);
-        $pdf->writeTextBold('100%', 149.9, 28.2, 10, [255,255,255]);//1
-        $pdf->writeTextBold('100%', 3.4, 36.8, 18);
+        //$pdf->writeTextBold('100%', 149.9, 28.2, 10, [255,255,255]);//1
+        $pdf->writeTextBold(round($data['management'][1]['total']).'%', 3.4, 36.8, 18);
 
 
-        $pdf->addUniversalBar(77, 60, 23, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//1.1 positive
-        $pdf->addUniversalBar(77, 70.6, 100, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//1.2 positive
-        $pdf->addUniversalBar(77, 81.2, 43, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//1.3 positive
+        $pdf->addUniversalBar(77, 60, $data['management'][1]['1_1']['+'], 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//1.1 positive
+        $pdf->addUniversalBar(77, 70.6, $data['management'][1]['1_2']['+'], 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//1.2 positive
+        $pdf->addUniversalBar(77, 81.2, $data['management'][1]['1_3']['+'], 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//1.3 positive
 
-        $pdf->addUniversalBar(152, 60, 23, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//1.1 negative
-        $pdf->addUniversalBar(152, 70.6, 98, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//1.2 negative
-        $pdf->addUniversalBar(152, 81.2, 23, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//1.3 negative
-        $pdf->addUniversalBar(152, 91.8, 100, 54.14, AssessmentPDF::ROUNDED_BOTH, AssessmentPDF::BAR_NEGATIVE);//1.4 negative
+        $pdf->addUniversalBar(152, 60, $data['management'][1]['1_1']['-'], 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//1.1 negative
+        $pdf->addUniversalBar(152, 70.6, $data['management'][1]['1_2']['-'], 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//1.2 negative
+        $pdf->addUniversalBar(152, 81.2, $data['management'][1]['1_3']['-'], 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//1.3 negative
+        $pdf->addUniversalBar(152, 91.8, $data['management'][1]['1_4']['-'], 54.14, AssessmentPDF::ROUNDED_BOTH, AssessmentPDF::BAR_NEGATIVE);//1.4 negative
 
         $pdf->addPage();
         $pdf->writeTextBold($username, 3.5, 3.5, 21);
-        $pdf->writeTextBold('100%', 149.9, 27.8, 10, [255,255,255]);//1
-        $pdf->writeTextBold('100%', 2.8, 36.8, 18);
+        //$pdf->writeTextBold('100%', 149.9, 27.8, 10, [255,255,255]);//1
+        $pdf->writeTextBold(round($data['management'][2]['total']).'%', 2.8, 36.8, 18);
 
-        $pdf->addUniversalBar(77, 60, 3, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//2.1 positive
-        $pdf->addUniversalBar(77, 70.6, 30, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//2.2 positive
-        $pdf->addUniversalBar(77, 81.2, 93, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//2.3 positive
+        $pdf->addUniversalBar(77, 60, $data['management'][2]['2_1']['+'], 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//2.1 positive
+        $pdf->addUniversalBar(77, 70.6, $data['management'][2]['2_2']['+'], 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//2.2 positive
+        $pdf->addUniversalBar(77, 81.2, $data['management'][2]['2_3']['+'], 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//2.3 positive
 
-        $pdf->addUniversalBar(152, 60, 26, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.1 negative
-        $pdf->addUniversalBar(152, 70.6, 0, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.2 negative
-        $pdf->addUniversalBar(152, 81.2, 2, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.3 negative
+        $pdf->addUniversalBar(152, 60, $data['management'][2]['2_1']['-'], 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.1 negative
+        $pdf->addUniversalBar(152, 70.6, $data['management'][2]['2_2']['-'], 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.2 negative
+        $pdf->addUniversalBar(152, 81.2, $data['management'][2]['2_3']['-'], 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//2.3 negative
 
         $pdf->addPage();
 
         $pdf->writeTextBold($username, 3.5, 3.5, 21);
-        $pdf->writeTextBold('100%', 148.7, 28, 10, [255,255,255]);//1
-        $pdf->writeTextBold('100%', 2.8, 36.8, 18);
+        //$pdf->writeTextBold('100%', 148.7, 28, 10, [255,255,255]);//1
+        $pdf->writeTextBold(round($data['management'][3]['total']).'%', 2.8, 36.8, 18);
 
-        $pdf->addUniversalBar(77, 60, 3, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.1 positive
-        $pdf->addUniversalBar(77, 70.6, 30, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.2 positive
-        $pdf->addUniversalBar(77, 81.2, 93, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.3 positive
-        $pdf->addUniversalBar(77, 91.8, 93, 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.4 positive
+        $pdf->addUniversalBar(77, 60, $data['management'][3]['3_1']['+'], 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.1 positive
+        $pdf->addUniversalBar(77, 70.6, $data['management'][3]['3_2']['+'], 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.2 positive
+        $pdf->addUniversalBar(77, 81.2, $data['management'][3]['3_3']['+'], 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.3 positive
+        $pdf->addUniversalBar(77, 91.8, $data['management'][3]['3_4']['+'], 71.38, AssessmentPDF::ROUNDED_LEFT, AssessmentPDF::BAR_POSITIVE);//3.4 positive
 
-        $pdf->addUniversalBar(152, 60, 26, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.1 negative
-        $pdf->addUniversalBar(152, 70.6, 0, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.2 negative
-        $pdf->addUniversalBar(152, 81.2, 2, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.3 negative
-        $pdf->addUniversalBar(152, 91.8, 2, 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.4 negative
+        $pdf->addUniversalBar(152, 60, $data['management'][3]['3_1']['-'], 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.1 negative
+        $pdf->addUniversalBar(152, 70.6, $data['management'][3]['3_2']['-'], 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.2 negative
+        $pdf->addUniversalBar(152, 81.2, $data['management'][3]['3_3']['-'], 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.3 negative
+        $pdf->addUniversalBar(152, 91.8, $data['management'][3]['3_4']['-'], 54.14, AssessmentPDF::ROUNDED_RIGHT, AssessmentPDF::BAR_NEGATIVE);//3.4 negative
 
         $pdf->renderOnBrowser('Assessment_v2');
     }
