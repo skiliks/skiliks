5415c1d37b86dcb83f03f1ce28a34ccc62e18809
slavkaa <nikoolin@ukr.net>
2013-12-10 21:24:18 +0200

    SKILIKS-5075.

---
+++ protected/views/global_partials/_system_mismatch_popup.php
@@ -0,0 +1,3 @@
+<div class="system-mismatch-popup" style="display: none;">
+    <?= $this->renderPartial('//static/pages/system-mismatch') ?>
+</div>
---
+++ protected/views/static/pages/system-mismatch.php
@@ -0,0 +1,27 @@
+<section class="browsers">
+    <h2>К сожалению, в данный момент мы не поддерживаем ваш браузер</h2>
+    <span>
+        для <strong>WINDOWS</strong> (XP и выше) или
+        <strong>MAC OS</strong> (10.7 и выше) предлагаем установить
+    </span>
+    <div>
+        <span class="browser chrome">
+            <label>Google Chrome 27+</label>
+            <div class="ico"></div>
+            <a class="light-btn" href="http://google.com/chrome" target="_blank">Установить</a>
+        </span>
+        <span class="or">или </span>
+        <span class="browser firefox">
+            <label>Mozilla Firefox 18+</label>
+            <div class="ico"></div>
+            <a class="light-btn" href="http://www.mozilla.org/firefox/new/" target="_blank">Установить</a>
+        </span>
+        <span class="or">или </span>
+        <span class="browser internet-explorer">
+            <label>Intertet Explorer 10+</label>
+            <div class="ico"></div>
+            <a class="light-btn" href="http://windows.microsoft.com/en-us/internet-explorer/ie-10-worldwide-languages" target="_blank">
+                Установить</a>
+        </span>
+    </div>
+</section>
