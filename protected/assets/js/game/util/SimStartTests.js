3a1e89b728b2a6444a62342d7081cebafa03b2e2
slavkaa <nikoolin@ukr.net>
2013-12-10 21:34:59 +0200

    SKILIKS-5083.

--- protected/assets/js/game/util/SimStartTests.js
+++ protected/assets/js/game/util/SimStartTests.js
@@ -71,7 +71,7 @@ try {
                     async: false
                 });
 
-                if(processorTestResult.average > 0) {
+                if(processorTestResult.average > 1) {
                     updateImageLoaderBar('Проверка текущего быстродействия... OK!', 0.90, true);
                     return true;
                 }
