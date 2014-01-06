ad9bd4e4bd89ac62e99095ac9c058f436fee46cc
slavkaa <nikoolin@ukr.net>
2014-01-06 20:22:35 +0200

    SKILIKS-5186. Fix recipients list in mail client.

--- protected/controllers/CharacterController.php
+++ protected/controllers/CharacterController.php
@@ -13,9 +13,17 @@ class CharacterController extends SimulationBaseController
         $characters = $simulation->game_type->getCharacters([]);
         $data['data'] = array_map(
             function (Character $character) {
-                return $character->getAttributes([
+                $characterData = $character->getAttributes([
                     'id', 'title', 'fio', 'email', 'code', 'phone',
                 ]);
+
+                // этот метод вызывается 1 раз за игру, поэтому проще поместить запросы в базк сюда,
+                // чем наращивать колонки в БД
+                $characterData['has_mail_theme'] = (int) (0 < OutboxMailTheme::model()->countByAttributes(
+                    ['character_to_id' => $characterData['id']]
+                ));
+
+                return $characterData;
             },
         $characters);
 
