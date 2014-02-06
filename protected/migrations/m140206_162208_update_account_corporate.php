73eda82f488b5d6b2df2f45870d7354d67c008d8
Ivan Tugay <ivan@iblurbs.net>
2014-02-06 19:02:31 +0200

    SKILIKS-5375

---
+++ protected/migrations/m140206_162208_update_account_corporate.php
@@ -0,0 +1,17 @@
+<?php
+
+class m140206_162208_update_account_corporate extends CDbMigration
+{
+	public function up()
+	{
+        $import = new ImportCorporateInfo();
+        $import->import();
+	}
+
+	public function down()
+	{
+		echo "m140206_162208_update_account_corporate does not support migration down.\n";
+		return false;
+	}
+
+}
