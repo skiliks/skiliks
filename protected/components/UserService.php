4fd2425f9e4fae34654225537f67ffbcea802bb3
slavkaa <nikoolin@ukr.net>
2014-02-06 16:36:30 +0200

    SKILIKS-5339

--- protected/components/UserService.php
+++ protected/components/UserService.php
@@ -1344,48 +1344,6 @@ class UserService {
     }
 
     /**
-<<<<<<< HEAD
-=======
-     * Ставит в очередь на отправку письма-поздравления с новым годом 2014.
-     *
-     * @param string[] $emails
-     */
-    public static function sendNyGreetings($emails)
-    {
-        foreach($emails as $email) {
-            $mailOptions           = new SiteEmailOptions();
-            $mailOptions->from     = 'support@skiliks.com';
-            $mailOptions->to       = $email;
-            $mailOptions->subject  = 'Новогоднее поздравление и подарок';
-
-            /**
-             * Формируем HTML письма
-             */
-            $mailOptions->body = UserService::renderEmailPartial('new-year', [
-                'title' => $mailOptions->subject,
-            ]);
-
-            /**
-             * В стандартном дизайне участвует всего три картинки.
-             */
-            $mailOptions->embeddedImages = [
-                [
-                    'path'     => Yii::app()->basePath.'/assets/img/site/emails/ny/skiliks_ny.jpg',
-                    'cid'      => 'skiliks_ny',
-                    'name'     => 'skiliks_ny',
-                    'encoding' => 'base64',
-                    'type'     => 'image/png',
-                ]
-            ];
-
-            /**
-             * Добавляем письмо в лчетедь писем
-             */
-            MailHelper::addMailToQueue($mailOptions);
-        }
-    }
-
-    /**
      * Возвращает информацию о серверах кода и базы
      * @return mixed array
      */
