8ae8778ea5ef6c6bfc16deb73f330523b351ad9a
Ivan Tugay <ivan@iblurbs.net>
2014-02-13 18:20:47 +0200

    SKILIKS-4809

--- protected/assets/js/game/views/documents/SKXLSDisplayView.js
+++ protected/assets/js/game/views/documents/SKXLSDisplayView.js
@@ -69,7 +69,7 @@ define([
                     css_id: doc.getCssId(),
                     sheets: doc.get('sheets')
                 }) );
-                me.$('#'+doc.getCssId()).find('.table-container').css('min-height', me.$('#'+doc.getCssId()).height());
+                me.$('#'+doc.getCssId()).find('.table-container').css('min-height', me.$('#'+doc.getCssId()).height()-26);
                 console.log(me.$('#'+doc.getCssId()).height());
                 SocialCalc.Constants.defaultImagePrefix = SKApp.get('assetsUrl') + '/img/excel/sc-';
                 me.sheets = [];
