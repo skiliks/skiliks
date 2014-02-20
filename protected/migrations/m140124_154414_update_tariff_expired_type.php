af71cfe94c5f21ddf26907aaedfe86f16eb5e8ee
slavkaa <nikoolin@ukr.net>
2014-02-20 16:29:45 +0200

    SKILIKS-5437.

--- protected/migrations/m140124_154414_update_tariff_expired_type.php
+++ protected/migrations/m140124_154414_update_tariff_expired_type.php
@@ -4,14 +4,14 @@ class m140124_154414_update_tariff_expired_type extends CDbMigration
 {
 	public function up()
 	{
-        $this->alterColumn('user_account_corporate', 'expire_invite_rule', "varchar(15) default '".UserAccountCorporate::EXPIRE_INVITE_RULE_BY_TARIFF."'");
+        // $this->alterColumn('user_account_corporate', 'expire_invite_rule', "varchar(15) default '".UserAccountCorporate::EXPIRE_INVITE_RULE_BY_TARIFF."'");
 
         /* @var UserAccountCorporate[] $accounts  */
-        $accounts = UserAccountCorporate::model()->findAll();
-        foreach( $accounts as $account ) {
-            $account->expire_invite_rule = UserAccountCorporate::EXPIRE_INVITE_RULE_BY_TARIFF;
-            $account->save(false);
-        }
+//        $accounts = UserAccountCorporate::model()->findAll();
+//        foreach( $accounts as $account ) {
+//            $account->expire_invite_rule = UserAccountCorporate::EXPIRE_INVITE_RULE_BY_TARIFF;
+//            $account->save(false);
+//        }
 	}
 
 	public function down()
