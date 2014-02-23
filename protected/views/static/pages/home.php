13a2f45bfbb1b3852232326cc8cbd25bcc04a5ba
slavkaa <nikoolin@ukr.net>
2014-02-23 12:58:13 +0200

    SKILIKS-5497.

--- protected/views/static/pages/home.php
+++ protected/views/static/pages/home.php
@@ -5,7 +5,7 @@
 </script>
 
 <!--features-->
-<section class="us-heroes-background">
+
     <section class="pull-center us-column-full us-homepage-content">
         <section class="features">
             <h1><?= Yii::t('site', 'Easy &amp; reliable way to discover your people management skills!') ?></h1>
@@ -44,6 +44,7 @@
                         </div>
                         <!-- FORM } -->
                     <?php endif ?>
+
                 </span>
                 <span class="column-1-2">
                     <div class="videosocwrap clearfix">
@@ -114,35 +115,36 @@
                 </span>
             </div>
 
-            <section class="anjela">
-                <p class="heroes-comment right">
+            <section class="">
+                <p class="us-comment us-angela-comment">
                     <?php echo Yii::t('site', '&quot;Remarkably comprehensive<br />&nbsp;and deep assessment of<br />&nbsp;&nbsp;&nbsp;skills - now I know what<br />&nbsp;&nbsp;I can expect from<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;newcomers&quot;') ?>
                 </p>
             </section>
+
+            <img class="us-homepage-angela" src="<?= $this->assetsUrl ?>/img/site/1280/homepage/angela.png" />
 
         </section>
         <!--features end-->
 
         <!--main article-->
-        <section class="pull-content-left column-2-3-wide">
-            <section class="trudyakin">
-                <p class="heroes-comment left">
-                <p class="heroes-comment left">
+        <section class="pull-content-left column-2-3-wide us-bottom-content">
+            <img class="us-homepage-trudyakin" src="<?= $this->assetsUrl ?>/img/site/1280/homepage/trudyakin.png" />
+
+            <section class="">
+                <p class="us-comment us-trudyakin-comment">
                     <?php echo Yii::t('site', '&quot;It&lsquo;s a real game with<br />&nbsp;great challenge and high<br />&nbsp;&nbsp;&nbsp;&nbsp;immersion - I haven&lsquo;t even<br />&nbsp;&nbsp;noticed how the time<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;passed by&quot;') ?>
                 </p>
             </section>
-            <article>
-                <h3><?= Yii::t('site', 'Easy')?></h3>
+            <article class="us-middle-content text-29313D">
+                <h2><?= Yii::t('site', 'Easy')?></h2>
                 <ul>
                     <li><?= Yii::t('site', 'Saves your time')?></li>
                     <li><?= Yii::t('site', 'Can be used by an unlimited number of applicants in any part of the world')?></li>
                     <li><?= Yii::t('site', 'No hard-, soft- or any-ware required! ! Just make sure you are online')?></li>
                     <li><?= Yii::t('site', 'Results can be obtained and used immediately')?></li>
                 </ul>
-            </article>
 
-            <article>
-                <h3><?= Yii::t('site', 'Reliable')?></h3>
+                <h2><?= Yii::t('site', 'Reliable')?></h2>
                 <ul>
                     <li><?= Yii::t('site', 'Focused on key practical skills')?></li>
                     <li><?= Yii::t('site', 'Based on best working practices')?></li>
@@ -155,20 +157,25 @@
 
         <!--clients-->
         <section class="pull-content-left">
-            <h3><?=Yii::t('site', 'Our Clients')?></h3>
+            <article class="us-our-client text-29313D">
+            <h2><?=Yii::t('site', 'Our Clients')?></h2>
 
             <ul>
-                <li style="display:none;"><?=CHtml::image("$assetsUrl/img/skiliks-fb.png")?></li>
-                <li><?=CHtml::image("$assetsUrl/img/icon-hipway.png")?></a></li>
-                <li><?=CHtml::image("$assetsUrl/img/icon-mif.png")?></li>
-                <li><?=CHtml::image("$assetsUrl/img/icon-wikimart.png")?></li>
-                <li><?=CHtml::image("$assetsUrl/img/icon-mcg.png")?></li>
+                <li><?= CHtml::image("$assetsUrl/img/site/1280/homepage/hipway.png")?></a></li>
+                <li><?= CHtml::image("$assetsUrl/img/site/1280/homepage/mif.png")?></li>
+                <li><?= CHtml::image("$assetsUrl/img/site/1280/homepage/wikimart.png")?></li>
+                <li><?= CHtml::image("$assetsUrl/img/site/1280/homepage/mcg.png")?></li>
+                <li><?= CHtml::image("$assetsUrl/img/site/1280/homepage/fabrica-ocon.png")?></li>
+                <li><?= CHtml::image("$assetsUrl/img/site/1280/homepage/nettrader.png")?></li>
+                <li><?= CHtml::image("$assetsUrl/img/site/1280/homepage/eksmo.png")?></li>
             </ul>
+            </article>
         </section>
         <!--clients end-->
     </section>
-</section>
-<div class="clearfix"></div>
+
+
+<img class="us-homepage-heroes-group" src="<?= $this->assetsUrl ?>/img/site/1280/homepage/heroes-group.png" />
 
 <?=$this->renderPartial('//global_partials/_system_mismatch_popup')?>
 
