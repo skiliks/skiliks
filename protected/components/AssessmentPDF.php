8ed013ceb149a4456aec0fc18dabddb5247ad060
Ivan Tugay <ivan@iblurbs.net>
2014-02-26 14:49:45 +0200

    SKILIKS-5527

--- protected/components/AssessmentPDF.php
+++ protected/components/AssessmentPDF.php
@@ -123,7 +123,7 @@ class AssessmentPDF {
 
     public function saveOnDisk($name) {
         $this->pdf->Output($name.'.pdf', 'F');
-        //exec("convert ".$name.".pdf ".$name.".jpg");
+        exec("convert ".$name.".pdf ".$name.".jpg");
     }
 
     /**
