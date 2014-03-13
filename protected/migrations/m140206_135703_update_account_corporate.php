fedb801383a573b53b22e9d73917a17e1cbcbb2d
Ivan Tugay <ivan@iblurbs.net>
2014-03-13 19:01:32 +0200

    Hotfix migration

--- protected/migrations/m140206_135703_update_account_corporate.php
+++ protected/migrations/m140206_135703_update_account_corporate.php
@@ -4,18 +4,18 @@ class m140206_135703_update_account_corporate extends CDbMigration
 {
 	public function up()
 	{
-        $this->addColumn(UserAccountCorporate::model()->tableName(), 'site', 'varchar(250) default null');
-        $this->addColumn(UserAccountCorporate::model()->tableName(), 'description_for_sales', 'text default null');
-        $this->addColumn(UserAccountCorporate::model()->tableName(), 'contacts_for_sales', 'text default null');
-        $this->addColumn(UserAccountCorporate::model()->tableName(), 'status_for_sales', 'varchar(250) default null');
+        $this->addColumn('user_account_corporate', 'site', 'varchar(250) default null');
+        $this->addColumn('user_account_corporate', 'description_for_sales', 'text default null');
+        $this->addColumn('user_account_corporate', 'contacts_for_sales', 'text default null');
+        $this->addColumn('user_account_corporate', 'status_for_sales', 'varchar(250) default null');
 	}
 
 	public function down()
 	{
-        $this->dropColumn(UserAccountCorporate::model()->tableName(), 'site');
-        $this->dropColumn(UserAccountCorporate::model()->tableName(), 'description_for_sales');
-        $this->dropColumn(UserAccountCorporate::model()->tableName(), 'contacts_for_sales');
-        $this->dropColumn(UserAccountCorporate::model()->tableName(), 'status_for_sales');
+        $this->dropColumn('user_account_corporate', 'site');
+        $this->dropColumn('user_account_corporate', 'description_for_sales');
+        $this->dropColumn('user_account_corporate', 'contacts_for_sales');
+        $this->dropColumn('user_account_corporate', 'status_for_sales');
 	}
 
 }
--- protected/migrations/m140303_093427_discount.php
+++ protected/migrations/m140303_093427_discount.php
@@ -4,16 +4,16 @@ class m140303_093427_discount extends CDbMigration
 {
 	public function up()
 	{
-        $this->addColumn(UserAccountCorporate::model()->tableName(), 'discount', 'float(6,2) default 0');
-        $this->addColumn(UserAccountCorporate::model()->tableName(), 'start_discount', 'date default null');
-        $this->addColumn(UserAccountCorporate::model()->tableName(), 'end_discount', 'date default null');
+        $this->addColumn('user_account_corporate', 'discount', 'float(6,2) default 0');
+        $this->addColumn('user_account_corporate', 'start_discount', 'date default null');
+        $this->addColumn('user_account_corporate', 'end_discount', 'date default null');
 	}
 
 	public function down()
 	{
-		$this->dropColumn(UserAccountCorporate::model()->tableName(), 'discount');
-		$this->dropColumn(UserAccountCorporate::model()->tableName(), 'start_discount');
-		$this->dropColumn(UserAccountCorporate::model()->tableName(), 'end_discount');
+		$this->dropColumn('user_account_corporate', 'discount');
+		$this->dropColumn('user_account_corporate', 'start_discount');
+		$this->dropColumn('user_account_corporate', 'end_discount');
 	}
 
 }
