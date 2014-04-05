ce019114591bb91e5788931a7da100e3c3f6f8f8
slavkaa <nikoolin@ukr.net>
2014-04-05 19:08:07 +0300

    SKILIKS-5806.

--- protected/views/global_partials/_popup_result_simulation_container.php
+++ protected/views/global_partials/_popup_result_simulation_container.php
@@ -1,5 +1,14 @@
+<?php /* @var Simulation $display_results_for */ ?>
+
 <?php if (null != $display_results_for): ?>
-    <script type="text/javascript">
-        window.display_results_for = <?= $display_results_for->id ?>;
-    </script>
+
+    <?php /* @var Invite $invite */ ?>
+    <?php $invite = Invite::model()->findByAttributes(['simulation_id' => $display_results_for->id]) ?>
+
+    <?php if (null != $invite && 1 == $invite->is_display_simulation_results ): ?>
+
+        <script type="text/javascript">
+            window.display_results_for = <?= $display_results_for->id ?>;
+        </script>
+    <?php endif ?>
 <?php endif ?>
