45e3b923dca0748ca835ad3b5f11e4759943df8f
slavkaa <nikoolin@ukr.net>
2014-04-18 18:20:29 +0300

    SKILIKS-5926.

--- protected/config/stage_live.php
+++ protected/config/stage_live.php
@@ -25,7 +25,8 @@ return CMap::mergeArray(require(dirname(__FILE__) . '/base.php'), array(
             'class'=>'CLogRouter',
             'routes'=>array(
                 array(
-                    'class'  => 'ext.yii-debug-toolbar.YiiDebugToolbarRoute',
+                    'class'     => 'ext.yii-debug-toolbar.YiiDebugToolbarRoute',
+                    'ipFilters' => array('127.0.0.1', '46.4.112.55'),
                 ),
                 array(
                     // Team 'Develop - live.skiliks.com'
