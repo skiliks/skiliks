abe831d0cef24d4053fb093bdbdd7f94eeb3c6ca
slavkaa <nikoolin@ukr.net>
2014-04-23 13:59:12 +0300

    SKILIKS-5952.

--- protected/views/global_partials/_popup_result_simulation_container.php
+++ protected/views/global_partials/_popup_result_simulation_container.php
@@ -1,11 +1,10 @@
 <?php /* @var Simulation $display_results_for */ ?>
 
 <?php if (null != $display_results_for): ?>
-
     <?php /* @var Invite $invite */ ?>
     <?php $invite = Invite::model()->findByAttributes(['simulation_id' => $display_results_for->id]) ?>
 
-    <?php if (null != $invite && 1 == $invite->is_display_simulation_results ): ?>
+    <?php if ($display_results_for->isAllowedToSeeResults(Yii::app()->user->data())): ?>
 
         <script type="text/javascript">
             window.display_results_for = <?= $display_results_for->id ?>;
