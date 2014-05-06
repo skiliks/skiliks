1c4d1c0a8b6703591c40e964a12ad6129238832a
slavkaa <nikoolin@ukr.net>
2014-05-06 16:21:19 +0300

    SKILIKS-5994.

--- protected/controllers/MeetingController.php
+++ protected/controllers/MeetingController.php
@@ -9,7 +9,7 @@ class MeetingController extends SimulationBaseController {
 
         // после 18:00 game_type надо переключать флаги, даже есть getState() ещё не дошел
         // для https://skiliks.atlassian.net/browse/SKILIKS-5994
-        if ($simulation->game_type->scenario_config->game_end_workday_timestamp < $simulation->getGameTime()) {
+        if ($simulation->game_type->scenario_config->game_end_workday_timestamp <= $simulation->getGameTime()) {
             FlagsService::checkFlagsDelay($simulation);
         }
 
