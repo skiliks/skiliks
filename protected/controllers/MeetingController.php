49e3d462767b87099d0d9fb66352760225c72ed2
slavkaa <nikoolin@ukr.net>
2014-05-06 16:09:58 +0300

    SKILIKS-5994.

--- protected/controllers/MeetingController.php
+++ protected/controllers/MeetingController.php
@@ -4,7 +4,14 @@ class MeetingController extends SimulationBaseController {
     
     public function actionGetSubjects()
     {
-        //throw new Exception("Stop");
+        /** @var Simulation $simulation */
+        $simulation = $this->getSimulationEntity();
+
+        // после 18:00game_type
+        if ($simulation->game_type->scenario_config->game_end_workday_timestamp < $simulation->getGameTime()) {
+            FlagsService::checkFlagsDelay($simulation);
+        }
+
         $this->sendJSON([
             'result' => self::STATUS_SUCCESS,
             'data'   => MeetingService::getList(
