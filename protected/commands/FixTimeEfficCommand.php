06f7548205338d824986cd71299a5621feb58295
slavkaa <nikoolin@ukr.net>
2014-05-08 18:47:53 +0300

    SKILIKS-5995.

--- protected/commands/FixTimeEfficCommand.php
+++ protected/commands/FixTimeEfficCommand.php
@@ -17,7 +17,8 @@ class FixTimeEfficCommand extends CConsoleCommand
 // x.4
 //         $simulations = Simulation::model()->findAll(" id = 5224 ");
 //         $simulations = Simulation::model()->findAll(" id = 8802 ");
-         $simulations = Simulation::model()->findAll(" id = 9515 ");
+//           $simulations = Simulation::model()->findAll(" id = 9515 ");
+//         $simulations = Simulation::model()->findAll(" id = 6995 ");
 
 // x.2. {
 //        $simulations = Simulation::model()->findAll(" results_popup_cache is null
@@ -84,7 +85,15 @@ class FixTimeEfficCommand extends CConsoleCommand
         foreach ($tmpArrayG as $behaviour) {
             $negative_1_4_behaviours_ids[] = $behaviour->id;
         }
-        unset($tmpArray);
+
+        $negative_3_2_behaviours_ids = [];
+        $tmpArray_3_2_negative = HeroBehaviour::model()->findAllByAttributes([
+            'scenario_id' => 2,
+            'code'        => ['3324', '3325'],
+        ]);
+        foreach ($tmpArray_3_2_negative as $behaviour) {
+            $negative_3_2_behaviours_ids[] = $behaviour->id;
+        }
 
 // х.3 суммарый негатив по 1.3 { negative.csv
 //        $simulations = Simulation::model()->findAll(" scenario_id = 2 and start > '2013-08-01 00:00:00' and status = 'complete' ");
@@ -330,138 +339,138 @@ class FixTimeEfficCommand extends CConsoleCommand
                 $data['management'][1]['1_4']['-'] = '100.00';
             }
 
-            $value_1_1_positive = ($data['management'][1]['1_1']['+'] / 100) * 5.5;
-            $value_1_2_positive = ($data['management'][1]['1_2']['+'] / 100) * 15;
-            $value_1_3_positive = ($data['management'][1]['1_3']['+'] / 100) * 24.5;
-
-            $value_1_3_negative_db = AssessmentAggregated::model()->findAllByAttributes([
-                'sim_id'   => $simulation->id,
-                'point_id' => $negative_1_3_behaviours_ids,
-            ]);
-            $value_1_4_negative_db = AssessmentAggregated::model()->findAllByAttributes([
-                'sim_id'   => $simulation->id,
-                'point_id' => $negative_1_4_behaviours_ids,
-            ]);
-
-            $value_1_3_negative = 0;
-            foreach ($value_1_3_negative_db as $value) {
-                $value_1_3_negative += abs($value->value);
-            }
-
-            $value_1_4_negative = 0;
-            foreach ($value_1_4_negative_db as $value) {
-                $value_1_4_negative += abs($value->value);
-            }
-
-            $value_1_3_total_negative = $value_1_3_negative + $value_1_4_negative;
-
-            if (120 < $value_1_3_total_negative) {
-                $value_1_3_total_negative = 120;
-            }
-
-            $value_1_1_negative = ($data['management'][1]['1_1']['-'] / 100) * 4;
-            $value_1_2_negative = ($data['management'][1]['1_2']['-'] / 100) * 20;
-
-
-            if (isset($data['management'][1]['1_5'])) {
-                // continue;
-                $value_1_1_positive = ($data['management'][1]['1_2']['+'] / 100) * 5.5;
-                $value_1_2_positive = ($data['management'][1]['1_3']['+'] / 100) * 15;
-                $value_1_3_positive = ($data['management'][1]['1_4']['+'] / 100) * 24.5;
-
-                $value_1_1_negative = ($data['management'][1]['1_2']['-'] / 100) * 4;
-                $value_1_2_negative = ($data['management'][1]['1_3']['-'] / 100) * 20;
-            }
-
-            $managerial_1_value = (
-                    $value_1_1_positive * (1 - $value_1_1_negative/4)
-                    + $value_1_2_positive * (1 - $value_1_2_negative/20)
-                    + $value_1_3_positive * (1 - $value_1_3_total_negative/120)
-                ) / 45;
-
-            $managerial_1_value = round( $managerial_1_value * 100, 2);
-
-            // 1.x
-            if ($managerial_1_value != $data['management'][1]['total'] &&
-                round($managerial_1_value, 0) != round($data['management'][1]['total'], 0)
-                && abs(round($managerial_1_value, 2) - round($data['management'][1]['total'], 2)) > 0.5
-            ) {
-                echo '| ', $simulation->id, '| 1.х |' , $managerial_1_value,
-                ' | ', $data['management'][1]['total'],
-                ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
-                echo "\n";
-                $managerial_1_N++;
-
-                $data['management'][1]['total'] = $managerial_1_value;
-            }
-
-            // v1
-            $new_1_3 = number_format($value_1_3_negative*100/60 ,2);
-            if (60 < $value_1_3_negative) {
-                $new_1_3 = '100.00';
-            }
-            $new_1_4 = number_format($value_1_4_negative*100/60 ,2);
-            if (60 < $value_1_4_negative) {
-                $new_1_4 = '100.00';
-            }
-
-            if (isset($data['management'][1]['1_5'])) {
-
-                // [1][3][-]
-                if ($new_1_3 != $data['management'][1]['1_4']['-'] &&
-                    round($new_1_3, 0) != round($data['management'][1]['1_4']['-'], 0)
-                    && abs(round($new_1_3, 0) - round($data['management'][1]['1_4']['-'], 0)) > 0.5
-                ) {
-                    echo '| ', $simulation->id, '| 1.3 (v1) |' , $new_1_3,
-                    ' | ', $data['management'][1]['1_4']['-'],
-                    ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
-                    echo "\n";
-
-                    $data['management'][1]['1_4']['-'] = $new_1_3;
-                }
-
-                // [1][4][-]
-                if ($new_1_4 != $data['management'][1]['1_5']['-'] &&
-                    round($new_1_4, 0) != round($data['management'][1]['1_5']['-'], 0)
-                    && abs(round($new_1_4, 0) - round($data['management'][1]['1_5']['-'], 0)) > 0.5
-                ) {
-                    echo '| ', $simulation->id, '| 1.4 (v1) |', $new_1_4,
-                    ' | ', $data['management'][1]['1_5']['-'],
-                    ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
-                    echo "\n";
-
-                    $data['management'][1]['1_5']['-'] = $new_1_4;
-                }
-            } else {
-
-                // [1][3][-]
-                if ($new_1_3 != $data['management'][1]['1_3']['-'] &&
-                    round($new_1_3, 0) != round($data['management'][1]['1_3']['-'], 0)
-                    && abs(round($new_1_3, 0) - round($data['management'][1]['1_3']['-'], 0)) > 0.5
-                ) {
-                    echo '| ', $simulation->id, '| 1.3 (v2) |' , $new_1_3,
-                    ' | ', $data['management'][1]['1_3']['-'],
-                    ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
-                    echo "\n";
-                    $managerial_1_N++;
-
-                    $data['management'][1]['1_3']['-'] = $new_1_3;
-                }
-
-                // [1][4][-]
-                if ($new_1_4 != $data['management'][1]['1_4']['-'] &&
-                    round($new_1_4, 0) != round($data['management'][1]['1_4']['-'], 0)
-                    && abs(round($new_1_4, 0) - round($data['management'][1]['1_4']['-'], 0)) > 0.5
-                ) {
-                    echo '| ', $simulation->id, '| 1.4 (v2) |', $new_1_4,
-                    ' | ', $data['management'][1]['1_4']['-'],
-                    ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
-                    echo "\n";
-                    $managerial_1_N++;
-
-                    $data['management'][1]['1_4']['-'] = $new_1_4;
-                }
-            }
+//            $value_1_1_positive = ($data['management'][1]['1_1']['+'] / 100) * 5.5;
+//            $value_1_2_positive = ($data['management'][1]['1_2']['+'] / 100) * 15;
+//            $value_1_3_positive = ($data['management'][1]['1_3']['+'] / 100) * 24.5;
+//
+//            $value_1_3_negative_db = AssessmentAggregated::model()->findAllByAttributes([
+//                'sim_id'   => $simulation->id,
+//                'point_id' => $negative_1_3_behaviours_ids,
+//            ]);
+//            $value_1_4_negative_db = AssessmentAggregated::model()->findAllByAttributes([
+//                'sim_id'   => $simulation->id,
+//                'point_id' => $negative_1_4_behaviours_ids,
+//            ]);
+//
+//            $value_1_3_negative = 0;
+//            foreach ($value_1_3_negative_db as $value) {
+//                $value_1_3_negative += abs($value->value);
+//            }
+//
+//            $value_1_4_negative = 0;
+//            foreach ($value_1_4_negative_db as $value) {
+//                $value_1_4_negative += abs($value->value);
+//            }
+//
+//            $value_1_3_total_negative = $value_1_3_negative + $value_1_4_negative;
+//
+//            if (120 < $value_1_3_total_negative) {
+//                $value_1_3_total_negative = 120;
+//            }
+//
+//            $value_1_1_negative = ($data['management'][1]['1_1']['-'] / 100) * 4;
+//            $value_1_2_negative = ($data['management'][1]['1_2']['-'] / 100) * 20;
+//
+//
+//            if ('//simulation_details_popup/v1' == $simulation->results_popup_partials_path) {
+//                // continue;
+//                $value_1_1_positive = ($data['management'][1]['1_2']['+'] / 100) * 5.5;
+//                $value_1_2_positive = ($data['management'][1]['1_3']['+'] / 100) * 15;
+//                $value_1_3_positive = ($data['management'][1]['1_4']['+'] / 100) * 24.5;
+//
+//                $value_1_1_negative = ($data['management'][1]['1_2']['-'] / 100) * 4;
+//                $value_1_2_negative = ($data['management'][1]['1_3']['-'] / 100) * 20;
+//            }
+//
+//            $managerial_1_value = (
+//                    $value_1_1_positive * (1 - $value_1_1_negative/4)
+//                    + $value_1_2_positive * (1 - $value_1_2_negative/20)
+//                    + $value_1_3_positive * (1 - $value_1_3_total_negative/120)
+//                ) / 45;
+//
+//            $managerial_1_value = round( $managerial_1_value * 100, 2);
+//
+//            // 1.x
+//            if ($managerial_1_value != $data['management'][1]['total'] &&
+//                round($managerial_1_value, 0) != round($data['management'][1]['total'], 0)
+//                && abs(round($managerial_1_value, 2) - round($data['management'][1]['total'], 2)) > 0.5
+//            ) {
+//                echo '| ', $simulation->id, '| 1.х |' , $managerial_1_value,
+//                ' | ', $data['management'][1]['total'],
+//                ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
+//                echo "\n";
+//                $managerial_1_N++;
+//
+//                $data['management'][1]['total'] = $managerial_1_value;
+//            }
+//
+//            // v1
+//            $new_1_3 = number_format($value_1_3_negative*100/60 ,2);
+//            if (60 < $value_1_3_negative) {
+//                $new_1_3 = '100.00';
+//            }
+//            $new_1_4 = number_format($value_1_4_negative*100/60 ,2);
+//            if (60 < $value_1_4_negative) {
+//                $new_1_4 = '100.00';
+//            }
+//
+//            if ('//simulation_details_popup/v1' == $simulation->results_popup_partials_path) {
+//
+//                // [1][3][-]
+//                if ($new_1_3 != $data['management'][1]['1_4']['-'] &&
+//                    round($new_1_3, 0) != round($data['management'][1]['1_4']['-'], 0)
+//                    && abs(round($new_1_3, 0) - round($data['management'][1]['1_4']['-'], 0)) > 0.5
+//                ) {
+//                    echo '| ', $simulation->id, '| 1.3 (v1) |' , $new_1_3,
+//                    ' | ', $data['management'][1]['1_4']['-'],
+//                    ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
+//                    echo "\n";
+//
+//                    $data['management'][1]['1_4']['-'] = $new_1_3;
+//                }
+//
+//                // [1][4][-]
+//                if ($new_1_4 != $data['management'][1]['1_5']['-'] &&
+//                    round($new_1_4, 0) != round($data['management'][1]['1_5']['-'], 0)
+//                    && abs(round($new_1_4, 0) - round($data['management'][1]['1_5']['-'], 0)) > 0.5
+//                ) {
+//                    echo '| ', $simulation->id, '| 1.4 (v1) |', $new_1_4,
+//                    ' | ', $data['management'][1]['1_5']['-'],
+//                    ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
+//                    echo "\n";
+//
+//                    $data['management'][1]['1_5']['-'] = $new_1_4;
+//                }
+//            } else {
+//
+//                // [1][3][-]
+//                if ($new_1_3 != $data['management'][1]['1_3']['-'] &&
+//                    round($new_1_3, 0) != round($data['management'][1]['1_3']['-'], 0)
+//                    && abs(round($new_1_3, 0) - round($data['management'][1]['1_3']['-'], 0)) > 0.5
+//                ) {
+//                    echo '| ', $simulation->id, '| 1.3 (v2) |' , $new_1_3,
+//                    ' | ', $data['management'][1]['1_3']['-'],
+//                    ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
+//                    echo "\n";
+//                    $managerial_1_N++;
+//
+//                    $data['management'][1]['1_3']['-'] = $new_1_3;
+//                }
+//
+//                // [1][4][-]
+//                if ($new_1_4 != $data['management'][1]['1_4']['-'] &&
+//                    round($new_1_4, 0) != round($data['management'][1]['1_4']['-'], 0)
+//                    && abs(round($new_1_4, 0) - round($data['management'][1]['1_4']['-'], 0)) > 0.5
+//                ) {
+//                    echo '| ', $simulation->id, '| 1.4 (v2) |', $new_1_4,
+//                    ' | ', $data['management'][1]['1_4']['-'],
+//                    ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
+//                    echo "\n";
+//                    $managerial_1_N++;
+//
+//                    $data['management'][1]['1_4']['-'] = $new_1_4;
+//                }
+//            }
             // managerial 1 }
 
             // managerial 2 {
@@ -495,42 +504,87 @@ class FixTimeEfficCommand extends CConsoleCommand
 //            }
             // managerial 2 }
 
-            // managerial 3 {
-//            $value_3_1_positive = ($data['management'][3]['3_1']['+'] / 100) * 6;
-//            $value_3_2_positive = ($data['management'][3]['3_2']['+'] / 100) * 17;
-//            $value_3_3_positive = ($data['management'][3]['3_3']['+'] / 100) * 5;
-//            $value_3_4_positive = ($data['management'][3]['3_4']['+'] / 100) * 7;
-//
-//            $value_3_2_negative = ($data['management'][3]['3_2']['-'] / 100) * 8;
-//            $value_3_4_negative = ($data['management'][3]['3_4']['-']/ 100) * 12.5;
-//
-//            $managerial_3_value = (
-//                    $value_3_1_positive
-//                    + $value_3_2_positive * (1 - $value_3_2_negative/8)
-//                    + $value_3_3_positive
-//                    + $value_3_4_positive * (1 - $value_3_4_negative/12.5)
-//                ) / 35;
-//
-//            $managerial_3_value = round( $managerial_3_value * 100, 2);
-//
-//            if ($managerial_3_value != $data['management'][3]['total']
-//                && round($managerial_3_value, 0) != round($data['management'][3]['total'], 0)
-//                && abs(round($managerial_3_value, 0) - round($data['management'][3]['total'], 0)) > 0.5
-//            ) {
-//                echo '| ', $simulation->id, '| 3.х |' , $managerial_3_value,
-//                ' | ', $data['management'][2]['total'],
-//                ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
-//                echo "\n";
-//                $managerial_3_N++;
-//
-//                $data['management'][3]['total'] = $managerial_3_value;
-//            }
-            //     managerial 3 }
-
+            // 3.2 {
             if ('true' == $is_fix) {
                 $simulation->results_popup_cache = serialize($data);
                 $simulation->save(false);
             }
+
+            $value_3_2_negative_db = AssessmentAggregated::model()->findAllByAttributes([
+                'sim_id'   => $simulation->id,
+                'point_id' => $negative_3_2_behaviours_ids,
+            ]);
+            $value_3_2_negative = 0;
+            foreach ($value_3_2_negative_db as $value) {
+                $value_3_2_negative += abs($value->value);
+            }
+
+
+            if ('//simulation_details_popup/v1' == $simulation->results_popup_partials_path) {
+                $new_3_2_negative = ($value_3_2_negative*100/4);
+                $type = 'v1';
+            } else {
+                $new_3_2_negative = ($value_3_2_negative*100/8);
+                $type = 'v2';
+            }
+
+            if ($new_3_2_negative != $data['management'][3]['3_2']['-']
+                && round($new_3_2_negative, 0) != round($data['management'][3]['3_2']['-'], 0)
+                && abs(round($new_3_2_negative, 0) - round($data['management'][3]['3_2']['-'], 0)) > 0.5
+            ) {
+                echo '| ', $simulation->id, '| 3.2.- ', $type, '|' , $new_3_2_negative,
+                ' | ', $data['management'][3]['3_2']['-'],
+                ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
+                echo "\n";
+                $managerial_3_N++;
+
+                $data['management'][3]['3_2']['-'] = $new_3_2_negative;
+            }
+            // 3.2 }
+
+            // managerial 3 {
+            $value_3_1_positive = ($data['management'][3]['3_1']['+'] / 100) * 6;
+            $value_3_2_positive = ($data['management'][3]['3_2']['+'] / 100) * 17;
+            $value_3_3_positive = ($data['management'][3]['3_3']['+'] / 100) * 5;
+            $value_3_4_positive = ($data['management'][3]['3_4']['+'] / 100) * 7;
+
+            $value_3_4_negative = ($data['management'][3]['3_4']['-']/ 100) * 12.5;
+
+            if ('//simulation_details_popup/v1' == $simulation->results_popup_partials_path) {
+                $value_3_2_negative = ($data['management'][3]['3_2']['-'] / 100) * 4;
+                $managerial_3_value = (
+                        $value_3_1_positive
+                        + $value_3_2_positive * (1 - $value_3_2_negative/4)
+                        + $value_3_3_positive
+                        + $value_3_4_positive * (1 - $value_3_4_negative/12.5)
+                    ) / 35;
+                $type = 'v1';
+            } else {
+                $value_3_2_negative = ($data['management'][3]['3_2']['-'] / 100) * 8;
+                $managerial_3_value = (
+                        $value_3_1_positive
+                        + $value_3_2_positive * (1 - $value_3_2_negative/8)
+                        + $value_3_3_positive
+                        + $value_3_4_positive * (1 - $value_3_4_negative/12.5)
+                    ) / 35;
+                $type = 'v2';
+            }
+
+            $managerial_3_value = round( $managerial_3_value * 100, 2);
+
+            if ($managerial_3_value != $data['management'][3]['total']
+                && round($managerial_3_value, 0) != round($data['management'][3]['total'], 0)
+                && abs(round($managerial_3_value, 0) - round($data['management'][3]['total'], 0)) > 0.5
+            ) {
+                echo '| ', $simulation->id, '| 3.х ', $type, '|' , $managerial_3_value,
+                ' | ', $data['management'][2]['total'],
+                ' | ', $simulation->end, ' | ', $simulation->user->profile->email, ' |';
+                echo "\n";
+                $managerial_3_N++;
+
+                $data['management'][3]['total'] = $managerial_3_value;
+            }
+            //     managerial 3 }
         }
 
         echo $efficiencyN;
