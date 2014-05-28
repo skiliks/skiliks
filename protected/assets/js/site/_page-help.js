08073e2b02404cb89a5041b154516d3ec12ce7b0
slavkaa <nikoolin@ukr.net>
2014-05-28 17:24:25 +0300

    SKILIKS-6033

--- protected/assets/js/site/_page-help.js
+++ protected/assets/js/site/_page-help.js
@@ -2,11 +2,15 @@
 $(document).ready(function() {
     $(".action-toggle-help-paragraph").click(function() {
         if (!$(this).children("div").is(":visible")) {
-            $(this).children("div").slideDown("fast", stickyFooterAndBackground());
+            $(this).children("div").slideDown("fast", function() {
+                setTimeout(stickyFooterAndBackground, 100);
+            });
             $(this).css('color', '#146672');
             $(this).addClass("active");
         } else {
-            $(this).children("div").slideUp("fast", stickyFooterAndBackground());
+            $(this).children("div").slideUp("fast", function() {
+                setTimeout(stickyFooterAndBackground, 100);
+            });
             $(this).css('color', '#555742');
             $(this).removeClass("active");
         }
