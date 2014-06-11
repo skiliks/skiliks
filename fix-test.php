0a98fe3cc66ad8565c6113b68b427b9739facb58
slavkaa <nikoolin@ukr.net>
2014-06-11 19:33:14 +0300

    SKILIKS-6049

--- fix-test.php
+++ fix-test.php
@@ -4,8 +4,6 @@ if ($_GET['anctmd'] !== 'tvns-iolj-scyu-isvkih-5vf7qwc') {
     die();
 }
 
-// `sudo -u skiliks /usr/local/bin/fix-test`;
-
 `echo ' ' > /opt/TeamCity/buildAgent/work/skiliks-unit/protected/views/static/applicationcache/preload_images.php`;
 `chmod 777 /opt/TeamCity/buildAgent/work/skiliks-unit/protected/views/static/applicationcache/preload_images.php`;
 `cd /opt/TeamCity/buildAgent/work/skiliks-unit`;
