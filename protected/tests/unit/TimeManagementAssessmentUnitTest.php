a9c64d1d2450b4efe1e1975e91fb3ba34564a73d
slavkaa <nikoolin@ukr.net>
2014-06-26 00:24:10 +0300

    Fix tests.

--- protected/tests/unit/TimeManagementAssessmentUnitTest.php
+++ protected/tests/unit/TimeManagementAssessmentUnitTest.php
@@ -688,10 +688,9 @@ class TimeManagementAssessmentUnitTest extends CDbTestCase
         $a = '';
         foreach ($assessments as $assessment) {
             $values[$assessment->slug] = $assessment->value;
-            $a .= '` `'.$assessment->slug;
         }
 
-        $this->assertTrue(false, $a);
+        $this->assertTrue(false, $values['efficiency']);
 
         $this->assertEquals(
             63.64, // percentage
